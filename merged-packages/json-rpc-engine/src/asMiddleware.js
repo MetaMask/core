27b53fd2f3c12ee5806adffd199bde994d226f9f
kumavis <aaron@kumavis.me>
2017-04-03 22:07:46 -0700

    lint fix

--- merged-packages/json-rpc-engine/src/asMiddleware.js
+++ merged-packages/json-rpc-engine/src/asMiddleware.js
@@ -1,8 +1,8 @@
 module.exports = asMiddleware
 
 function asMiddleware (engine) {
-  return function engineAsMiddleware(req, res, next, end){
-    engine._runMiddleware(req, res, function(err, isComplete){
+  return function engineAsMiddleware (req, res, next, end) {
+    engine._runMiddleware(req, res, function (err, isComplete) {
       if (err) return end(err)
       if (isComplete) {
         end()
--- merged-packages/json-rpc-engine/test/basic.spec.js
+++ merged-packages/json-rpc-engine/test/basic.spec.js
@@ -1,72 +1,63 @@
 const assert = require('assert')
 const RpcEngine = require('../src/index.js')
 
-describe('basic tests', function(){
-
-  it('basic middleware test', function(done){
-
+describe('basic tests', function () {
+  it('basic middleware test', function (done) {
     let engine = new RpcEngine()
 
-    engine.push(function(req, res, next, end){
+    engine.push(function (req, res, next, end) {
       res.result = 42
       end()
     })
 
     let payload = { id: 1, jsonrpc: '2.0', method: 'hello' }
 
-    engine.handle(payload, function(err, res){
+    engine.handle(payload, function (err, res) {
       assert.ifError(err, 'did not error')
       assert(res, 'has res')
       assert.equal(res.result, 42, 'has expected result')
       done()
     })
-
   })
 
-  it('interacting middleware test', function(done){
-
+  it('interacting middleware test', function (done) {
     let engine = new RpcEngine()
 
-    engine.push(function(req, res, next, end){
+    engine.push(function (req, res, next, end) {
       req.resultShouldBe = 42
       next()
     })
 
-    engine.push(function(req, res, next, end){
+    engine.push(function (req, res, next, end) {
       res.result = req.resultShouldBe
       end()
     })
 
     let payload = { id: 1, jsonrpc: '2.0', method: 'hello' }
 
-    engine.handle(payload, function(err, res){
+    engine.handle(payload, function (err, res) {
       assert.ifError(err, 'did not error')
       assert(res, 'has res')
       assert.equal(res.result, 42, 'has expected result')
       done()
     })
-
   })
 
-  it('empty middleware test', function(done){
-
+  it('empty middleware test', function (done) {
     let engine = new RpcEngine()
 
     let payload = { id: 1, jsonrpc: '2.0', method: 'hello' }
 
-    engine.handle(payload, function(err, res){
+    engine.handle(payload, function (err, res) {
       assert(err, 'did error')
       done()
     })
-
   })
 
-
-  it('handle batch payloads', function(done){
-
+  it('handle batch payloads', function (done) {
     let engine = new RpcEngine()
 
-    engine.push(function(req, res, next, end){
+    engine.push(function (req, res, next, end) {
       res.result = req.id
       end()
     })
@@ -75,7 +66,7 @@ describe('basic tests', function(){
     let payloadB = { id: 2, jsonrpc: '2.0', method: 'hello' }
     let payload = [payloadA, payloadB]
 
-    engine.handle(payload, function(err, res){
+    engine.handle(payload, function (err, res) {
       assert.ifError(err, 'did not error')
       assert(res, 'has res')
       assert(Array.isArray(res), 'res is array')
@@ -83,65 +74,61 @@ describe('basic tests', function(){
       assert.equal(res[1].result, 2, 'has expected result')
       done()
     })
-
   })
 
-  it('return handlers test', function(done){
-
+  it('return handlers test', function (done) {
     let engine = new RpcEngine()
 
-    engine.push(function(req, res, next, end){
-      next(function(cb){
+    engine.push(function (req, res, next, end) {
+      next(function (cb) {
         res.sawReturnHandler = true
         cb()
       })
     })
 
-    engine.push(function(req, res, next, end){
+    engine.push(function (req, res, next, end) {
       end()
     })
 
     let payload = { id: 1, jsonrpc: '2.0', method: 'hello' }
 
-    engine.handle(payload, function(err, res){
+    engine.handle(payload, function (err, res) {
       assert.ifError(err, 'did not error')
       assert(res, 'has res')
-      assert(res.sawReturnHandler,'saw return handler')
+      assert(res.sawReturnHandler, 'saw return handler')
       done()
     })
-
   })
 
-  it('return order of events', function(done){
-
+  it('return order of events', function (done) {
     let engine = new RpcEngine()
 
     let events = []
 
-    engine.push(function(req, res, next, end){
+    engine.push(function (req, res, next, end) {
       events.push('1-next')
-      next(function(cb){
+      next(function (cb) {
         events.push('1-return')
         cb()
       })
     })
 
-    engine.push(function(req, res, next, end){
+    engine.push(function (req, res, next, end) {
       events.push('2-next')
-      next(function(cb){
+      next(function (cb) {
         events.push('2-return')
         cb()
       })
     })
 
-    engine.push(function(req, res, next, end){
+    engine.push(function (req, res, next, end) {
       events.push('3-end')
       end()
     })
 
     let payload = { id: 1, jsonrpc: '2.0', method: 'hello' }
 
-    engine.handle(payload, function(err, res){
+    engine.handle(payload, function (err, res) {
       assert.ifError(err, 'did not error')
       assert.equal(events[0], '1-next', '(event 0) was "1-next"')
       assert.equal(events[1], '2-next', '(event 1) was "2-next"')
@@ -150,7 +137,5 @@ describe('basic tests', function(){
       assert.equal(events[4], '1-return', '(event 4) was "1-return"')
       done()
     })
-
   })
-
 })
