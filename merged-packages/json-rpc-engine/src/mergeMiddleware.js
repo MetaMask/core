2e2a352054ec0e09dd7bd97e3513467d415daf82
kumavis <aaron@kumavis.me>
2018-05-16 15:22:34 -0700

    add mergeMiddleware utility

---
+++ merged-packages/json-rpc-engine/src/mergeMiddleware.js
@@ -0,0 +1,10 @@
+const JsonRpcEngine = require('./index')
+const asMiddleware = require('./asMiddleware')
+
+module.exports = mergeMiddleware
+
+function mergeMiddleware(middlewareStack) {
+  const engine = new JsonRpcEngine()
+  middlewareStack.forEach(middleware => engine.push(middleware))
+  return asMiddleware(engine)
+}
