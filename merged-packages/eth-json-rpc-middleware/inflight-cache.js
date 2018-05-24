6ffffd257f7e6b43f3c203fdb26e69a5560042b0
kumavis <aaron@kumavis.me>
2018-05-24 12:34:00 -0700

    inflight-cache - rewrite as asyncMiddleware and wrap activeRequestHandlers in try-catch

--- merged-packages/eth-json-rpc-middleware/inflight-cache.js
+++ merged-packages/eth-json-rpc-middleware/inflight-cache.js
@@ -1,3 +1,4 @@
+const createAsyncMiddleware = require('json-rpc-engine/src/createAsyncMiddleware')
 const cacheIdentifierForPayload = require('./cache-utils').cacheIdentifierForPayload
 
 module.exports = createInflightCache
@@ -5,7 +6,7 @@ module.exports = createInflightCache
 function createInflightCache () {
   const inflightRequests = {}
 
-  return (req, res, next, end) => {
+  return createAsyncMiddleware(async (req, res, next) => {
     const cacheId = cacheIdentifierForPayload(req)
     // if not cacheable, skip
     if (!cacheId) return next()
@@ -13,26 +14,52 @@ function createInflightCache () {
     let activeRequestHandlers = inflightRequests[cacheId]
     // if found, wait for the active request to be handled
     if (activeRequestHandlers) {
-      // setup the response lister
-      activeRequestHandlers.push((handledRes) => {
-        // copy the result and error from the handledRes
-        res.result = handledRes.result
-        res.error = handledRes.error
-        end()
-      })
-    } else {
-      // setup response handler array for subsequent requests
-      activeRequestHandlers = []
-      inflightRequests[cacheId] = activeRequestHandlers
-      // allow request to be handled normally
-      next((done) => {
-        // clear inflight requests
-        delete inflightRequests[cacheId]
-        // complete this request
-        done()
-        // once request has been handled, call all waiting handlers
-        activeRequestHandlers.forEach((handler) => handler(res))
-      })
+      // setup the response listener and wait for it to be called
+      // it will handle copying the result and request fields
+      await createActiveRequestHandler(res, activeRequestHandlers)
+      return
     }
+    // setup response handler array for subsequent requests
+    activeRequestHandlers = []
+    inflightRequests[cacheId] = activeRequestHandlers
+    // allow request to be handled normally
+    await next()
+    // clear inflight requests
+    delete inflightRequests[cacheId]
+    // schedule activeRequestHandlers to be handled
+    handleActiveRequest(res, activeRequestHandlers)
+    // complete
+    return
+  })
+
+  function createActiveRequestHandler(res, activeRequestHandlers) {
+    const { resolve, promise } = deferredPromise()
+    activeRequestHandlers.push((handledRes) => {
+      // copy the result and error from the handledRes
+      res.result = handledRes.result
+      res.error = handledRes.error
+      resolve()
+    })
+    return promise
   }
+
+  function handleActiveRequest(res, activeRequestHandlers) {
+    // use setTimeout so we can resolve our original request first
+    setTimeout(() => {
+      activeRequestHandlers.forEach((handler) => {
+        try {
+          handler(res)
+        } catch (err) {
+          // catch error so all requests are handled correctly
+          console.error(err)
+        }
+      })
+    })
+  }
+}
+
+function deferredPromise() {
+  let resolve
+  const promise = new Promise(_resolve => { resolve = _resolve })
+  return { resolve, promise }
 }
