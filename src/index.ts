012313ca2923e61049f269b8744c9b8376577b6b
bitpshr <mail@bitpshr.net>
2018-07-31 22:16:11 -0400

    Compile to UMD

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import * as util from './util';
+
+export * from './AccountTrackerController';
+export * from './AddressBookController';
+export * from './BaseController';
+export * from './BlockHistoryController';
+export * from './ComposableController';
+export * from './CurrencyRateController';
+export * from './KeyringController';
+export * from './NetworkController';
+export * from './NetworkStatusController';
+export * from './PhishingController';
+export * from './PreferencesController';
+export * from './ShapeShiftController';
+export * from './TokenRatesController';
+export { util };
