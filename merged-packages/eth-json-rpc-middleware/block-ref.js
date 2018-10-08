80c5ab13f36eac483a660bcc9048bf316a0651b6
kumavis <aaron@kumavis.me>
2018-10-08 12:33:45 -0400

    re-add original block-ref middleware as block-ref-rewrite

--- merged-packages/eth-json-rpc-middleware/block-ref.js
+++ merged-packages/eth-json-rpc-middleware/block-ref.js
@@ -3,12 +3,12 @@ const clone = require('clone')
 const createAsyncMiddleware = require('json-rpc-engine/src/createAsyncMiddleware')
 const blockTagParamIndex = require('./cache-utils').blockTagParamIndex
 
-module.exports = createBlockRefRewriteMiddleware
+module.exports = createBlockRefMiddleware
 
-function createBlockRefRewriteMiddleware (opts = {}) {
+function createBlockRefMiddleware (opts = {}) {
   const { provider, blockTracker } = opts
-  if (!provider) throw Error('BlockRefRewriteMiddleware - mandatory "provider" option is missing.')
-  if (!blockTracker) throw Error('BlockRefRewriteMiddleware - mandatory "blockTracker" option is missing.')
+  if (!provider) throw Error('BlockRefMiddleware - mandatory "provider" option is missing.')
+  if (!blockTracker) throw Error('BlockRefMiddleware - mandatory "blockTracker" option is missing.')
 
   return createAsyncMiddleware(async (req, res, next) => {
     const blockRefIndex = blockTagParamIndex(req)
