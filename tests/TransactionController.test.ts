927181e3ad465e3a8d42f2be1ac6a538ad9950e2
Esteban Miño <efmino@uc.cl>
2019-11-11 12:55:16 -0300

    Feature: speed up transaction (#172)
    * speed upo method
    
    * correctly update tx and test
    
    * handle fail
    
    * fix cancel transaction update
    
    * rm safely execute
    
    * fix tests

--- tests/TransactionController.test.ts
+++ tests/TransactionController.test.ts
@@ -515,4 +515,30 @@ describe('TransactionController', () => {
 			}
 		});
 	});
+
+	it('should speed up a transaction', async () => {
+		return new Promise(async (resolve) => {
+			const controller = new TransactionController({
+				provider: PROVIDER,
+				sign: async (transaction: any) => transaction
+			});
+			const from = '0xc38bf1ad06ef69f0c04e29dbeb4152b4175f0a8d';
+			controller.context = {
+				NetworkController: MOCK_NETWORK
+			} as any;
+			controller.onComposed();
+			await controller.addTransaction({
+				from,
+				gas: '0x0',
+				gasPrice: '0x1',
+				to: from,
+				value: '0x0'
+			});
+			await controller.speedUpTransaction(controller.state.transactions[0].id);
+
+			expect(controller.state.transactions.length).toBe(2);
+			expect(controller.state.transactions[1].transaction.gasPrice).toBe('0x1.199999999999a');
+			resolve();
+		});
+	});
 });
