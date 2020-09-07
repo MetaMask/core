95ab544b7a5ee0aa54c58d652a747ebf51f8f267
Whymarrh Whitby <whymarrh.whitby@gmail.com>
2020-09-07 16:23:12 -0230

    Mark all operands for delete as optional

--- src/message-manager/AbstractMessageManager.ts
+++ src/message-manager/AbstractMessageManager.ts
@@ -54,7 +54,7 @@ export interface AbstractMessageParams {
  * @property origin? - Added for request origin identification
  */
 export interface AbstractMessageParamsMetamask extends AbstractMessageParams {
-  metamaskId: string;
+  metamaskId?: string;
 }
 
 /**
@@ -210,7 +210,7 @@ export abstract class AbstractMessageManager<
    * @returns - Promise resolving to the messageParams with the metamaskId property removed
    */
   approveMessage(messageParams: PM): Promise<P> {
-    this.setMessageStatusApproved(messageParams.metamaskId);
+    this.setMessageStatusApproved(messageParams.metamaskId!);
     return this.prepMessageForSigning(messageParams);
   }
 
