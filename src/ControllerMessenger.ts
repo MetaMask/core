3ed24a62d19768258ca9925329e1bd84fe1172ea
Mark Stacey <markjstacey@gmail.com>
2021-03-09 01:37:50 -0330

    Add JSDOc comment for ControllerMessenger class

--- src/ControllerMessenger.ts
+++ src/ControllerMessenger.ts
@@ -18,6 +18,13 @@ export type ExtractEventPayload<Event, T> = Event extends { type: T; payload: in
 type ActionConstraint = { type: string; handler: (...args: any) => unknown };
 type EventConstraint = { type: string; payload: unknown[] };
 
+/**
+ * A messaging system for controllers.
+ *
+ * The controller messenger allows registering functions as 'actions' that can be called elsewhere,
+ * and it allows publishing and subscribing to events. Both actions and events are identified by
+ * unique strings.
+ */
 export class ControllerMessenger<Action extends ActionConstraint, Event extends EventConstraint> {
   private actions = new Map<Action['type'], unknown>();
 
