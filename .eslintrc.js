66572aea8551e2bc2131188de11e4056fd135e89
Mark Stacey <markjstacey@gmail.com>
2021-03-15 11:59:19 -0230

    Add missing dependency and disallow extraneous requires (#392)
    The `node/no-extraneous-require` and `import/no-extraneous-dependencies`
    rules have been re-enabled. The one violation was `ethereumjs-tx`,
    which is a module we are using that wasn't listed among our
    dependencies. It has been added, using the version that was being used
    in practice via another dependency (which was v1.3.7).

--- .eslintrc.js
+++ .eslintrc.js
@@ -50,7 +50,6 @@ module.exports = {
     'guard-for-in': 'off',
     'implicit-arrow-linebreak': 'off',
     'import/no-anonymous-default-export': 'off',
-    'import/no-extraneous-dependencies': 'off',
     'import/no-unassigned-import': 'off',
     'lines-around-comment': 'off',
     'no-async-promise-executor': 'off',
@@ -63,8 +62,6 @@ module.exports = {
     'no-useless-escape': 'off',
     radix: 'off',
     'require-atomic-updates': 'off',
-
-    'node/no-extraneous-require': 'off',
 
     'jest/expect-expect': 'off',
     'jest/no-conditional-expect': 'off',
