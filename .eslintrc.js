c21e497bcebd080f85f2f55522aab8afc41c9642
Angela Lu <angxlfish@gmail.com>
2021-03-19 21:57:41 -0700

    remove commented out lint rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -51,7 +51,6 @@ module.exports = {
     'no-negated-condition': 'off',
     'no-new': 'off',
     'no-param-reassign': 'off',
-    // 'no-prototype-builtins': 'off',
     radix: 'off',
     'require-atomic-updates': 'off',
 
