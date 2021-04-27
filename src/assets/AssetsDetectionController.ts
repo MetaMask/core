c445115c81a0a3f9da5aebcd99449f6efda40a5d
Esteban Mino <efmino@uc.cl>
2021-04-27 12:17:04 -0400

    openseainterface

--- src/assets/AssetsDetectionController.ts
+++ src/assets/AssetsDetectionController.ts
@@ -17,42 +17,77 @@ const MAINNET = 'mainnet';
  * Collectible object coming from OpenSea api
  *
  * @property token_id - The collectible identifier
- * @property image_original_url - URI of collectible image associated with this collectible
+ * @property num_sales - Number of sales
+ * @property background_color - The background color to be displayed with the item
+ * @property image_url - URI of an image associated with this collectible
+ * @property image_preview_url - URI of a smaller image associated with this collectible
+ * @property image_thumbnail_url - URI of a thumbnail image associated with this collectible
+ * @property image_original_url - URI of the original image associated with this collectible
+ * @property animation_url - URI of a animation associated with this collectible
+ * @property animation_original_url - URI of the original animation associated with this collectible
  * @property name - The collectible name
  * @property description - The collectible description
- * @property assetContract - The collectible contract basic information, in this case the address
+ * @property external_link - External link containing additional information
+ * @property assetContract - The collectible contract information object
+ * @property creator - The collectible owner information object
  */
 export interface ApiCollectible {
   token_id: string;
   num_sales: 0;
   background_color: string;
   image_url: string;
-  image_preview_url: string;
-  image_thumbnail_url: string;
-  image_original_url: string;
-  animation_url: string;
-  animation_original_url: string;
-  name: string;
-  description: string;
-  external_link: string;
+  image_preview_url: string | null;
+  image_thumbnail_url: string | null;
+  image_original_url: string | null;
+  animation_url: string | null;
+  animation_original_url: string | null;
+  name: string | null;
+  description: string | null;
+  external_link: string | null;
   asset_contract: ApiCollectibleContract;
+  creator: ApiCollectibleCreator;
 }
 
+/**
+ * @type ApiCollectibleContract
+ *
+ * Collectible contract object coming from OpenSea api
+ *
+ * @property address - Address of the collectible contract
+ * @property asset_contract_type - The collectible type, it could be `semi-fungible` or `non-fungible`
+ * @property created_date - Creation date
+ * @property name - The collectible contract name
+ * @property schema_name - The schema followed by the contract, it could be `ERC721` or `ERC1155`
+ * @property symbol - The collectible contract symbol
+ * @property total_supply - Total supply of collectibles
+ * @property description - The collectible contract description
+ * @property external_link - External link containing additional information
+ * @property image_url - URI of an image associated with this collectible contract
+ */
 export interface ApiCollectibleContract {
   address: string;
   asset_contract_type: string;
   created_date: string;
-  name: string;
+  name: string | null;
   schema_name: string;
-  symbol: string;
-  total_supply: string;
-  description: string;
-  external_link: string;
-  image_url: string;
+  symbol: string | null;
+  total_supply: string | null;
+  description: string | null;
+  external_link: string | null;
+  image_url: string | null;
 }
 
+/**
+ * @type ApiCollectibleCreator
+ *
+ * Collectible creator object coming from OpenSea api
+ *
+ * @property user - Object containing a `username`
+ * @property profile_img_url - URI of collectible image associated with this owner
+ * @property address - The owner address
+ */
 export interface ApiCollectibleCreator {
-  user: {username: string};
+  user: { username: string };
   profile_img_url: string;
   address: string;
 }
