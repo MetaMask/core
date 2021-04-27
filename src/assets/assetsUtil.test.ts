b018c349e2e3dbd1dee722c7bd764c8e2bce861e
Esteban Mino <efmino@uc.cl>
2021-04-27 16:02:35 -0400

    compareCollectiblesMetadatatest

--- src/assets/assetsUtil.test.ts
+++ src/assets/assetsUtil.test.ts
@@ -30,6 +30,23 @@ describe('assetsUtil', () => {
             const different = assetsUtil.compareCollectiblesMetadata(collectibleMetadata, collectible)
             expect(different).toStrictEqual(true);
         });
+
+        it('should resolve true if any key is different as always as metadata is not undefined', () => {
+            const collectibleMetadata: CollectibleMetadata = {
+                image: 'image',
+                externalLink: 'externalLink',
+            }
+            const collectible: Collectible = {
+                address: 'address',
+                tokenId: 123,
+                name: 'name',
+                image: 'image',
+                backgroundColor: 'backgroundColor',
+                externalLink: 'externalLink',
+            }
+            const different = assetsUtil.compareCollectiblesMetadata(collectibleMetadata, collectible)
+            expect(different).toStrictEqual(false);
+        });
     
         it('should resolve false if no key is different', () => {
             const collectibleMetadata: CollectibleMetadata = {
--- src/assets/assetsUtil.ts
+++ src/assets/assetsUtil.ts
@@ -2,15 +2,17 @@ import { Collectible, CollectibleMetadata } from "./AssetsController"
 
 /**
  * Compares collectible metadata entries to any collectible entry
+ * We need this method when comparing a new fetched collectible metadata, in case a entry changed to a defined value,
+ * there's a need to update the collectible in state.
  * 
  * @param collectibleMetadata - Collectible metadata object
  * @param collectible - Collectible object to compare with
  * @returns - Whether there are differences
  */
- export function compareCollectiblesMetadata(collectibleMetadata: CollectibleMetadata, collectible: Collectible) {
+ export function compareCollectiblesMetadata(newCollectibleMetadata: CollectibleMetadata, collectible: Collectible) {
       const keys: Array<keyof CollectibleMetadata> = ['image', 'backgroundColor', 'imagePreview', 'imageThumbnail', 'imageOriginal', 'animation', 'animationOriginal', 'externalLink']
-      const differentValues = keys.reduce((value, key) => {
-        if (collectibleMetadata?.[key] && (collectibleMetadata?.[key] !== collectible[key])) {
+     const differentValues = keys.reduce((value, key) => {
+        if (newCollectibleMetadata[key] && (newCollectibleMetadata[key] !== collectible[key])) {
           return value + 1
         }
         return value
