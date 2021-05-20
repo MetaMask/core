da1137b6b497c296b639930266fab940a6399bae
Esteban Mino <efmino@uc.cl>
2021-05-20 12:02:40 -0400

    Revert "Revert "Add BaseControllerV2 support to ComposableController (#447)""
    This reverts commit c3ebf47bc11d5c00d4e3d9d17c9189f786b2597a.

--- src/BaseControllerV2.ts
+++ src/BaseControllerV2.ts
@@ -122,7 +122,12 @@ export class BaseController<
     string
   >;
 
-  private name: N;
+  /**
+   * The name of the controller.
+   *
+   * This is used by the ComposableController to construct a composed application state.
+   */
+  public readonly name: N;
 
   public readonly metadata: StateMetadata<S>;
 
