65b176157bbbb6509d26caf5262986c52b888abb
Dan Miller <danjm.com@gmail.com>
2021-06-14 12:32:53 -0230

    First draft of gas fee controller

--- src/network/NetworkController.ts
+++ src/network/NetworkController.ts
@@ -46,6 +46,14 @@ export interface ProviderConfig {
   nickname?: string;
 }
 
+export interface Block {
+  baseFee?: string;
+}
+
+export interface NetworkProperties {
+  isEIP1559Compatible?: boolean;
+}
+
 /**
  * @type NetworkConfig
  *
@@ -70,6 +78,7 @@ export interface NetworkConfig extends BaseConfig {
 export interface NetworkState extends BaseState {
   network: string;
   provider: ProviderConfig;
+  properties: NetworkProperties;
 }
 
 const LOCALHOST_RPC_URL = 'http://localhost:8545';
@@ -203,6 +212,7 @@ export class NetworkController extends BaseController<
       provider: { type: 'mainnet', chainId: NetworksChainId.mainnet },
     };
     this.initialize();
+    this.getNetworkProperties();
   }
 
   /**
@@ -286,6 +296,19 @@ export class NetworkController extends BaseController<
       },
     });
     this.refreshNetwork();
+  }
+
+  getNetworkProperties() {
+    this.ethQuery.sendAsync(
+      { method: 'eth_getBlockByNumber', params: ['latest', false] },
+      (error: Error, block: Block) => {
+        this.update({
+          properties: {
+            isEIP1559Compatible: typeof block.baseFee !== undefined,
+          }
+        });
+      },
+    );
   }
 }
 
