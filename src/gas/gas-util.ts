85bc661fcd4204295edc480b1c1842b2e556cf7d
Dan Miller <danjm.com@gmail.com>
2021-06-21 07:18:33 -0230

    Add getTimeEstimate to GasFee controller

--- src/gas/gas-util.ts
+++ src/gas/gas-util.ts
@@ -1,5 +1,11 @@
+import { BN } from 'ethereumjs-util';
 import { query } from '../util';
-import { GasFeeEstimates, LegacyGasPriceEstimate } from './GasFee.controller';
+import {
+  GasFeeEstimates,
+  LegacyGasPriceEstimate,
+  EstimatedGasFeeTimeBounds,
+  unknownString,
+} from './GasFee.controller';
 
 // import { handleFetch } from '../util';
 
@@ -149,4 +155,67 @@ export async function fetchLegacyGasPriceEstimate(
   ethQuery: any,
 ): Promise<LegacyGasPriceEstimate> {
   return await query(ethQuery, 'gasPrice');
+}
+
+function gweiHexToWEIBN(n: any) {
+  const BN_1000 = new BN(1000, 10);
+  return new BN(n, 16).mul(BN_1000);
+}
+
+export function calculateTimeEstimate(
+  maxPriorityFeePerGas: string,
+  maxFeePerGas: string,
+  gasFeeEstimates: GasFeeEstimates,
+): EstimatedGasFeeTimeBounds {
+  const { low, medium, high, estimatedBaseFee } = gasFeeEstimates;
+
+  const maxPriorityFeePerGasInWEI = gweiHexToWEIBN(maxPriorityFeePerGas);
+  const maxFeePerGasInWEI = gweiHexToWEIBN(maxFeePerGas);
+  const estimatedBaseFeeInWEI = gweiHexToWEIBN(estimatedBaseFee);
+
+  const effectiveMaxPriorityFee = BN.min(
+    maxPriorityFeePerGasInWEI,
+    maxFeePerGasInWEI.sub(estimatedBaseFeeInWEI),
+  );
+
+  const lowMaxPriorityFeeInWEI = gweiHexToWEIBN(
+    low.suggestedMaxPriorityFeePerGas,
+  );
+  const mediumMaxPriorityFeeInWEI = gweiHexToWEIBN(
+    medium.suggestedMaxPriorityFeePerGas,
+  );
+  const highMaxPriorityFeeInWEI = gweiHexToWEIBN(
+    high.suggestedMaxPriorityFeePerGas,
+  );
+
+  let lowerTimeBound;
+  let upperTimeBound;
+
+  if (effectiveMaxPriorityFee.lt(lowMaxPriorityFeeInWEI)) {
+    lowerTimeBound = null;
+    upperTimeBound = 'unknown' as unknownString;
+  } else if (
+    effectiveMaxPriorityFee.gte(lowMaxPriorityFeeInWEI) &&
+    effectiveMaxPriorityFee.lt(mediumMaxPriorityFeeInWEI)
+  ) {
+    lowerTimeBound = low.minWaitTimeEstimate;
+    upperTimeBound = low.maxWaitTimeEstimate;
+  } else if (
+    effectiveMaxPriorityFee.gte(mediumMaxPriorityFeeInWEI) &&
+    effectiveMaxPriorityFee.lt(highMaxPriorityFeeInWEI)
+  ) {
+    lowerTimeBound = medium.minWaitTimeEstimate;
+    upperTimeBound = medium.maxWaitTimeEstimate;
+  } else if (effectiveMaxPriorityFee.eq(highMaxPriorityFeeInWEI)) {
+    lowerTimeBound = high.minWaitTimeEstimate;
+    upperTimeBound = high.maxWaitTimeEstimate;
+  } else {
+    lowerTimeBound = 0;
+    upperTimeBound = high.maxWaitTimeEstimate;
+  }
+
+  return {
+    lowerTimeBound,
+    upperTimeBound,
+  };
 }
