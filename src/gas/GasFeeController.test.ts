97e9e8f8ffe4b14c50b492111cb50af24a43f6a8
Ricky Miller <ricky.miller@gmail.com>
2021-06-23 15:28:55 -0400

    Add GasFeeController.test.ts

---
+++ src/gas/GasFeeController.test.ts
@@ -0,0 +1,38 @@
+import { stub } from 'sinon';
+import {
+  ControllerMessenger,
+  RestrictedControllerMessenger,
+} from '../ControllerMessenger';
+import {
+  GasFeeController,
+  GetGasFeeState,
+  GasFeeStateChange,
+} from './GasFeeController';
+
+const name = 'GasFeeController';
+
+const controllerMessenger = new RestrictedControllerMessenger<
+  typeof name,
+  GetGasFeeState,
+  GasFeeStateChange,
+  never,
+  never
+>({
+  name,
+  controllerMessenger: new ControllerMessenger(),
+});
+
+describe('GasFeeController', () => {
+  it('should initialize', () => {
+    const controller = new GasFeeController({
+      interval: 10000,
+      // TODO: fix this
+      messenger: controllerMessenger,
+      getProvider: () => stub(),
+      onNetworkStateChange: () => stub(),
+      getCurrentNetworkEIP1559Compatibility: () => Promise.resolve(true), // change this for networkController.state.properties.isEIP1559Compatible ???
+    });
+
+    expect(controller.name).toBe(name);
+  });
+});
