6c06471df2cefe548d39817074a31c691fd0acec
Cal Leung <cleun007@gmail.com>
2021-07-27 11:24:00 -0700

    Fix linting error

--- src/apis/token-service.ts
+++ src/apis/token-service.ts
@@ -12,7 +12,7 @@ function getTokenMetadataURL(chainId: string, tokenAddress: string) {
   return `${END_POINT}/tokens/${chainId}?address=${tokenAddress}`;
 }
 
-// Token list averages 1.6 MB in size. 
+// Token list averages 1.6 MB in size
 // timeoutFetch by default has a 500ms timeout, which will almost always timeout given the response size.
 const timeout = 2000;
 
@@ -71,6 +71,10 @@ export async function fetchTokenMetadata(
   };
   fetchOptions.headers = new window.Headers();
   fetchOptions.headers.set('Content-Type', 'application/json');
-  const tokenResponse = await timeoutFetch(tokenMetadataURL, fetchOptions, timeout);
+  const tokenResponse = await timeoutFetch(
+    tokenMetadataURL,
+    fetchOptions,
+    timeout,
+  );
   return await tokenResponse.json();
 }
