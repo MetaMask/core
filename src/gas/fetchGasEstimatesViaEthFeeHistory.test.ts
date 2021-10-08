46c3788d7e5f0aabe209de22209d81645c3b1023
Elliot Winkler <elliot.winkler@gmail.com>
2021-10-08 16:28:07 -0600

    Use eth_feeHistory as a fallback for gas estimates
    If we are on an EIP-1559-supported network and the Metaswap API fails
    for some reason, fall back to using `eth_feeHistory` to calculate gas
    estimates (which the API uses anyway). This code is more or less taken
    from the code for the API ([1]).
    
    [1]: https://gitlab.com/ConsenSys/codefi/products/metaswap/gas-api/-/blob/eae6927b1a0c445e02cb3cba9e9e6b0f35857a12/src/eip1559/feeHistory.ts

---
+++ src/gas/fetchGasEstimatesViaEthFeeHistory.test.ts
@@ -0,0 +1,70 @@
+import { BN } from 'ethereumjs-util';
+import { mocked } from 'ts-jest/utils';
+import fetchBlockFeeHistory from './fetchBlockFeeHistory';
+import fetchGasEstimatesViaEthFeeHistory from './fetchGasEstimatesViaEthFeeHistory';
+
+jest.mock('./fetchBlockFeeHistory');
+
+const mockedFetchFeeHistory = mocked(fetchBlockFeeHistory, true);
+
+describe('fetchGasEstimatesViaEthFeeHistory', () => {
+  it('calculates target fees for low, medium, and high transaction priority levels', async () => {
+    const ethQuery = {};
+    mockedFetchFeeHistory.mockResolvedValue([
+      {
+        number: new BN(1),
+        baseFeePerGas: new BN(0),
+        gasUsedRatio: 1,
+        priorityFeesByPercentile: {
+          10: new BN(0),
+          20: new BN(1_000_000_000),
+          30: new BN(0),
+        },
+      },
+      {
+        number: new BN(2),
+        baseFeePerGas: new BN(0),
+        gasUsedRatio: 1,
+        priorityFeesByPercentile: {
+          10: new BN(500_000_000),
+          20: new BN(1_600_000_000),
+          30: new BN(3_000_000_000),
+        },
+      },
+      {
+        number: new BN(3),
+        baseFeePerGas: new BN(100_000_000_000),
+        gasUsedRatio: 1,
+        priorityFeesByPercentile: {
+          10: new BN(500_000_000),
+          20: new BN(2_000_000_000),
+          30: new BN(3_000_000_000),
+        },
+      },
+    ]);
+
+    const gasFeeEstimates = await fetchGasEstimatesViaEthFeeHistory(ethQuery);
+
+    expect(gasFeeEstimates).toStrictEqual({
+      low: {
+        minWaitTimeEstimate: 15_000,
+        maxWaitTimeEstimate: 30_000,
+        suggestedMaxPriorityFeePerGas: '1',
+        suggestedMaxFeePerGas: '111',
+      },
+      medium: {
+        minWaitTimeEstimate: 15_000,
+        maxWaitTimeEstimate: 45_000,
+        suggestedMaxPriorityFeePerGas: '1.552',
+        suggestedMaxFeePerGas: '121.552',
+      },
+      high: {
+        minWaitTimeEstimate: 15_000,
+        maxWaitTimeEstimate: 60_000,
+        suggestedMaxPriorityFeePerGas: '2.94',
+        suggestedMaxFeePerGas: '127.94',
+      },
+      estimatedBaseFee: '100',
+    });
+  });
+});
