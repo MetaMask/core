57e3623d175f053bb4b4d3af90dfa797b265746f
gantunesr <gantunes@uc.cl>
2021-10-27 12:47:46 -0300

    Add rinkeby network id as constant

--- src/constants.ts
+++ src/constants.ts
@@ -2,5 +2,9 @@ export const MAINNET = 'mainnet';
 export const RPC = 'rpc';
 export const FALL_BACK_VS_CURRENCY = 'ETH';
 
+// NETWORKS ID
+export const RINKEBY_ID = '4';
+
+// TOKEN STANDARDS
 export const ERC721 = 'ERC721';
 export const ERC1155 = 'ERC1155';
