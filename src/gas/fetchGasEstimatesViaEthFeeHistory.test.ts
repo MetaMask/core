85d0027da0745ee1969dcb6c27de07dd946738a4
Elliot Winkler <elliot.winkler@gmail.com>
2021-11-23 12:02:31 -0700

    Refactor EIP-1559 v2 fallback code to ease expansion
    In future commits we will bring in more gas fee related data points from
    the MetaSwap API. When we do this, we will have to implement fallbacks
    for all of these data points. Every data point is different and will
    require a different strategy and different testing. This commit breaks
    up the existing fallback code so this is a lot easier to do.

--- src/gas/fetchGasEstimatesViaEthFeeHistory.test.ts
+++ src/gas/fetchGasEstimatesViaEthFeeHistory.test.ts
@@ -1,83 +1,99 @@
 import { BN } from 'ethereumjs-util';
 import { mocked } from 'ts-jest/utils';
 import { when } from 'jest-when';
-import fetchBlockFeeHistory from './fetchBlockFeeHistory';
+import BlockFeeHistoryDatasetFetcher from './fetchGasEstimatesViaEthFeeHistory/BlockFeeHistoryDatasetFetcher';
+import calculateGasFeeEstimatesForPriorityLevels from './fetchGasEstimatesViaEthFeeHistory/calculateGasFeeEstimatesForPriorityLevels';
+import calculateNetworkCongestion from './fetchGasEstimatesViaEthFeeHistory/calculateNetworkCongestion';
+import fetchLatestBlock from './fetchGasEstimatesViaEthFeeHistory/fetchLatestBlock';
 import fetchGasEstimatesViaEthFeeHistory from './fetchGasEstimatesViaEthFeeHistory';
 
-jest.mock('./fetchBlockFeeHistory');
+jest.mock('./fetchGasEstimatesViaEthFeeHistory/BlockFeeHistoryDatasetFetcher');
+jest.mock(
+  './fetchGasEstimatesViaEthFeeHistory/calculateGasFeeEstimatesForPriorityLevels',
+);
+jest.mock('./fetchGasEstimatesViaEthFeeHistory/calculateNetworkCongestion');
+jest.mock('./fetchGasEstimatesViaEthFeeHistory/fetchLatestBlock');
 
-const mockedFetchBlockFeeHistory = mocked(fetchBlockFeeHistory, true);
+const mockedBlockFeeHistoryDatasetFetcherConstructor = mocked(
+  BlockFeeHistoryDatasetFetcher,
+  true,
+);
+const mockedCalculateGasFeeEstimatesForPriorityLevels = mocked(
+  calculateGasFeeEstimatesForPriorityLevels,
+  true,
+);
+const mockedCalculateNetworkCongestion = mocked(
+  calculateNetworkCongestion,
+  true,
+);
+const mockedFetchLatestBlock = mocked(fetchLatestBlock, true);
 
 describe('fetchGasEstimatesViaEthFeeHistory', () => {
+  const latestBlock = {
+    number: new BN(1),
+    baseFeePerGas: new BN(100_000_000_000),
+  };
+  const ethQuery = {
+    blockNumber: async () => latestBlock.number,
+    getBlockByNumber: async () => latestBlock,
+  };
+
   it('calculates target fees for low, medium, and high transaction priority levels, as well as the network congestion level', async () => {
-    const ethQuery = {};
-    when(mockedFetchBlockFeeHistory)
-      .calledWith({
-        ethQuery,
-        numberOfBlocks: 5,
-        percentiles: [10, 20, 30],
-      })
-      .mockResolvedValue([
+    const blocksByDataset = {
+      longRange: [
         {
           number: new BN(1),
-          baseFeePerGas: new BN(300_000_000_000),
+          baseFeePerGas: new BN(1),
           gasUsedRatio: 1,
-          priorityFeesByPercentile: {
-            10: new BN(0),
-            20: new BN(1_000_000_000),
-            30: new BN(0),
-          },
+          priorityFeesByPercentile: {},
         },
+      ],
+      mediumRange: [
         {
           number: new BN(2),
-          baseFeePerGas: new BN(100_000_000_000),
+          baseFeePerGas: new BN(1),
           gasUsedRatio: 1,
           priorityFeesByPercentile: {
-            10: new BN(500_000_000),
-            20: new BN(1_600_000_000),
-            30: new BN(3_000_000_000),
+            10: new BN('0'),
+            95: new BN('0'),
           },
         },
+      ],
+      smallRange: [
         {
           number: new BN(3),
-          baseFeePerGas: new BN(200_000_000_000),
+          baseFeePerGas: new BN(1),
           gasUsedRatio: 1,
           priorityFeesByPercentile: {
-            10: new BN(500_000_000),
-            20: new BN(2_000_000_000),
-            30: new BN(3_000_000_000),
+            10: new BN('0'),
+            20: new BN('0'),
+            30: new BN('0'),
           },
         },
-      ])
-      .calledWith({
-        ethQuery,
-        numberOfBlocks: 20_000,
-        endBlock: new BN(3),
-      })
-      .mockResolvedValue([
+      ],
+      tinyRange: [
         {
-          number: new BN(1),
-          baseFeePerGas: new BN(300_000_000_000),
+          number: new BN(4),
+          baseFeePerGas: new BN(1),
           gasUsedRatio: 1,
-          priorityFeesByPercentile: {},
+          priorityFeesByPercentile: {
+            50: new BN('0'),
+          },
         },
+      ],
+      latest: [
         {
-          number: new BN(2),
-          baseFeePerGas: new BN(100_000_000_000),
+          number: new BN(5),
+          baseFeePerGas: new BN(1),
           gasUsedRatio: 1,
-          priorityFeesByPercentile: {},
+          priorityFeesByPercentile: {
+            10: new BN('0'),
+            95: new BN('0'),
+          },
         },
-        {
-          number: new BN(3),
-          baseFeePerGas: new BN(200_000_000_000),
-          gasUsedRatio: 1,
-          priorityFeesByPercentile: {},
-        },
-      ]);
-
-    const gasFeeEstimates = await fetchGasEstimatesViaEthFeeHistory(ethQuery);
-
-    expect(gasFeeEstimates).toStrictEqual({
+      ],
+    };
+    const levelSpecificEstimates = {
       low: {
         minWaitTimeEstimate: 15_000,
         maxWaitTimeEstimate: 30_000,
@@ -96,8 +112,32 @@ describe('fetchGasEstimatesViaEthFeeHistory', () => {
         suggestedMaxPriorityFeePerGas: '2.94',
         suggestedMaxFeePerGas: '252.94',
       },
-      estimatedBaseFee: '200',
-      networkCongestion: 0.5,
+    };
+    const networkCongestion = 0.5;
+
+    mockedFetchLatestBlock.mockResolvedValue(latestBlock);
+    mockedBlockFeeHistoryDatasetFetcherConstructor.prototype.forLongRange.mockResolvedValue(
+      blocksByDataset.longRange,
+    );
+
+    mockedBlockFeeHistoryDatasetFetcherConstructor.prototype.forSmallRange.mockResolvedValue(
+      blocksByDataset.smallRange,
+    );
+
+    when(mockedCalculateGasFeeEstimatesForPriorityLevels)
+      .calledWith(blocksByDataset.smallRange)
+      .mockReturnValue(levelSpecificEstimates);
+
+    when(mockedCalculateNetworkCongestion)
+      .calledWith(blocksByDataset.longRange)
+      .mockReturnValue(networkCongestion);
+
+    const gasFeeEstimates = await fetchGasEstimatesViaEthFeeHistory(ethQuery);
+
+    expect(gasFeeEstimates).toStrictEqual({
+      ...levelSpecificEstimates,
+      estimatedBaseFee: '100',
+      networkCongestion,
     });
   });
 });
