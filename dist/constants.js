a85cc40903ede4e0f985482ee99ba9c0e83677ec
gantunesr <gantunes@uc.cl>
2021-12-01 21:22:30 -0300

    Add dist

---
+++ dist/constants.js
@@ -0,0 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.ERC1155 = exports.ERC721 = exports.RINKEBY_CHAIN_ID = exports.IPFS_DEFAULT_GATEWAY_URL = exports.FALL_BACK_VS_CURRENCY = exports.RPC = exports.MAINNET = void 0;
+exports.MAINNET = 'mainnet';
+exports.RPC = 'rpc';
+exports.FALL_BACK_VS_CURRENCY = 'ETH';
+exports.IPFS_DEFAULT_GATEWAY_URL = 'https://cloudflare-ipfs.com/ipfs/';
+// NETWORKS ID
+exports.RINKEBY_CHAIN_ID = '4';
+// TOKEN STANDARDS
+exports.ERC721 = 'ERC721';
+exports.ERC1155 = 'ERC1155';
+//# sourceMappingURL=constants.js.map
