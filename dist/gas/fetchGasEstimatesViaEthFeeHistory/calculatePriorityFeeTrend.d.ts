064d540ba2fda3a1a6d26c434ccc478040a0e509
gantunesr <gantunes@uc.cl>
2021-12-15 22:13:17 -0300

    Remove dist

--- dist/gas/fetchGasEstimatesViaEthFeeHistory/calculatePriorityFeeTrend.d.ts
+++
@@ -1,9 +0,0 @@
-import { ExistingFeeHistoryBlock } from '../fetchBlockFeeHistory';
-/**
- * Given a collection of blocks, returns an indicator of whether the base fee is moving up, down, or
- * holding steady, based on comparing the last base fee in the collection to the first.
- *
- * @param blocks - A set of blocks obtained via {@link BlockFeeHistoryDatasetFetcher}.
- * @returns The indicator ("up", "down", or "level").
- */
-export default function calculatePriorityFeeTrend(blocks: ExistingFeeHistoryBlock<50>[]): "down" | "up" | "level";
