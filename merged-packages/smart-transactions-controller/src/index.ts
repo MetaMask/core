6e642645d384f98248d20715a8cc558d82de5f61
Matthew Epps <mepps32@gmail.com>
2022-01-11 04:11:04 -0800

    Add isomorphic-fetch to stx controller (#38)

--- merged-packages/smart-transactions-controller/src/index.ts
+++ merged-packages/smart-transactions-controller/src/index.ts
@@ -1,3 +1,5 @@
+// eslint-disable-next-line import/no-unassigned-import
+import 'isomorphic-fetch';
 import SmartTransactionsController from './SmartTransactionsController';
 
 export default SmartTransactionsController;
