ab0ea202cb265bd6cf1f2c7438d2b43ecc43b351
Soralit <soralitria@gmail.com>
2022-02-18 19:41:52 +0800

    use keyring name as account label

--- src/keyring/KeyringController.ts
+++ src/keyring/KeyringController.ts
@@ -689,7 +689,7 @@ export class KeyringController extends BaseController<
     newAccounts.forEach((address: string) => {
       if (!oldAccounts.includes(address)) {
         if (this.setAccountLabel) {
-          this.setAccountLabel(address, `QR Hardware ${index}`);
+          this.setAccountLabel(address, `${keyring.getName()} ${index}`);
         }
         this.setSelectedAddress(address);
       }
