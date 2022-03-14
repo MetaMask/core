dd48bba89f3ae1a39fa5b170686c84bdf9ce0e3d
Elliot Winkler <elliot.winkler@gmail.com>
2022-03-14 10:41:43 -0600

    Remove non-critical gas estimate fallback data  (#712)
    If a request to the Gas API fails for any reason, we attempt to hit
    Infura directly and compute the same pieces of data that the Gas API
    computes. However, considering that this data supports the new gas fee
    UI design in the extension, it is not necessary for us to emulate the
    API 100%. We only need data that is critical to allow users to see and
    select fees for different priority levels in order to make transactions.
    Everything else — a snapshot of recent base and priority fees, whether
    the base and priority fees have risen or fallen recently, and how busy
    the network is — is not important.
    
    This commit removes these extra computations so as to further limit the
    amount of requests that are made to Infura.

--- src/gas/fetchGasEstimatesViaEthFeeHistory.test.ts
+++ src/gas/fetchGasEstimatesViaEthFeeHistory.test.ts
@@ -1,41 +1,20 @@
 import { BN } from 'ethereumjs-util';
 import { mocked } from 'ts-jest/utils';
 import { when } from 'jest-when';
-import BlockFeeHistoryDatasetFetcher from './fetchGasEstimatesViaEthFeeHistory/BlockFeeHistoryDatasetFetcher';
+import fetchBlockFeeHistory from './fetchBlockFeeHistory';
 import calculateGasFeeEstimatesForPriorityLevels from './fetchGasEstimatesViaEthFeeHistory/calculateGasFeeEstimatesForPriorityLevels';
-import calculateBaseFeeRange from './fetchGasEstimatesViaEthFeeHistory/calculateBaseFeeRange';
-import calculateBaseFeeTrend from './fetchGasEstimatesViaEthFeeHistory/calculateBaseFeeTrend';
-import calculatePriorityFeeRange from './fetchGasEstimatesViaEthFeeHistory/calculatePriorityFeeRange';
-import calculatePriorityFeeTrend from './fetchGasEstimatesViaEthFeeHistory/calculatePriorityFeeTrend';
-import calculateNetworkCongestion from './fetchGasEstimatesViaEthFeeHistory/calculateNetworkCongestion';
 import fetchLatestBlock from './fetchGasEstimatesViaEthFeeHistory/fetchLatestBlock';
 import fetchGasEstimatesViaEthFeeHistory from './fetchGasEstimatesViaEthFeeHistory';
 
-jest.mock('./fetchGasEstimatesViaEthFeeHistory/BlockFeeHistoryDatasetFetcher');
+jest.mock('./fetchBlockFeeHistory');
 jest.mock(
   './fetchGasEstimatesViaEthFeeHistory/calculateGasFeeEstimatesForPriorityLevels',
 );
-jest.mock('./fetchGasEstimatesViaEthFeeHistory/calculateBaseFeeRange');
-jest.mock('./fetchGasEstimatesViaEthFeeHistory/calculateBaseFeeTrend');
-jest.mock('./fetchGasEstimatesViaEthFeeHistory/calculatePriorityFeeRange');
-jest.mock('./fetchGasEstimatesViaEthFeeHistory/calculatePriorityFeeTrend');
-jest.mock('./fetchGasEstimatesViaEthFeeHistory/calculateNetworkCongestion');
 jest.mock('./fetchGasEstimatesViaEthFeeHistory/fetchLatestBlock');
 
-const mockedBlockFeeHistoryDatasetFetcherConstructor = mocked(
-  BlockFeeHistoryDatasetFetcher,
-  true,
-);
+const mockedFetchBlockFeeHistory = mocked(fetchBlockFeeHistory, true);
 const mockedCalculateGasFeeEstimatesForPriorityLevels = mocked(
   calculateGasFeeEstimatesForPriorityLevels,
-  true,
-);
-const mockedCalculateBaseFeeRange = mocked(calculateBaseFeeRange, true);
-const mockedCalculateBaseFeeTrend = mocked(calculateBaseFeeTrend, true);
-const mockedCalculatePriorityFeeRange = mocked(calculatePriorityFeeRange, true);
-const mockedCalculatePriorityFeeTrend = mocked(calculatePriorityFeeTrend, true);
-const mockedCalculateNetworkCongestion = mocked(
-  calculateNetworkCongestion,
   true,
 );
 const mockedFetchLatestBlock = mocked(fetchLatestBlock, true);
@@ -50,80 +29,19 @@ describe('fetchGasEstimatesViaEthFeeHistory', () => {
     getBlockByNumber: async () => latestBlock,
   };
 
-  it('calculates target fees for low, medium, and high transaction priority levels, as well as the network congestion level', async () => {
-    const blocksByDataset = {
-      mediumRange: [
-        {
-          number: new BN(2),
-          baseFeePerGas: new BN(1),
-          gasUsedRatio: 1,
-          priorityFeesByPercentile: {
-            10: new BN('0'),
-            95: new BN('0'),
-          },
+  it('calculates target fees for low, medium, and high transaction priority levels', async () => {
+    const blocks = [
+      {
+        number: new BN(3),
+        baseFeePerGas: new BN(1),
+        gasUsedRatio: 1,
+        priorityFeesByPercentile: {
+          10: new BN('0'),
+          20: new BN('0'),
+          30: new BN('0'),
         },
-      ],
-      smallRange: [
-        {
-          number: new BN(3),
-          baseFeePerGas: new BN(1),
-          gasUsedRatio: 1,
-          priorityFeesByPercentile: {
-            10: new BN('0'),
-            20: new BN('0'),
-            30: new BN('0'),
-          },
-        },
-      ],
-      tinyRange: [
-        {
-          number: new BN(4),
-          baseFeePerGas: new BN(1),
-          gasUsedRatio: 1,
-          priorityFeesByPercentile: {
-            50: new BN('0'),
-          },
-        },
-      ],
-      tinyRangeWithNextBlock: [
-        {
-          number: new BN(5),
-          baseFeePerGas: new BN(1),
-          gasUsedRatio: 1,
-          priorityFeesByPercentile: {
-            50: new BN('0'),
-          },
-        },
-      ],
-      latest: [
-        {
-          number: new BN(6),
-          baseFeePerGas: new BN(1),
-          gasUsedRatio: 1,
-          priorityFeesByPercentile: {
-            10: new BN('0'),
-            95: new BN('0'),
-          },
-        },
-      ],
-      latestWithNextBlock: [
-        {
-          number: new BN(6),
-          baseFeePerGas: new BN(1),
-          gasUsedRatio: 1,
-          priorityFeesByPercentile: {
-            10: new BN('0'),
-            95: new BN('0'),
-          },
-        },
-        {
-          number: new BN(7),
-          baseFeePerGas: new BN(1),
-          gasUsedRatio: null,
-          priorityFeesByPercentile: null,
-        },
-      ],
-    };
+      },
+    ];
     const levelSpecificEstimates = {
       low: {
         minWaitTimeEstimate: 15_000,
@@ -144,55 +62,32 @@ describe('fetchGasEstimatesViaEthFeeHistory', () => {
         suggestedMaxFeePerGas: '252.94',
       },
     };
-    const historicalBaseFeeRange: [string, string] = ['100', '200'];
-    const baseFeeTrend = 'up';
-    const latestPriorityFeeRange: [string, string] = ['1', '2'];
-    const historicalPriorityFeeRange: [string, string] = ['2', '4'];
-    const priorityFeeTrend = 'down';
-    const networkCongestion = 0.5;
 
     mockedFetchLatestBlock.mockResolvedValue(latestBlock);
-    mockedBlockFeeHistoryDatasetFetcherConstructor.prototype.forAll.mockResolvedValue(
-      blocksByDataset,
-    );
+    when(mockedFetchBlockFeeHistory)
+      .calledWith({
+        ethQuery,
+        endBlock: latestBlock.number,
+        numberOfBlocks: 5,
+        percentiles: [10, 20, 30],
+      })
+      .mockResolvedValue(blocks);
 
     when(mockedCalculateGasFeeEstimatesForPriorityLevels)
-      .calledWith(blocksByDataset.smallRange)
+      .calledWith(blocks)
       .mockReturnValue(levelSpecificEstimates);
-
-    when(mockedCalculateBaseFeeRange)
-      .calledWith(blocksByDataset.mediumRange)
-      .mockReturnValue(historicalBaseFeeRange);
-
-    when(mockedCalculateBaseFeeTrend)
-      .calledWith(blocksByDataset.latestWithNextBlock)
-      .mockReturnValue(baseFeeTrend);
-
-    when(mockedCalculatePriorityFeeRange)
-      .calledWith(blocksByDataset.latest)
-      .mockReturnValue(latestPriorityFeeRange)
-      .calledWith(blocksByDataset.mediumRange)
-      .mockReturnValue(historicalPriorityFeeRange);
-
-    when(mockedCalculatePriorityFeeTrend)
-      .calledWith(blocksByDataset.tinyRange)
-      .mockReturnValue(priorityFeeTrend);
-
-    when(mockedCalculateNetworkCongestion)
-      .calledWith([])
-      .mockReturnValue(networkCongestion);
 
     const gasFeeEstimates = await fetchGasEstimatesViaEthFeeHistory(ethQuery);
 
     expect(gasFeeEstimates).toStrictEqual({
       ...levelSpecificEstimates,
       estimatedBaseFee: '100',
-      historicalBaseFeeRange,
-      baseFeeTrend,
-      latestPriorityFeeRange,
-      historicalPriorityFeeRange,
-      priorityFeeTrend,
-      networkCongestion,
+      historicalBaseFeeRange: null,
+      baseFeeTrend: null,
+      latestPriorityFeeRange: null,
+      historicalPriorityFeeRange: null,
+      priorityFeeTrend: null,
+      networkCongestion: null,
     });
   });
 });
