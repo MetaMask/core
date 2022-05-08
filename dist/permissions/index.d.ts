fb412d7e288680ca0a77b1a526251f81169da03e
gantunesr <gantunes@uc.cl>
2022-05-08 13:36:54 -0400

    Remove dist dir

--- dist/permissions/index.d.ts
+++
@@ -1,6 +0,0 @@
-export * from './Caveat';
-export * from './Permission';
-export * from './PermissionController';
-export * from './utils';
-export * as permissionRpcMethods from './rpc-methods';
-export { endowmentPermissionBuilders } from './endowments';
