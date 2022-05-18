1ac3790f770571042f3db285487ce6bec35d646d
Frederik Bolding <frederik.bolding@gmail.com>
2022-05-18 19:28:41 +0200

    Fix nit

--- src/assets/Standards/ERC20Standard.test.ts
+++ src/assets/Standards/ERC20Standard.test.ts
@@ -81,7 +81,7 @@ describe('ERC20Standard', () => {
         method: 'eth_call',
         params: [
           {
-            to: '0x9f8F72aA9304c8B593d555F12eF6589cC3A579A2',
+            to: MKR_ADDRESS,
             data: '0x313ce567',
           },
           'latest',
@@ -99,7 +99,7 @@ describe('ERC20Standard', () => {
         method: 'eth_call',
         params: [
           {
-            to: '0x9f8F72aA9304c8B593d555F12eF6589cC3A579A2',
+            to: MKR_ADDRESS,
             data: '0x95d89b41',
           },
           'latest',
