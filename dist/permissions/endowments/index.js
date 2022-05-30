106566ccc148e8357e09125096caec1b26d48ef9
gantunesr <gantunes@uc.cl>
2022-05-30 11:38:36 -0400

    Rm dist dir

--- dist/permissions/endowments/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.endowmentPermissionBuilders = void 0;
-const network_access_1 = require("./network-access");
-exports.endowmentPermissionBuilders = {
-    [network_access_1.networkAccessEndowmentBuilder.targetKey]: network_access_1.networkAccessEndowmentBuilder,
-};
-//# sourceMappingURL=index.js.map
