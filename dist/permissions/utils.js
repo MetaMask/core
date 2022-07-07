3df2f4cef90fce9b3b5d6818313ab2180d5bc0f8
sethkfman <10342624+sethkfman@users.noreply.github.com>
2022-07-07 15:31:12 -0600

    Revert "[CHANGED] - Testnet Ticker format update <network name>ETH (#856)" (#867)
    This reverts commit fe771ee26f77be1211bcdff25992a916155a83e4.

--- dist/permissions/utils.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.MethodNames = void 0;
-var MethodNames;
-(function (MethodNames) {
-    MethodNames["requestPermissions"] = "wallet_requestPermissions";
-    MethodNames["getPermissions"] = "wallet_getPermissions";
-})(MethodNames = exports.MethodNames || (exports.MethodNames = {}));
-//# sourceMappingURL=utils.js.map
