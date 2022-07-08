daywednes/controllers

fix publishing configuration, closes #858

scripts/prepack.sh
@@ -1,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -x
+set -e
+set -o pipefail
+
+if [[ -z $SKIP_PREPACK ]]; then
+  yarn build
+  exit 0
+fi
+
+echo "Notice: skipping prepack."

MetaMask/core

Co-authored-by: Elliot Winkler <elliot.winkler@gmail.com>

scripts/prepack.sh
@@ -1,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -x
+set -e
+set -o pipefail
+
+if [[ -n $SKIP_PREPACK ]]; then
+  echo "Notice: skipping prepack."
+  exit 0
+fi
+
+yarn build