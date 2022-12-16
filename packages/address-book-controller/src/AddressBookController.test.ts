62544644f50c6076ce629192476de95d8f4b28e5
Elliot Winkler <elliot.winkler@gmail.com>
2022-12-16 13:04:41 -0700

    Revert "Intentionally cause a failure"
    This reverts commit 7c8adb26dcea36fa91d5dba01d83c8ac3412f5c0.

--- packages/address-book-controller/src/AddressBookController.test.ts
+++ packages/address-book-controller/src/AddressBookController.test.ts
@@ -3,7 +3,7 @@ import { AddressBookController } from './AddressBookController';
 describe('AddressBookController', () => {
   it('should set default state', () => {
     const controller = new AddressBookController();
-    expect(controller.state).toBe('oops');
+    expect(controller.state).toStrictEqual({ addressBook: {} });
   });
 
   it('should add a contact entry', () => {
@@ -203,12 +203,12 @@ describe('AddressBookController', () => {
     const controller = new AddressBookController();
     expect(
       controller.set('0x32Be343B94f860124dC4fEe278FDCBD38C102D88', 'foo'),
-    ).toBe(true);
+    ).toStrictEqual(true);
   });
 
   it('should return false to indicate an address book entry has NOT been added', () => {
     const controller = new AddressBookController();
-    expect(controller.set('0x00', 'foo')).toBe(false);
+    expect(controller.set('0x00', 'foo')).toStrictEqual(false);
   });
 
   it('should return true to indicate an address book entry has been deleted', () => {
@@ -216,13 +216,13 @@ describe('AddressBookController', () => {
     controller.set('0x32Be343B94f860124dC4fEe278FDCBD38C102D88', 'foo');
     expect(
       controller.delete('1', '0x32Be343B94f860124dC4fEe278FDCBD38C102D88'),
-    ).toBe(true);
+    ).toStrictEqual(true);
   });
 
   it('should return false to indicate an address book entry has NOT been deleted', () => {
     const controller = new AddressBookController();
     controller.set('0x32Be343B94f860124dC4fEe278FDCBD38C102D88', '0x00');
-    expect(controller.delete('1', '0x01')).toBe(false);
+    expect(controller.delete('1', '0x01')).toStrictEqual(false);
   });
 
   it('should normalize addresses so adding and removing entries work across casings', () => {
