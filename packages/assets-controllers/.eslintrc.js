77a4ad4833ff9f786bd08faa6dff8b3a1987b77d
Maarten Zuidhoorn <maarten@zuidhoorn.com>
2022-12-21 11:18:36 +0100

    Disable no-shadow for assets-controllers

--- packages/assets-controllers/.eslintrc.js
+++ packages/assets-controllers/.eslintrc.js
@@ -5,10 +5,18 @@ module.exports = {
     tsconfigRootDir: __dirname,
   },
 
+  rules: {
+    '@typescript-eslint/no-shadow': 'off',
+  },
+
   overrides: [
     {
       files: ['*.ts'],
       extends: ['@metamask/eslint-config-browser'],
+    },
+    {
+      files: ['*.test.ts'],
+      extends: ['@metamask/eslint-config-nodejs'],
     },
   ],
 };
