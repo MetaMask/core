42b108aa01e9db6440631d04b291b47c162db1a7
dragana8 <dragana.simic@consensys.net>
2023-01-31 16:26:32 +0100

    fixed lint errors

--- packages/logging-controller/src/LoggingController.ts
+++ packages/logging-controller/src/LoggingController.ts
@@ -1,26 +1,26 @@
 import {
-    BaseController,
-    BaseConfig,
-    BaseState,
-  } from '@metamask/base-controller';
-  import { v1 as random } from 'uuid';
+  BaseController,
+  BaseConfig,
+  BaseState,
+} from '@metamask/base-controller';
+import { v1 as random } from 'uuid';
 
 export enum LogType {
-    eth_sign_proposed = "eth_sign_proposed",
-    eth_sign_signed = "eth_sign_signed",
-    eth_sign_rejected = "eth_sign_rejected",
-    personal_sign_proposed = "personal_sign_proposed",
-    personal_sign_signed = "personal_sign_signed",
-    personal_sign_rejected = "personal_sign_rejected",
-    eth_signTypedData_proposed = "eth_signTypedData_proposed",
-    eth_signTypedData_signed = "eth_signTypedData_signed",
-    eth_signTypedData_rejected = "eth_signTypedData_rejected",
-    eth_signTypedData_v3_proposed = "eth_signTypedData_v3",
-    eth_signTypedData_v3_signed = "eth_signTypedData_v3",
-    eth_signTypedData_v3_rejected = "eth_signTypedData_v3",
-    eth_signTypedData_v4_proposed = "eth_signTypedData_v4_proposed",
-    eth_signTypedData_v4_signed = "eth_signTypedData_v4_signed",
-    eth_signTypedData_v4_rejected = "eth_signTypedData_v4_rejected"
+  eth_sign_proposed = 'eth_sign_proposed',
+  eth_sign_signed = 'eth_sign_signed',
+  eth_sign_rejected = 'eth_sign_rejected',
+  personal_sign_proposed = 'personal_sign_proposed',
+  personal_sign_signed = 'personal_sign_signed',
+  personal_sign_rejected = 'personal_sign_rejected',
+  eth_signTypedData_proposed = 'eth_signTypedData_proposed',
+  eth_signTypedData_signed = 'eth_signTypedData_signed',
+  eth_signTypedData_rejected = 'eth_signTypedData_rejected',
+  eth_signTypedData_v3_proposed = 'eth_signTypedData_v3',
+  eth_signTypedData_v3_signed = 'eth_signTypedData_v3',
+  eth_signTypedData_v3_rejected = 'eth_signTypedData_v3',
+  eth_signTypedData_v4_proposed = 'eth_signTypedData_v4_proposed',
+  eth_signTypedData_v4_signed = 'eth_signTypedData_v4_signed',
+  eth_signTypedData_v4_rejected = 'eth_signTypedData_v4_rejected',
 }
 
 /**
@@ -33,10 +33,10 @@ export enum LogType {
  * @property data - Additional data about signature request
  */
 export interface Log {
-	id: string,
-	timestamp: number,
-	type: LogType,
-	data?: Object
+  id: string;
+  timestamp: number;
+  type: LogType;
+  data?: any;
 }
 
 /**
@@ -45,45 +45,35 @@ export interface Log {
  * Logging controller state
  * @property logs - Array of Logs
  */
- export interface LogsState extends BaseState {
+export interface LogsState extends BaseState {
   logs: Log[];
 }
 
 /**
  * Controller that manages a list of logs for signature requests.
  */
-export class LoggingController extends BaseController<
-  BaseConfig,
-  LogsState
-> {
-
-    /**
+export class LoggingController extends BaseController<BaseConfig, LogsState> {
+  /**
    * Creates an LoggingController instance.
    *
    * @param config - Initial options used to configure this controller.
    * @param state - Initial state to set on this controller.
    */
-     constructor(config?: Partial<BaseConfig>, state?: Partial<LogsState>) {
-      super(config, state);
-  
-      this.defaultState = { logs: [] };
-  
-      this.initialize();
-    }
+  constructor(config?: Partial<BaseConfig>, state?: Partial<LogsState>) {
+    super(config, state);
 
+    this.defaultState = { logs: [] };
 
-    /**
+    this.initialize();
+  }
+
+  /**
    * Add log to the state.
    *
    * @param data - Data from signature reqest that is being added to list.
-   * @param id - Generated UUID.
-   * @param timestamp - Timestamp when the log is being added.
    * @param type - Type of signature request.
    */
-  addLog(
-    data: object,
-    type: LogType,
-  ) {
+  addLog(data: any, type: LogType) {
     const newLog: Log = {
       data,
       id: random(),
@@ -91,13 +81,10 @@ export class LoggingController extends BaseController<
       type,
     };
 
-    (this.state.logs).forEach(async (log) => {
-      if(log.id !== newLog.id) {
+    this.state.logs.forEach(async (log) => {
+      if (log.id !== newLog.id) {
         this.update({
-          logs: [
-            ...this.state.logs,
-            newLog,
-          ],
+          logs: [...this.state.logs, newLog],
         });
       }
     });
@@ -109,8 +96,6 @@ export class LoggingController extends BaseController<
   clear() {
     this.update({ logs: [] });
   }
-
 }
 
 export default LoggingController;
-
