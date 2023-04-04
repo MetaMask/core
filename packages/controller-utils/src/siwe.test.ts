472fa4bc5446122fb3d59c46582e998582e124e3
legobt <6wbvkn0j@anonaddy.me>
2023-04-04 03:19:56 +0000

    feat: controller-utils: Add SIWE origin validation functionality   - handle userinfo and port   - add tests

--- packages/controller-utils/src/siwe.test.ts
+++ packages/controller-utils/src/siwe.test.ts
@@ -1,5 +1,5 @@
 import { ParsedMessage } from '@spruceid/siwe-parser';
-import { detectSIWE } from './siwe';
+import { detectSIWE, isValidSIWEOrigin } from './siwe';
 
 const mockedParsedMessage = {
   domain: 'example.eth',
@@ -8,21 +8,255 @@ const mockedParsedMessage = {
 
 jest.mock('@spruceid/siwe-parser');
 
-describe('detectSIWE', () => {
-  const parsedMessageMock = ParsedMessage as any;
-  it('returns an object with isSIWEMessage set to true and parsedMessage', () => {
-    parsedMessageMock.mockReturnValue(mockedParsedMessage);
-    const result = detectSIWE({ data: '0xVALIDDATA' });
-    expect(result.isSIWEMessage).toBe(true);
-    expect(result.parsedMessage).toBe(mockedParsedMessage);
+describe('siwe', () => {
+  describe('detectSIWE', () => {
+    const parsedMessageMock = ParsedMessage as any;
+    it('returns an object with isSIWEMessage set to true and parsedMessage', () => {
+      parsedMessageMock.mockReturnValue(mockedParsedMessage);
+      const result = detectSIWE({ data: '0xVALIDDATA' });
+      expect(result.isSIWEMessage).toBe(true);
+      expect(result.parsedMessage).toBe(mockedParsedMessage);
+    });
+
+    it('returns an object with isSIWEMessage set to false and parsedMessage set to null', () => {
+      parsedMessageMock.mockImplementation(() => {
+        throw new Error('Invalid SIWE message');
+      });
+      const result = detectSIWE({ data: '0xINVALIDDATA' });
+      expect(result.isSIWEMessage).toBe(false);
+      expect(result.parsedMessage).toBeNull();
+    });
   });
 
-  it('returns an object with isSIWEMessage set to false and parsedMessage set to null', () => {
-    parsedMessageMock.mockImplementation(() => {
-      throw new Error('Invalid SIWE message');
-    });
-    const result = detectSIWE({ data: '0xINVALIDDATA' });
-    expect(result.isSIWEMessage).toBe(false);
-    expect(result.parsedMessage).toBeNull();
+  describe('isValidSIWEOrigin', () => {
+    const msg = {
+      domain: 'example.com',
+      address: '0x0',
+      statement: '',
+      uri: 'https://example.com',
+      version: '1',
+      chainId: 1,
+      nonce: '',
+      issuedAt: '',
+      expirationTime: null,
+      notBefore: null,
+      requestId: 'foo',
+      resources: [],
+    };
+    const checks = [
+      {
+        name: 'identical domain',
+        expected: true,
+        cases: [
+          {
+            domain: 'example.com',
+            origin: 'https://example.com',
+          },
+          {
+            domain: 'example.com',
+            origin: 'http://example.com',
+          },
+          {
+            domain: 'example.com',
+            origin: 'https://example.com:443',
+          },
+          {
+            domain: 'example.com',
+            origin: 'http://example.com:80',
+          },
+          {
+            domain: 'example.com',
+            origin: 'https://user:password@example.com',
+          },
+          {
+            domain: 'example.com',
+            origin: 'https://user@example.com',
+          },
+          {
+            domain: 'example.com',
+            origin: 'http://user:password@example.com:8090',
+          },
+          {
+            domain: 'example.com',
+            origin: 'http://user@example.com:8090',
+          },
+          {
+            domain: 'example.com',
+            origin: 'http://example.com:8090',
+          },
+          {
+            domain: 'example.com',
+            origin: 'https://example.com:8090',
+          },
+        ],
+      },
+      {
+        name: 'matching domain and port',
+        expected: true,
+        cases: [
+          {
+            domain: 'example.com:443',
+            origin: 'https://example.com:443',
+          },
+          {
+            domain: 'example.com:443',
+            origin: 'https://example.com',
+          },
+          {
+            domain: 'example.com:443',
+            origin: 'http://example.com:443',
+          },
+          {
+            domain: 'example.com:80',
+            origin: 'http://example.com',
+          },
+          {
+            domain: 'example.com:80',
+            origin: 'http://example.com:80',
+          },
+          {
+            domain: 'example.com:80',
+            origin: 'https://example.com:80',
+          },
+          {
+            domain: 'example.com:8090',
+            origin: 'http://example.com:8090',
+          },
+          {
+            domain: 'example.com:8080',
+            origin: 'https://example.com:8080',
+          },
+        ],
+      },
+      {
+        name: 'matching userinfo',
+        expected: true,
+        cases: [
+          {
+            domain: 'alice@example.com',
+            origin: 'https://alice:password@example.com',
+          },
+          {
+            domain: 'alice@example.com',
+            origin: 'https://alice@example.com',
+          },
+          {
+            domain: 'alice@example.com:8090',
+            origin: 'https://alice@example.com:8090',
+          },
+        ],
+      },
+      {
+        name: 'mismatching userinfo',
+        expected: false,
+        cases: [
+          {
+            domain: 'alice@example.com',
+            origin: 'https://bob@example.com',
+          },
+          {
+            domain: 'alice@example.com',
+            origin: 'https://example.com',
+          },
+          {
+            domain: 'alice@example.com:8090',
+            origin: 'https://bob:alice@example.com:8090',
+          },
+        ],
+      },
+      {
+        name: 'mismatching domain',
+        expected: false,
+        cases: [
+          {
+            domain: 'example.com',
+            origin: 'http://www.example.com',
+          },
+          {
+            domain: 'www.example.com',
+            origin: 'http://example.com',
+          },
+          {
+            domain: 'example.com',
+            origin: 'http://www.example.com',
+          },
+          {
+            domain: 'example.com',
+            origin: 'https://foo.example.com',
+          },
+          {
+            domain: 'foo.example.com',
+            origin: 'https://example.com',
+          },
+          {
+            domain: 'localhost',
+            origin: 'http://127.0.0.1',
+          },
+          {
+            domain: '127.0.0.1',
+            origin: 'http://localhost',
+          },
+        ],
+      },
+      {
+        name: 'mismatching port',
+        expected: false,
+        cases: [
+          {
+            domain: 'www.example.com:8091',
+            origin: 'http://www.example.com:8090',
+          },
+          {
+            domain: 'www.example.com:8091',
+            origin: 'https://www.example.com:8090',
+          },
+          {
+            domain: 'example.com:8090',
+            origin: 'http://example.com',
+          },
+          {
+            domain: '127.0.0.1:8090',
+            origin: 'https://127.0.0.1',
+          },
+          {
+            domain: 'localhost:8090',
+            origin: 'http://localhost',
+          },
+          {
+            domain: '127.0.0.1:8090',
+            origin: 'https://localhost',
+          },
+          {
+            domain: 'example.com:443',
+            origin: 'http://example.com',
+          },
+          {
+            domain: 'example.com:80',
+            origin: 'https://example.com',
+          },
+        ],
+      },
+    ];
+    for (const { name, expected, cases } of checks) {
+      for (const { domain, origin } of cases) {
+        it(`should return ${expected} for ${name} ${JSON.stringify({
+          domain,
+          origin,
+        })}`, () => {
+          const result = isValidSIWEOrigin({
+            from: '0x0',
+            origin,
+            siwe: {
+              isSIWEMessage: true,
+              parsedMessage: {
+                ...msg,
+                domain,
+              },
+            },
+          });
+          expect(result).toBe(expected);
+        });
+      }
+    }
   });
 });
--- packages/controller-utils/src/siwe.ts
+++ packages/controller-utils/src/siwe.ts
@@ -35,9 +35,129 @@ function msgHexToText(hex: string): string {
 }
 
 /**
+ * @type WrappedSIWERequest
+ *
+ * Sign-In With Ethereum (SIWE)(EIP-4361) message with request metadata
+ * @property {string} from - Subject account address
+ * @property {string} origin - The RFC 3986 originating authority of the signing request, including scheme
+ * @property {ParsedMessage} siwe - The data parsed from the message
+ */
+export interface WrappedSIWERequest {
+  from: string;
+  origin: string;
+  siwe: SIWEMessage;
+}
+
+interface HostParts extends DomainParts {
+  scheme: string;
+}
+
+interface DomainParts {
+  userinfo: string | null;
+  host: string;
+  port: number | null;
+}
+
+export const ORIGIN_REGEX =
+  /^(?<scheme>[^:/]+):\/\/((?<userinfo>[^@]*)@)?(?<host>[^:@/?]+)(:(?<port>[0-9]+))?/u;
+export const DOMAIN_REGEX =
+  /^((?<userinfo>[^@]*)@)?(?<host>[^:@/?]+)(:(?<port>[0-9]+))?$/u;
+const DEFAULT_PORTS_BY_SCHEME = {
+  http: 80,
+  https: 443,
+} as { [key: string]: number | undefined };
+
+/**
+ * Parses parts from RFC 3986 authority from environment relevant for EIP-4361 origin validation.
+ *
+ * @param authority - input string
+ * @returns parsed parts
+ */
+export const parseAuthorityParts = (authority: string): HostParts => {
+  const match = authority.match(ORIGIN_REGEX);
+  if (!match) {
+    throw new Error(`Unrecognized origin format "${origin}".`);
+  }
+  const { scheme, userinfo, host, port } = match.groups as {
+    scheme: string;
+    userinfo: string | undefined;
+    host: string;
+    port: string | undefined;
+  };
+  return {
+    scheme,
+    // strip password from userinfo
+    userinfo: userinfo ? userinfo.replace(/:.*$/u, '') : null,
+    host,
+    port: port ? parseInt(port) : DEFAULT_PORTS_BY_SCHEME[scheme] || null,
+  };
+};
+
+/**
+ * Parses parts from RFC 3986 authority from EIP-4361 `domain` field.
+ *
+ * @param domain - input string
+ * @returns parsed parts
+ */
+export const parseDomainParts = (domain: string): DomainParts => {
+  const match = domain.match(DOMAIN_REGEX);
+  if (!match) {
+    throw new Error(`Unrecognized domain format "${domain}".`);
+  }
+  const { userinfo, host, port } = match.groups as {
+    userinfo: string | undefined;
+    host: string;
+    port: string | undefined;
+  };
+  return {
+    // strip password from userinfo
+    userinfo: userinfo ? userinfo.replace(/:.*$/u, '') : null,
+    host,
+    port: port ? parseInt(port) : null,
+  };
+};
+
+/**
+ * Validates origin of a Sign-In With Ethereum (SIWE)(EIP-4361) request.
+ *
+ * @param req - Signature request
+ * @returns true if origin matches domain; false otherwise
+ */
+export const isValidSIWEOrigin = (req: WrappedSIWERequest): boolean => {
+  const { origin, siwe } = req;
+
+  // origin = scheme://[user[:password]@]domain[:port]
+  // origin is supplied by environment and must match domain claim in message
+  if (!origin || !siwe?.parsedMessage?.domain) {
+    return false;
+  }
+
+  // TOREVIEW: Handle error and log here instead of propagating?
+  const originParts = parseAuthorityParts(origin);
+  const domainParts = parseDomainParts(siwe.parsedMessage.domain);
+
+  if (domainParts.host !== originParts.host) {
+    return false;
+  }
+
+  if (domainParts.port !== null && domainParts.port !== originParts.port) {
+    return false;
+  }
+
+  if (
+    domainParts.userinfo !== null &&
+    domainParts.userinfo !== originParts.userinfo
+  ) {
+    return false;
+  }
+
+  return true;
+};
+
+/**
  * A locally defined object used to provide data to identify a Sign-In With Ethereum (SIWE)(EIP-4361) message and provide the parsed message
  *
- * @typedef localSIWEObject
+ * @typedef SIWEMessage
  * @param {boolean} isSIWEMessage - Does the intercepted message conform to the SIWE specification?
  * @param {ParsedMessage} parsedMessage - The data parsed out of the message
  */
