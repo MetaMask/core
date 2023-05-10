MetaMask/core

commit 7d5057b425857ff1628155bcccc62b0062f2dc79

Merge extension ens controller (#1170)
Merge extension ens controller

packages/ens-controller/src/EnsController.ts
@@ -3,10 +3,38 @@ import {
   RestrictedControllerMessenger,
 } from '@metamask/base-controller';
 import {
+  Web3Provider,
+  ExternalProvider,
+  JsonRpcFetchFunc,
+} from '@ethersproject/providers';
+import { createProjectLogger } from '@metamask/utils';
+import {
   normalizeEnsName,
   isValidHexAddress,
   toChecksumHexAddress,
+  NETWORK_ID_TO_ETHERS_NETWORK_NAME_MAP,
+  convertHexToDecimal,
+  hasProperty,
 } from '@metamask/controller-utils';
+import { toASCII } from 'punycode/';
+import ensNetworkMap from 'ethereum-ens-network-map';
+
+const log = createProjectLogger('ens-controller');
+
+/**
+ * Checks whether the given string is a known network ID.
+ *
+ * @param networkId - Network id.
+ * @returns Boolean indicating if the network ID is recognized.
+ */
+function isKnownNetworkId(
+  networkId: string | null,
+): networkId is keyof typeof NETWORK_ID_TO_ETHERS_NETWORK_NAME_MAP {
+  return (
+    networkId !== null &&
+    hasProperty(NETWORK_ID_TO_ETHERS_NETWORK_NAME_MAP, networkId)
+  );
+}
 
 const name = 'EnsController';
 
@@ -36,6 +64,7 @@ export type EnsControllerState = {
       [ensName: string]: EnsEntry;
     };
   };
+  ensResolutionsByAddress: { [key: string]: string };
 };
 
 export type EnsControllerMessenger = RestrictedControllerMessenger<
@@ -48,11 +77,16 @@ export type EnsControllerMessenger = RestrictedControllerMessenger<
 
 const metadata = {
   ensEntries: { persist: true, anonymous: false },
+  ensResolutionsByAddress: { persist: true, anonymous: false },
 };
 
 const defaultState = {
   ensEntries: {},
+  ensResolutionsByAddress: {},
 };
+
+const ZERO_ADDRESS = '0x0000000000000000000000000000000000000000';
+const ZERO_X_ERROR_ADDRESS = '0x';
 
 /**
  * Controller that manages a list ENS names and their resolved addresses
@@ -63,19 +97,35 @@ export class EnsController extends BaseControllerV2<
   EnsControllerState,
   EnsControllerMessenger
 > {
+  #ethProvider: Web3Provider | null = null;
+
   /**
    * Creates an EnsController instance.
    *
    * @param options - Constructor options.
    * @param options.messenger - A reference to the messaging system.
    * @param options.state - Initial state to set on this controller.
+   * @param options.provider - Provider instance.
+   * @param options.onNetworkStateChange - Allows registering an event handler for
+   * when the network controller state updated.
    */
   constructor({
     messenger,
-    state,
+    state = {},
+    provider,
+    onNetworkStateChange,
   }: {
     messenger: EnsControllerMessenger;
     state?: Partial<EnsControllerState>;
+    provider?: ExternalProvider | JsonRpcFetchFunc;
+    onNetworkStateChange?: (
+      listener: (networkState: {
+        network: string;
+        providerConfig: {
+          chainId: string;
+        };
+      }) => void,
+    ) => void;
   }) {
     super({
       name,
@@ -85,6 +135,35 @@ export class EnsController extends BaseControllerV2<
         ...defaultState,
         ...state,
       },
+    });
+
+    if (provider && onNetworkStateChange) {
+      onNetworkStateChange((networkState) => {
+        this.resetState();
+        const currentNetwork =
+          networkState.network === 'loading' ? null : networkState.network;
+        if (
+          isKnownNetworkId(currentNetwork) &&
+          this.#getNetworkEnsSupport(currentNetwork)
+        ) {
+          this.#ethProvider = new Web3Provider(provider, {
+            chainId: convertHexToDecimal(networkState.providerConfig.chainId),
+            name: NETWORK_ID_TO_ETHERS_NETWORK_NAME_MAP[currentNetwork],
+            ensAddress: ensNetworkMap[currentNetwork],
+          });
+        } else {
+          this.#ethProvider = null;
+        }
+      });
+    }
+  }
+
+  /**
+   * Clears ensResolutionsByAddress state property.
+   */
+  resetState() {
+    this.update((currentState) => {
+      currentState.ensResolutionsByAddress = {};
     });
   }
 
@@ -192,6 +271,73 @@ export class EnsController extends BaseControllerV2<
       };
     });
     return true;
+  }
+
+  /**
+   * Check if network supports ENS.
+   *
+   * @param networkId - Network id.
+   * @returns Boolean indicating if the network supports ENS.
+   */
+  #getNetworkEnsSupport(networkId: string) {
+    return Boolean(ensNetworkMap[networkId]);
+  }
+
+  /**
+   * Resolve ens by address.
+   *
+   * @param nonChecksummedAddress - address
+   * @returns ens resolution
+   */
+  async reverseResolveAddress(nonChecksummedAddress: string) {
+    if (!this.#ethProvider) {
+      return undefined;
+    }
+
+    const address = toChecksumHexAddress(nonChecksummedAddress);
+    if (this.state.ensResolutionsByAddress[address]) {
+      return this.state.ensResolutionsByAddress[address];
+    }
+
+    let domain: string | null;
+    try {
+      domain = await this.#ethProvider.lookupAddress(address);
+    } catch (error) {
+      log(error);
+      return undefined;
+    }
+
+    if (!domain) {
+      return undefined;
+    }
+
+    let registeredAddress: string | null;
+    try {
+      registeredAddress = await this.#ethProvider.resolveName(domain);
+    } catch (error) {
+      log(error);
+      return undefined;
+    }
+
+    if (!registeredAddress) {
+      return undefined;
+    }
+
+    if (
+      registeredAddress === ZERO_ADDRESS ||
+      registeredAddress === ZERO_X_ERROR_ADDRESS
+    ) {
+      return undefined;
+    }
+    if (toChecksumHexAddress(registeredAddress) !== address) {
+      return undefined;
+    }
+
+    this.update((state) => {
+      state.ensResolutionsByAddress[address] = toASCII(domain as string);
+    });
+
+    return domain;
   }
 }
 

packages/ens-controller/src/EnsController.test.ts
@@ -1,6 +1,18 @@
 import { ControllerMessenger } from '@metamask/base-controller';
 import { toChecksumHexAddress } from '@metamask/controller-utils';
+import * as providersModule from '@ethersproject/providers';
 import { EnsController } from './EnsController';
+
+jest.mock('@ethersproject/providers', () => {
+  const originalModule = jest.requireActual('@ethersproject/providers');
+
+  return {
+    __esModule: true,
+    ...originalModule,
+  };
+});
+
+const ZERO_X_ERROR_ADDRESS = '0x';
 
 const address1 = '0x32Be343B94f860124dC4fEe278FDCBD38C102D88';
 const address2 = '0xc38bf1ad06ef69f0c04e29dbeb4152b4175f0a8d';
@@ -25,13 +37,25 @@ function getMessenger() {
   });
 }
 
+/**
+ * Creates a mock provider.
+ *
+ * @returns mock provider
+ */
+function getProvider() {
+  return () => Promise.resolve(null);
+}
+
 describe('EnsController', () => {
   it('should set default state', () => {
     const messenger = getMessenger();
     const controller = new EnsController({
       messenger,
     });
-    expect(controller.state).toStrictEqual({ ensEntries: {} });
+    expect(controller.state).toStrictEqual({
+      ensEntries: {},
+      ensResolutionsByAddress: {},
+    });
   });
 
   it('should add a new ENS entry and return true', () => {
@@ -50,7 +74,54 @@ describe('EnsController', () => {
           },
         },
       },
+      ensResolutionsByAddress: {},
     });
+  });
+
+  it('should clear ensResolutionsByAddress state propery when resetState is called', async () => {
+    const messenger = getMessenger();
+    const controller = new EnsController({
+      messenger,
+      state: {
+        ensResolutionsByAddress: {
+          [address1Checksum]: 'peaksignal.eth',
+        },
+      },
+    });
+
+    expect(controller.state).toStrictEqual({
+      ensResolutionsByAddress: {
+        [address1Checksum]: 'peaksignal.eth',
+      },
+      ensEntries: {},
+    });
+
+    controller.resetState();
+
+    expect(controller.state.ensResolutionsByAddress).toStrictEqual({});
+  });
+
+  it('should clear ensResolutionsByAddress state propery on networkStateChange', async () => {
+    const messenger = getMessenger();
+    const controller = new EnsController({
+      messenger,
+      state: {
+        ensResolutionsByAddress: {
+          [address1Checksum]: 'peaksignal.eth',
+        },
+      },
+      provider: getProvider(),
+      onNetworkStateChange: (listener) => {
+        listener({
+          network: '1',
+          providerConfig: {
+            chainId: '1',
+          },
+        });
+      },
+    });
+
+    expect(controller.state.ensResolutionsByAddress).toStrictEqual({});
   });
 
   it('should add a new ENS entry with null address and return true', () => {
@@ -69,6 +140,7 @@ describe('EnsController', () => {
           },
         },
       },
+      ensResolutionsByAddress: {},
     });
   });
 
@@ -89,6 +161,7 @@ describe('EnsController', () => {
           },
         },
       },
+      ensResolutionsByAddress: {},
     });
   });
 
@@ -109,6 +182,7 @@ describe('EnsController', () => {
           },
         },
       },
+      ensResolutionsByAddress: {},
     });
   });
 
@@ -129,6 +203,7 @@ describe('EnsController', () => {
           },
         },
       },
+      ensResolutionsByAddress: {},
     });
   });
 
@@ -149,6 +224,7 @@ describe('EnsController', () => {
           },
         },
       },
+      ensResolutionsByAddress: {},
     });
   });
 
@@ -183,6 +259,7 @@ describe('EnsController', () => {
           },
         },
       },
+      ensResolutionsByAddress: {},
     });
   });
 
@@ -227,7 +304,10 @@ describe('EnsController', () => {
     }).toThrow(
       'Invalid ENS entry: { chainId:a, ensName:foobarb.eth, address:0x32Be343B94f860124dC4fEe278FDCBD38C102D88}',
     );
-    expect(controller.state).toStrictEqual({ ensEntries: {} });
+    expect(controller.state).toStrictEqual({
+      ensEntries: {},
+      ensResolutionsByAddress: {},
+    });
   });
 
   it('should throw on attempt to set invalid ENS entry: ENS name', () => {
@@ -238,7 +318,10 @@ describe('EnsController', () => {
     expect(() => {
       controller.set('1', 'foo.eth', address1);
     }).toThrow('Invalid ENS name: foo.eth');
-    expect(controller.state).toStrictEqual({ ensEntries: {} });
+    expect(controller.state).toStrictEqual({
+      ensEntries: {},
+      ensResolutionsByAddress: {},
+    });
   });
 
   it('should throw on attempt to set invalid ENS entry: address', () => {
@@ -251,7 +334,10 @@ describe('EnsController', () => {
     }).toThrow(
       'Invalid ENS entry: { chainId:1, ensName:foobarb.eth, address:foo}',
     );
-    expect(controller.state).toStrictEqual({ ensEntries: {} });
+    expect(controller.state).toStrictEqual({
+      ensEntries: {},
+      ensResolutionsByAddress: {},
+    });
   });
 
   it('should remove an ENS entry and return true', () => {
@@ -261,7 +347,10 @@ describe('EnsController', () => {
     });
     expect(controller.set('1', name1, address1)).toStrictEqual(true);
     expect(controller.delete('1', name1)).toStrictEqual(true);
-    expect(controller.state).toStrictEqual({ ensEntries: {} });
+    expect(controller.state).toStrictEqual({
+      ensEntries: {},
+      ensResolutionsByAddress: {},
+    });
   });
 
   it('should return false if an ENS entry was NOT deleted', () => {
@@ -282,6 +371,7 @@ describe('EnsController', () => {
           },
         },
       },
+      ensResolutionsByAddress: {},
     });
   });
 
@@ -311,6 +401,7 @@ describe('EnsController', () => {
           },
         },
       },
+      ensResolutionsByAddress: {},
     });
   });
 
@@ -323,6 +414,224 @@ describe('EnsController', () => {
     expect(controller.set('1', name2, address2)).toStrictEqual(true);
     expect(controller.set('2', name1, address1)).toStrictEqual(true);
     controller.clear();
-    expect(controller.state).toStrictEqual({ ensEntries: {} });
+    expect(controller.state).toStrictEqual({
+      ensEntries: {},
+      ensResolutionsByAddress: {},
+    });
+  });
+
+  describe('reverseResolveName', () => {
+    it('should return undefined when eth provider is not defined', async () => {
+      const messenger = getMessenger();
+      const ens = new EnsController({
+        messenger,
+      });
+      expect(await ens.reverseResolveAddress(address1)).toBeUndefined();
+    });
+
+    it('should return undefined when network is loading', async function () {
+      const messenger = getMessenger();
+      const ens = new EnsController({
+        messenger,
+        provider: getProvider(),
+        onNetworkStateChange: (listener) => {
+          listener({
+            network: 'loading',
+            providerConfig: {
+              chainId: '1',
+            },
+          });
+        },
+      });
+      expect(await ens.reverseResolveAddress(address1)).toBeUndefined();
+    });
+
+    it('should return undefined when network is not ens supported', async function () {
+      const messenger = getMessenger();
+      const ens = new EnsController({
+        messenger,
+        provider: getProvider(),
+        onNetworkStateChange: (listener) => {
+          listener({
+            network: '1544',
+            providerConfig: {
+              chainId: '1',
+            },
+          });
+        },
+      });
+      expect(await ens.reverseResolveAddress(address1)).toBeUndefined();
+    });
+
+    it('should only resolve an ENS name once', async () => {
+      const messenger = getMessenger();
+      const ethProvider = new providersModule.Web3Provider(getProvider());
+      jest.spyOn(ethProvider, 'resolveName').mockResolvedValue(address1);
+      jest
+        .spyOn(ethProvider, 'lookupAddress')
+        .mockResolvedValue('peaksignal.eth');
+      jest.spyOn(providersModule, 'Web3Provider').mockReturnValue(ethProvider);
+
+      const ens = new EnsController({
+        messenger,
+        provider: getProvider(),
+        onNetworkStateChange: (listener) => {
+          listener({
+            network: '1',
+            providerConfig: {
+              chainId: '1',
+            },
+          });
+        },
+      });
+
+      expect(await ens.reverseResolveAddress(address1)).toStrictEqual(
+        'peaksignal.eth',
+      );
+      expect(await ens.reverseResolveAddress(address1)).toStrictEqual(
+        'peaksignal.eth',
+      );
+    });
+
+    it('should fail if lookupAddress through an error', async () => {
+      const messenger = getMessenger();
+      const ethProvider = new providersModule.Web3Provider(getProvider());
+      jest.spyOn(ethProvider, 'lookupAddress').mockRejectedValue('error');
+      jest.spyOn(providersModule, 'Web3Provider').mockReturnValue(ethProvider);
+      const ens = new EnsController({
+        messenger,
+        provider: getProvider(),
+        onNetworkStateChange: (listener) => {
+          listener({
+            network: '1',
+            providerConfig: {
+              chainId: '1',
+            },
+          });
+        },
+      });
+
+      expect(await ens.reverseResolveAddress(address1)).toBeUndefined();
+    });
+
+    it('should fail if lookupAddress returns a null value', async () => {
+      const messenger = getMessenger();
+      const ethProvider = new providersModule.Web3Provider(getProvider());
+      jest.spyOn(ethProvider, 'lookupAddress').mockResolvedValue(null);
+      jest.spyOn(providersModule, 'Web3Provider').mockReturnValue(ethProvider);
+      const ens = new EnsController({
+        messenger,
+        provider: getProvider(),
+        onNetworkStateChange: (listener) => {
+          listener({
+            network: '1',
+            providerConfig: {
+              chainId: '1',
+            },
+          });
+        },
+      });
+
+      expect(await ens.reverseResolveAddress(address1)).toBeUndefined();
+    });
+
+    it('should fail if resolveName through an error', async () => {
+      const messenger = getMessenger();
+      const ethProvider = new providersModule.Web3Provider(getProvider());
+      jest
+        .spyOn(ethProvider, 'lookupAddress')
+        .mockResolvedValue('peaksignal.eth');
+      jest.spyOn(ethProvider, 'resolveName').mockRejectedValue('error');
+      jest.spyOn(providersModule, 'Web3Provider').mockReturnValue(ethProvider);
+      const ens = new EnsController({
+        messenger,
+        provider: getProvider(),
+        onNetworkStateChange: (listener) => {
+          listener({
+            network: '1',
+            providerConfig: {
+              chainId: '1',
+            },
+          });
+        },
+      });
+
+      expect(await ens.reverseResolveAddress(address1)).toBeUndefined();
+    });
+
+    it('should fail if resolveName returns a null value', async () => {
+      const messenger = getMessenger();
+      const ethProvider = new providersModule.Web3Provider(getProvider());
+      jest.spyOn(ethProvider, 'resolveName').mockResolvedValue(null);
+      jest
+        .spyOn(ethProvider, 'lookupAddress')
+        .mockResolvedValue('peaksignal.eth');
+      jest.spyOn(providersModule, 'Web3Provider').mockReturnValue(ethProvider);
+      const ens = new EnsController({
+        messenger,
+        provider: getProvider(),
+        onNetworkStateChange: (listener) => {
+          listener({
+            network: '1',
+            providerConfig: {
+              chainId: '1',
+            },
+          });
+        },
+      });
+
+      expect(await ens.reverseResolveAddress(address1)).toBeUndefined();
+    });
+
+    it('should fail if registred address is zero x error address', async () => {
+      const messenger = getMessenger();
+      const ethProvider = new providersModule.Web3Provider(getProvider());
+      jest
+        .spyOn(ethProvider, 'resolveName')
+        .mockResolvedValue(ZERO_X_ERROR_ADDRESS);
+      jest
+        .spyOn(ethProvider, 'lookupAddress')
+        .mockResolvedValue('peaksignal.eth');
+      jest.spyOn(providersModule, 'Web3Provider').mockReturnValue(ethProvider);
+      const ens = new EnsController({
+        messenger,
+        provider: getProvider(),
+        onNetworkStateChange: (listener) => {
+          listener({
+            network: '1',
+            providerConfig: {
+              chainId: '1',
+            },
+          });
+        },
+      });
+
+      expect(await ens.reverseResolveAddress(address1)).toBeUndefined();
+    });
+
+    it('should fail if the name is registered to a different address than the reverse resolved', async () => {
+      const messenger = getMessenger();
+
+      const ethProvider = new providersModule.Web3Provider(getProvider());
+      jest.spyOn(ethProvider, 'resolveName').mockResolvedValue(address2);
+      jest
+        .spyOn(ethProvider, 'lookupAddress')
+        .mockResolvedValue('peaksignal.eth');
+      jest.spyOn(providersModule, 'Web3Provider').mockReturnValue(ethProvider);
+      const ens = new EnsController({
+        messenger,
+        provider: getProvider(),
+        onNetworkStateChange: (listener) => {
+          listener({
+            network: '1',
+            providerConfig: {
+              chainId: '1',
+            },
+          });
+        },
+      });
+
+      expect(await ens.reverseResolveAddress(address1)).toBeUndefined();
+    });
   });
 });

packages/controller-utils/src/constants.ts
@@ -1,4 +1,9 @@
-import { NetworkType, NetworksTicker, NetworksChainId } from './types';
+import {
+  NetworkType,
+  NetworksTicker,
+  NetworksChainId,
+  NetworkId,
+} from './types';
 
 export const RPC = 'rpc';
 export const FALL_BACK_VS_CURRENCY = 'ETH';
@@ -110,3 +115,12 @@ export enum ApprovalType {
   Unlock = 'unlock',
   ConnectAccounts = 'connect_accounts',
 }
+
+export const NETWORK_ID_TO_ETHERS_NETWORK_NAME_MAP: Record<
+  NetworkId,
+  NetworkType
+> = {
+  [NetworkId.goerli]: NetworkType.goerli,
+  [NetworkId.sepolia]: NetworkType.sepolia,
+  [NetworkId.mainnet]: NetworkType.mainnet,
+};

packages/controller-utils/src/types.ts
@@ -27,6 +27,12 @@ export enum NetworksChainId {
   rpc = '',
 }
 
+export enum NetworkId {
+  mainnet = '1',
+  goerli = '5',
+  sepolia = '11155111',
+}
+
 export enum NetworksTicker {
   mainnet = 'ETH',
   goerli = 'GoerliETH',

types/ethereum-ens-network-map.d.ts
@@ -1,0 +1,1 @@
+declare module 'ethereum-ens-network-map';

commit 0bb0b54c51e3080f1aca9ff7309134ba427750f1

Fix ENS controller network change handler
The network change handler for the ENS controller has been broken since
the PR #1170 due to a conflict with #1196, which was merged around the
same time. It referenced the `network` property of the network state
that we have removed.

The change handler has been updated to use `networkId` instead.
Additionally, the `NetworkState` type has been imported so that we're
less likely to make this mistake again.

packages/ens-controller/src/EnsController.ts
@@ -15,6 +15,7 @@ import {
   NETWORK_ID_TO_ETHERS_NETWORK_NAME_MAP,
   convertHexToDecimal,
 } from '@metamask/controller-utils';
+import type { NetworkState } from '@metamask/network-controller';
 import { toASCII } from 'punycode/';
 import ensNetworkMap from 'ethereum-ens-network-map';
 
@@ -118,12 +119,9 @@ export class EnsController extends BaseControllerV2<
     state?: Partial<EnsControllerState>;
     provider?: ExternalProvider | JsonRpcFetchFunc;
     onNetworkStateChange?: (
-      listener: (networkState: {
-        network: string;
-        providerConfig: {
-          chainId: string;
-        };
-      }) => void,
+      listener: (
+        networkState: Pick<NetworkState, 'networkId' | 'providerConfig'>,
+      ) => void,
     ) => void;
   }) {
     super({
@@ -139,8 +137,7 @@ export class EnsController extends BaseControllerV2<
     if (provider && onNetworkStateChange) {
       onNetworkStateChange((networkState) => {
         this.resetState();
-        const currentNetwork =
-          networkState.network === 'loading' ? null : networkState.network;
+        const currentNetwork = networkState.networkId;
         if (
           isKnownNetworkId(currentNetwork) &&
           this.#getNetworkEnsSupport(currentNetwork)

packages/ens-controller/src/EnsController.test.ts
@@ -1,5 +1,5 @@
 import { ControllerMessenger } from '@metamask/base-controller';
-import { toChecksumHexAddress } from '@metamask/controller-utils';
+import { NetworkType, toChecksumHexAddress } from '@metamask/controller-utils';
 import * as providersModule from '@ethersproject/providers';
 import { EnsController } from './EnsController';
 
@@ -113,9 +113,10 @@ describe('EnsController', () => {
       provider: getProvider(),
       onNetworkStateChange: (listener) => {
         listener({
-          network: '1',
+          networkId: '1',
           providerConfig: {
             chainId: '1',
+            type: NetworkType.mainnet,
           },
         });
       },
@@ -436,9 +437,10 @@ describe('EnsController', () => {
         provider: getProvider(),
         onNetworkStateChange: (listener) => {
           listener({
-            network: 'loading',
+            networkId: null,
             providerConfig: {
               chainId: '1',
+              type: NetworkType.mainnet,
             },
           });
         },
@@ -453,9 +455,10 @@ describe('EnsController', () => {
         provider: getProvider(),
         onNetworkStateChange: (listener) => {
           listener({
-            network: '1544',
+            networkId: '1544',
             providerConfig: {
               chainId: '1',
+              type: NetworkType.mainnet,
             },
           });
         },
@@ -477,9 +480,10 @@ describe('EnsController', () => {
         provider: getProvider(),
         onNetworkStateChange: (listener) => {
           listener({
-            network: '1',
+            networkId: '1',
             providerConfig: {
               chainId: '1',
+              type: NetworkType.mainnet,
             },
           });
         },
@@ -503,9 +507,10 @@ describe('EnsController', () => {
         provider: getProvider(),
         onNetworkStateChange: (listener) => {
           listener({
-            network: '1',
+            networkId: '1',
             providerConfig: {
               chainId: '1',
+              type: NetworkType.mainnet,
             },
           });
         },
@@ -524,9 +529,10 @@ describe('EnsController', () => {
         provider: getProvider(),
         onNetworkStateChange: (listener) => {
           listener({
-            network: '1',
+            networkId: '1',
             providerConfig: {
               chainId: '1',
+              type: NetworkType.mainnet,
             },
           });
         },
@@ -548,9 +554,10 @@ describe('EnsController', () => {
         provider: getProvider(),
         onNetworkStateChange: (listener) => {
           listener({
-            network: '1',
+            networkId: '1',
             providerConfig: {
               chainId: '1',
+              type: NetworkType.mainnet,
             },
           });
         },
@@ -572,9 +579,10 @@ describe('EnsController', () => {
         provider: getProvider(),
         onNetworkStateChange: (listener) => {
           listener({
-            network: '1',
+            networkId: '1',
             providerConfig: {
               chainId: '1',
+              type: NetworkType.mainnet,
             },
           });
         },
@@ -598,9 +606,10 @@ describe('EnsController', () => {
         provider: getProvider(),
         onNetworkStateChange: (listener) => {
           listener({
-            network: '1',
+            networkId: '1',
             providerConfig: {
               chainId: '1',
+              type: NetworkType.mainnet,
             },
           });
         },
@@ -623,9 +632,10 @@ describe('EnsController', () => {
         provider: getProvider(),
         onNetworkStateChange: (listener) => {
           listener({
-            network: '1',
+            networkId: '1',
             providerConfig: {
               chainId: '1',
+              type: NetworkType.mainnet,
             },
           });
         },