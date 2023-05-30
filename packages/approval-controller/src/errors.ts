71918704d0f3025bdb54431a1c47deb8eb506648
Matthew Walsh <matthew.walsh@consensys.net>
2023-05-30 11:40:10 +0100

    Support optional result indication when accepting an approval request (#1396)

--- packages/approval-controller/src/errors.ts
+++ packages/approval-controller/src/errors.ts
@@ -3,3 +3,11 @@ export class ApprovalRequestNotFoundError extends Error {
     super(`Approval request with id '${id}' not found.`);
   }
 }
+
+export class ApprovalRequestNoResultSupportError extends Error {
+  constructor(id: string) {
+    super(
+      `Approval acceptance requested result but request with id '${id}' does not support it.`,
+    );
+  }
+}
