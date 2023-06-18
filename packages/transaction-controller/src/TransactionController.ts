03aed239c5e57b4d160116786b9597c4711e2dd0
Matthew Walsh <matthew.walsh@consensys.net>
2023-06-18 21:11:49 +0100

    Update result callbacks

--- packages/transaction-controller/src/TransactionController.ts
+++ packages/transaction-controller/src/TransactionController.ts
@@ -24,7 +24,11 @@ import {
   NetworkType,
   RPC,
 } from '@metamask/controller-utils';
-import { AddApprovalRequest } from '@metamask/approval-controller';
+import {
+  AcceptResultCallbacks,
+  AddApprovalRequest,
+  AddResult,
+} from '@metamask/approval-controller';
 import {
   normalizeTransaction,
   validateTransaction,
@@ -1376,9 +1380,11 @@ export class TransactionController extends BaseController<
   ): Promise<string> {
     const transactionId = transactionMeta.id;
     let rejected = false;
+    let resultCallbacks: AcceptResultCallbacks | undefined;
 
     try {
-      await this.requestApproval(transactionMeta);
+      const acceptResult = await this.requestApproval(transactionMeta);
+      resultCallbacks = acceptResult.resultCallbacks;
 
       const updatedMeta = this.getTransaction(transactionId);
 
@@ -1388,6 +1394,8 @@ export class TransactionController extends BaseController<
       if (updatedMeta && !isCompleted) {
         await this.approveTransaction(transactionId);
       }
+
+      resultCallbacks?.success();
     } catch (error: any) {
       const updatedMeta = this.getTransaction(transactionId);
 
@@ -1402,6 +1410,8 @@ export class TransactionController extends BaseController<
           this.failTransaction(updatedMeta, error);
         }
       }
+
+      resultCallbacks?.error(error);
     }
 
     if (rejected) {
@@ -1431,22 +1441,23 @@ export class TransactionController extends BaseController<
     );
   }
 
-  private async requestApproval(txMeta: TransactionMeta) {
+  private async requestApproval(txMeta: TransactionMeta): Promise<AddResult> {
     const id = this.getApprovalId(txMeta);
     const { origin } = txMeta;
     const type = 'transaction';
     const requestData = { txId: txMeta.id };
 
-    return await this.messagingSystem.call(
+    return this.messagingSystem.call(
       'ApprovalController:addRequest',
       {
         id,
         origin: origin || 'metamask',
         type,
         requestData,
+        expectsResult: true,
       },
       true,
-    );
+    ) as Promise<AddResult>;
   }
 
   private getApprovalId(txMeta: TransactionMeta) {
