2aaa8eab9d138fc73d3ac5ec92af10860cd2d78b
Michele Esposito <34438276+mikesposito@users.noreply.github.com>
2023-07-11 19:39:36 +0200

    Add address related utils (#112)
    * feat: add isValidHexAddress function
    
    * fix: accept only prefixed addresses
    
    * fix: remove case insensitive flag
    
    * test: add test case for 0X
    
    * feat: validate checksum addresses
    
    * feat: break out address checksum encoding to erc55EncodeAddress function (#113)
    
    * feat: break out address checksum encoding to erc55EncodeAddress function
    
    * deps: ethereum-cryptography@2.0.0->2.1.0; dedupe @noble/hashes
    
    * Update jsdoc
    
    Co-authored-by: Maarten Zuidhoorn <maarten@zuidhoorn.com>
    
    * refactor: rename erc55EncodeAddress function
    
    * test: add test cases for getChecksumAddress
    
    ---------
    
    Co-authored-by: Maarten Zuidhoorn <maarten@zuidhoorn.com>
    Co-authored-by: Michele Esposito <michele@esposito.codes>
    
    * docs: edit isValidHexAddress jsdoc description
    
    * Update src/hex.ts
    
    Co-authored-by: Mark Stacey <markjstacey@gmail.com>
    
    * fix: add validation assertions
    
    ---------
    
    Co-authored-by: legobeat <109787230+legobeat@users.noreply.github.com>
    Co-authored-by: Maarten Zuidhoorn <maarten@zuidhoorn.com>
    Co-authored-by: Mark Stacey <markjstacey@gmail.com>

--- merged-packages/utils/src/hex.test.ts
+++ merged-packages/utils/src/hex.test.ts
@@ -1,10 +1,14 @@
 import {
+  Hex,
   add0x,
   assertIsHexString,
   assertIsStrictHexString,
+  isValidChecksumAddress,
   isHexString,
   isStrictHexString,
+  isValidHexAddress,
   remove0x,
+  getChecksumAddress,
 } from './hex';
 
 describe('isHexString', () => {
@@ -148,6 +152,92 @@ describe('assertIsStrictHexString', () => {
     expect(() => assertIsStrictHexString(hexString)).toThrow(
       'Value must be a hexadecimal string, starting with "0x".',
     );
+  });
+});
+
+describe('isValidHexAddress', () => {
+  it.each([
+    '0x0000000000000000000000000000000000000000' as Hex,
+    '0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045' as Hex,
+  ])('returns true for a valid prefixed hex address', (hexString) => {
+    expect(isValidHexAddress(hexString)).toBe(true);
+  });
+
+  it.each([
+    '0000000000000000000000000000000000000000',
+    'd8dA6BF26964aF9D7eEd9e03E53415D37aA96045',
+  ])('returns false for a valid non-prefixed hex address', (hexString) => {
+    // @ts-expect-error - testing invalid input
+    expect(isValidHexAddress(hexString)).toBe(false);
+  });
+
+  it.each([
+    '12345g',
+    '1234567890abcdefg',
+    '1234567890abcdefG',
+    '1234567890abcdefABCDEFg',
+    '1234567890abcdefABCDEF1234567890abcdefABCDEFg',
+    '0x',
+    '0x0',
+    '0x12345g',
+    '0x1234567890abcdefg',
+    '0x1234567890abcdefG',
+    '0x1234567890abcdefABCDEFg',
+    '0x1234567890abcdefABCDEF1234567890abcdefABCDEFg',
+    '0xD8DA6BF26964AF9D7EED9E03E53415D37AA96045',
+    '0xCF5609B003B2776699EEA1233F7C82D5695CC9AA',
+    '0Xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045',
+  ])('returns false for an invalid hex address', (hexString) => {
+    // @ts-expect-error - testing invalid input
+    expect(isValidHexAddress(hexString)).toBe(false);
+  });
+});
+
+describe('getChecksumAddress', () => {
+  it('returns the checksum address for a valid hex address', () => {
+    expect(
+      getChecksumAddress('0x5aaeb6053f3e94c9b9a09f33669435e7ef1beaed'),
+    ).toBe('0x5aAeb6053F3E94C9b9A09f33669435E7Ef1BeAed');
+
+    expect(
+      getChecksumAddress('0xfb6916095ca1df60bb79ce92ce3ea74c37c5d359'),
+    ).toBe('0xfB6916095ca1df60bB79Ce92cE3Ea74c37c5d359');
+
+    expect(
+      getChecksumAddress('0x52908400098527886e0f7030069857d2e4169ee7'),
+    ).toBe('0x52908400098527886E0F7030069857D2E4169EE7');
+
+    expect(
+      getChecksumAddress('0xde709f2102306220921060314715629080e2fb77'),
+    ).toBe('0xde709f2102306220921060314715629080e2fb77');
+
+    expect(
+      getChecksumAddress('0x0000000000000000000000000000000000000000'),
+    ).toBe('0x0000000000000000000000000000000000000000');
+  });
+
+  it('throws for an invalid hex address', () => {
+    expect(() => getChecksumAddress('0x')).toThrow('Invalid hex address.');
+  });
+});
+
+describe('isValidChecksumAddress', () => {
+  it.each([
+    '0x0000000000000000000000000000000000000000' as Hex,
+    '0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045' as Hex,
+    '0xCf5609B003B2776699eEA1233F7C82D5695cC9AA' as Hex,
+    '0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045' as Hex,
+    '0x8617E340B3D01FA5F11F306F4090FD50E238070D' as Hex,
+  ])('returns true for a valid checksum address', (hexString) => {
+    expect(isValidChecksumAddress(hexString)).toBe(true);
+  });
+
+  it.each([
+    '0xz' as Hex,
+    '0xD8DA6BF26964AF9D7EED9E03E53415D37AA96045' as Hex,
+    '0xCF5609B003B2776699EEA1233F7C82D5695CC9AA' as Hex,
+  ])('returns false for an invalid checksum address', (hexString) => {
+    expect(isValidChecksumAddress(hexString)).toBe(false);
   });
 });
 
--- merged-packages/utils/src/hex.ts
+++ merged-packages/utils/src/hex.ts
@@ -1,6 +1,8 @@
+import { keccak_256 as keccak256 } from '@noble/hashes/sha3';
 import { is, pattern, string, Struct } from 'superstruct';
 
 import { assert } from './assert';
+import { bytesToHex } from './bytes';
 
 export type Hex = `0x${string}`;
 
@@ -9,6 +11,14 @@ export const StrictHexStruct = pattern(string(), /^0x[0-9a-f]+$/iu) as Struct<
   Hex,
   null
 >;
+export const HexAddressStruct = pattern(
+  string(),
+  /^0x[0-9a-f]{40}$/u,
+) as Struct<Hex, null>;
+export const HexChecksumAddressStruct = pattern(
+  string(),
+  /^0x[0-9a-fA-F]{40}$/u,
+) as Struct<Hex, null>;
 
 /**
  * Check if a string is a valid hex string.
@@ -53,6 +63,58 @@ export function assertIsStrictHexString(value: unknown): asserts value is Hex {
     isStrictHexString(value),
     'Value must be a hexadecimal string, starting with "0x".',
   );
+}
+
+/**
+ * Validate that the passed prefixed hex string is an all-lowercase
+ * hex address, or a valid mixed-case checksum address.
+ *
+ * @param possibleAddress - Input parameter to check against.
+ * @returns Whether or not the input is a valid hex address.
+ */
+export function isValidHexAddress(possibleAddress: Hex) {
+  return (
+    is(possibleAddress, HexAddressStruct) ||
+    isValidChecksumAddress(possibleAddress)
+  );
+}
+
+/**
+ * Encode a passed hex string as an ERC-55 mixed-case checksum address.
+ *
+ * @param address - The hex address to encode.
+ * @returns The address encoded according to ERC-55.
+ * @see https://eips.ethereum.org/EIPS/eip-55
+ */
+export function getChecksumAddress(address: Hex) {
+  assert(is(address, HexChecksumAddressStruct), 'Invalid hex address.');
+  const unPrefixed = remove0x(address.toLowerCase());
+  const unPrefixedHash = remove0x(bytesToHex(keccak256(unPrefixed)));
+  return `0x${unPrefixed
+    .split('')
+    .map((character, nibbleIndex) => {
+      const hashCharacter = unPrefixedHash[nibbleIndex];
+      assert(is(hashCharacter, string()), 'Hash shorter than address.');
+      return parseInt(hashCharacter, 16) > 7
+        ? character.toUpperCase()
+        : character;
+    })
+    .join('')}`;
+}
+
+/**
+ * Validate that the passed hex string is a valid ERC-55 mixed-case
+ * checksum address.
+ *
+ * @param possibleChecksum - The hex address to check.
+ * @returns True if the address is a checksum address.
+ */
+export function isValidChecksumAddress(possibleChecksum: Hex) {
+  if (!is(possibleChecksum, HexChecksumAddressStruct)) {
+    return false;
+  }
+
+  return getChecksumAddress(possibleChecksum) === possibleChecksum;
 }
 
 /**
