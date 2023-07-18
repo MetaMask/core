2832e9a83ca775e4bb1ceb14e025e861e54e7b16
Mark Stacey <markjstacey@gmail.com>
2023-07-18 16:17:58 -0230

    Improve error handling when state metadata is missing
    The state derivation functions will throw an error today if any state
    properties are missing metadata. This interrupts the state operation
    completely. There are a few state operations that we want to ensure are
    never interrupted, even if some data is invalid, so this behavior has
    been changed.
    
    Instead we will skip any state properties with missing metadata, and
    throw an error in a timeout handler so that the problem is still
    captured.

--- packages/base-controller/src/BaseControllerV2.test.ts
+++ packages/base-controller/src/BaseControllerV2.test.ts
@@ -556,6 +556,34 @@ describe('getAnonymizedState', () => {
 
     expect(anonymizedState).toStrictEqual({ count: 1 });
   });
+
+  it('should suppress errors thrown when deriving state', () => {
+    const setTimeoutStub = sinon.stub(globalThis, 'setTimeout');
+    const persistentState = getAnonymizedState(
+      {
+        extraState: 'extraState',
+        privateKey: '123',
+        network: 'mainnet',
+      },
+      // @ts-expect-error Intentionally testing invalid state
+      {
+        privateKey: {
+          anonymous: true,
+          persist: true,
+        },
+        network: {
+          anonymous: false,
+          persist: false,
+        },
+      },
+    );
+    expect(persistentState).toStrictEqual({
+      privateKey: '123',
+    });
+    expect(setTimeoutStub.callCount).toBe(1);
+    const onTimeout = setTimeoutStub.firstCall.args[0];
+    expect(() => onTimeout()).toThrow(`No metadata found for 'extraState'`);
+  });
 });
 
 describe('getPersistentState', () => {
@@ -701,6 +729,34 @@ describe('getPersistentState', () => {
     );
 
     expect(persistentState).toStrictEqual({ count: 1 });
+  });
+
+  it('should suppress errors thrown when deriving state', () => {
+    const setTimeoutStub = sinon.stub(globalThis, 'setTimeout');
+    const persistentState = getPersistentState(
+      {
+        extraState: 'extraState',
+        privateKey: '123',
+        network: 'mainnet',
+      },
+      // @ts-expect-error Intentionally testing invalid state
+      {
+        privateKey: {
+          anonymous: false,
+          persist: true,
+        },
+        network: {
+          anonymous: false,
+          persist: false,
+        },
+      },
+    );
+    expect(persistentState).toStrictEqual({
+      privateKey: '123',
+    });
+    expect(setTimeoutStub.callCount).toBe(1);
+    const onTimeout = setTimeoutStub.firstCall.args[0];
+    expect(() => onTimeout()).toThrow(`No metadata found for 'extraState'`);
   });
 
   describe('inter-controller communication', () => {
