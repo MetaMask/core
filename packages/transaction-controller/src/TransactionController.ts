42dd9b8b4e12bd98a2d224a0d6dc8acba58a1170
OGPoyraz <omergoktugpoyraz@gmail.com>
2023-08-16 11:13:58 +0200

    Update packages/transaction-controller/src/TransactionController.ts
    Co-authored-by: Matthew Walsh <matthew.walsh@consensys.net>

--- packages/transaction-controller/src/TransactionController.ts
+++ packages/transaction-controller/src/TransactionController.ts
@@ -340,7 +340,7 @@ export class TransactionController extends BaseController<
    * @param opts - Additional options to control how the transaction is added.
    * @param opts.deviceConfirmedOn - An enum to indicate what device confirmed the transaction.
    * @param opts.origin - The origin of the transaction request, such as a dApp hostname.
-   * @param opts.requireApproval - Whether to skip creating approval for transactions.
+   * @param opts.requireApproval - Whether the transaction requires approval by the user, defaults to true unless explicitly disabled.
    * @returns Object containing a promise resolving to the transaction hash if approved.
    */
   async addTransaction(
