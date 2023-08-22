9cd58092b90ae83b50c5ec61978f127c6490b334
Elliot Winkler <elliot.winkler@gmail.com>
2023-08-22 15:20:39 -0600

    Publish preview builds to NPM instead of GitHub
    Currently, when publishing core packages, we use NPM for "production"
    releases but GitHub Package Registry for "preview" builds. This works to
    keep the list of production versions free of clutter and mitigate the
    risk of publishing preview builds as production releases accidentally,
    but it also introduces a major pain point.
    
    Usually in order to access a package with a preview build, the developer
    must instruct Yarn to use GitHub Package Registry instead of NPM along
    with an authentication token, and then update the version of the package
    in the manifest to use the preview version. If, however, the package
    introduces a new transitive dependency that is not available in preview
    form, the developer must undo the changes to the Yarn configuration or
    even temporarily add the dependency explicitly to the project in order
    to fully install it. In some cases, these steps don't work at all.
    
    To remove this pain point, this commit changes how preview builds are
    published so that instead of using a separate registry, we use a
    separate NPM scope. This preserves the separation between preview builds
    and non-preview builds, and it means that the developer can mix
    non-preview and preview versions in the manifest without needing to
    reconfigure Yarn.

---
+++ scripts/prepare-preview-builds.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+# This script prepares a package to be published as a preview build
+# to GitHub Packages.
+
+if [[ $# -eq 0 ]]; then
+  echo "Missing commit hash."
+  exit 1
+fi
+
+# We use the short commit hash as the prerelease version. This ensures each
+# preview build is unique and can be linked to a specific commit.
+shorthash="$1"
+
+prepare-preview-manifest() {
+  local manifest_file="$1"
+
+  # jq does not support in-place modification of files, so a temporary file is
+  # used to store the result of the operation. The original file is then
+  # overwritten with the temporary file.
+  jq --raw-output --arg hash "$shorthash" --from-file scripts/prepare-preview-builds.jq "$manifest_file" > temp.json
+  mv temp.json "$manifest_file"
+}
+
+echo "Preparing manifests..."
+while IFS=$'\t' read -r location name; do
+  echo "- $name"
+  prepare-preview-manifest "$location/package.json"
+done < <(yarn workspaces list --no-private --json | jq --slurp --raw-output 'map(select(.location != ".")) | map([.location, .name]) | map(@tsv) | .[]')
+
+echo "Installing dependencies..."
+yarn install --no-immutable
