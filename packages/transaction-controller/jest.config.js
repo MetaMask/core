2fbe1c1bd54b8bee65c01621545d7c314e64c1a1
Goktug Poyraz <goktug.poyraz@consensys.net>
2023-08-25 10:33:19 +0200

    update coverageThreshold

--- packages/transaction-controller/jest.config.js
+++ packages/transaction-controller/jest.config.js
@@ -17,10 +17,10 @@ module.exports = merge(baseConfig, {
   // An object that configures minimum threshold enforcement for coverage results
   coverageThreshold: {
     global: {
-      branches: 83.91,
-      functions: 92.68,
-      lines: 95.35,
-      statements: 95.46,
+      branches: 71.88,
+      functions: 86.33,
+      lines: 88.67,
+      statements: 88.9,
     },
   },
 
