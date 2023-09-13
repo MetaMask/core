551ac2ce85608d9863b814e855174f200266b68c
Jyoti Puri <jyotipuri@gmail.com>
2023-09-13 23:04:59 +0530

    Adding code changes to include blockaid response to transaction metadata to transaction controller patch branch (#1671)
    Cherry-picking https://github.com/MetaMask/core/pull/1636 into branch
    `refactor/transaction-controller-patch-mobile` used to create
    transaction controller patches.

--- packages/transaction-controller/src/TransactionController.ts
+++ packages/transaction-controller/src/TransactionController.ts
@@ -365,6 +365,7 @@ export class TransactionController extends BaseController<
    * @param opts - Additional options to control how the transaction is added.
    * @param opts.deviceConfirmedOn - An enum to indicate what device confirmed the transaction.
    * @param opts.origin - The origin of the transaction request, such as a dApp hostname.
+   * @param opts.securityAlertResponse - Response from security validator.
    * @returns Object containing a promise resolving to the transaction hash if approved.
    */
   async addTransaction(
@@ -372,9 +373,11 @@ export class TransactionController extends BaseController<
     {
       deviceConfirmedOn,
       origin,
+      securityAlertResponse,
     }: {
       deviceConfirmedOn?: WalletDevice;
       origin?: string;
+      securityAlertResponse?: Record<string, unknown>;
     } = {},
   ): Promise<Result> {
     const { providerConfig, networkId } = this.getNetworkState();
@@ -392,6 +395,7 @@ export class TransactionController extends BaseController<
       transaction,
       deviceConfirmedOn,
       verifiedOnBlockchain: false,
+      securityAlertResponse,
     };
 
     try {
