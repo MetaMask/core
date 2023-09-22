98efc47e30c92045c107572166b00d410c6ac564
Shane Jonas <jonas.shane@gmail.com>
2023-09-22 10:45:16 -0400

    Added polling-controller

---
+++ packages/polling-controller/src/PollingController.test.ts
@@ -0,0 +1,214 @@
+import { ControllerMessenger } from '@metamask/base-controller';
+
+import type { PollingCompleteType } from './PollingController';
+import GasFeeControllerPolling from './PollingController';
+
+describe('PollingController', () => {
+  let executePollMock: GasFeeControllerPolling<any, any, any>['executePoll'];
+
+  beforeEach(() => {
+    executePollMock = jest.fn().mockImplementation(async () => {
+      return true;
+    });
+  });
+
+  describe('start', () => {
+    it('should start polling if not polling', () => {
+      jest.useFakeTimers();
+
+      class MyGasFeeController extends GasFeeControllerPolling<any, any, any> {
+        executePoll = executePollMock;
+      }
+      const mockMessenger = new ControllerMessenger<any, any>();
+
+      const controller = new MyGasFeeController({
+        messenger: mockMessenger,
+        metadata: {},
+        name: 'GasFeeControllerPolling',
+        state: { foo: 'bar' },
+      });
+      controller.start('mainnet');
+      jest.advanceTimersByTime(1500);
+      controller.stopAll();
+      expect(executePollMock).toHaveBeenCalledTimes(1);
+    });
+  });
+  describe('stop', () => {
+    it('should stop polling if polling', () => {
+      jest.useFakeTimers();
+      class MyGasFeeController extends GasFeeControllerPolling<any, any, any> {
+        executePoll = executePollMock;
+      }
+      const mockMessenger = new ControllerMessenger<any, any>();
+
+      const controller = new MyGasFeeController({
+        messenger: mockMessenger,
+        metadata: {},
+        name: 'GasFeeControllerPolling',
+        state: { foo: 'bar' },
+      });
+      const pollingToken = controller.start('mainnet');
+      jest.advanceTimersByTime(1500);
+      controller.stop(pollingToken);
+      jest.advanceTimersByTime(1500);
+      expect(executePollMock).toHaveBeenCalledTimes(1);
+      controller.stopAll();
+    });
+    it('should not stop polling if multiple polling tokens exist', async () => {
+      jest.useFakeTimers();
+      class MyGasFeeController extends GasFeeControllerPolling<any, any, any> {
+        executePoll = executePollMock;
+      }
+      const mockMessenger = new ControllerMessenger<any, any>();
+
+      const controller = new MyGasFeeController({
+        messenger: mockMessenger,
+        metadata: {},
+        name: 'GasFeeControllerPolling',
+        state: { foo: 'bar' },
+      });
+      const pollingToken1 = controller.start('mainnet');
+      controller.start('mainnet');
+      jest.advanceTimersByTime(1400);
+      await Promise.resolve();
+      controller.stop(pollingToken1);
+      jest.advanceTimersByTime(1400);
+      await Promise.resolve();
+      expect(executePollMock).toHaveBeenCalledTimes(2);
+      controller.stopAll();
+    });
+    it('should error if no poll token is passed', () => {
+      jest.useFakeTimers();
+      class MyGasFeeController extends GasFeeControllerPolling<any, any, any> {
+        executePoll = executePollMock;
+      }
+      const mockMessenger = new ControllerMessenger<any, any>();
+
+      const controller = new MyGasFeeController({
+        messenger: mockMessenger,
+        metadata: {},
+        name: 'GasFeeControllerPolling',
+        state: { foo: 'bar' },
+      });
+      controller.start('mainnet');
+      expect(() => {
+        controller.stop(undefined as unknown as any);
+      }).toThrow('pollingToken required');
+      controller.stopAll();
+    });
+    it('should error if no poll token is found', () => {
+      jest.useFakeTimers();
+      class MyGasFeeController extends GasFeeControllerPolling<any, any, any> {
+        executePoll = executePollMock;
+      }
+      const mockMessenger = new ControllerMessenger<any, any>();
+
+      const controller = new MyGasFeeController({
+        messenger: mockMessenger,
+        metadata: {},
+        name: 'GasFeeControllerPolling',
+        state: { foo: 'bar' },
+      });
+      controller.start('mainnet');
+      expect(() => {
+        controller.stop('potato');
+      }).toThrow('pollingToken not found');
+      controller.stopAll();
+    });
+  });
+  describe('poll', () => {
+    it('should call executePoll if polling', async () => {
+      jest.useFakeTimers();
+
+      class MyGasFeeController extends GasFeeControllerPolling<any, any, any> {
+        executePoll = executePollMock;
+      }
+      const mockMessenger = new ControllerMessenger<any, any>();
+
+      const controller = new MyGasFeeController({
+        messenger: mockMessenger,
+        metadata: {},
+        name: 'GasFeeControllerPolling',
+        state: { foo: 'bar' },
+      });
+      controller.start('mainnet');
+      jest.advanceTimersByTime(1200);
+      await Promise.resolve();
+      jest.advanceTimersByTime(1200);
+      await Promise.resolve();
+      expect(executePollMock).toHaveBeenCalledTimes(2);
+    });
+    it('should continue calling executePoll when start is called again with the same networkClientId', async () => {
+      jest.useFakeTimers();
+
+      class MyGasFeeController extends GasFeeControllerPolling<any, any, any> {
+        executePoll = executePollMock;
+      }
+      const mockMessenger = new ControllerMessenger<any, any>();
+
+      const controller = new MyGasFeeController({
+        messenger: mockMessenger,
+        metadata: {},
+        name: 'GasFeeControllerPolling',
+        state: { foo: 'bar' },
+      });
+      controller.start('mainnet');
+      controller.start('mainnet');
+      jest.advanceTimersByTime(1200);
+      await Promise.resolve();
+      jest.advanceTimersByTime(1200);
+      await Promise.resolve();
+      expect(executePollMock).toHaveBeenCalledTimes(2);
+      controller.stopAll();
+    });
+    it('should publish polligComplete when stop is called', async () => {
+      jest.useFakeTimers();
+      const pollingComplete: any = jest.fn();
+      class MyGasFeeController extends GasFeeControllerPolling<any, any, any> {
+        executePoll = executePollMock;
+      }
+      const name = 'GasFeeControllerPolling';
+
+      const mockMessenger = new ControllerMessenger<
+        any,
+        PollingCompleteType<typeof name>
+      >();
+
+      mockMessenger.subscribe(`${name}:pollingComplete`, pollingComplete);
+
+      const controller = new MyGasFeeController({
+        messenger: mockMessenger,
+        metadata: {},
+        name,
+        state: { foo: 'bar' },
+      });
+      const pollingToken = controller.start('mainnet');
+      controller.stop(pollingToken);
+      expect(pollingComplete).toHaveBeenCalledTimes(1);
+    });
+  });
+  describe('multiple networkClientIds', () => {
+    it('should poll for each networkClientId', async () => {
+      jest.useFakeTimers();
+      class MyGasFeeController extends GasFeeControllerPolling<any, any, any> {
+        executePoll = executePollMock;
+      }
+      const mockMessenger = new ControllerMessenger<any, any>();
+
+      const controller = new MyGasFeeController({
+        messenger: mockMessenger,
+        metadata: {},
+        name: 'GasFeeControllerPolling',
+        state: { foo: 'bar' },
+      });
+      controller.start('mainnet');
+      controller.start('rinkeby');
+      jest.advanceTimersByTime(1200);
+      await Promise.resolve();
+      jest.advanceTimersByTime(1200);
+      await Promise.resolve();
+      expect(executePollMock).toHaveBeenCalledTimes(4);
+      controller.stopAll();
+    });
+  });
+});
---
+++ packages/polling-controller/src/PollingController.ts
@@ -0,0 +1,99 @@
+import { BaseControllerV2 } from '@metamask/base-controller';
+import type { RestrictedControllerMessenger } from '@metamask/base-controller';
+import type { NetworkClientId } from '@metamask/network-controller';
+import type { Json } from '@metamask/utils';
+import { v1 as random } from 'uuid';
+
+export type PollingCompleteType<N extends string> = {
+  type: `${N}:pollingComplete`;
+  payload: [string];
+};
+
+export default abstract class ControllerPolling<
+  N extends string,
+  S extends Record<string, Json>,
+  messenger extends RestrictedControllerMessenger<
+    N,
+    any,
+    PollingCompleteType<N> | any,
+    string,
+    string
+  >,
+> extends BaseControllerV2<N, S, messenger> {
+  private readonly intervalLength = 1000;
+
+  private readonly networkClientIdTokensMap: Map<NetworkClientId, Set<string>> =
+    new Map();
+
+  private readonly intervalIds: Record<NetworkClientId, NodeJS.Timeout> = {};
+
+  start(networkClientId: NetworkClientId) {
+    const innerPollToken = random();
+    if (this.networkClientIdTokensMap.has(networkClientId)) {
+      //
+      const set = this.networkClientIdTokensMap.get(networkClientId);
+      set?.add(innerPollToken);
+    } else {
+      const set = new Set<string>();
+      set.add(innerPollToken);
+      this.networkClientIdTokensMap.set(networkClientId, set);
+    }
+    this.#poll(networkClientId);
+
+    // call _poll
+    // add the inner poll token to the poll tokens set
+    return innerPollToken;
+  }
+
+  stopAll() {
+    this.networkClientIdTokensMap.forEach((tokens, _networkClientId) => {
+      tokens.forEach((token) => {
+        this.stop(token);
+      });
+    });
+  }
+
+  stop(pollingToken: string) {
+    if (!pollingToken) {
+      throw new Error('pollingToken required');
+    }
+    let found = false;
+    this.networkClientIdTokensMap.forEach((tokens, networkClientId) => {
+      if (tokens.has(pollingToken)) {
+        found = true;
+        this.networkClientIdTokensMap
+          .get(networkClientId)
+          ?.delete(pollingToken);
+        if (this.networkClientIdTokensMap.get(networkClientId)?.size === 0) {
+          clearTimeout(this.intervalIds[networkClientId]);
+          delete this.intervalIds[networkClientId];
+          this.networkClientIdTokensMap.delete(networkClientId);
+          this.messagingSystem.publish(
+            `${this.name}:pollingComplete`,
+            networkClientId,
+          );
+        }
+      }
+    });
+    if (!found) {
+      throw new Error('pollingToken not found');
+    }
+  }
+
+  abstract executePoll(networkClientId: NetworkClientId): Promise<void>;
+
+  #poll(networkClientId: NetworkClientId) {
+    if (this.intervalIds[networkClientId]) {
+      clearTimeout(this.intervalIds[networkClientId]);
+      delete this.intervalIds[networkClientId];
+    }
+    this.intervalIds[networkClientId] = setTimeout(async () => {
+      try {
+        await this.executePoll(networkClientId);
+      } catch (error) {
+        console.error(error);
+      }
+      this.#poll(networkClientId);
+    }, this.intervalLength);
+  }
+}
