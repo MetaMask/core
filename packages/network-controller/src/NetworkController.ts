9690018c2c2b5ceab12879a27def616bf35c10ac
Zachary Belford <belfordz66@gmail.com>
2023-09-25 19:42:20 -0700

    queueing

--- packages/network-controller/src/NetworkController.ts
+++ packages/network-controller/src/NetworkController.ts
@@ -227,9 +227,9 @@ function buildCustomNetworkClientId(
   ...args:
     | [NetworkConfigurationId]
     | [
-        ProviderConfig & { type: typeof NetworkType.rpc; rpcUrl: string },
-        NetworkConfigurations,
-      ]
+      ProviderConfig & { type: typeof NetworkType.rpc; rpcUrl: string },
+      NetworkConfigurations,
+    ]
 ): CustomNetworkClientId {
   if (args.length === 1) {
     return args[0];
@@ -433,6 +433,20 @@ export type NetworkControllerGetNetworkClientByIdAction = {
 export type NetworkControllerGetEIP1559CompatibilityAction = {
   type: `NetworkController:getEIP1559Compatibility`;
   handler: NetworkController['getEIP1559Compatibility'];
+
+export type NetworkControllerFindNetworkClientIdByChainIdAction = {
+    type: `NetworkController:findNetworkClientIdByChainId`;
+    handler: NetworkController['findNetworkClientIdByChainId'];
+  };
+
+export type NetworkControllerSetProviderTypeAction = {
+  type: `NetworkController:setProviderType`;
+  handler: NetworkController['setProviderType'];
+};
+
+export type NetworkControllerSetActiveNetworkAction = {
+  type: `NetworkController:setActiveNetwork`;
+  handler: NetworkController['setActiveNetwork'];
 };
 
 export type NetworkControllerActions =
@@ -440,7 +454,10 @@ export type NetworkControllerActions =
   | NetworkControllerGetProviderConfigAction
   | NetworkControllerGetEthQueryAction
   | NetworkControllerGetNetworkClientByIdAction
-  | NetworkControllerGetEIP1559CompatibilityAction;
+  | NetworkControllerGetEIP1559CompatibilityAction
+  | NetworkControllerFindNetworkClientIdByChainIdAction
+  | NetworkControllerSetActiveNetworkAction
+  | NetworkControllerSetProviderTypeAction;
 
 export type NetworkControllerMessenger = RestrictedControllerMessenger<
   typeof name,
@@ -601,8 +618,8 @@ export class NetworkController extends BaseControllerV2<
   getProviderAndBlockTracker(): {
     provider: SwappableProxy<ProxyWithAccessibleTarget<Provider>> | undefined;
     blockTracker:
-      | SwappableProxy<ProxyWithAccessibleTarget<BlockTracker>>
-      | undefined;
+    | SwappableProxy<ProxyWithAccessibleTarget<BlockTracker>>
+    | undefined;
   } {
     return {
       provider: this.#providerProxy,
@@ -665,7 +682,7 @@ export class NetworkController extends BaseControllerV2<
     if (isInfuraProviderType(networkClientId)) {
       const infuraNetworkClient =
         autoManagedNetworkClientRegistry[NetworkClientType.Infura][
-          networkClientId
+        networkClientId
         ];
       if (!infuraNetworkClient) {
         throw new Error(
@@ -677,7 +694,7 @@ export class NetworkController extends BaseControllerV2<
 
     const customNetworkClient =
       autoManagedNetworkClientRegistry[NetworkClientType.Custom][
-        networkClientId
+      networkClientId
       ];
     if (!customNetworkClient) {
       throw new Error(
@@ -1434,12 +1451,12 @@ export class NetworkController extends BaseControllerV2<
    */
   #buildIdentifiedNetworkClientConfigurationsFromProviderConfig():
     | [
-        [
-          NetworkClientType.Custom,
-          CustomNetworkClientId,
-          CustomNetworkClientConfiguration,
-        ],
-      ]
+      [
+        NetworkClientType.Custom,
+        CustomNetworkClientId,
+        CustomNetworkClientConfiguration,
+      ],
+    ]
     | [] {
     const { providerConfig } = this.state;
 
