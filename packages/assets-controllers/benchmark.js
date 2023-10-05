130f1b335615489bde98695583a402339a615f88
Brian Bergeron <brian.e.bergeron@gmail.com>
2023-10-05 10:07:49 -0500

    Update benchmark.js

--- packages/assets-controllers/benchmark.js
+++ packages/assets-controllers/benchmark.js
@@ -1,37 +1,31 @@
-const { AssetsContractController } = require('./');
+const { AssetsContractController } = require('.');
+const { NetworkController } = require('../network-controller');
 const HttpProvider = require('ethjs-provider-http');
 const { Contract } = require('@ethersproject/contracts');
 const { Web3Provider } = require('@ethersproject/providers');
 
-(async function main() {
-
-  // Use same provider for both approaches
-  const provider = new HttpProvider(
-    `https://sepolia.infura.io/v3/<OMITTED>`
-  );
+const benchmark = async (provider, iterations) => {
+  const tokenAddress = "0x5974D2B77eedae32B576b0ADF32373a339d73833";
+  const userAddress = "0x9f36f6073a1e27d93c767d899470fb8eeaf8ae86";
 
   // Existing approach of multiple RPC calls
   const controller = new AssetsContractController({
-    chainId: 11155111,
+    chainId: 11155111, // sepolia
     onPreferencesStateChange: () => { },
     onNetworkStateChange: () => { },
-    getNetworkClientById: () => ({ provider })
+    getNetworkClientById: () => ({provider})
   });
 
   // Take average time of <n> requests
-  const count = 10;
   let start = performance.now();
-  for (let i = 0; i < count; i++) {
+  for (let i = 0; i < iterations; i++) {
     details = await controller.getTokenStandardAndDetails(
-      "0x5974D2B77eedae32B576b0ADF32373a339d73833",
-      "0x9f36f6073a1e27d93c767d899470fb8eeaf8ae86",
-      undefined,
-      'sepolia'
-    );
+      tokenAddress, userAddress, undefined, 'sepolia');
   }
-  console.log((performance.now() - start) / count);
+  console.log("Existing multi RPC calls:")
+  console.log((performance.now() - start) / iterations);
   console.log(details);
-
+  
   // Smart contract approach
   const abi = [
     {
@@ -61,29 +55,62 @@ const { Web3Provider } = require('@ethersproject/providers');
 
   // Take average time of <n> requests
   start = performance.now();
-  for (let i = 0; i < count; i++) {
-    details = await contract(
-      "0x5974D2B77eedae32B576b0ADF32373a339d73833",
-      "0x9f36f6073a1e27d93c767d899470fb8eeaf8ae86");
+  for (let i = 0; i < iterations; i++) {
+    details = await contract(tokenAddress, userAddress);
   }
-  console.log((performance.now() - start) / count);
+  console.log("Contract with 1 RPC call:")
+  console.log((performance.now() - start) / iterations);
   const { decimals, symbol, balance, standard } = details;
   console.log({ decimals, symbol, balance, standard })
 
+  // TODO: benchmark multicall3: https://github.com/mds1/multicall
+}
+
+(async function main() {
+  const iterations = process.argv[2] ?? 1;
+  const infuraProjectId = '<OMITTED>';
+
+  // Benchmark with same provider as product
+  const networkController = new NetworkController({
+    infuraProjectId,
+    messenger: {
+      registerActionHandler: () => {},
+      publish: () => {},
+      subscribe: () => {},
+      unsubscribe: () => {},
+    }
+  });
+  networkController.setProviderType('sepolia');
+  let {provider} = networkController.getProviderAndBlockTracker();
+  console.log(`NetworkController provider, ${iterations} iterations:`)
+  await benchmark(provider, iterations);
+  console.log();
+
+  // Benchmark with normal provider
+  provider = new HttpProvider(`https://sepolia.infura.io/v3/${infuraProjectId}`);
+  console.log(`HttpProvider, ${iterations} iterations:`)
+  await benchmark(provider, iterations);
+
 })().catch(e => { console.error(e) })
 
-// 2277.029479202628
-// {
-//   decimals: '4',
-//   symbol: 'TST',
-//   balance: <BN: 186a0>,
-//   standard: 'ERC20'
-// }
+// Results:
+//
+// NetworkController provider, 1 iterations:
+//   Existing multi RPC calls: 1127.5343329906464
+//   Contract with 1 RPC call: 184.28245800733566
+//
+// NetworkController provider, 10 iterations:
+//   Existing multi RPC calls: 507.6427625000477
+//   Contract with 1 RPC call: 20.540170902013777
+//
+// HttpProvider, 1 iterations:
+//   Existing multi RPC calls: 2203.581832975149
+//   Contract with 1 RPC call: 555.5011250078678
+//
+// HttpProvider, 10 iterations:
+//   Existing multi RPC calls: 2101.5782083004715
+//   Contract with 1 RPC call: 392.4244249999523
+//
+// Question: Why is NetworkController faster with more iterations?
+//           Is there some caching going on?
 
-// 396.6844749987125
-// {
-//   decimals: 4,
-//   symbol: 'TST',
-//   balance: BigNumber { _hex: '0x0186a0', _isBigNumber: true },
-//   standard: 'ERC20'
-// }
