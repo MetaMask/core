72345041932ad739318f8e25ea91685347d780bb
Shane <jonas.shane@gmail.com>
2023-10-05 07:26:40 -0700

    Update packages/polling-controller/src/PollingController.test.ts

--- packages/polling-controller/src/PollingController.test.ts
+++ packages/polling-controller/src/PollingController.test.ts
@@ -211,7 +211,7 @@ describe('PollingController', () => {
       await Promise.resolve();
       expect(controller.executePoll).toHaveBeenCalledTimes(2);
     });
-    it('should not poll twice for the same networkClientId and options', async () => {
+    it('should poll for the same networkClientId and options', async () => {
       jest.useFakeTimers();
 
       class MyGasFeeController extends PollingController<any, any, any> {
