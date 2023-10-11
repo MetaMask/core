a96e022fc999e2c596ebd6de7fbaf6dad5a116be
Jongsun Suh <34228073+MajorLift@users.noreply.github.com>
2023-10-11 14:23:55 -0700

    Implement tag-porting script function for filtering version numbers and corresponding commit hashes
    - Extracts corresponding pairs of release version number and release commit hash from the migrated git history in `merged-packages/<package-name>`.
      - The regex used here can be optionally specified using the second argument passed into the script.
      - If no second argument is passed, the default regex looks for commit messages starting with a semver-formatted version string (anything more specific runs into edge cases).

--- scripts/migrate-tags.sh
+++ scripts/migrate-tags.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env bash
 
 package_name="$1"
+release_commits_regex=${2:-'^\d\{1,3\}\.\d\{1,3\}\.\d\{1,3\}'}
+
+get-version-commit-pairs() {
+  for log in "$(git log --oneline --grep $release_commits_regex merged-packages/$package_name)"; do
+    echo "$log" | cut -d' ' -f1,2 | sed 's/\([^ ]*\)\([ ]*\) \([^ ]*\)\([ ]*\)/\3\4 \1\2/'
+  done
+}
