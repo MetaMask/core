0561048ba7ebaceff35faab844aba418d342b4d8
Salah-Eddine Saakoun <salah-eddine.saakoun@consensys.net>
2023-10-12 20:35:39 +0100

    Init permission log controller import

---
+++ packages/permission-log/README.md
@@ -0,0 +1,15 @@
+# `@metamask/permission-log-controller`
+
+Controller with middleware for logging requests and responses to restricted and permissions-related methods.
+
+## Installation
+
+`yarn add @metamask/permission-log-controller`
+
+or
+
+`npm install @metamask/permission-log-controller`
+
+## Contributing
+
+This package is part of a monorepo. Instructions for contributing can be found in the [monorepo README](https://github.com/MetaMask/core#readme).
---
+++ packages/permission-log/src/permissions.ts
@@ -0,0 +1,393 @@
+import deepFreeze from 'deep-freeze-strict';
+
+export const CaveatTypes = Object.freeze({
+  restrictReturnedAccounts: 'restrictReturnedAccounts' as const,
+});
+
+export const RestrictedMethods = Object.freeze({
+  eth_accounts: 'eth_accounts',
+} as const);
+
+/**
+ * This file contains mocks for the PermissionLogController tests.
+ */
+
+export const noop = () => undefined;
+
+const keyringAccounts = deepFreeze([
+  '0x0dcd5d886577d5081b0c52e242ef29e70be3e7bc',
+  '0xc42edfcc21ed14dda456aa0756c153f7985d8813',
+  '0x7ae1cdd37bcbdb0e1f491974da8022bfdbf9c2bf',
+  '0xcc74c7a59194e5d9268476955650d1e285be703c',
+]);
+
+const SUBJECTS = {
+  a: { origin: 'https://foo.xyz' },
+  b: { origin: 'https://bar.abc' },
+  c: { origin: 'https://baz.def' },
+};
+
+const PERM_NAMES = {
+  eth_accounts: 'eth_accounts',
+  test_method: 'test_method',
+  does_not_exist: 'does_not_exist',
+};
+
+const ACCOUNTS = {
+  a: {
+    permitted: keyringAccounts.slice(0, 3),
+    primary: keyringAccounts[0],
+  },
+  b: {
+    permitted: [keyringAccounts[0]],
+    primary: keyringAccounts[0],
+  },
+  c: {
+    permitted: [keyringAccounts[1]],
+    primary: keyringAccounts[1],
+  },
+};
+
+/**
+ * Helpers for getting mock caveats.
+ */
+const CAVEATS = {
+  /**
+   * Gets a correctly formatted eth_accounts restrictReturnedAccounts caveat.
+   *
+   * @param accounts - The accounts for the caveat
+   * @returns An eth_accounts restrictReturnedAccounts caveats
+   */
+  eth_accounts: (accounts: string[]) => {
+    return [
+      {
+        type: CaveatTypes.restrictReturnedAccounts,
+        value: accounts,
+      },
+    ];
+  },
+};
+
+/**
+ * Each function here corresponds to what would be a type or interface consumed
+ * by permissions controller functions if we used TypeScript.
+ */
+const PERMS = {
+  /**
+   * Requested permissions objects, as passed to wallet_requestPermissions.
+   */
+  requests: {
+    /**
+     * eth_accounts
+     *
+     * @returns A permissions request object with eth_accounts
+     */
+    eth_accounts: () => {
+      return { eth_accounts: {} };
+    },
+
+    /**
+     * test_method
+     *
+     * @returns A permissions request object with test_method
+     */
+    test_method: () => {
+      return { test_method: {} };
+    },
+
+    /**
+     * does_not_exist
+     *
+     * @returns A permissions request object with does_not_exist
+     */
+    does_not_exist: () => {
+      return { does_not_exist: {} };
+    },
+  },
+
+  /**
+   * Partial members of res.result for successful:
+   * - wallet_requestPermissions
+   * - wallet_getPermissions
+   */
+  granted: {
+    /**
+     * eth_accounts
+     *
+     * @param accounts - The accounts for the eth_accounts permission caveat
+     * @returns A granted permissions object with eth_accounts and its caveat
+     */
+    eth_accounts: (accounts: string[]) => {
+      return {
+        parentCapability: PERM_NAMES.eth_accounts,
+        caveats: CAVEATS.eth_accounts(accounts),
+      };
+    },
+
+    /**
+     * test_method
+     *
+     * @returns A granted permissions object with test_method
+     */
+    test_method: () => {
+      return {
+        parentCapability: PERM_NAMES.test_method,
+      };
+    },
+  },
+};
+
+/**
+ * Objects with function values for getting correctly formatted permissions,
+ * caveats, errors, permissions requests etc.
+ */
+export const getters = deepFreeze({
+  PERMS,
+
+  /**
+   * Getters for mock RPC request objects.
+   */
+  RPC_REQUESTS: {
+    /**
+     * Gets an arbitrary RPC request object.
+     *
+     * @param origin - The origin of the request
+     * @param method - The request method
+     * @param params - The request parameters
+     * @param [id] - The request id
+     * @returns An RPC request object
+     */
+    custom: (origin: string, method: string, params: any[], id: string) => {
+      const req: any = {
+        origin,
+        method,
+        params: params || [],
+      };
+      if (id !== undefined) {
+        req.id = id;
+      }
+      return req;
+    },
+
+    /**
+     * Gets an eth_accounts RPC request object.
+     *
+     * @param origin - The origin of the request
+     * @returns An RPC request object
+     */
+    eth_accounts: (origin: string) => {
+      return {
+        origin,
+        method: 'eth_accounts',
+        params: [],
+      };
+    },
+
+    /**
+     * Gets a test_method RPC request object.
+     *
+     * @param origin - The origin of the request
+     * @param param - The request param
+     * @returns An RPC request object
+     */
+    test_method: (origin: string, param = false) => {
+      return {
+        origin,
+        method: 'test_method',
+        params: [param],
+      };
+    },
+
+    /**
+     * Gets an eth_requestAccounts RPC request object.
+     *
+     * @param origin - The origin of the request
+     * @returns An RPC request object
+     */
+    eth_requestAccounts: (origin: string) => {
+      return {
+        origin,
+        method: 'eth_requestAccounts',
+        params: [],
+      };
+    },
+
+    /**
+     * Gets a wallet_requestPermissions RPC request object,
+     * for a single permission.
+     *
+     * @param origin - The origin of the request
+     * @param permissionName - The name of the permission to request
+     * @returns An RPC request object
+     */
+    requestPermission: (
+      origin: string,
+      permissionName: 'eth_accounts' | 'test_method' | 'does_not_exist',
+    ) => {
+      return {
+        origin,
+        method: 'wallet_requestPermissions',
+        params: [PERMS.requests[permissionName]()],
+      };
+    },
+
+    /**
+     * Gets a wallet_requestPermissions RPC request object,
+     * for multiple permissions.
+     *
+     * @param origin - The origin of the request
+     * @param permissions - A permission request object
+     * @returns An RPC request object
+     */
+    requestPermissions: (origin: string, permissions = {}) => {
+      return {
+        origin,
+        method: 'wallet_requestPermissions',
+        params: [permissions],
+      };
+    },
+
+    /**
+     * Gets a metamask_sendDomainMetadata RPC request object.
+     *
+     * @param origin - The origin of the request
+     * @param name - The subjectMetadata name
+     * @param args - Any other data for the request's subjectMetadata
+     * @returns An RPC request object
+     */
+    metamask_sendDomainMetadata: (
+      origin: string,
+      name: any,
+      ...args: any[]
+    ) => {
+      return {
+        origin,
+        method: 'metamask_sendDomainMetadata',
+        params: {
+          ...args,
+          name,
+        },
+      };
+    },
+  },
+});
+
+/**
+ * Objects with immutable mock values.
+ */
+export const constants = deepFreeze({
+  REQUEST_IDS: {
+    a: '1',
+    b: '2',
+    c: '3',
+  },
+
+  SUBJECTS: { ...SUBJECTS },
+
+  ACCOUNTS: { ...ACCOUNTS },
+
+  PERM_NAMES: { ...PERM_NAMES },
+
+  RESTRICTED_METHODS: new Set(['eth_accounts', 'test_method']),
+
+  /**
+   * Mock permissions history objects.
+   */
+  EXPECTED_HISTORIES: {
+    case1: [
+      {
+        [SUBJECTS.a.origin]: {
+          [PERM_NAMES.eth_accounts]: {
+            lastApproved: 1,
+            accounts: {
+              [ACCOUNTS.a.permitted[0]]: 1,
+              [ACCOUNTS.a.permitted[1]]: 1,
+              [ACCOUNTS.a.permitted[2]]: 1,
+            },
+          },
+        },
+      },
+      {
+        [SUBJECTS.a.origin]: {
+          [PERM_NAMES.eth_accounts]: {
+            lastApproved: 2,
+            accounts: {
+              [ACCOUNTS.a.permitted[0]]: 2,
+              [ACCOUNTS.a.permitted[1]]: 1,
+              [ACCOUNTS.a.permitted[2]]: 1,
+            },
+          },
+        },
+      },
+    ],
+
+    case2: [
+      {
+        [SUBJECTS.a.origin]: {
+          [PERM_NAMES.eth_accounts]: {
+            lastApproved: 1,
+            accounts: {},
+          },
+        },
+      },
+    ],
+
+    case3: [
+      {
+        [SUBJECTS.a.origin]: {
+          [PERM_NAMES.test_method]: { lastApproved: 1 },
+        },
+        [SUBJECTS.b.origin]: {
+          [PERM_NAMES.eth_accounts]: {
+            lastApproved: 1,
+            accounts: {
+              [ACCOUNTS.b.permitted[0]]: 1,
+            },
+          },
+        },
+        [SUBJECTS.c.origin]: {
+          [PERM_NAMES.test_method]: { lastApproved: 1 },
+          [PERM_NAMES.eth_accounts]: {
+            lastApproved: 1,
+            accounts: {
+              [ACCOUNTS.c.permitted[0]]: 1,
+            },
+          },
+        },
+      },
+      {
+        [SUBJECTS.a.origin]: {
+          [PERM_NAMES.test_method]: { lastApproved: 2 },
+        },
+        [SUBJECTS.b.origin]: {
+          [PERM_NAMES.eth_accounts]: {
+            lastApproved: 1,
+            accounts: {
+              [ACCOUNTS.b.permitted[0]]: 1,
+            },
+          },
+        },
+        [SUBJECTS.c.origin]: {
+          [PERM_NAMES.test_method]: { lastApproved: 1 },
+          [PERM_NAMES.eth_accounts]: {
+            lastApproved: 2,
+            accounts: {
+              [ACCOUNTS.c.permitted[0]]: 1,
+              [ACCOUNTS.b.permitted[0]]: 2,
+            },
+          },
+        },
+      },
+    ],
+
+    case4: [
+      {
+        [SUBJECTS.a.origin]: {
+          [PERM_NAMES.test_method]: {
+            lastApproved: 1,
+          },
+        },
+      },
+    ],
+  },
+});
