ee4a6286789f15eeed688fe21815e2577cc079c5
Kanthesha Devaramane <kanthesha.devaramane@consensys.net>
2023-10-18 19:17:08 +0100

    applied eslint rules from core monorepo and fixed the errors (#172)
    * applied eslint rules from core monorepo and fixed the errors
    
    * added eslint-import-resolver-typescript devDependencies

--- merged-packages/json-rpc-engine/src/createAsyncMiddleware.ts
+++ merged-packages/json-rpc-engine/src/createAsyncMiddleware.ts
@@ -71,7 +71,7 @@ export function createAsyncMiddleware<
         returnHandlerCallback = runReturnHandlersCallback;
         resolveNextPromise();
       });
-      await nextPromise;
+      return nextPromise;
     };
 
     try {
