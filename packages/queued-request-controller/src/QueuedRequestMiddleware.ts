0d13eaaa3f0168614b3d2eab8d4a4cef823a71e5
BelfordZ <belfordz66@gmail.com>
2023-10-18 13:56:48 -0700

    lint fixup

--- packages/queued-request-controller/src/QueuedRequestMiddleware.ts
+++ packages/queued-request-controller/src/QueuedRequestMiddleware.ts
@@ -1,8 +1,9 @@
-import type { AddApprovalRequest, ApprovalRequest } from '@metamask/approval-controller';
+import type {
+  AddApprovalRequest,
+  ApprovalRequest,
+} from '@metamask/approval-controller';
 import type { ControllerMessenger } from '@metamask/base-controller';
 import type { InfuraNetworkType } from '@metamask/controller-utils';
-import { serializeError } from '@metamask/rpc-errors';
-
 import {
   ApprovalType,
   BUILT_IN_NETWORKS,
@@ -18,10 +19,10 @@ import type {
   NetworkControllerSetActiveNetworkAction,
   NetworkControllerSetProviderTypeAction,
 } from '@metamask/network-controller';
-import { CustomNetworkClientConfiguration } from '@metamask/network-controller/dist/types';
+import { serializeError } from '@metamask/rpc-errors';
 import type { SelectedNetworkControllerSetNetworkClientIdForDomainAction } from '@metamask/selected-network-controller';
 import { SelectedNetworkControllerActionTypes } from '@metamask/selected-network-controller';
-import { Json, JsonRpcParams, JsonRpcRequest, JsonRpcResponse } from '@metamask/utils';
+import type { Json, JsonRpcParams, JsonRpcRequest } from '@metamask/utils';
 
 import type { QueuedRequestControllerEnqueueRequestAction } from './QueuedRequestController';
 import { QueuedRequestControllerActionTypes } from './QueuedRequestController';
@@ -51,114 +52,117 @@ export const createQueuedRequestMiddleware = (
   >,
   useRequestQueue: () => boolean,
 ): JsonRpcMiddleware<JsonRpcParams, Json> => {
-  return createAsyncMiddleware(async (
-    req: JsonRpcRequest & {
-      origin?: string;
-      networkClientId?: NetworkClientId;
-    },
-    res,
-    next,
-  ) => {
-    const { origin, networkClientId } = req;
-
-    if (!origin) {
-      throw new Error("Request object is lacking an 'origin'");
-    }
-
-    if (!networkClientId) {
-      throw new Error("Request object is lacking a 'networkClientId'");
-    }
-
-    if (!useRequestQueue() || isConfirmationMethod(req.method)) {
-      next();
-      return;
-    }
-
-    const networkClientIdForRequest = req.networkClientId as NetworkClientId;
-
-    await messenger.call(
-      QueuedRequestControllerActionTypes.enqueueRequest,
-      async () => {
-        if (req.method === 'wallet_switchEthereumChain') {
-          // eslint-disable-next-line n/callback-return
-          return next();
-        }
-
-        const isBuiltIn = isNetworkType(networkClientIdForRequest);
-        let networkConfigurationForRequest;
-        if (isBuiltIn) {
-          const builtIn = BUILT_IN_NETWORKS[networkClientIdForRequest];
-          if (builtIn.chainId) { // only the infura provided ones have chainid (rpc doesnt)
-            networkConfigurationForRequest = builtIn;
-          }
-        }
-
-        networkConfigurationForRequest ??= messenger.call(
-          'NetworkController:getNetworkClientById',
-          networkClientIdForRequest,
-        ).configuration;
-
-        const currentProviderConfig = messenger.call(
-          'NetworkController:getState',
-        ).providerConfig;
-
-        const currentChainId = currentProviderConfig.chainId;
-        
-        // if the 'globally selected network' is already on the correct chain
-        // continue with the request as normal.
-        if (currentChainId === networkConfigurationForRequest.chainId) {
-          // eslint-disable-next-line n/callback-return
-          return next();
-        }
-        
-        // todo once we have 'batches':
-        // if is switch eth chain call
-        // clear request queue when the switch ethereum chain call completes (success, but maybe not if it fails?)
-        // This is because a dapp-requested switch ethereum chain invalidates any requests they've made after this switch, since we dont know if they were expecting the chain after the switch or before.
-        // with the queue batching approach, this would mean clearing any batch for that origin (batches being per-origin.)
-        const requestData = {
-          toNetworkConfiguration: networkConfigurationForRequest,
-          fromNetworkConfiguration: currentProviderConfig,
-        };
-
-        try {
-          const approvedRequestData = await messenger.call(
-            'ApprovalController:addRequest',
-            {
-              origin,
-              type: ApprovalType.SwitchEthereumChain,
-              requestData,
-            },
-            true,
-          ) as ApprovalRequest<typeof requestData> & {
-            type: InfuraNetworkType;
-          };;
-
-          if (isBuiltIn) {
-            await messenger.call(
-              'NetworkController:setProviderType',
-              approvedRequestData.type,
-            );
-          } else {
-            await messenger.call(
-              'NetworkController:setActiveNetwork',
-              approvedRequestData.id,
-            );
-          }
-
-          messenger.call(
-            SelectedNetworkControllerActionTypes.setNetworkClientIdForDomain,
-            origin,
-            networkClientIdForRequest,
-          );
-        } catch (error) {
-          res.error = serializeError(error); 
-          return error;
-        }
-
-        // eslint-disable-next-line n/callback-return
-        return next();
+  return createAsyncMiddleware(
+    async (
+      req: JsonRpcRequest & {
+        origin?: string;
+        networkClientId?: NetworkClientId;
       },
-    );
-  });
+      res,
+      next,
+    ) => {
+      const { origin, networkClientId } = req;
+
+      if (!origin) {
+        throw new Error("Request object is lacking an 'origin'");
+      }
+
+      if (!networkClientId) {
+        throw new Error("Request object is lacking a 'networkClientId'");
+      }
+
+      if (!useRequestQueue() || isConfirmationMethod(req.method)) {
+        next();
+        return;
+      }
+
+      const networkClientIdForRequest = req.networkClientId as NetworkClientId;
+
+      await messenger.call(
+        QueuedRequestControllerActionTypes.enqueueRequest,
+        async () => {
+          if (req.method === 'wallet_switchEthereumChain') {
+            // eslint-disable-next-line n/callback-return
+            return next();
+          }
+
+          const isBuiltIn = isNetworkType(networkClientIdForRequest);
+          let networkConfigurationForRequest;
+          if (isBuiltIn) {
+            const builtIn = BUILT_IN_NETWORKS[networkClientIdForRequest];
+            if (builtIn.chainId) {
+              // only the infura provided ones have chainid (rpc doesnt)
+              networkConfigurationForRequest = builtIn;
+            }
+          }
+
+          networkConfigurationForRequest ??= messenger.call(
+            'NetworkController:getNetworkClientById',
+            networkClientIdForRequest,
+          ).configuration;
+
+          const currentProviderConfig = messenger.call(
+            'NetworkController:getState',
+          ).providerConfig;
+
+          const currentChainId = currentProviderConfig.chainId;
+
+          // if the 'globally selected network' is already on the correct chain
+          // continue with the request as normal.
+          if (currentChainId === networkConfigurationForRequest.chainId) {
+            // eslint-disable-next-line n/callback-return
+            return next();
+          }
+
+          // todo once we have 'batches':
+          // if is switch eth chain call
+          // clear request queue when the switch ethereum chain call completes (success, but maybe not if it fails?)
+          // This is because a dapp-requested switch ethereum chain invalidates any requests they've made after this switch, since we dont know if they were expecting the chain after the switch or before.
+          // with the queue batching approach, this would mean clearing any batch for that origin (batches being per-origin.)
+          const requestData = {
+            toNetworkConfiguration: networkConfigurationForRequest,
+            fromNetworkConfiguration: currentProviderConfig,
+          };
+
+          try {
+            const approvedRequestData = (await messenger.call(
+              'ApprovalController:addRequest',
+              {
+                origin,
+                type: ApprovalType.SwitchEthereumChain,
+                requestData,
+              },
+              true,
+            )) as ApprovalRequest<typeof requestData> & {
+              type: InfuraNetworkType;
+            };
+
+            if (isBuiltIn) {
+              await messenger.call(
+                'NetworkController:setProviderType',
+                approvedRequestData.type,
+              );
+            } else {
+              await messenger.call(
+                'NetworkController:setActiveNetwork',
+                approvedRequestData.id,
+              );
+            }
+
+            messenger.call(
+              SelectedNetworkControllerActionTypes.setNetworkClientIdForDomain,
+              origin,
+              networkClientIdForRequest,
+            );
+          } catch (error) {
+            res.error = serializeError(error);
+            return error;
+          }
+
+          // eslint-disable-next-line n/callback-return
+          return next();
+        },
+      );
+    },
+  );
 };
