ebd05df3cc3cdf7b649f24750a77d4e074765503
BelfordZ <belfordz66@gmail.com>
2023-10-23 11:45:20 -0700

    Revert "fix linting errors"
    This reverts commit 08948c8d4e71b291ec76572efda5c19eee9a4af3.

--- packages/queued-request-controller/src/QueuedRequestMiddleware.ts
+++ packages/queued-request-controller/src/QueuedRequestMiddleware.ts
@@ -1,4 +1,8 @@
-import type { ApprovalRequest } from '@metamask/approval-controller';
+import type {
+  AddApprovalRequest,
+  ApprovalRequest,
+} from '@metamask/approval-controller';
+import type { ControllerMessenger } from '@metamask/base-controller';
 import type { InfuraNetworkType } from '@metamask/controller-utils';
 import {
   ApprovalType,
@@ -7,12 +11,20 @@ import {
 } from '@metamask/controller-utils';
 import type { JsonRpcMiddleware } from '@metamask/json-rpc-engine';
 import { createAsyncMiddleware } from '@metamask/json-rpc-engine';
-import type { NetworkClientId } from '@metamask/network-controller';
+import type {
+  NetworkClientId,
+  NetworkControllerFindNetworkClientIdByChainIdAction,
+  NetworkControllerGetNetworkClientByIdAction,
+  NetworkControllerGetStateAction,
+  NetworkControllerSetActiveNetworkAction,
+  NetworkControllerSetProviderTypeAction,
+} from '@metamask/network-controller';
 import { serializeError } from '@metamask/rpc-errors';
+import type { SelectedNetworkControllerSetNetworkClientIdForDomainAction } from '@metamask/selected-network-controller';
 import { SelectedNetworkControllerActionTypes } from '@metamask/selected-network-controller';
 import type { Json, JsonRpcParams, JsonRpcRequest } from '@metamask/utils';
 
-import type { QueuedRequestControllerMessenger } from './QueuedRequestController';
+import type { QueuedRequestControllerEnqueueRequestAction } from './QueuedRequestController';
 import { QueuedRequestControllerActionTypes } from './QueuedRequestController';
 
 const isConfirmationMethod = (method: string) => {
