9b4792978a21f1c93740cd4f4442fce35c973164
Jongsun Suh <34228073+MajorLift@users.noreply.github.com>
2023-10-23 18:51:12 -0700

    Improved typing for `deriveStateFromMetadata`

--- packages/base-controller/src/BaseControllerV2.ts
+++ packages/base-controller/src/BaseControllerV2.ts
@@ -247,29 +247,30 @@ function deriveStateFromMetadata<S extends Record<string, Json>>(
   metadata: StateMetadata<S>,
   metadataProperty: 'anonymous' | 'persist',
 ): Record<string, Json> {
-  return Object.keys(state).reduce((persistedState, key) => {
-    try {
-      const stateMetadata = metadata[key as keyof S];
-      if (!stateMetadata) {
-        throw new Error(`No metadata found for '${key}'`);
+  return Object.keys(state).reduce(
+    (persistedState: Record<string, Json>, key: keyof S) => {
+      try {
+        const stateMetadata = metadata[key];
+        if (!stateMetadata) {
+          throw new Error(`No metadata found for '${String(key)}'`);
+        }
+        const propertyMetadata = stateMetadata[metadataProperty];
+        const stateProperty = state[key];
+        if (typeof propertyMetadata === 'function') {
+          persistedState[String(key)] = propertyMetadata(stateProperty);
+        } else if (propertyMetadata) {
+          persistedState[String(key)] = stateProperty;
+        }
+        return persistedState;
+      } catch (error) {
+        // Throw error after timeout so that it is captured as a console error
+        // (and by Sentry) without interrupting state-related operations
+        setTimeout(() => {
+          throw error;
+        });
+        return persistedState;
       }
-      const propertyMetadata = stateMetadata[metadataProperty];
-      const stateProperty = state[key];
-      if (typeof propertyMetadata === 'function') {
-        persistedState[key as string] = propertyMetadata(
-          stateProperty as S[keyof S],
-        );
-      } else if (propertyMetadata) {
-        persistedState[key as string] = stateProperty;
-      }
-      return persistedState;
-    } catch (error) {
-      // Throw error after timeout so that it is captured as a console error
-      // (and by Sentry) without interrupting state-related operations
-      setTimeout(() => {
-        throw error;
-      });
-      return persistedState;
-    }
-  }, {} as Record<string, Json>);
+    },
+    {},
+  );
 }
