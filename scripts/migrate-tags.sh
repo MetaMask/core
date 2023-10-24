e984edc8aca45c26114c1350be8a90cf0e00c1a3
Jongsun Suh <34228073+MajorLift@users.noreply.github.com>
2023-10-24 16:03:09 -0700

    Refactor to accept named arguments and handle tag prefix change for renamed packages

--- scripts/migrate-tags.sh
+++ scripts/migrate-tags.sh
@@ -1,27 +1,68 @@
 #!/usr/bin/env bash
 
-package_name="$1"
-remote=${2:-'origin'}
-release_commits_regex=${3:-'^\d\{1,3\}\.\d\{1,3\}\.\d\{1,3\}'}
+source "$PWD/scripts/semver.sh"
+
+remote='origin'
+release_commits_regex='^\d\{1,3\}\.\d\{1,3\}\.\d\{1,3\}'
+version_before_package_rename='99999.0.0'
+tag_prefix_before_package_rename="$1"
+
+while [[ $# -gt 0 ]]; do
+  key="$1"
+
+  case $key in
+  -r | --remote)
+    remote="$2"
+    shift # past argument
+    shift # past value
+    ;;
+  -p | --regex-pattern)
+    release_commits_regex="$2"
+    shift # past argument
+    shift # past value
+    ;;
+  -v | --version-before-package-rename)
+    version_before_package_rename="$2"
+    shift # past argument
+    shift # past value
+    ;;
+  -t | --tag-prefix-before-package-rename)
+    tag_prefix_before_package_rename="$2"
+    shift # past argument
+    shift # past value
+    ;;
+  *) # package name
+    package_name="$1"
+    shift # past argument
+    ;;
+  esac
+done
 
 get-version-commit-pairs() {
-  for log in "$(git log --oneline --grep $release_commits_regex merged-packages/$package_name)"; do
-    echo "$log" | cut -d' ' -f1,2 | sed 's/\([^ ]*\)\([ ]*\) \([^ ]*\)\([ ]*\)/\3\4 \1\2/'
-  done
+  while read -r log; do
+    commit="$(echo "$log" | cut -d' ' -f1)"
+    version="$(echo "$log" | cut -d' ' -f2)"
+    echo "$version $commit"
+  done <<<"$(git log --oneline --grep $release_commits_regex merged-packages/$package_name)"
 }
 
-prepend-package-name() {
-  for pair in "$(get-version-commit-pairs)"; do
-    echo "$pair" | sed "s/\([^ ]*\)\([ ]*\) \([^ ]*\)\([ ]*\)/@metamask\/$package_name@\1\2 \3\4/"
-  done
+prepend-tag-name() {
+  while read -r pair; do
+    version="$(echo "$pair" | cut -d' ' -f1)"
+    commit="$(echo "$pair" | cut -d' ' -f2)"
+    if semverLT "$version" "$version_before_package_rename" || semverEQ "$version" "$version_before_package_rename"; then
+      tag_name="$tag_prefix_before_package_rename@$version"
+    else
+      tag_name="@metamask/$package_name@$version"
+    fi
+    echo "$tag_name $commit"
+  done <<<"$(get-version-commit-pairs)"
 }
 
-tag_commit_pairs=$(prepend-package-name)
-
-for pair in "$tag_commit_pairs"; do
-  echo "$pair" | xargs -n 2 bash -c 'git tag "$0" "$1"'
-done
-
-for pair in "$tag_commit_pairs"; do
-  echo "$pair" | cut -d' ' -f1 | xargs -n 1 bash -c 'git push '"$remote"' "$0"'
-done
+while read -r pair; do
+  tag="$(echo "$pair" | cut -d' ' -f1)"
+  commit="$(echo "$pair" | cut -d' ' -f2)"
+  # echo "$tag $commit"
+  git tag "$tag" "$commit"
+  git push "$remote" "$tag"
+done <<<"$(prepend-tag-name)"
