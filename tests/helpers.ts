fb37dec49341799861b403e17a44d89831bb86b4
Alex <adonesky@gmail.com>
2023-11-02 14:40:49 -0500

    add back overwritten comment + fix typo

--- tests/helpers.ts
+++ tests/helpers.ts
@@ -9,24 +9,25 @@ const flushPromises = () => {
 
 /**
  * Advances the provided fake timer by a specified duration in incremental steps.
- * Between each step, any enqueued promises are processed. This function is especially
- * useful for ensuring that chained promises and timers are fully processed within tests.
+ * Between each step, any enqueued promises are processed. Fake timers in testing libraries
+ * allow simulation of time without actually waiting. However, they don't always account for
+ * promises or other asynchronous operations that may get enqueued during the timer's duration.
+ * By advancing time in incremental steps and flushing promises between each step,
+ * this function ensures that both timers and promises are comprehensively processed.
  * @param options - The options object.
  * @param options.clock - The Sinon fake timer instance used to manipulate time in tests.
  * @param options.duration - The total amount of time (in milliseconds) to advance the timer by.
- * @param options.stepSize - The incremental step size (in milliseconds) by which the timer is advanced in each iteration. Default is 2000ms.
+ * @param options.stepSize - The incremental step size (in milliseconds) by which the timer is advanced in each iteration. Default is 1/4 of the duration.
  */
 export async function advanceTime({
   clock,
   duration,
-  stepSize,
+  stepSize = duration / 4,
 }: {
   clock: sinon.SinonFakeTimers;
   duration: number;
   stepSize?: number;
 }): Promise<void> {
-  // if stepSize is not provided, default to 1/4 of the duration
-  stepSize ??= duration / 4;
   do {
     await clock.tickAsync(stepSize);
     await flushPromises();
