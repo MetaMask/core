3be295a73dabeb7631e9b41f783c030bd6278a0e
Jyoti Puri <jyotipuri@gmail.com>
2023-11-03 19:58:37 +0530

    update

--- packages/transaction-controller/jest.config.js
+++ packages/transaction-controller/jest.config.js
@@ -17,10 +17,10 @@ module.exports = merge(baseConfig, {
   // An object that configures minimum threshold enforcement for coverage results
   coverageThreshold: {
     global: {
-      branches: 89.02,
+      branches: 89.11,
       functions: 94.76,
-      lines: 98.49,
-      statements: 98.5,
+      lines: 98.56,
+      statements: 98.58,
     },
   },
 
