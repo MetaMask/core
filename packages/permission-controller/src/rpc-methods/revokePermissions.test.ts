b98cf863a395120f3bebf12ca74bc4578ac9cb78
Shane Jonas <jonas.shane@gmail.com>
2023-11-20 12:11:30 -0500

    Added more test coverage for revokePermissions rpc method

--- packages/permission-controller/src/rpc-methods/revokePermissions.test.ts
+++ packages/permission-controller/src/rpc-methods/revokePermissions.test.ts
@@ -64,6 +64,67 @@ describe('revokePermissions RPC method', () => {
     expect(mockRevokePermissionsForOrigin).not.toHaveBeenCalled();
   });
 
+  it('returns an error if the permissionKeys is a plain object', async () => {
+    const { implementation } = revokePermissionsHandler;
+    const mockRevokePermissionsForOrigin = jest.fn();
+
+    const engine = new JsonRpcEngine();
+    engine.push((req, res, next, end) =>
+      implementation(req as any, res as any, next, end, {
+        revokePermissionsForOrigin: mockRevokePermissionsForOrigin,
+      }),
+    );
+
+    const req = {
+      jsonrpc: '2.0',
+      id: 1,
+      method: 'wallet_revokePermissions',
+      params: [{}],
+    };
+
+    const expectedError = rpcErrors
+      .invalidParams({
+        data: { request: { ...req } },
+      })
+      .serialize();
+    delete expectedError.stack;
+
+    const response: any = await engine.handle(req as any);
+    delete response.error.stack;
+    expect(response.error).toStrictEqual(expectedError);
+    expect(mockRevokePermissionsForOrigin).not.toHaveBeenCalled();
+  });
+
+  it('returns an error if the params are not set', async () => {
+    const { implementation } = revokePermissionsHandler;
+    const mockRevokePermissionsForOrigin = jest.fn();
+
+    const engine = new JsonRpcEngine();
+    engine.push((req, res, next, end) =>
+      implementation(req as any, res as any, next, end, {
+        revokePermissionsForOrigin: mockRevokePermissionsForOrigin,
+      }),
+    );
+
+    const req = {
+      jsonrpc: '2.0',
+      id: 1,
+      method: 'wallet_revokePermissions',
+    };
+
+    const expectedError = rpcErrors
+      .invalidParams({
+        data: { request: { ...req } },
+      })
+      .serialize();
+    delete expectedError.stack;
+
+    const response: any = await engine.handle(req as any);
+    delete response.error.stack;
+    expect(response.error).toStrictEqual(expectedError);
+    expect(mockRevokePermissionsForOrigin).not.toHaveBeenCalled();
+  });
+
   it('returns an error if the request params is an empty array', async () => {
     const { implementation } = revokePermissionsHandler;
     const mockRevokePermissionsForOrigin = jest.fn();
