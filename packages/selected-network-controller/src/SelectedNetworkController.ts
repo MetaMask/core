1a83ed0fcd133fce53a7d38cad3b752b692e30a6
BelfordZ <belfordz66@gmail.com>
2023-11-28 14:05:50 -0800

    allow initial state to be passed into the constructor

--- packages/selected-network-controller/src/SelectedNetworkController.ts
+++ packages/selected-network-controller/src/SelectedNetworkController.ts
@@ -86,6 +86,7 @@ export type SelectedNetworkControllerMessenger = RestrictedControllerMessenger<
 >;
 
 export type SelectedNetworkControllerOptions = {
+  state?: SelectedNetworkControllerState;
   messenger: SelectedNetworkControllerMessenger;
 };
 
@@ -110,12 +111,12 @@ export class SelectedNetworkController extends BaseController<
    * @param options - The controller options.
    * @param options.messenger - The restricted controller messenger for the EncryptionPublicKey controller.
    */
-  constructor({ messenger }: SelectedNetworkControllerOptions) {
+  constructor({ messenger, state = getDefaultState() }: SelectedNetworkControllerOptions) {
     super({
       name: controllerName,
       metadata: stateMetadata,
       messenger,
-      state: getDefaultState(),
+      state
     });
     this.#registerMessageHandlers();
   }
