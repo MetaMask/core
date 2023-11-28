3492ac88ffe42bafa79508a9de5378ca6bf7c5d0
Matthew Walsh <matthew.walsh@consensys.net>
2023-11-28 15:01:07 +0000

    Support custom nonce value (#3579)
    Encapsulate all nonce logic in a new file with additional logging.
    Update transaction metadata after approval if the approval result includes the value.txMeta property.
    Add type to incoming transactions.

--- packages/transaction-controller/jest.config.js
+++ packages/transaction-controller/jest.config.js
@@ -17,10 +17,10 @@ module.exports = merge(baseConfig, {
   // An object that configures minimum threshold enforcement for coverage results
   coverageThreshold: {
     global: {
-      branches: 88.78,
-      functions: 94.16,
-      lines: 98.07,
-      statements: 98.03,
+      branches: 89.18,
+      functions: 94.18,
+      lines: 98.09,
+      statements: 98.05,
     },
   },
 
