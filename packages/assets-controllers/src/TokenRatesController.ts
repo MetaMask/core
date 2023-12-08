a20836b81b7ad33224f48beba5a92ff109edecab
Elliot Winkler <elliot.winkler@gmail.com>
2023-12-08 14:57:51 -0700

    Prevent redundant token rate updates
    The token rates controller fetches rates for certain collections of
    tokens obtained via the tokens controller. If any of these collections
    change, then new rates need to be fetched.
    
    Unfortunately, because the check to determine whether these collections
    have changed is simplistic, new rates are fetched every single time the
    tokens controller state is changed.
    
    To address this, we can consider two things:
    
    - The token rates controller only uses the addresses of tokens in these
      collections to fetch rates, and it also normalizes those addresses
      when updating state.
    - These collections of tokens may contain duplicates.
    
    So to reduce the number of times rates are fetched and to optimize each
    request, this commit updates the tokens state callback to assemble the
    aforementioned token collections into a deduplicated, normalized list of
    token addresses, and then it checks to see if this list has any changes
    to know whether or not to re-fetch rates.

--- packages/assets-controllers/src/TokenRatesController.ts
+++ packages/assets-controllers/src/TokenRatesController.ts
@@ -13,6 +13,7 @@ import type {
 import { PollingControllerV1 } from '@metamask/polling-controller';
 import type { PreferencesState } from '@metamask/preferences-controller';
 import type { Hex } from '@metamask/utils';
+import { isDeepStrictEqual } from 'util';
 
 import { fetchExchangeRate as fetchNativeCurrencyExchangeRate } from './crypto-compare';
 import type { AbstractTokenPricesService } from './token-prices-service/abstract-token-prices-service';
@@ -237,15 +238,16 @@ export class TokenRatesController extends PollingControllerV1<
     });
 
     onTokensStateChange(async ({ allTokens, allDetectedTokens }) => {
-      // These two state properties are assumed to be immutable
+      const previousTokenAddresses = this.#getTokenAddresses(
+        this.config.chainId,
+      );
+      this.configure({ allTokens, allDetectedTokens });
+      const newTokenAddresses = this.#getTokenAddresses(this.config.chainId);
       if (
-        this.config.allTokens !== allTokens ||
-        this.config.allDetectedTokens !== allDetectedTokens
+        !isDeepStrictEqual(previousTokenAddresses, newTokenAddresses) &&
+        this.#pollState === PollState.Active
       ) {
-        this.configure({ allTokens, allDetectedTokens });
-        if (this.#pollState === PollState.Active) {
-          await this.updateExchangeRates();
-        }
+        await this.updateExchangeRates();
       }
     });
 
@@ -276,7 +278,13 @@ export class TokenRatesController extends PollingControllerV1<
     const detectedTokens =
       allDetectedTokens[chainId]?.[this.config.selectedAddress] || [];
 
-    return [...tokens, ...detectedTokens].map((token) => toHex(token.address));
+    return [
+      ...new Set(
+        [...tokens, ...detectedTokens].map((token) =>
+          toHex(toChecksumHexAddress(token.address)),
+        ),
+      ),
+    ].sort();
   }
 
   /**
@@ -482,7 +490,7 @@ export class TokenRatesController extends PollingControllerV1<
       (obj, [tokenContractAddress, tokenPrice]) => {
         return {
           ...obj,
-          [toChecksumHexAddress(tokenContractAddress)]: tokenPrice.value,
+          [tokenContractAddress]: tokenPrice.value,
         };
       },
       {},
@@ -531,7 +539,7 @@ export class TokenRatesController extends PollingControllerV1<
       (obj, [tokenContractAddress, tokenPrice]) => {
         return {
           ...obj,
-          [toChecksumHexAddress(tokenContractAddress)]:
+          [tokenContractAddress]:
             tokenPrice.value * fallbackCurrencyToNativeCurrencyConversionRate,
         };
       },
