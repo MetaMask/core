dd0e83ed9c38e0abce20d0d2e20c0fd0ec6770c8
Matthew Walsh <matthew.walsh@consensys.net>
2023-12-11 20:57:35 +0000

    Monitor pending user operations (#3622)
    Add the PendingUserOperationTracker helper.
    Include multi-chain support using the PollingController.

--- packages/user-operation-controller/jest.config.js
+++ packages/user-operation-controller/jest.config.js
@@ -17,10 +17,10 @@ module.exports = merge(baseConfig, {
   // An object that configures minimum threshold enforcement for coverage results
   coverageThreshold: {
     global: {
-      branches: 0,
-      functions: 0,
-      lines: 0,
-      statements: 0,
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100,
     },
   },
 });
