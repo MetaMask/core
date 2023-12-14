3e1416de48c225f000357fb2c84ca2e1ef03c8d4
Jongsun Suh <jongsun.suh@icloud.com>
2023-12-13 19:26:41 -0800

    Revert "Add `#networkClientId` class field and update using `seletedNetworkClientId` in `networkDidChange` listener"
    This reverts commit a41a218e9c84b1bf337cebf7283889c56d3774cb.

--- packages/assets-controllers/src/TokenDetectionController.ts
+++ packages/assets-controllers/src/TokenDetectionController.ts
@@ -83,8 +83,6 @@ export class TokenDetectionController extends StaticIntervalPollingController<
 
   #selectedAddress: string;
 
-  #networkClientId: NetworkClientId;
-
   #isDetectionEnabledFromPreferences: boolean;
 
   #isDetectionEnabledForNetwork: boolean;
@@ -148,7 +146,6 @@ export class TokenDetectionController extends StaticIntervalPollingController<
 
     this.setIntervalLength(interval);
     this.#chainId = chainId;
-    this.#networkClientId = chainId;
     this.#selectedAddress = selectedAddress;
     this.#isDetectionEnabledFromPreferences = defaultUseTokenDetection;
     this.#isDetectionEnabledForNetwork = isTokenDetectionSupportedForNetwork(
@@ -191,17 +188,12 @@ export class TokenDetectionController extends StaticIntervalPollingController<
 
     this.messagingSystem.subscribe(
       'NetworkController:networkDidChange',
-      async ({
-        providerConfig: { chainId: newChainId },
-        selectedNetworkClientId,
-      }) => {
-        const isChainIdChanged = this.#chainId !== newChainId;
-        this.#chainId = newChainId;
-
-        this.#networkClientId = selectedNetworkClientId;
-
+      async ({ providerConfig: { chainId: newChainId } }) => {
         this.#isDetectionEnabledForNetwork =
           isTokenDetectionSupportedForNetwork(newChainId);
+        const isChainIdChanged = this.#chainId !== newChainId;
+
+        this.#chainId = newChainId;
 
         if (this.#isDetectionEnabledForNetwork && isChainIdChanged) {
           await this.detectTokens();
