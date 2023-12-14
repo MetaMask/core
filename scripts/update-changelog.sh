df12ef2e8b88aaae11dd1a8aa6b52d98193d03a8
Salah-Eddine Saakoun <salah-eddine.saakoun@consensys.net>
2023-12-14 16:33:56 +0100

    fix typos

--- scripts/update-changelog.sh
+++ scripts/update-changelog.sh
@@ -2,29 +2,28 @@
 
 set -euo pipefail
 
-# Check if the current directory is a Git repository
+# Check if the current directory is a git repository
 if ! git rev-parse --git-dir >/dev/null 2>&1; then
   echo "Not in a git repository."
   exit 1
 fi
 
-# Get the current Git branch
+# Get the current git branch
 branch=$(git rev-parse --abbrev-ref HEAD)
 
-# Check if if we are not i a release branch
+# Check if current branch is not a release branch
 if [[ ! $branch =~ ^release/ ]]; then
   echo "Not in a release branch."
   exit 1
-else
-  # Get the current package name
-  if [[ $# -eq 0 ]]; then
-    echo "Missing package name."
-    exit 1
-  fi
-
-  package_name="$1"
-  shift # remove package name from arguments
-
-  yarn auto-changelog update --tag-prefix "${package_name}@" --rc "$@"
-
 fi
+
+# Get the current package name
+if [[ $# -eq 0 ]]; then
+  echo "Missing package name."
+  exit 1
+fi
+
+package_name="$1"
+shift # remove package name from arguments
+
+yarn auto-changelog update --tag-prefix "${package_name}@" --rc "$@"
