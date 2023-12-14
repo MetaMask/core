58fd974c63db40e9d81203240c44f015e1b68ab1
Salah-Eddine Saakoun <salah-eddine.saakoun@consensys.net>
2023-12-14 22:59:56 +0100

    Add changelog:update to constraints

--- scripts/update-changelog.sh
+++ scripts/update-changelog.sh
@@ -2,21 +2,6 @@
 
 set -euo pipefail
 
-# Check if the current directory is a git repository
-if ! git rev-parse --git-dir >/dev/null 2>&1; then
-  echo "Not in a git repository."
-  exit 1
-fi
-
-# Get the current git branch
-branch=$(git rev-parse --abbrev-ref HEAD)
-
-# Check if current branch is not a release branch
-if [[ ! $branch =~ ^release/ ]]; then
-  echo "Not in a release branch."
-  exit 1
-fi
-
 # Get the current package name
 if [[ $# -eq 0 ]]; then
   echo "Missing package name."
@@ -26,4 +11,11 @@ fi
 package_name="$1"
 shift # remove package name from arguments
 
-yarn auto-changelog update --tag-prefix "${package_name}@" --rc "$@"
+# Get the current git branch
+branch=$(git rev-parse --abbrev-ref HEAD)
+
+if [[ $branch =~ "^release/" ]]; then
+  yarn auto-changelog update --tag-prefix "${package_name}@" --rc "$@"
+else
+  yarn auto-changelog update --tag-prefix "${package_name}@" "$@"
+fi
