89654542c9c61308cfad6a310f7fe2b4b669117b
Matthew Walsh <matthew.walsh@consensys.net>
2023-12-19 11:42:23 +0000

    Fix transaction status incorrectly set to dropped, submitted, or failed (#3676)
    When checking if pending transactions have been dropped in the
    `PendingTransactionTracker`, look at wallet transactions only from the
    current chain.
    Check if the block tracker polling should be started or stopped when
    the network changes, in addition to when the transaction controller
    state changes.
    Fix R, S, and V update to ensure zero values are recorded.
    Ignore transaction receipts with a `null` or invalid `status`.

--- packages/transaction-controller/src/TransactionController.ts
+++ packages/transaction-controller/src/TransactionController.ts
@@ -223,7 +223,7 @@ export class TransactionController extends BaseControllerV1<
   TransactionConfig,
   TransactionState
 > {
-  private ethQuery: EthQuery;
+  private readonly ethQuery: EthQuery;
 
   private readonly isHistoryDisabled: boolean;
 
@@ -237,7 +237,7 @@ export class TransactionController extends BaseControllerV1<
 
   // TODO: Replace `any` with type
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  private registry: any;
+  private readonly registry: any;
 
   private readonly provider: Provider;
 
@@ -540,7 +540,11 @@ export class TransactionController extends BaseControllerV1<
       getTransactions: () => this.state.transactions,
       isResubmitEnabled: pendingTransactions.isResubmitEnabled,
       nonceTracker: this.nonceTracker,
-      onStateChange: this.subscribe.bind(this),
+      onStateChange: (listener) => {
+        this.subscribe(listener);
+        onNetworkStateChange(listener);
+        listener();
+      },
       publishTransaction: this.publishTransaction.bind(this),
       hooks: {
         beforeCheckPendingTransaction:
@@ -552,8 +556,7 @@ export class TransactionController extends BaseControllerV1<
     this.addPendingTransactionTrackerListeners();
 
     onNetworkStateChange(() => {
-      this.ethQuery = new EthQuery(this.provider);
-      this.registry = new MethodRegistry({ provider: this.provider });
+      log('Detected network change', this.getChainId());
       this.onBootCleanup();
     });
 
@@ -2430,11 +2433,14 @@ export class TransactionController extends BaseControllerV1<
     const transactionMeta = this.getTransaction(transactionId);
     const nonce = transactionMeta?.txParams?.nonce;
     const from = transactionMeta?.txParams?.from;
+
     const sameNonceTxs = this.state.transactions.filter(
       (transaction) =>
+        transaction.id !== transactionId &&
         transaction.txParams.from === from &&
         transaction.txParams.nonce === nonce &&
-        transaction.chainId === chainId,
+        transaction.chainId === chainId &&
+        transaction.type !== TransactionType.incoming,
     );
 
     if (!sameNonceTxs.length) {
@@ -2443,9 +2449,6 @@ export class TransactionController extends BaseControllerV1<
 
     // Mark all same nonce transactions as dropped and give it a replacedBy hash
     for (const transaction of sameNonceTxs) {
-      if (transaction.id === transactionId) {
-        continue;
-      }
       transaction.replacedBy = transactionMeta?.hash;
       transaction.replacedById = transactionMeta?.id;
       // Drop any transaction that wasn't previously failed (off chain failure)
@@ -2505,16 +2508,14 @@ export class TransactionController extends BaseControllerV1<
     transactionMeta: TransactionMeta,
     signedTx: TypedTransaction,
   ): Promise<void> {
-    if (signedTx.r) {
-      transactionMeta.r = addHexPrefix(signedTx.r.toString(16));
-    }
+    for (const key of ['r', 's', 'v'] as const) {
+      const value = signedTx[key];
 
-    if (signedTx.s) {
-      transactionMeta.s = addHexPrefix(signedTx.s.toString(16));
-    }
+      if (value === undefined || value === null) {
+        continue;
+      }
 
-    if (signedTx.v) {
-      transactionMeta.v = addHexPrefix(signedTx.v.toString(16));
+      transactionMeta[key] = addHexPrefix(value.toString(16));
     }
   }
 
