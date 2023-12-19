c8c35ef45de12429028b36474ad4eb5c19f67f47
Matthew Walsh <matthew.walsh@consensys.net>
2023-12-19 15:29:56 +0000

    Remove unnecessary changes

--- packages/polling-controller/src/index.ts
+++ packages/polling-controller/src/index.ts
@@ -1,26 +1,13 @@
-import {
-  StaticIntervalPollingControllerOnly,
-  StaticIntervalPollingController,
-  StaticIntervalPollingControllerV1,
-} from './StaticIntervalPollingController';
-
 export {
   BlockTrackerPollingControllerOnly,
   BlockTrackerPollingController,
   BlockTrackerPollingControllerV1,
 } from './BlockTrackerPollingController';
 
-const PollingControllerOnly = StaticIntervalPollingControllerOnly;
-const PollingController = StaticIntervalPollingController;
-const PollingControllerV1 = StaticIntervalPollingControllerV1;
-
 export {
   StaticIntervalPollingControllerOnly,
   StaticIntervalPollingController,
   StaticIntervalPollingControllerV1,
-  PollingControllerOnly,
-  PollingController,
-  PollingControllerV1,
-};
+} from './StaticIntervalPollingController';
 
 export type { IPollingController } from './AbstractPollingController';
