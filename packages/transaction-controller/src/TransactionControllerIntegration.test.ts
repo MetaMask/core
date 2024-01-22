a3007d9826c6ee230ded353f5e5ce555d1a83af4
Jiexi Luan <jiexiluan@gmail.com>
2024-01-22 15:07:32 -0800

    Fix submitted state

--- packages/transaction-controller/src/TransactionControllerIntegration.test.ts
+++ packages/transaction-controller/src/TransactionControllerIntegration.test.ts
@@ -98,7 +98,8 @@ describe('TransactionController Integration', () => {
       mockNetwork({
         networkClientConfiguration: mainnetNetworkClientConfiguration,
         mocks: [
-          // BlockTracker start
+          // NetworkController
+          // BlockTracker
           {
             request: {
               method: 'eth_blockNumber',
@@ -121,7 +122,8 @@ describe('TransactionController Integration', () => {
         mockNetwork({
           networkClientConfiguration: mainnetNetworkClientConfiguration,
           mocks: [
-            // BlockTracker start
+            // NetworkController
+            // BlockTracker
             {
               request: {
                 method: 'eth_blockNumber',
@@ -136,7 +138,8 @@ describe('TransactionController Integration', () => {
         mockNetwork({
           networkClientConfiguration,
           mocks: [
-            // BlockTracker start
+            // NetworkController
+            // BlockTracker
             {
               request: {
                 method: 'eth_blockNumber',
@@ -155,7 +158,7 @@ describe('TransactionController Integration', () => {
               },
               response: {
                 result:
-                  '0x', // non contract address
+                  '0x', // non contract
               },
             },
             // getSuggestedGasFees
@@ -188,6 +191,8 @@ describe('TransactionController Integration', () => {
         mockNetwork({
           networkClientConfiguration: mainnetNetworkClientConfiguration,
           mocks: [
+            // NetworkController
+            // BlockTracker
             {
               request: {
                 method: 'eth_blockNumber',
@@ -197,35 +202,13 @@ describe('TransactionController Integration', () => {
                 result: '0x3b3301',
               },
             },
-            {
-              request: {
-                method: 'eth_getBlockByNumber',
-                params: ['0x3b3301'],
-              },
-              response: {
-                result: {
-                  baseFeePerGas: '0x63c498a46',
-                  number: '0x3b3301',
-                },
-              },
-            },
-            {
-              request: {
-                method: 'eth_getTransactionCount',
-                params: [
-                  '0x6bf137f335ea1b8f193b8f6ea92561a60d23a207',
-                  '0x3b3301',
-                ],
-              },
-              response: {
-                result: '0x1',
-              },
-            },
           ],
         });
         mockNetwork({
           networkClientConfiguration,
           mocks: [
+            // NetworkController
+            // BlockTracker
             {
               request: {
                 method: 'eth_blockNumber',
@@ -235,26 +218,7 @@ describe('TransactionController Integration', () => {
                 result: '0x1',
               },
             },
-            {
-              request: {
-                method: 'eth_blockNumber',
-                params: [],
-              },
-              response: {
-                result: '0x2',
-              },
-            },
-            {
-              request: {
-                method: 'eth_getCode',
-                params: [ACCOUNT_2_MOCK, '0x1'],
-              },
-              response: {
-                result:
-                  // what should this be?
-                  '0x00000000000000000000000000000000000000000000000000000000000000200000000000000000000000000000000000000000000000000000000000000024657468546f546f6b656e53776170496e7075742875696e743235362c75696e743235362900000000000000000000000000000000000000000000000000000000',
-              },
-            },
+            // NetworkController
             {
               request: {
                 method: 'eth_getBlockByNumber',
@@ -267,15 +231,29 @@ describe('TransactionController Integration', () => {
                 },
               },
             },
+            // BlockTracker
             {
               request: {
-                method: 'eth_gasPrice',
+                method: 'eth_blockNumber',
                 params: [],
               },
               response: {
-                result: '0x1',
+                result: '0x2',
               },
             },
+            // readAddressAsContract
+            // requiresFixedGas (cached)
+            {
+              request: {
+                method: 'eth_getCode',
+                params: [ACCOUNT_2_MOCK, '0x1'],
+              },
+              response: {
+                result:
+                  '0x' // non contract
+              },
+            },
+            // estimateGas
             {
               request: {
                 method: 'eth_estimateGas',
@@ -292,6 +270,17 @@ describe('TransactionController Integration', () => {
                 result: '0x1',
               },
             },
+            // getSuggestedGasFees
+            {
+              request: {
+                method: 'eth_gasPrice',
+                params: [],
+              },
+              response: {
+                result: '0x1',
+              },
+            },
+            // NonceTracker
             {
               request: {
                 method: 'eth_getTransactionCount',
@@ -301,11 +290,12 @@ describe('TransactionController Integration', () => {
                 result: '0x1',
               },
             },
+            // publishTransaction
             {
               request: {
                 method: 'eth_sendRawTransaction',
                 params: [
-                  '0x02e005010101019408f137f335ea1b8f193b8f6ea92561a60d23a2118080c0808080',
+                  '0x02e2050101018252089408f137f335ea1b8f193b8f6ea92561a60d23a2118080c0808080',
                 ],
               },
               response: {
