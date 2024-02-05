d654b0bb7409ea818d0ca5b0d2ea19299bf03eb3
Jiexi Luan <jiexiluan@gmail.com>
2024-02-05 12:07:49 -0800

    Revert "address trackingMap clarity comment"
    This reverts commit 6dc68ba8e399ccc268e2e54fe47ff011ebd74d76.

--- packages/transaction-controller/src/TransactionController.ts
+++ packages/transaction-controller/src/TransactionController.ts
@@ -876,10 +876,8 @@ export class TransactionController extends BaseControllerV1<
   stopAllIncomingTransactionPolling() {
     this.incomingTransactionHelper.stop();
     if (this.#enableMultichain) {
-      for (const { incomingTransactionHelper } of Object.values(
-        this.trackingMap,
-      )) {
-        incomingTransactionHelper.stop();
+      for (const [, trackingMap] of this.trackingMap) {
+        trackingMap.incomingTransactionHelper.stop();
       }
     }
   }
@@ -3064,10 +3062,8 @@ export class TransactionController extends BaseControllerV1<
     // PendingTransactionTracker reads state through its getTransactions hook
     this.pendingTransactionTracker.startIfPendingTransactions();
     if (this.#enableMultichain) {
-      for (const { pendingTransactionTracker } of Object.values(
-        this.trackingMap,
-      )) {
-        pendingTransactionTracker.startIfPendingTransactions();
+      for (const [, trackingMap] of this.trackingMap) {
+        trackingMap.pendingTransactionTracker.startIfPendingTransactions();
       }
     }
   };
