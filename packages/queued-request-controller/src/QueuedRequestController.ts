26ae5912b6ffa858aaa4bd04c96ecd4d0a9f0bfa
Mark Stacey <markjstacey@gmail.com>
2024-02-26 20:29:05 -0330

    chore(queued-request-controller): Remove `countChanged` event (#3985)
    ## Explanation
    
    The event `QueuedRequestController:countChanged` and the method `length`
    have been removed. Both the event and method were deprecated as part of
    #3919, which also provided an alternative by adding the queued request
    count to the controller state.
    
    ## References
    
    Related to #3919
    
    ## Changelog
    
    ### `@metamask/queued-request-controller`
    
    #### Removed
    - **BREAKING**: Remove the `QueuedRequestController:countChanged` event
    - The number of queued requests is now tracked in controller state, as
    the `queuedRequestCount` property. Use the
    `QueuedRequestController:stateChange` event to be notified of count
    changes instead.
    - **BREAKING**: Remove the `length` method
    - The number of queued requests is now tracked in controller state, as
    the `queuedRequestCount` property.
    
    ## Checklist
    
    - [x] I've updated the test suite for new or updated code as appropriate
    - [x] I've updated documentation (JSDoc, Markdown, etc.) for new or
    updated code as appropriate
    - [x] I've highlighted breaking changes using the "BREAKING" category
    above as appropriate

--- packages/queued-request-controller/src/QueuedRequestController.ts
+++ packages/queued-request-controller/src/QueuedRequestController.ts
@@ -27,7 +27,6 @@ export type QueuedRequestControllerEnqueueRequestAction = {
 };
 
 export const QueuedRequestControllerEventTypes = {
-  countChanged: `${controllerName}:countChanged` as const,
   stateChange: `${controllerName}:stateChange` as const,
 };
 
@@ -37,19 +36,8 @@ export type QueuedRequestControllerStateChangeEvent =
     QueuedRequestControllerState
   >;
 
-/**
- * This event is fired when the number of queued requests changes.
- *
- * @deprecated Use the `QueuedRequestController:stateChange` event instead
- */
-export type QueuedRequestControllerCountChangedEvent = {
-  type: typeof QueuedRequestControllerEventTypes.countChanged;
-  payload: [number];
-};
-
 export type QueuedRequestControllerEvents =
-  | QueuedRequestControllerCountChangedEvent
-  | QueuedRequestControllerStateChangeEvent;
+  QueuedRequestControllerStateChangeEvent;
 
 export type QueuedRequestControllerActions =
   | QueuedRequestControllerGetStateAction
@@ -113,27 +101,10 @@ export class QueuedRequestController extends BaseController<
     );
   }
 
-  /**
-   * Gets the current count of enqueued requests in the request queue. This count represents the number of
-   * pending requests that are waiting to be processed sequentially.
-   *
-   * @returns The current count of enqueued requests. This count reflects the number of pending
-   * requests in the queue, which are yet to be processed. It allows you to monitor the queue's workload
-   * and assess the volume of requests awaiting execution.
-   * @deprecated This method is deprecated; use `state.queuedRequestCount` directly instead.
-   */
-  length() {
-    return this.state.queuedRequestCount;
-  }
-
   #updateCount(change: -1 | 1) {
     this.update((state) => {
       state.queuedRequestCount += change;
     });
-    this.messagingSystem.publish(
-      'QueuedRequestController:countChanged',
-      this.state.queuedRequestCount,
-    );
   }
 
   /**
--- packages/queued-request-controller/src/index.ts
+++ packages/queued-request-controller/src/index.ts
@@ -1,6 +1,5 @@
 export type {
   QueuedRequestControllerState,
-  QueuedRequestControllerCountChangedEvent,
   QueuedRequestControllerEnqueueRequestAction,
   QueuedRequestControllerGetStateAction,
   QueuedRequestControllerStateChangeEvent,
