ef69636b4c8ce3d70a2c2522bfcb4e0c31a50ba4
Matthew Walsh <matthew.walsh@consensys.net>
2024-03-04 11:41:49 +0000

    Add supported chains

--- packages/transaction-controller/src/utils/simulation-api.ts
+++ packages/transaction-controller/src/utils/simulation-api.ts
@@ -1,15 +1,25 @@
 import { JsonRpcProvider } from '@ethersproject/providers';
 import { createModuleLogger, type Hex } from '@metamask/utils';
 
+import { CHAIN_IDS } from '../constants';
 import { projectLogger } from '../logger';
 
 const log = createModuleLogger(projectLogger, 'simulation-api');
 
 const RPC_METHOD = 'infura_simulateTransactions';
+const BASE_URL = 'https://tx-sentinel-{0}.api.cx.metamask.io/';
 
-const URLS_BY_CHAIN_ID: Record<Hex, string> = {
-  '0x1': 'https://tx-sentinel-ethereum-mainnet.api.cx.metamask.io/',
-  '0x5': 'https://tx-sentinel-ethereum-goerli.api.cx.metamask.io/',
+const SUBDOMAIN_BY_CHAIN_ID: Record<Hex, string> = {
+  [CHAIN_IDS.MAINNET]: 'ethereum-mainnet',
+  [CHAIN_IDS.GOERLI]: 'ethereum-goerli',
+  [CHAIN_IDS.SEPOLIA]: 'ethereum-sepolia',
+  [CHAIN_IDS.LINEA_MAINNET]: 'linea-mainnet',
+  [CHAIN_IDS.LINEA_GOERLI]: 'linea-goerli',
+  [CHAIN_IDS.ARBITRUM]: 'arbitrum-mainnet',
+  [CHAIN_IDS.AVALANCHE]: 'avalanche-mainnet',
+  [CHAIN_IDS.OPTIMISM]: 'optimism-mainnet',
+  [CHAIN_IDS.POLYGON]: 'polygon-mainnet',
+  [CHAIN_IDS.BSC]: 'bsc-mainnet',
 };
 
 export type SimulationRequestTransaction = {
@@ -79,12 +89,7 @@ export async function simulateTransactions(
   chainId: Hex,
   request: SimulationRequest,
 ): Promise<SimulationResponse> {
-  const url = URLS_BY_CHAIN_ID[chainId];
-
-  if (!url) {
-    log('Chain is not supported', chainId);
-    throw new Error(`Chain is not supported: ${chainId}`);
-  }
+  const url = getUrl(chainId);
 
   log('Sending request', url, request);
 
@@ -94,4 +99,20 @@ export async function simulateTransactions(
   log('Received response', response);
 
   return response;
+}
+
+/**
+ * Get the URL for the transaction simulation API.
+ * @param chainId - The chain ID to get the URL for.
+ * @returns The URL for the transaction simulation API.
+ */
+function getUrl(chainId: Hex): string {
+  const subdomain = SUBDOMAIN_BY_CHAIN_ID[chainId];
+
+  if (!subdomain) {
+    log('Chain is not supported', chainId);
+    throw new Error(`Chain is not supported: ${chainId}`);
+  }
+
+  return BASE_URL.replace('{0}', subdomain);
 }
