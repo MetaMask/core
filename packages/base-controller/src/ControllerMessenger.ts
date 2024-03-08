a57d49144eb35431bbe027301ebe8a76b3591f25
Jongsun Suh <jongsun.suh@icloud.com>
2024-03-08 00:55:53 -0500

    Add `string` generic constraint to `NamespacedBy`,`NotNamespacedBy` `Name` generic param

--- packages/base-controller/src/ControllerMessenger.ts
+++ packages/base-controller/src/ControllerMessenger.ts
@@ -90,12 +90,12 @@ type EventSubscriptionMap<
  */
 export type NamespacedBy<
   Namespace extends string,
-  Name,
+  Name extends string,
 > = Name extends `${Namespace}:${string}` ? Name : never;
 
 export type NotNamespacedBy<
   Namespace extends string,
-  Name,
+  Name extends string,
 > = Name extends `${Namespace}:${string}` ? never : Name;
 
 export type NamespacedName<Namespace extends string = string> =
