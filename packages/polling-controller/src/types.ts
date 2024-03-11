3df7665e0b77fda6e87fc2b02048cbe735049616
Jongsun Suh <jongsun.suh@icloud.com>
2024-03-11 11:14:40 -0400

    Add comment explaining that `any` is required for mixin constructors

--- packages/polling-controller/src/types.ts
+++ packages/polling-controller/src/types.ts
@@ -1,2 +1,10 @@
+/**
+ * TypeScript enforces this type for mixin constructors.
+ *
+ * Removing the `any` type results in the following error:
+ * 'A mixin class must have a constructor with a single rest parameter of type 'any[]'.ts(2545)'
+ *
+ * A potential future refactor that removes the mixin pattern may be able to fix this.
+ */
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export type Constructor = new (...args: any[]) => object;
