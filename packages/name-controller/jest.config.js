a145237447a84bf3fa534ad0bc54cd7b22601a37
Jongsun Suh <jongsun.suh@icloud.com>
2024-03-14 13:01:19 -0400

    test: Write unit tests for `isSafeDynamicKey` validation

--- packages/name-controller/jest.config.js
+++ packages/name-controller/jest.config.js
@@ -17,10 +17,10 @@ module.exports = merge(baseConfig, {
   // An object that configures minimum threshold enforcement for coverage results
   coverageThreshold: {
     global: {
-      branches: 99.15,
+      branches: 100,
       functions: 100,
-      lines: 99.72,
-      statements: 99.72,
+      lines: 100,
+      statements: 100,
     },
   },
 });
