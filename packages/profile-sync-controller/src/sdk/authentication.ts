3b9e037497ae7c8af162ad6e4130bfdb13a56b0a
Dovydas Stankevicius <dovydas.stankevicius@consensys.net>
2024-04-08 16:21:06 +0100

    feat: created profile-sync-sdk package

---
+++ packages/profile-sync-controller/src/sdk/authentication.ts
@@ -0,0 +1,240 @@
+import { Env, getEnvUrls } from './env';
+import { NonceRetrievalError, SignInError, UnsupportedAuthTypeError, ValidationError } from './errors';
+import { MESSAGE_SIGNING_SNAP, connectSnap } from './messaging-signing-snap';
+
+export enum AuthType {
+    /* sign in using a private key derived from your secret recovery phrase (SRP). 
+       Uses message signing snap to perform this operation */
+    SRP = 'SRP',
+
+    /* sign in with Ethereum */
+    SiWE = 'SiWE',
+}
+
+export type AuthConfig = {
+    env: Env;
+    type: AuthType;
+};
+
+export type AccessToken = {
+    accessToken: string;
+    expiresIn: number;
+    obtainedAt: number;
+};
+
+export type UserProfile = {
+    identifierId: string;
+    profileId: string;
+    metaMetricsId: string;
+};
+
+type NonceResponse = {
+    nonce: string;
+    identifier: string;
+    expiresIn: number;
+};
+
+type LoginResponse = {
+    token: AccessToken;
+    profile: UserProfile;
+};
+
+type SrpLoginResponse = {
+    token: string;
+    expiresIn: number;
+    profile: UserProfile;
+};
+
+export abstract class BaseAuth {
+    protected accessToken: AccessToken | null = null;
+    protected userProfile: UserProfile | null = null;
+    protected envUrls: { authApiUrl: string; oidcApiUrl: string };
+
+    constructor(protected config: AuthConfig) {
+        this.envUrls = getEnvUrls(config.env);
+    }
+
+    abstract getAccessToken(): Promise<AccessToken>;
+    abstract getUserProfile(): Promise<UserProfile>;
+    protected abstract login(): Promise<LoginResponse>;
+
+    async logout(): Promise<void> {
+        this.accessToken = null;
+        this.userProfile = null;
+    }
+
+    protected async getNonce(id: string): Promise<NonceResponse> {
+        const nonceUrl = new URL(`${this.envUrls.authApiUrl}/api/v2/nonce`);
+        nonceUrl.searchParams.set('identifier', id);
+
+        try {
+            const nonceResponse = await fetch(nonceUrl.toString());
+            if (!nonceResponse.ok) {
+                const responseBody = await nonceResponse.json()
+                throw new Error(`HTTP error message: ${responseBody.message}, error: ${responseBody.error}`);
+            }
+
+            const nonceJson = await nonceResponse.json();
+            return {
+                nonce: nonceJson.nonce,
+                identifier: nonceJson.identifier,
+                expiresIn: nonceJson.expires_in,
+            };
+        } catch (e) {
+            throw new NonceRetrievalError(`failed to generate nonce ${e}`);
+        }
+    }
+}
+
+export class JwtBearerAuth extends BaseAuth {
+    private grantType = 'urn:ietf:params:oauth:grant-type:jwt-bearer';
+
+    async #getAccessToken(jwtToken: string): Promise<AccessToken> {
+        const headers = new Headers({
+            'Content-Type': 'application/x-www-form-urlencoded',
+        });
+
+        const urlEncodedBody = new URLSearchParams();
+        urlEncodedBody.append('grant_type', this.grantType);
+        urlEncodedBody.append('client_id', this.#getOidcClientId(this.config.env));
+        urlEncodedBody.append('assertion', jwtToken);
+
+        try {
+            const response = await fetch(`${this.envUrls.oidcApiUrl}/oauth2/token`, {
+                method: 'POST',
+                headers,
+                body: urlEncodedBody.toString(),
+            });
+
+            if (!response.ok) {
+                const responseBody = await response.json()
+                throw new Error(`HTTP error: ${responseBody.error_description}, error code: ${responseBody.error}`);
+            }
+
+            const accessTokenResponse = await response.json();
+            return {
+                accessToken: accessTokenResponse.access_token,
+                expiresIn: accessTokenResponse.expires_in,
+                obtainedAt: Date.now(),
+            };
+        } catch (e) {
+            throw new SignInError(`unable to get access token ${e}`);
+        }
+    }
+
+    async getAccessToken(): Promise<AccessToken> {
+        if (this.#hasValidAuthSession()) {
+            if (this.accessToken) {
+                return this.accessToken;
+            }
+        }
+        const loginResponse = await this.login();
+        if (!loginResponse || !loginResponse.token) {
+            throw new SignInError('login failed: access token not received');
+        }
+        return loginResponse.token;
+    }
+
+    async getUserProfile(): Promise<UserProfile> {
+        if (this.#hasValidAuthSession()) {
+            if (this.userProfile) {
+                return this.userProfile;
+            }
+        }
+        const loginResponse = await this.login();
+        if (!loginResponse || !loginResponse.profile) {
+            throw new SignInError('login failed: user profile not received');
+        }
+        return loginResponse.profile;
+    }
+
+    protected async login(): Promise<LoginResponse> {
+        switch (this.config.type) {
+            case AuthType.SRP: {
+                await connectSnap();
+                const publicKey = await MESSAGE_SIGNING_SNAP.getPublicKey();
+                const nonceRes = await this.getNonce(publicKey);
+
+                const rawMessage = this.#createSrpLoginRawMessage(nonceRes.nonce, publicKey);
+                const signature = await MESSAGE_SIGNING_SNAP.signMessage(rawMessage);
+                const loginResponse = await this.#srpLogin(rawMessage, signature);
+                if (!loginResponse) {
+                    throw new SignInError('SRP login failed');
+                }
+
+                const tokenResponse = await this.#getAccessToken(loginResponse.token);
+                this.userProfile = loginResponse.profile;
+                this.accessToken = tokenResponse;
+                return {
+                    profile: this.userProfile,
+                    token: this.accessToken,
+                };
+            }
+            default:
+                throw new UnsupportedAuthTypeError('Unsupported login type');
+        }
+    }
+
+    async #srpLogin(rawMessage: string, signature: string): Promise<SrpLoginResponse> {
+        try {
+            const response = await fetch(`${this.envUrls.authApiUrl}/api/v2/srp/login`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    signature,
+                    raw_message: rawMessage,
+                }),
+            });
+
+            if (!response.ok) {
+                const responseBody = await response.json();
+                throw new Error(`SRP login HTTP error: ${responseBody.message}, error code: ${responseBody.error}`);
+            }
+
+            const loginResponse = await response.json();
+            return {
+                token: loginResponse.token,
+                expiresIn: loginResponse.expires_in,
+                profile: {
+                    identifierId: loginResponse.profile.identifier_id,
+                    metaMetricsId: loginResponse.profile.metametrics_id,
+                    profileId: loginResponse.profile.profile_id,
+                },
+            };
+        } catch (e) {
+            throw new SignInError(`unable to perform SRP login ${e}`);
+        }
+    }
+
+    #hasValidAuthSession(): boolean {
+        if (!this.accessToken || !this.accessToken.obtainedAt) {
+            return false;
+        }
+
+        const sessionLifetimeMs = 45 * 60 * 1000; // 45 minutes in milliseconds
+        const currentTime = Date.now();
+        const sessionAge = currentTime - this.accessToken.obtainedAt;
+        return sessionAge < sessionLifetimeMs;
+    }
+
+    #createSrpLoginRawMessage(
+        nonce: string,
+        publicKey: string,
+    ): `metamask:${string}:${string}` {
+        return `metamask:${nonce}:${publicKey}` as const;
+    }
+
+    #getOidcClientId(env: Env):string{
+        switch(env){
+            case Env.DEV:
+                return 'f1a963d7-50dc-4cb5-8d81-f1f3654f0df3';
+            case Env.PROD:
+                return '1132f10a-b4e5-4390-a5f2-d9c6022db564';
+            default: 
+                throw new ValidationError('missing oidc client id');
+        }
+    }
+}
+
---
+++ packages/profile-sync-controller/src/sdk/user-storage.ts
@@ -0,0 +1,119 @@
+import { BaseAuth } from './authentication';
+import encryption from './encryption';
+import { createSHA256Hash } from './encryption'
+import { MESSAGE_SIGNING_SNAP } from './messaging-signing-snap';
+import { Env, getEnvUrls } from './env';
+import { NotFoundError, SignInError, UserStorageError, ValidationError } from './errors';
+
+export type UserStorageConfig = {
+    env: Env;
+    auth: BaseAuth;
+};
+
+export class UserStorage {
+    protected envUrls: { userStorageApiUrl: string };
+
+    constructor(protected config: UserStorageConfig) { 
+        this.envUrls = getEnvUrls(config.env);
+    }
+
+    async setItem(feature: string, key: string, value: string): Promise<void> {
+        if (!feature.trim() || !key.trim()) {
+            throw new ValidationError('feature or key cannot be empty strings');
+        }
+        await this.#upsertUserStorage(feature, key, value)
+    }
+
+    async getItem(feature: string, key: string): Promise<string> {
+        if (!feature.trim() || !key.trim()) {
+            throw new ValidationError('feature or key cannot be empty strings');
+        }
+        return this.#getUserStorage(feature, key)
+    }
+
+    async #getStorageKey(): Promise<string> {
+        const userProfile = await this.config.auth.getUserProfile()
+        if (!userProfile) {
+            throw new SignInError('unable to create storage key: user profile missing');
+        }
+
+        const storageKeySignature = await MESSAGE_SIGNING_SNAP.signMessage(`metamask:${userProfile.profileId}`);
+        return createSHA256Hash(storageKeySignature);
+    }
+
+    async #upsertUserStorage(feature: string, key: string, data: string): Promise<void> {
+        try {
+            const storageKey = await this.#getStorageKey();
+            const encryptedData = encryption.encryptString(data, storageKey);
+            const url = new URL(`${this.envUrls.userStorageApiUrl}/api/v1/userstorage/${this.#getEntryPath(feature, key, storageKey)}`);
+
+            const headers = await this.#getAuthorizationHeader();
+
+            const response = await fetch(url.toString(), {
+                method: 'PUT',
+                headers: {
+                    'Content-Type': 'application/json',
+                    ...headers,
+                },
+                body: JSON.stringify({ data: encryptedData }),
+            });
+
+            if (!response.ok) {
+                const responseBody = await response.json()
+                throw new Error(`HTTP error message: ${responseBody.message}, error: ${responseBody.error}`);
+            }
+        } catch (error) {
+            throw new UserStorageError(`failed to upsert user storage for feature '${feature}' and key '${key}'. ${error}`);
+        }
+    }
+
+    async #getUserStorage(feature: string, key: string): Promise<string> {
+        try {
+            const storageKey = await this.#getStorageKey();
+            const url = new URL(`${this.envUrls.userStorageApiUrl}/api/v1/userstorage/${this.#getEntryPath(feature, key, storageKey)}`);
+            const headers = await this.#getAuthorizationHeader();
+
+            const response = await fetch(url.toString(), {
+                headers: {
+                    'Content-Type': 'application/json',
+                    ...headers,
+                },
+            });
+
+            if (response.status === 404) {
+                throw new NotFoundError(`feature/key set not found for feature '${feature}' and key '${key}'.`);
+            }
+
+            if (!response.ok) {
+                const responseBody = await response.json()
+                throw new Error(`HTTP error message: ${responseBody.message}, error: ${responseBody.error}`);
+            }
+
+            const { Data: encryptedData } = await response.json();
+            return encryption.decryptString(encryptedData, storageKey);
+        } catch (error) {
+            if (error instanceof NotFoundError) {
+                throw error;
+            }
+
+            throw new UserStorageError(`failed to get user storage for feature '${feature}' and key '${key}'. ${error}`, );
+        }
+    }
+
+    #getEntryPath(
+        feature: string,
+        key: string,
+        storageKey: string,
+      ): string {
+        const hashedKey = createSHA256Hash(key + storageKey);
+        return `${feature}/${hashedKey}`;
+      }
+
+    async #getAuthorizationHeader(): Promise<{ Authorization: string }> {
+        const accessToken = await this.config.auth.getAccessToken();
+        if (!accessToken) {
+            throw new SignInError('Access token is missing, unable to authenticate.');
+        }
+        return { Authorization: `Bearer ${accessToken.accessToken}` };
+    }
+}
