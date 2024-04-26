28ed771944c94154c9a4ce49fa066a7423b48f9e
Charly Chevalier <charly.chevalier@consensys.net>
2024-04-26 17:23:16 +0200

    feat(chain-controller): add getBalances support

---
+++ packages/chain-controller/src/ChainController.test.ts
@@ -0,0 +1,102 @@
+import type { SnapId } from '@metamask/snaps-sdk';
+
+import { ChainController } from './ChainController';
+
+describe('ChainController', () => {
+  const snapId = 'local:localhost:3000' as SnapId;
+  const snapClient = {
+    submitRequest: jest.fn(),
+  };
+  const snapController = {
+    handleRequest: (request) => {
+      if (request.snapId === snapId) {
+        return snapClient.submitRequest(request);
+      }
+      throw new Error(`Unknown Snap: ${request.snapId as string}`);
+    },
+  };
+
+  const messenger = {
+    call: jest.fn(() => ({
+      catch: jest.fn(),
+    })),
+    registerActionHandler: jest.fn(),
+    registerInitialEventPayload: jest.fn(),
+    publish: jest.fn(),
+    subscribe: jest.fn(),
+  };
+
+  const makeController = () => {
+    return new ChainController({
+      state: {
+        dummy: false,
+      },
+      messenger,
+      getSnapController: () => snapController,
+    });
+  };
+
+  const address = 'bc1qrp0yzgkf8rawkuvdlhnjfj2fnjwm0m8727kgah';
+  const scope = 'bip122:000000000019d6689c085ae165831e93';
+  const asset = `${scope}/asset:0`;
+
+  describe('registerProvider', () => {
+    it('returns false if there is no known provider', () => {
+      const controller = makeController();
+
+      expect(controller.hasProviderFor(scope)).toBe(false);
+    });
+
+    it('registers a chain provider for a given scope', () => {
+      const controller = makeController();
+
+      expect(controller.registerProvider(scope, snapId)).toBeDefined();
+      expect(controller.hasProviderFor(scope)).toBe(true);
+    });
+
+    it('fails to register another provider for an existing scope', () => {
+      const controller = makeController();
+      const anotherSnapId = 'local:localhost:4000' as SnapId;
+
+      expect(controller.registerProvider(scope, snapId)).toBeDefined();
+      expect(() => controller.registerProvider(scope, anotherSnapId)).toThrow(
+        `Found an already existing provider for scope: "${scope}"`,
+      );
+    });
+  });
+
+  describe('getBalances', () => {
+    const response = {
+      balances: {
+        [address]: {
+          [asset]: {
+            amount: '70.02255139',
+          },
+        },
+      },
+    };
+
+    it('is successful', async () => {
+      const controller = makeController();
+
+      const provider = controller.registerProvider(scope, snapId);
+      const providerSpy = jest.spyOn(provider, 'getBalances');
+
+      snapClient.submitRequest.mockResolvedValue(response);
+      const result = await controller.getBalances(scope, [address], [asset]);
+
+      expect(providerSpy).toHaveBeenCalledWith(scope, [address], [asset]);
+      expect(result).toStrictEqual(response);
+    });
+
+    it('fails if not provider is registered', async () => {
+      const controller = makeController();
+
+      // We do not register any provider
+
+      await expect(async () =>
+        await controller.getBalances(scope, [address], [asset]),
+      ).rejects.toThrow(`No Chain provider found for scope: "${scope}"`);
+    });
+  });
+});
--- packages/chain-controller/src/ChainController.ts
+++ packages/chain-controller/src/ChainController.ts
@@ -4,6 +4,14 @@ import type {
   RestrictedControllerMessenger,
 } from '@metamask/base-controller';
 import { BaseController } from '@metamask/base-controller';
+import type { CaipAssetType, BalancesResult, Chain } from '@metamask/chain-api';
+import type { InternalAccount } from '@metamask/keyring-api';
+import type { SnapController } from '@metamask/snaps-controllers';
+import type { SnapId } from '@metamask/snaps-sdk';
+import type { CaipChainId } from '@metamask/utils';
+
+import { SnapChainProviderClient } from './SnapChainProviderClient';
+import { SnapControllerClient } from './SnapControllerClient';
 
 const controllerName = 'ChainController';
 
@@ -44,24 +52,34 @@ const defaultState: ChainControllerState = {
 /**
  * Controller that manages chain-agnostic providers throught the chain API.
  */
-export class ChainController extends BaseController<
-  typeof controllerName,
-  ChainControllerState,
-  ChainControllerMessenger
-> {
+export class ChainController
+  extends BaseController<
+    typeof controllerName,
+    ChainControllerState,
+    ChainControllerMessenger
+  >
+  implements Chain
+{
+  #providers: Record<CaipChainId, SnapChainProviderClient>;
+
+  #snapClient: SnapControllerClient;
+
   /**
    * Constructor for ChainController.
    *
    * @param options - The controller options.
    * @param options.messenger - The messenger object.
    * @param options.state - Initial state to set on this controller
+   * @param options.getSnapController - Snaps controller.
    */
   constructor({
     messenger,
     state,
+    getSnapController,
   }: {
     messenger: ChainControllerMessenger;
     state: ChainControllerState;
+    getSnapController: () => SnapController;
   }) {
     super({
       messenger,
@@ -78,7 +96,65 @@ export class ChainController extends BaseController<
       },
     });
 
+    this.#snapClient = new SnapControllerClient({
+      controller: getSnapController(),
+    });
+
+    this.#providers = {};
+
     this.#registerMessageHandlers();
+  }
+
+  #getProviderClient(scope: CaipChainId): SnapChainProviderClient {
+    if (scope in this.#providers) {
+      return this.#providers[scope];
+    }
+
+    const error = `No Chain provider found for scope: "${scope}"`;
+    console.error(error, this.#providers);
+    throw new Error(error);
+  }
+
+  getBalances = async (
+    scope: CaipChainId,
+    accounts: string[],
+    assets: CaipAssetType[],
+  ): Promise<BalancesResult> => {
+    return await this.#getProviderClient(scope).getBalances(
+      scope,
+      accounts,
+      assets,
+    );
+  };
+
+  getBalancesFromAccount = async (
+    scope: CaipChainId,
+    account: InternalAccount,
+    assets: CaipAssetType[],
+  ): Promise<BalancesResult> => {
+    return this.getBalances(scope, [account.address], assets);
+  };
+
+  hasProviderFor(scope: CaipChainId): boolean {
+    return scope in this.#providers;
+  }
+
+  registerProvider(
+    scope: CaipChainId,
+    snapId: SnapId,
+  ): SnapChainProviderClient {
+    // TODO: Should this be idempotent?
+    const client = this.#snapClient.withSnapId(snapId as SnapId);
+    const provider = new SnapChainProviderClient(client);
+
+    if (this.hasProviderFor(scope)) {
+      // For now, we avoid this to make sure no other provider can replace the existings ones!
+      throw new Error(
+        `Found an already existing provider for scope: "${scope}"`,
+      );
+    }
+    this.#providers[scope] = provider;
+    return provider;
   }
 
   /**
