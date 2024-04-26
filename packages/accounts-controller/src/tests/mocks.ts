f5f6c111998a79ca9787b28712212ffc2680c22f
Monte Lai <monte.lai@consensys.net>
2024-04-26 10:00:00 +0800

    feat: add mocks and evm check to accounts controller

---
+++ packages/accounts-controller/src/tests/mocks.ts
@@ -0,0 +1,59 @@
+import type { InternalAccount } from '@metamask/keyring-api';
+import { EthAccountType, EthMethod } from '@metamask/keyring-api';
+import { KeyringTypes } from '@metamask/keyring-controller';
+import { v4 } from 'uuid';
+
+export const createMockInternalAccount = ({
+  id = v4(),
+  address = '0x2990079bcdee240329a520d2444386fc119da21a',
+  type = EthAccountType.Eoa,
+  name = 'Account 1',
+  keyringType = KeyringTypes.hd,
+  snap,
+  importTime = Date.now(),
+  lastSelected = Date.now(),
+}: {
+  id?: string;
+  address?: string;
+  type?: EthAccountType;
+  name?: string;
+  keyringType?: KeyringTypes;
+  snap?: {
+    id: string;
+    enabled: boolean;
+    name: string;
+  };
+  importTime?: number;
+  lastSelected?: number;
+} = {}): InternalAccount => {
+  const methods =
+    type === EthAccountType.Eoa
+      ? [
+          EthMethod.PersonalSign,
+          EthMethod.Sign,
+          EthMethod.SignTransaction,
+          EthMethod.SignTypedDataV1,
+          EthMethod.SignTypedDataV3,
+          EthMethod.SignTypedDataV4,
+        ]
+      : [
+          EthMethod.PatchUserOperation,
+          EthMethod.PrepareUserOperation,
+          EthMethod.SignUserOperation,
+        ];
+
+  return {
+    id,
+    address,
+    options: {},
+    methods,
+    type: EthAccountType.Eoa,
+    metadata: {
+      name,
+      keyring: { type: keyringType },
+      importTime,
+      lastSelected,
+      snap: snap && snap,
+    },
+  };
+};
