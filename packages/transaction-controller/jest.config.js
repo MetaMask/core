a5c970567c681959b159865cbfb839044d871d91
Derek Brans <dbrans@gmail.com>
2024-05-02 13:03:01 -0400

    tweak jest.config.js

--- packages/transaction-controller/jest.config.js
+++ packages/transaction-controller/jest.config.js
@@ -17,7 +17,7 @@ module.exports = merge(baseConfig, {
   // An object that configures minimum threshold enforcement for coverage results
   coverageThreshold: {
     global: {
-      branches: 93.84,
+      branches: 93.69,
       functions: 98.65,
       lines: 98.92,
       statements: 98.93,
