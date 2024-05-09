07a7a40ce23fa80c6aae5bfc3d78bddb41c0fdfb
Jongsun Suh <jongsun.suh@icloud.com>
2024-05-09 13:12:47 -0400

    Register `getState` action handler in constructor

--- packages/assets-controllers/src/TokensController.ts
+++ packages/assets-controllers/src/TokensController.ts
@@ -252,6 +252,12 @@ export class TokensController extends BaseControllerV1<
 
     this.messagingSystem = messenger;
 
+    // TODO: Remove once `TokensController` is upgraded to V2.
+    this.messagingSystem.registerActionHandler(
+      `${controllerName}:getState`,
+      () => this.state,
+    );
+
     this.messagingSystem.registerActionHandler(
       `${controllerName}:addDetectedTokens` as const,
       this.addDetectedTokens.bind(this),
