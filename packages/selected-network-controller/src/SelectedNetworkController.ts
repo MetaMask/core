56957f713365a7305028291012c75e3637f9376a
Jiexi Luan <jiexiluan@gmail.com>
2024-06-04 17:28:54 -0700

    patch

--- packages/selected-network-controller/src/SelectedNetworkController.ts
+++ packages/selected-network-controller/src/SelectedNetworkController.ts
@@ -166,7 +166,13 @@ export class SelectedNetworkController extends BaseController<
             path[0] === 'subjects' && path[1] !== undefined;
           if (isChangingSubject && typeof path[1] === 'string') {
             const domain = path[1];
-            if (op === 'add' && this.state.domains[domain] === undefined) {
+            if (
+              op === 'add' &&
+              this.state.domains[domain] === undefined &&
+              // PR: https://github.com/MetaMask/core/pull/4368
+              // Patch Branch: jl/patch-selected-network-controller-12.0.1-permission-state-change-guard
+              this.#useRequestQueuePreference
+            ) {
               this.setNetworkClientIdForDomain(
                 domain,
                 this.messagingSystem.call('NetworkController:getState')
