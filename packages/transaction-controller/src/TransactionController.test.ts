3bfdfb23fb58f74c0de814ac3732c67a8e8595d7
legobt <6wbvkn0j@anonaddy.me>
2024-06-10 02:45:12 +0000

    chore(transaction-controller): agnostic on infura_project_id being identical across provider urls

--- packages/transaction-controller/src/TransactionController.test.ts
+++ packages/transaction-controller/src/TransactionController.test.ts
@@ -307,7 +307,7 @@ function waitForTransactionFinished(
 const MOCK_PREFERENCES = { state: { selectedAddress: 'foo' } };
 const INFURA_PROJECT_ID = 'testinfuraid';
 const HTTP_PROVIDERS = {
-  goerli: new HttpProvider(`https://goerli.infura.io/v3/${INFURA_PROJECT_ID}`),
+  goerli: new HttpProvider('https://goerli.infura.io/v3/goerli-pid'),
   mainnet: new HttpProvider(
     `https://mainnet.infura.io/v3/${INFURA_PROJECT_ID}`,
   ),
