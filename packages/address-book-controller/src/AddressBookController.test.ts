42a5a258504ebae07e516274104ae422775a6d28
Kanthesha Devaramane <kanthesha.devaramane@consensys.net>
2024-06-11 09:13:00 +0100

    upgrade address book controller to base controller v2 (#4392)
    ## Explanation
    
    <!--
    Thanks for your contribution! Take a moment to answer these questions so
    that reviewers have the information they need to properly understand
    your changes:
    
    * What is the current state of things and why does it need to change?
    * What is the solution your changes offer and how does it work?
    * Are there any changes whose purpose might not obvious to those
    unfamiliar with the domain?
    * If your primary goal was to update one package but you found you had
    to update another one along the way, why did you do so?
    * If you had to upgrade a dependency, why did you do so?
    -->
    In this, the AddressBookController has been upgraded to
    BaseControllerV2. The upgrade includes AddressBookController inheriting
    BaseController v2 instead of BaseController v1. This affects the
    constructor and the also the way state is updated inside the controller.
    
    ## References
    
    <!--
    Are there any issues that this pull request is tied to? Are there other
    links that reviewers should consult to understand these changes better?
    
    For example:
    
    * Fixes #12345
    * Related to #67890
    -->
    Fixes #4070 
    
    ## Changelog
    
    <!--
    If you're making any consumer-facing changes, list those changes here as
    if you were updating a changelog, using the template below as a guide.
    
    (CATEGORY is one of BREAKING, ADDED, CHANGED, DEPRECATED, REMOVED, or
    FIXED. For security-related issues, follow the Security Advisory
    process.)
    
    Please take care to name the exact pieces of the API you've added or
    changed (e.g. types, interfaces, functions, or methods).
    
    If there are any breaking changes, make sure to offer a solution for
    consumers to follow once they upgrade to the changes.
    
    Finally, if you're only making changes to development scripts or tests,
    you may replace the template below with "None".
    -->
    
    `@metamask/address-book-controller`
    ### Added
    
    - New types for `AddressBookController` messenger actions 
      - `AddressBookControllerGetStateAction`
      -  `AddressBookControllerActions`
    - New types for `AddressBookController` messenger events
      - `AddressBookControllerStateChangeEvent`
      - `AddressBookControllerEvents`
    - New `AddressBookControllerMessenger` type
    - New `getDefaultAddressBookControllerState` function to get the default
    state of the controller
    
    ### Changed
    
    - **BREAKING:** Changed superclass of `AddressBookController` from
    BaseController v1 to BaseController v2
    - **BREAKING:** Renamed `AddressBookState` to
    `AddressBookControllerState`
    - **BREAKING:** Changed `constructor` arguments. Removed `config` and
    added `messenger` for communication.
    
    ### Removed
    
    - **BREAKING:** Removed `AddressBookConfig` type
    
    ## Checklist
    
    - [x] I've updated the test suite for new or updated code as appropriate
    - [x] I've updated documentation (JSDoc, Markdown, etc.) for new or
    updated code as appropriate
    - [x] I've highlighted breaking changes using the "BREAKING" category
    above as appropriate

--- packages/address-book-controller/src/AddressBookController.test.ts
+++ packages/address-book-controller/src/AddressBookController.test.ts
@@ -1,15 +1,45 @@
+import { ControllerMessenger } from '@metamask/base-controller';
 import { toHex } from '@metamask/controller-utils';
 
-import { AddressBookController, AddressType } from './AddressBookController';
+import type {
+  AddressBookControllerActions,
+  AddressBookControllerEvents,
+} from './AddressBookController';
+import {
+  AddressBookController,
+  AddressType,
+  controllerName,
+} from './AddressBookController';
+
+/**
+ * Constructs a restricted controller messenger.
+ *
+ * @returns A restricted controller messenger.
+ */
+function getRestrictedMessenger() {
+  const controllerMessenger = new ControllerMessenger<
+    AddressBookControllerActions,
+    AddressBookControllerEvents
+  >();
+  return controllerMessenger.getRestricted({
+    name: controllerName,
+    allowedActions: [],
+    allowedEvents: [],
+  });
+}
 
 describe('AddressBookController', () => {
   it('should set default state', () => {
-    const controller = new AddressBookController();
+    const controller = new AddressBookController({
+      messenger: getRestrictedMessenger(),
+    });
     expect(controller.state).toStrictEqual({ addressBook: {} });
   });
 
   it('should add a contact entry', () => {
-    const controller = new AddressBookController();
+    const controller = new AddressBookController({
+      messenger: getRestrictedMessenger(),
+    });
     controller.set('0x32Be343B94f860124dC4fEe278FDCBD38C102D88', 'foo');
 
     expect(controller.state).toStrictEqual({
@@ -29,7 +59,9 @@ describe('AddressBookController', () => {
   });
 
   it('should add a contact entry with chainId and memo', () => {
-    const controller = new AddressBookController();
+    const controller = new AddressBookController({
+      messenger: getRestrictedMessenger(),
+    });
     controller.set(
       '0x32Be343B94f860124dC4fEe278FDCBD38C102D88',
       'foo',
@@ -55,7 +87,9 @@ describe('AddressBookController', () => {
   });
 
   it('should add a contact entry with address type contract accounts', () => {
-    const controller = new AddressBookController();
+    const controller = new AddressBookController({
+      messenger: getRestrictedMessenger(),
+    });
     controller.set(
       '0x32Be343B94f860124dC4fEe278FDCBD38C102D88',
       'foo',
@@ -81,7 +115,9 @@ describe('AddressBookController', () => {
   });
 
   it('should add a contact entry with address type non accounts', () => {
-    const controller = new AddressBookController();
+    const controller = new AddressBookController({
+      messenger: getRestrictedMessenger(),
+    });
     controller.set(
       '0x32Be343B94f860124dC4fEe278FDCBD38C102D88',
       'foo',
@@ -107,7 +143,9 @@ describe('AddressBookController', () => {
   });
 
   it('should add multiple contact entries with different chainIds', () => {
-    const controller = new AddressBookController();
+    const controller = new AddressBookController({
+      messenger: getRestrictedMessenger(),
+    });
     controller.set(
       '0x32Be343B94f860124dC4fEe278FDCBD38C102D88',
       'foo',
@@ -149,7 +187,9 @@ describe('AddressBookController', () => {
   });
 
   it('should update a contact entry', () => {
-    const controller = new AddressBookController();
+    const controller = new AddressBookController({
+      messenger: getRestrictedMessenger(),
+    });
     controller.set('0x32Be343B94f860124dC4fEe278FDCBD38C102D88', 'foo');
 
     controller.set('0x32Be343B94f860124dC4fEe278FDCBD38C102D88', 'bar');
@@ -171,14 +211,18 @@ describe('AddressBookController', () => {
   });
 
   it('should not add invalid contact entry', () => {
-    const controller = new AddressBookController();
+    const controller = new AddressBookController({
+      messenger: getRestrictedMessenger(),
+    });
     // @ts-expect-error Intentionally invalid entry
     controller.set('0x01', 'foo', AddressType.externallyOwnedAccounts);
     expect(controller.state).toStrictEqual({ addressBook: {} });
   });
 
   it('should remove one contact entry', () => {
-    const controller = new AddressBookController();
+    const controller = new AddressBookController({
+      messenger: getRestrictedMessenger(),
+    });
     controller.set('0x32Be343B94f860124dC4fEe278FDCBD38C102D88', 'foo');
     controller.delete(toHex(1), '0x32Be343B94f860124dC4fEe278FDCBD38C102D88');
 
@@ -186,7 +230,9 @@ describe('AddressBookController', () => {
   });
 
   it('should remove only one contact entry', () => {
-    const controller = new AddressBookController();
+    const controller = new AddressBookController({
+      messenger: getRestrictedMessenger(),
+    });
     controller.set('0x32Be343B94f860124dC4fEe278FDCBD38C102D88', 'foo');
 
     controller.set('0xc38bf1ad06ef69f0c04e29dbeb4152b4175f0a8d', 'bar');
@@ -209,7 +255,9 @@ describe('AddressBookController', () => {
   });
 
   it('should add two contact entries with the same chainId', () => {
-    const controller = new AddressBookController();
+    const controller = new AddressBookController({
+      messenger: getRestrictedMessenger(),
+    });
     controller.set('0x32Be343B94f860124dC4fEe278FDCBD38C102D88', 'foo');
 
     controller.set('0xc38bf1ad06ef69f0c04e29dbeb4152b4175f0a8d', 'bar');
@@ -239,7 +287,9 @@ describe('AddressBookController', () => {
   });
 
   it('should correctly mark ens entries', () => {
-    const controller = new AddressBookController();
+    const controller = new AddressBookController({
+      messenger: getRestrictedMessenger(),
+    });
     controller.set(
       '0x32Be343B94f860124dC4fEe278FDCBD38C102D88',
       'metamask.eth',
@@ -262,7 +312,9 @@ describe('AddressBookController', () => {
   });
 
   it('should clear all contact entries', () => {
-    const controller = new AddressBookController();
+    const controller = new AddressBookController({
+      messenger: getRestrictedMessenger(),
+    });
     controller.set('0x32Be343B94f860124dC4fEe278FDCBD38C102D88', 'foo');
 
     controller.set('0xc38bf1ad06ef69f0c04e29dbeb4152b4175f0a8d', 'bar');
@@ -271,14 +323,18 @@ describe('AddressBookController', () => {
   });
 
   it('should return true to indicate an address book entry has been added', () => {
-    const controller = new AddressBookController();
+    const controller = new AddressBookController({
+      messenger: getRestrictedMessenger(),
+    });
     expect(
       controller.set('0x32Be343B94f860124dC4fEe278FDCBD38C102D88', 'foo'),
     ).toBe(true);
   });
 
   it('should return false to indicate an address book entry has NOT been added', () => {
-    const controller = new AddressBookController();
+    const controller = new AddressBookController({
+      messenger: getRestrictedMessenger(),
+    });
     expect(
       // @ts-expect-error Intentionally invalid entry
       controller.set('0x00', 'foo', AddressType.externallyOwnedAccounts),
@@ -286,7 +342,9 @@ describe('AddressBookController', () => {
   });
 
   it('should return true to indicate an address book entry has been deleted', () => {
-    const controller = new AddressBookController();
+    const controller = new AddressBookController({
+      messenger: getRestrictedMessenger(),
+    });
     controller.set('0x32Be343B94f860124dC4fEe278FDCBD38C102D88', 'foo');
 
     expect(
@@ -295,20 +353,26 @@ describe('AddressBookController', () => {
   });
 
   it('should return false to indicate an address book entry has NOT been deleted due to unsafe input', () => {
-    const controller = new AddressBookController();
+    const controller = new AddressBookController({
+      messenger: getRestrictedMessenger(),
+    });
     // @ts-expect-error Suppressing error to test runtime behavior
     expect(controller.delete('__proto__', '0x01')).toBe(false);
     expect(controller.delete(toHex(1), 'constructor')).toBe(false);
   });
 
   it('should return false to indicate an address book entry has NOT been deleted', () => {
-    const controller = new AddressBookController();
+    const controller = new AddressBookController({
+      messenger: getRestrictedMessenger(),
+    });
     controller.set('0x32Be343B94f860124dC4fEe278FDCBD38C102D88', '0x00');
     expect(controller.delete(toHex(1), '0x01')).toBe(false);
   });
 
   it('should normalize addresses so adding and removing entries work across casings', () => {
-    const controller = new AddressBookController();
+    const controller = new AddressBookController({
+      messenger: getRestrictedMessenger(),
+    });
     controller.set('0x32Be343B94f860124dC4fEe278FDCBD38C102D88', 'foo');
 
     controller.set('0xc38bf1ad06ef69f0c04e29dbeb4152b4175f0a8d', 'bar');
