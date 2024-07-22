8ed9d3b23e56c3183af3b6b57c50767ef06df226
Mathieu Artu <mathieu.artu@consensys.net>
2024-07-22 17:17:47 +0200

    refactor: consolidate any into a shared type

--- packages/profile-sync-controller/src/controllers/user-storage/schema.test.ts
+++ packages/profile-sync-controller/src/controllers/user-storage/schema.test.ts
@@ -1,29 +1,34 @@
 import { getFeatureAndKeyFromPath, USER_STORAGE_SCHEMA } from './schema';
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+type ErroneousUserStoragePath = any;
+
 describe('user-storage/schema.ts', () => {
   describe('getFeatureAndKeyFromPath', () => {
     it('should throw error if the feature.key format is incorrect', () => {
       const path = 'feature/key';
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      expect(() => getFeatureAndKeyFromPath(path as any)).toThrow(
+      expect(() =>
+        getFeatureAndKeyFromPath(path as ErroneousUserStoragePath),
+      ).toThrow(
         "user-storage - path is not in the correct format. Correct format: 'feature.key'",
       );
     });
 
     it('should throw error if feature is invalid', () => {
       const path = 'invalid.feature';
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      expect(() => getFeatureAndKeyFromPath(path as any)).toThrow(
-        'user-storage - invalid feature provided: invalid',
-      );
+      expect(() =>
+        getFeatureAndKeyFromPath(path as ErroneousUserStoragePath),
+      ).toThrow('user-storage - invalid feature provided: invalid');
     });
 
     it('should throw error if key is invalid', () => {
       const feature = 'notifications';
       const path = `${feature}.invalid`;
       const validKeys = USER_STORAGE_SCHEMA[feature].join(', ');
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      expect(() => getFeatureAndKeyFromPath(path as any)).toThrow(
+
+      expect(() =>
+        getFeatureAndKeyFromPath(path as ErroneousUserStoragePath),
+      ).toThrow(
         `user-storage - invalid key provided for this feature: invalid. Valid keys: ${validKeys}`,
       );
     });
