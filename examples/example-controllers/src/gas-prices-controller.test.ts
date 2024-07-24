7a3475f1b32835a9b89d496fca0959019d0f3a2f
Elliot Winkler <elliot.winkler@gmail.com>
2024-07-23 21:26:14 -0600

    Add example controllers
    This commit adds controllers which are not meant to be published, but
    instead serve as models to exemplify best practices for writing
    controllers, fulfilling a long-standing need. The controllers included
    here are implemented within a complete package which is linted just like
    other packages, and they ship with working tests which are run just like
    other tests. This lessens the chance that they will fall out of date in
    the future.
    
    The two controllers included in this commit are called
    `GasPricesController` and `PetNamesController`, which are roughly based
    on, but intentionally not drawn from, `GasFeeController` and
    `AddressBookController`. They demonstrate the following best practices:
    
    - Setting up a common structure for controllers, including creating
    complete types for the messenger and for state
    - Using the messenger to access data from another controller
    - Using service objects to make HTTP requests
    - Mocking the messenger in tests
    - Creating mock service objects
    - Mocking time in tests
    
    Certainly, more best practices can be demonstrated, but this should be a
    good first start.

---
+++ examples/example-controllers/src/gas-prices-controller.test.ts
@@ -0,0 +1,159 @@
+import { ControllerMessenger } from '@metamask/base-controller';
+
+import type {
+  ExtractAvailableAction,
+  ExtractAvailableEvent,
+} from '../tests/helpers';
+import type { GasPricesControllerMessenger } from './gas-prices-controller';
+import { GasPricesController } from './gas-prices-controller';
+import type { AbstractGasPricesService } from './gas-prices-service/abstract-gas-prices-service';
+import {
+  getDefaultNetworkControllerState,
+  type NetworkControllerGetStateAction,
+} from './network-controller-types';
+
+describe('GasPricesController', () => {
+  describe('constructor', () => {
+    it('uses all of the given state properties to initialize state', () => {
+      const gasPricesService = buildGasPricesService();
+      const givenState = {
+        gasPricesByChainId: {
+          '0x1': {
+            low: 10,
+            average: 15,
+            high: 20,
+            fetchedDate: '2024-01-01',
+          },
+        },
+      };
+      const controller = new GasPricesController({
+        messenger: getControllerMessenger(),
+        state: givenState,
+        gasPricesService,
+      });
+
+      expect(controller.state).toStrictEqual(givenState);
+    });
+
+    it('fills in missing state properties with default values', () => {
+      const gasPricesService = buildGasPricesService();
+      const controller = new GasPricesController({
+        messenger: getControllerMessenger(),
+        gasPricesService,
+      });
+
+      expect(controller.state).toMatchInlineSnapshot(`
+        Object {
+          "gasPricesByChainId": Object {},
+        }
+      `);
+    });
+  });
+
+  describe('updateGasPrices', () => {
+    beforeEach(() => {
+      jest.useFakeTimers().setSystemTime(new Date('2024-01-02'));
+    });
+
+    afterEach(() => {
+      jest.useRealTimers();
+    });
+
+    it('fetches gas prices for the current chain through the service object and updates state accordingly', async () => {
+      const gasPricesService = buildGasPricesService();
+      jest.spyOn(gasPricesService, 'fetchGasPrices').mockResolvedValue({
+        low: 5,
+        average: 10,
+        high: 15,
+      });
+      const rootMessenger = getRootControllerMessenger({
+        networkControllerGetStateActionHandler: () => ({
+          ...getDefaultNetworkControllerState(),
+          chainId: '0x42',
+        }),
+      });
+      const controller = new GasPricesController({
+        messenger: getControllerMessenger(rootMessenger),
+        gasPricesService,
+      });
+
+      await controller.updateGasPrices();
+
+      expect(controller.state).toStrictEqual({
+        gasPricesByChainId: {
+          '0x42': {
+            low: 5,
+            average: 10,
+            high: 15,
+            fetchedDate: '2024-01-02T00:00:00.000Z',
+          },
+        },
+      });
+    });
+  });
+});
+
+/**
+ * The union of actions that the root messenger allows.
+ */
+type RootAction = ExtractAvailableAction<GasPricesControllerMessenger>;
+
+/**
+ * The union of events that the root messenger allows.
+ */
+type RootEvent = ExtractAvailableEvent<GasPricesControllerMessenger>;
+
+/**
+ * Constructs the unrestricted messenger. This can be used to call actions and
+ * publish events within the tests for this controller.
+ *
+ * @param args - The arguments to this function.
+ * @param args.networkControllerGetStateActionHandler - Used to mock the
+ * `NetworkController:getState` action on the messenger.
+ * @returns The unrestricted messenger suited for GasPricesController.
+ */
+function getRootControllerMessenger({
+  networkControllerGetStateActionHandler = jest
+    .fn<
+      ReturnType<NetworkControllerGetStateAction['handler']>,
+      Parameters<NetworkControllerGetStateAction['handler']>
+    >()
+    .mockReturnValue(getDefaultNetworkControllerState()),
+}: {
+  networkControllerGetStateActionHandler?: NetworkControllerGetStateAction['handler'];
+} = {}): ControllerMessenger<RootAction, RootEvent> {
+  const rootMessenger = new ControllerMessenger<RootAction, RootEvent>();
+  rootMessenger.registerActionHandler(
+    'NetworkController:getState',
+    networkControllerGetStateActionHandler,
+  );
+  return rootMessenger;
+}
+
+/**
+ * Constructs the messenger which is restricted to relevant GasPricesController
+ * actions and events.
+ *
+ * @param rootMessenger - The root messenger to restrict.
+ * @returns The restricted messenger.
+ */
+function getControllerMessenger(
+  rootMessenger = getRootControllerMessenger(),
+): GasPricesControllerMessenger {
+  return rootMessenger.getRestricted({
+    name: 'GasPricesController',
+    allowedActions: ['NetworkController:getState'],
+    allowedEvents: [],
+  });
+}
+
+/**
+ * Constructs a mock GasPricesService object for use in testing.
+ *
+ * @returns The mock GasPricesService object.
+ */
+function buildGasPricesService(): AbstractGasPricesService {
+  return {
+    fetchGasPrices: jest.fn(),
+  };
+}
---
+++ examples/example-controllers/tests/helpers.ts
@@ -0,0 +1,30 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+import type { RestrictedControllerMessenger } from '@metamask/base-controller';
+
+// We don't care about the types marked with `any` for this type.
+export type ExtractAvailableAction<Messenger> =
+  Messenger extends RestrictedControllerMessenger<
+    any,
+    infer Action,
+    any,
+    any,
+    any
+  >
+    ? Action
+    : never;
+/* eslint-enable @typescript-eslint/no-explicit-any */
+
+/* eslint-disable @typescript-eslint/no-explicit-any */
+// We don't care about the types marked with `any` for this type.
+export type ExtractAvailableEvent<Messenger> =
+  Messenger extends RestrictedControllerMessenger<
+    any,
+    any,
+    infer Event,
+    any,
+    any
+  >
+    ? Event
+    : never;
+/* eslint-enable @typescript-eslint/no-explicit-any */
