98b454431e460b669af0d6e7ced7f9779a6f3584
Jongsun Suh <jongsun.suh@icloud.com>
2024-08-01 12:38:37 -0400

    Explicitly enumerate package-level exports from `ControllerMessenger`

--- packages/base-controller/src/index.ts
+++ packages/base-controller/src/index.ts
@@ -28,6 +28,20 @@ export {
   getPersistentState,
   isBaseController,
 } from './BaseControllerV2';
-export * from './ControllerMessenger';
+export type {
+  ActionHandler,
+  ExtractActionParameters,
+  ExtractActionResponse,
+  ExtractEventHandler,
+  ExtractEventPayload,
+  GenericEventHandler,
+  SelectorFunction,
+  ActionConstraint,
+  EventConstraint,
+  NamespacedBy,
+  NotNamespacedBy,
+  NamespacedName,
+} from './ControllerMessenger';
+export { ControllerMessenger } from './ControllerMessenger';
 export type { RestrictedControllerMessengerConstraint } from './RestrictedControllerMessenger';
 export { RestrictedControllerMessenger } from './RestrictedControllerMessenger';
