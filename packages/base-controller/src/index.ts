36b11c4fc21cb240ba4fd4f19a1ca4c3f7401c3b
Jongsun Suh <jongsun.suh@icloud.com>
2024-08-01 12:37:48 -0400

    Move utility types and functions currently defined in composable-controller to base-controller

--- packages/base-controller/src/index.ts
+++ packages/base-controller/src/index.ts
@@ -1,11 +1,24 @@
-export type { BaseConfig, BaseState, Listener } from './BaseControllerV1';
-export { BaseControllerV1 } from './BaseControllerV1';
 export type {
+  BaseConfig,
+  BaseControllerV1Instance,
+  BaseState,
+  ConfigConstraint as ConfigConstraintV1,
+  Listener,
+  StateConstraint as StateConstraintV1,
+} from './BaseControllerV1';
+export { BaseControllerV1, isBaseControllerV1 } from './BaseControllerV1';
+export type {
+  BaseControllerInstance,
+  ControllerInstance,
   Listener as ListenerV2,
   StateConstraint,
+  LegacyControllerStateConstraint,
   StateDeriver,
+  StateDeriverConstraint,
   StateMetadata,
+  StateMetadataConstraint,
   StatePropertyMetadata,
+  StatePropertyMetadataConstraint,
   ControllerGetStateAction,
   ControllerStateChangeEvent,
 } from './BaseControllerV2';
@@ -13,6 +26,8 @@ export {
   BaseController,
   getAnonymizedState,
   getPersistentState,
+  isBaseController,
 } from './BaseControllerV2';
 export * from './ControllerMessenger';
-export * from './RestrictedControllerMessenger';
+export type { RestrictedControllerMessengerConstraint } from './RestrictedControllerMessenger';
+export { RestrictedControllerMessenger } from './RestrictedControllerMessenger';
