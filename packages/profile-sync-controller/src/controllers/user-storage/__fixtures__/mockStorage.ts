29220af1a366f86f4cf69458cf9d0afa0a94eda6
Prithpal Sooriya <prithpal.sooriya@consensys.net>
2024-08-02 19:19:20 +0100

    docs: update dev note on fixtures

--- packages/profile-sync-controller/src/controllers/user-storage/__fixtures__/mockStorage.ts
+++ packages/profile-sync-controller/src/controllers/user-storage/__fixtures__/mockStorage.ts
@@ -4,7 +4,7 @@ export const MOCK_STORAGE_KEY_SIGNATURE = 'mockStorageKey';
 export const MOCK_STORAGE_KEY = createSHA256Hash(MOCK_STORAGE_KEY_SIGNATURE);
 export const MOCK_STORAGE_DATA = JSON.stringify({ hello: 'world' });
 
-// NOTE - bug on mobile that does not work well with encryption.encryptString on start time.
+// NOTE - using encryption.encryptString directly in fixtures causes issues on mobile.
 // This is because this fixture is getting added in at run time. Will be improved once we support multiple exports
 export const MOCK_ENCRYPTED_STORAGE_DATA = () =>
   encryption.encryptString(MOCK_STORAGE_DATA, MOCK_STORAGE_KEY);
