cd583cce79c55ec74c9133deee2ba40893d12104
Mathieu Artu <mathieu.artu@consensys.net>
2024-08-02 09:45:33 +0200

    feat: add nameLastUpdatedAt timestamp when setting account name

--- packages/accounts-controller/src/AccountsController.ts
+++ packages/accounts-controller/src/AccountsController.ts
@@ -422,7 +422,10 @@ export class AccountsController extends BaseController<
       throw new Error('Account name already exists');
     }
 
-    this.updateAccountMetadata(accountId, { name: accountName });
+    this.updateAccountMetadata(accountId, {
+      name: accountName,
+      nameLastUpdatedAt: Date.now(),
+    });
   }
 
   /**
@@ -445,7 +448,7 @@ export class AccountsController extends BaseController<
       };
       // Do not remove this comment - This error is flaky: Comment out or restore the `ts-expect-error` directive below as needed.
       // See: https://github.com/MetaMask/utils/issues/168
-      // // @ts-expect-error Known issue - `Json` causes recursive error in immer `Draft`/`WritableDraft` types
+      // @ts-expect-error Known issue - `Json` causes recursive error in immer `Draft`/`WritableDraft` types
       currentState.internalAccounts.accounts[accountId] = internalAccount;
     });
   }
