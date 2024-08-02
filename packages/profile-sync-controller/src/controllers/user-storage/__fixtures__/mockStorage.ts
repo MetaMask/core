78b0d599536f601d7f9b7fd7fcd73160905b9d54
Prithpal Sooriya <prithpal.sooriya@consensys.net>
2024-08-02 19:12:06 +0100

    fix: make fixture constants lazy
    this is a temp fix so that mobile does not compute these fixtures are runtime.
    we do need to support multiple exports to ensure that these are not called.

--- packages/profile-sync-controller/src/controllers/user-storage/__fixtures__/mockStorage.ts
+++ packages/profile-sync-controller/src/controllers/user-storage/__fixtures__/mockStorage.ts
@@ -3,7 +3,8 @@ import encryption, { createSHA256Hash } from '../encryption';
 export const MOCK_STORAGE_KEY_SIGNATURE = 'mockStorageKey';
 export const MOCK_STORAGE_KEY = createSHA256Hash(MOCK_STORAGE_KEY_SIGNATURE);
 export const MOCK_STORAGE_DATA = JSON.stringify({ hello: 'world' });
-export const MOCK_ENCRYPTED_STORAGE_DATA = encryption.encryptString(
-  MOCK_STORAGE_DATA,
-  MOCK_STORAGE_KEY,
-);
+
+// NOTE - bug on mobile that does not work well with encryption.encryptString on start time.
+// This is because this fixture is getting added in at run time. Will be improved once we support multiple exports
+export const MOCK_ENCRYPTED_STORAGE_DATA = () =>
+  encryption.encryptString(MOCK_STORAGE_DATA, MOCK_STORAGE_KEY);
