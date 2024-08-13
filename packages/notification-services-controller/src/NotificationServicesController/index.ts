8a035a4655f4c217b138bff0b68283964f6eccde
Prithpal Sooriya <prithpal.sooriya@consensys.net>
2024-08-13 14:44:32 +0100

    refactor: add multiple file exports for notification-services-controller

--- packages/notification-services-controller/src/NotificationServicesController/index.ts
+++ packages/notification-services-controller/src/NotificationServicesController/index.ts
@@ -1,9 +1,10 @@
 import Controller from './NotificationServicesController';
 
+const NotificationServicesController = Controller;
 export { Controller };
+export default NotificationServicesController;
 export * from './NotificationServicesController';
 export * as Types from './types';
-export * as Mocks from './__fixtures__';
 export * as Processors from './processors';
 export * as Constants from './constants';
 export * as UI from './ui';
--- packages/notification-services-controller/src/NotificationServicesPushController/index.ts
+++ packages/notification-services-controller/src/NotificationServicesPushController/index.ts
@@ -1,7 +1,8 @@
 import Controller from './NotificationServicesPushController';
 
+const NotificationServicesPushController = Controller;
 export { Controller };
+export default NotificationServicesPushController;
 export * from './NotificationServicesPushController';
 export * as Types from './types';
 export * as Utils from './utils';
-export * as Mocks from './__fixtures__';
