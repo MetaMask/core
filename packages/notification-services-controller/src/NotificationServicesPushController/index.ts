b4d5f3e7d6178634fed13067de53565b5e8070b5
Prithpal Sooriya <prithpal.sooriya@consensys.net>
2024-09-03 14:36:50 +0100

    refactor(notifications): cleanup notification subpath exports (#4650)
    ## Explanation
    
    This cleans up the notification team packages subpath exports
    (`@metamask/profile-sync-controller` &
    `@metamask/notification-services-controller`).
    
    Packages will now only export:
    - Main/root (where there is a barrel file containing all files)
    - `/mocks` for accessing mock data (also available as a named export in
    root)
    - `/ui` for some UI/framework files for specific platforms (web vs
    mobile). This is subject to change (we may have a clearer distinction
    for platforms, such as a React folder vs ReactNative folder).
    
    
    We also fix a `jest-haste-map` silent error that was introduced due to
    subpath modules.
    - Fix removes the internal subpath package.json names as these are
    internal only files.
    
    <details><summary>**jest haste map silent error**</summary>
    
    This was a silent error (CI still passes)
    
    ![Screenshot 2024-09-03 at 14 08
    44](https://github.com/user-attachments/assets/24738886-3aab-4077-9c44-173b76df62e2)
    
    </details> 
    
    ## References
    
    
    ## Changelog
    
    <!--
    If you're making any consumer-facing changes, list those changes here as
    if you were updating a changelog, using the template below as a guide.
    
    (CATEGORY is one of BREAKING, ADDED, CHANGED, DEPRECATED, REMOVED, or
    FIXED. For security-related issues, follow the Security Advisory
    process.)
    
    Please take care to name the exact pieces of the API you've added or
    changed (e.g. types, interfaces, functions, or methods).
    
    If there are any breaking changes, make sure to offer a solution for
    consumers to follow once they upgrade to the changes.
    
    Finally, if you're only making changes to development scripts or tests,
    you may replace the template below with "None".
    -->
    
    ### `@metamask/profile-sync-controller`
    
    - **CHANGED**: Updated subpath package.json files to be internal only to
    resolve the `jest-haste-map` errors.
    
    ### `@metamask/notification-services-controller`
    
    - **CHANGED**: Updated subpath package.json files to be internal only to
    resolve the `jest-haste-map` errors.
    - **REMOVED/BREAKING**: Removed `/types`, `/processors`, `/constants`
    subpath exports. These are still accessible through root export.
    
    ## Checklist
    
    - [x] I've updated the test suite for new or updated code as appropriate
    - [x] I've updated documentation (JSDoc, Markdown, etc.) for new or
    updated code as appropriate
    - [x] I've highlighted breaking changes using the "BREAKING" category
    above as appropriate

--- packages/notification-services-controller/src/NotificationServicesPushController/index.ts
+++ packages/notification-services-controller/src/NotificationServicesPushController/index.ts
@@ -5,5 +5,7 @@ export { Controller };
 export default NotificationServicesPushController;
 export * from './NotificationServicesPushController';
 export * as Types from './types';
+export * from './types';
 export * as Utils from './utils';
+export * from './utils';
 export * as Mocks from './__fixtures__';
