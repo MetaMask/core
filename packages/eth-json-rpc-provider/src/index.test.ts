a133c5064f517f200c22625925ded3407d0e8fd6
Jongsun Suh <jongsun.suh@icloud.com>
2024-09-05 13:50:18 -0400

    Remove `Array` from snapshots

--- packages/eth-json-rpc-provider/src/index.test.ts
+++ packages/eth-json-rpc-provider/src/index.test.ts
@@ -3,7 +3,7 @@ import * as allExports from '.';
 describe('Package exports', () => {
   it('has expected exports', () => {
     expect(Object.keys(allExports)).toMatchInlineSnapshot(`
-      Array [
+      [
         "SafeEventEmitterProvider",
         "providerFromEngine",
         "providerFromMiddleware",
