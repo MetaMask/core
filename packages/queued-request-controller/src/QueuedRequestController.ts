09a2ffd531b10951033daf75698acc767cd7e7bf
Alex <adonesky@gmail.com>
2024-09-25 15:01:00 -0500

    cleanup

--- packages/queued-request-controller/src/QueuedRequestController.ts
+++ packages/queued-request-controller/src/QueuedRequestController.ts
@@ -257,7 +257,6 @@ export class QueuedRequestController extends BaseController<
    * This should be called after a batch of requests has finished processing, if the queue is non-
    * empty.
    */
-  // TODO this needs to be modified for queued requests from the same origin but targeting different networkClientIds
   async #processNextBatch() {
     const firstRequest = this.#requestQueue.shift() as QueuedRequest;
     this.#networkClientIdOfCurrentBatch = firstRequest.networkClientId;
