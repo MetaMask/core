af65ed95db87ecbccd6cc2fdcadb79b81e9d266e
Derek Brans <dbrans@gmail.com>
2024-10-09 13:40:28 -0400

    update TxC/jest.config.js

--- packages/transaction-controller/jest.config.js
+++ packages/transaction-controller/jest.config.js
@@ -18,7 +18,7 @@ module.exports = merge(baseConfig, {
   coverageThreshold: {
     global: {
       branches: 94.42,
-      functions: 97.46,
+      functions: 97.45,
       lines: 98.44,
       statements: 98.46,
     },
