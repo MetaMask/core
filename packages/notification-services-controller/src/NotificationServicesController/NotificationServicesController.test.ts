dce046fa24ac540fd2fa4dd64dd07f1d2918ad8c
Hassan Malik <hbmalik88@gmail.com>
2024-11-02 04:19:45 +0900

    fix disable notifications

--- packages/notification-services-controller/src/NotificationServicesController/NotificationServicesController.test.ts
+++ packages/notification-services-controller/src/NotificationServicesController/NotificationServicesController.test.ts
@@ -43,6 +43,7 @@ import { processSnapNotification } from './processors/process-snap-notifications
 import * as OnChainNotifications from './services/onchain-notifications';
 import type { UserStorage } from './types/user-storage/user-storage';
 import * as Utils from './utils/utils';
+import { INotification } from './types';
 
 // Mock type used for testing purposes
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -834,7 +835,13 @@ describe('metamask-notifications - disableMetamaskNotifications()', () => {
     const controller = new NotificationServicesController({
       messenger: mocks.messenger,
       env: { featureAnnouncements: featureAnnouncementsEnv },
-      state: { isNotificationServicesEnabled: true },
+      state: {
+        isNotificationServicesEnabled: true,
+        metamaskNotificationsList: [
+          createMockFeatureAnnouncementRaw() as INotification,
+          createMockSnapNotification() as INotification,
+        ],
+      },
     });
 
     const promise = controller.disableNotificationServices();
@@ -847,6 +854,9 @@ describe('metamask-notifications - disableMetamaskNotifications()', () => {
     // Act - final state
     expect(controller.state.isUpdatingMetamaskNotifications).toBe(false);
     expect(controller.state.isNotificationServicesEnabled).toBe(false);
+    expect(controller.state.metamaskNotificationsList).toStrictEqual([
+      createMockSnapNotification(),
+    ]);
 
     expect(mocks.mockDisablePushNotifications).toHaveBeenCalled();
 
