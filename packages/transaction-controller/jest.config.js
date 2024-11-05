70f789615269e238f60e883f990330a46bb615c1
OGPoyraz <omergoktugpoyraz@gmail.com>
2024-11-05 15:33:09 +0100

    Update coverage thresholds

--- packages/transaction-controller/jest.config.js
+++ packages/transaction-controller/jest.config.js
@@ -17,10 +17,10 @@ module.exports = merge(baseConfig, {
   // An object that configures minimum threshold enforcement for coverage results
   coverageThreshold: {
     global: {
-      branches: 93.74,
-      functions: 97.51,
-      lines: 98.34,
-      statements: 98.35,
+      branches: 93.46,
+      functions: 97.31,
+      lines: 98.25,
+      statements: 98.25,
     },
   },
 
