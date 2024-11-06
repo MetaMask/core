108749f357385188b88ef408c09f5c940a428486
Jyoti Puri <jyotipuri@gmail.com>
2024-11-06 21:31:01 +0530

    update

--- packages/signature-controller/src/constants.ts
+++
@@ -1,8 +0,0 @@
-export const EthMethod = {
-  PersonalSign: 'personal_sign',
-  Sign: 'eth_sign',
-  SignTransaction: 'eth_signTransaction',
-  SignTypedDataV1: 'eth_signTypedData_v1',
-  SignTypedDataV3: 'eth_signTypedData_v3',
-  SignTypedDataV4: 'eth_signTypedData_v4',
-};
