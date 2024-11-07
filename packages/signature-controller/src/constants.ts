3bd61bfaccfdd8deba54d625a763bd6c0245dbdc
Jyoti Puri <jyotipuri@gmail.com>
2024-11-08 00:30:49 +0530

    Update

--- packages/signature-controller/src/constants.ts
+++ packages/signature-controller/src/constants.ts
@@ -1,20 +1,20 @@
 /**
  * Supported signature methods.
  */
-export const EthMethod = {
-  PersonalSign: 'personal_sign',
-  SignTransaction: 'eth_signTransaction',
-  SignTypedDataV1: 'eth_signTypedData_v1',
-  SignTypedDataV3: 'eth_signTypedData_v3',
-  SignTypedDataV4: 'eth_signTypedData_v4',
-};
+export enum EthMethod {
+  PersonalSign = 'personal_sign',
+  SignTransaction = 'eth_signTransaction',
+  SignTypedDataV1 = 'eth_signTypedData_v1',
+  SignTypedDataV3 = 'eth_signTypedData_v3',
+  SignTypedDataV4 = 'eth_signTypedData_v4',
+}
 
 /** Different decoding data state change types */
-export const DecodingDataChangeType = {
-  Receive: 'RECEIVE',
-  Transfer: 'TRANSFER',
-  Approve: 'APPROVE',
-  Revoke: 'REVOKE_APPROVE',
-  Bidding: 'BIDDING',
-  Listing: 'LISTING',
-};
+export enum DecodingDataChangeType {
+  Receive = 'RECEIVE',
+  Transfer = 'TRANSFER',
+  Approve = 'APPROVE',
+  Revoke = 'REVOKE_APPROVE',
+  Bidding = 'BIDDING',
+  Listing = 'LISTING',
+}
