86450dcb1d6127ffe05cf3678d6c73c10afd4b30
Alex <adonesky@gmail.com>
2024-11-11 11:34:33 -0600

    add caip25 error code snapshot tests

--- packages/multichain/src/scope/errors.test.ts
+++ packages/multichain/src/scope/errors.test.ts
@@ -5,17 +5,22 @@ describe('Caip25Errors', () => {
     expect(Caip25Errors.requestedChainsNotSupportedError().message).toBe(
       'Requested chains are not supported',
     );
+    expect(Caip25Errors.requestedChainsNotSupportedError().code).toBe(5100);
   });
 
   it('requestedMethodsNotSupportedError', () => {
     expect(Caip25Errors.requestedMethodsNotSupportedError().message).toBe(
       'Requested methods are not supported',
     );
+    expect(Caip25Errors.requestedMethodsNotSupportedError().code).toBe(5101);
   });
 
   it('requestedNotificationsNotSupportedError', () => {
     expect(Caip25Errors.requestedNotificationsNotSupportedError().message).toBe(
       'Requested notifications are not supported',
+    );
+    expect(Caip25Errors.requestedNotificationsNotSupportedError().code).toBe(
+      5102,
     );
   });
 
@@ -23,11 +28,13 @@ describe('Caip25Errors', () => {
     expect(Caip25Errors.unknownMethodsRequestedError().message).toBe(
       'Unknown method(s) requested',
     );
+    expect(Caip25Errors.unknownMethodsRequestedError().code).toBe(5201);
   });
 
   it('unknownNotificationsRequestedError', () => {
     expect(Caip25Errors.unknownNotificationsRequestedError().message).toBe(
       'Unknown notification(s) requested',
     );
+    expect(Caip25Errors.unknownNotificationsRequestedError().code).toBe(5202);
   });
 });
