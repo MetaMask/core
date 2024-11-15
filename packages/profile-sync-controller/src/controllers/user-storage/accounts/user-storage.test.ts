e23a91fa8a696fc8aca935b76859b96983370325
Mathieu Artu <mathieu.artu@consensys.net>
2024-11-15 13:41:19 +0100

    fix: tests

--- packages/profile-sync-controller/src/controllers/user-storage/accounts/user-storage.test.ts
+++ packages/profile-sync-controller/src/controllers/user-storage/accounts/user-storage.test.ts
@@ -1,8 +1,5 @@
 import { getMockRandomDefaultAccountName } from '../__fixtures__/mockAccounts';
-import {
-  isNameDefaultAccountName,
-  getDefaultNameAccountNumber,
-} from './user-storage';
+import { isNameDefaultAccountName } from './user-storage';
 
 describe('user-storage/acounts/isNameDefaultAccountName', () => {
   it('should return true for default account names', () => {
@@ -20,26 +17,5 @@ describe('user-storage/acounts/isNameDefaultAccountName', () => {
   it('should return false for non-default account names', () => {
     expect(isNameDefaultAccountName('My Account')).toBe(false);
     expect(isNameDefaultAccountName('Mon compte 34')).toBe(false);
-  });
-});
-
-describe('user-storage/acounts/getDefaultNameAccountNumber', () => {
-  it('should return the account number for a default account name', () => {
-    expect(
-      getDefaultNameAccountNumber(`${getMockRandomDefaultAccountName()} 89`),
-    ).toBe(89);
-    expect(
-      getDefaultNameAccountNumber(`${getMockRandomDefaultAccountName()} 1`),
-    ).toBe(1);
-    expect(
-      getDefaultNameAccountNumber(
-        `${getMockRandomDefaultAccountName()} 123543`,
-      ),
-    ).toBe(123543);
-  });
-
-  it('should return false for non-default account names', () => {
-    expect(getDefaultNameAccountNumber('My Account')).toBeUndefined();
-    expect(getDefaultNameAccountNumber('Mon compte 34')).toBeUndefined();
   });
 });
