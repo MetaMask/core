de5d2fda19732c60260a0bf1ce1dcc40d2d9f3b7
Matthew Walsh <matthew.walsh@consensys.net>
2024-11-20 23:43:50 +0000

    Update coverage targets

--- packages/transaction-controller/jest.config.js
+++ packages/transaction-controller/jest.config.js
@@ -17,10 +17,10 @@ module.exports = merge(baseConfig, {
   // An object that configures minimum threshold enforcement for coverage results
   coverageThreshold: {
     global: {
-      branches: 93.74,
-      functions: 97.51,
-      lines: 98.34,
-      statements: 98.35,
+      branches: 91.78,
+      functions: 95.53,
+      lines: 97.75,
+      statements: 97.69,
     },
   },
 
