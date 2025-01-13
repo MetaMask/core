ff896e3680db790afc7be111cd12cc64111d5c15
Antonio Regadas <antonio.regadas@consensys.net>
2025-01-13 10:52:57 +0000

    chore: reset file

--- packages/multichain-transactions-controller/jest.config.js
+++ packages/multichain-transactions-controller/jest.config.js
@@ -17,10 +17,10 @@ module.exports = merge(baseConfig, {
   // An object that configures minimum threshold enforcement for coverage results
   coverageThreshold: {
     global: {
-      branches: 100,
-      functions: 100,
-      lines: 100,
-      statements: 100,
+      branches: 80,
+      functions: 95,
+      lines: 90,
+      statements: 90,
     },
   },
 });
