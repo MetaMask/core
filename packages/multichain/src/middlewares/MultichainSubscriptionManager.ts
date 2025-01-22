16b3c05865c7e09870be73ca069fa2311e7c1303
jiexi <jiexiluan@gmail.com>
2025-01-22 11:05:01 -0800

    feat: Add Multichain API to `@metamask/multichain` (#4813)

---
+++ packages/multichain/src/middlewares/MultichainSubscriptionManager.ts
@@ -0,0 +1,173 @@
+import { toHex } from '@metamask/controller-utils';
+import createSubscriptionManager from '@metamask/eth-json-rpc-filters/subscriptionManager';
+import type { NetworkController } from '@metamask/network-controller';
+import SafeEventEmitter from '@metamask/safe-event-emitter';
+import type { CaipChainId, Hex } from '@metamask/utils';
+import { parseCaipChainId } from '@metamask/utils';
+
+import type { ExternalScopeString } from '../scope/types';
+import type { ExtendedJsonRpcMiddleware } from './MultichainMiddlewareManager';
+
+export type SubscriptionManager = {
+  events: SafeEventEmitter;
+  destroy?: () => void;
+  middleware: ExtendedJsonRpcMiddleware;
+};
+
+type SubscriptionNotificationEvent = {
+  jsonrpc: '2.0';
+  method: 'eth_subscription';
+  params: {
+    subscription: Hex;
+    result: unknown;
+  };
+};
+
+type SubscriptionKey = {
+  scope: ExternalScopeString;
+  origin: string;
+  tabId?: number;
+};
+type SubscriptionEntry = SubscriptionKey & {
+  subscriptionManager: SubscriptionManager;
+};
+
+type MultichainSubscriptionManagerOptions = {
+  findNetworkClientIdByChainId: NetworkController['findNetworkClientIdByChainId'];
+  getNetworkClientById: NetworkController['getNetworkClientById'];
+};
+
+/**
+ * A helper that facilates the lifecycle of a SubscriptionManager instance that
+ * is meant to handle subscriptons for only one specific scope, origin, and tabId combination.
+ */
+export class MultichainSubscriptionManager extends SafeEventEmitter {
+  #findNetworkClientIdByChainId: NetworkController['findNetworkClientIdByChainId'];
+
+  #getNetworkClientById: NetworkController['getNetworkClientById'];
+
+  #subscriptions: SubscriptionEntry[] = [];
+
+  /**
+   * Construct a MultichainSubscriptionManager.
+   *
+   * @param options - The controller options.
+   * @param options.findNetworkClientIdByChainId - The hook to get the networkClientId from a chainId.
+   * @param options.getNetworkClientById - The hook to get the network client instance by its networkClientId.
+   */
+  constructor(options: MultichainSubscriptionManagerOptions) {
+    super();
+    this.#findNetworkClientIdByChainId = options.findNetworkClientIdByChainId;
+    this.#getNetworkClientById = options.getNetworkClientById;
+  }
+
+  notify(
+    { scope, origin, tabId }: SubscriptionKey,
+    { method, params }: SubscriptionNotificationEvent,
+  ) {
+    this.emit('notification', origin, tabId, {
+      method: 'wallet_notify',
+      params: {
+        scope,
+        notification: { method, params },
+      },
+    });
+  }
+
+  #getSubscriptionEntry({
+    scope,
+    origin,
+    tabId,
+  }: SubscriptionKey): SubscriptionEntry | undefined {
+    return this.#subscriptions.find((subscriptionEntry) => {
+      return (
+        subscriptionEntry.scope === scope &&
+        subscriptionEntry.origin === origin &&
+        subscriptionEntry.tabId === tabId
+      );
+    });
+  }
+
+  #removeSubscriptionEntry({ scope, origin, tabId }: SubscriptionEntry) {
+    this.#subscriptions = this.#subscriptions.filter((subscriptionEntry) => {
+      return (
+        subscriptionEntry.scope !== scope ||
+        subscriptionEntry.origin !== origin ||
+        subscriptionEntry.tabId !== tabId
+      );
+    });
+  }
+
+  subscribe(subscriptionKey: SubscriptionKey) {
+    const subscriptionEntry = this.#getSubscriptionEntry(subscriptionKey);
+    if (subscriptionEntry) {
+      return subscriptionEntry.subscriptionManager;
+    }
+
+    const networkClientId = this.#findNetworkClientIdByChainId(
+      toHex(parseCaipChainId(subscriptionKey.scope as CaipChainId).reference),
+    );
+    const networkClient = this.#getNetworkClientById(networkClientId);
+    const subscriptionManager = createSubscriptionManager({
+      blockTracker: networkClient.blockTracker,
+      provider: networkClient.provider,
+    });
+
+    subscriptionManager.events.on(
+      'notification',
+      (message: SubscriptionNotificationEvent) => {
+        this.notify(subscriptionKey, message);
+      },
+    );
+
+    const newSubscriptionManagerEntry = {
+      ...subscriptionKey,
+      subscriptionManager,
+    };
+    subscriptionManager.destroy = subscriptionManager.middleware.destroy;
+    subscriptionManager.middleware.destroy = this.#unsubscribe.bind(
+      this,
+      newSubscriptionManagerEntry,
+    );
+
+    this.#subscriptions.push(newSubscriptionManagerEntry);
+
+    return subscriptionManager;
+  }
+
+  #unsubscribe(subscriptionEntry: SubscriptionEntry) {
+    subscriptionEntry.subscriptionManager.destroy?.();
+
+    this.#removeSubscriptionEntry(subscriptionEntry);
+  }
+
+  unsubscribeByScope(scope: ExternalScopeString) {
+    this.#subscriptions.forEach((subscriptionEntry) => {
+      if (subscriptionEntry.scope === scope) {
+        this.#unsubscribe(subscriptionEntry);
+      }
+    });
+  }
+
+  unsubscribeByScopeAndOrigin(scope: ExternalScopeString, origin: string) {
+    this.#subscriptions.forEach((subscriptionEntry) => {
+      if (
+        subscriptionEntry.scope === scope &&
+        subscriptionEntry.origin === origin
+      ) {
+        this.#unsubscribe(subscriptionEntry);
+      }
+    });
+  }
+
+  unsubscribeByOriginAndTabId(origin: string, tabId?: number) {
+    this.#subscriptions.forEach((subscriptionEntry) => {
+      if (
+        subscriptionEntry.origin === origin &&
+        subscriptionEntry.tabId === tabId
+      ) {
+        this.#unsubscribe(subscriptionEntry);
+      }
+    });
+  }
+}
