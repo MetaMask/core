4bfc7f444d1b2829f95db7236c30f4c7682af1c5
Salah-Eddine Saakoun <salah-eddine.saakoun@consensys.net>
2025-01-28 18:34:55 +0100

    fix: add missing abi

--- packages/transaction-controller/src/helpers/MethodDataHelper.ts
+++ packages/transaction-controller/src/helpers/MethodDataHelper.ts
@@ -11,6 +11,77 @@ import type { MethodData } from '../TransactionController';
 
 const log = createModuleLogger(projectLogger, 'method-data');
 
+const methodRegistryAbi = [
+  {
+    constant: false,
+    inputs: [{ name: '_new', type: 'address' }],
+    name: 'setOwner',
+    outputs: [],
+    payable: false,
+    type: 'function',
+  },
+  {
+    constant: true,
+    inputs: [],
+    name: 'totalSignatures',
+    outputs: [{ name: '', type: 'uint256' }],
+    payable: false,
+    type: 'function',
+  },
+  {
+    constant: true,
+    inputs: [],
+    name: 'owner',
+    outputs: [{ name: '', type: 'address' }],
+    payable: false,
+    type: 'function',
+  },
+  {
+    constant: false,
+    inputs: [],
+    name: 'drain',
+    outputs: [],
+    payable: false,
+    type: 'function',
+  },
+  {
+    constant: true,
+    inputs: [{ name: '', type: 'bytes4' }],
+    name: 'entries',
+    outputs: [{ name: '', type: 'string' }],
+    payable: false,
+    type: 'function',
+  },
+  {
+    constant: false,
+    inputs: [{ name: '_method', type: 'string' }],
+    name: 'register',
+    outputs: [{ name: '', type: 'bool' }],
+    payable: false,
+    type: 'function',
+  },
+  { inputs: [], type: 'constructor' },
+  {
+    anonymous: false,
+    inputs: [
+      { indexed: true, name: 'creator', type: 'address' },
+      { indexed: true, name: 'signature', type: 'bytes4' },
+      { indexed: false, name: 'method', type: 'string' },
+    ],
+    name: 'Registered',
+    type: 'event',
+  },
+  {
+    anonymous: false,
+    inputs: [
+      { indexed: true, name: 'old', type: 'address' },
+      { indexed: true, name: 'current', type: 'address' },
+    ],
+    name: 'NewOwner',
+    type: 'event',
+  },
+];
+
 export class MethodDataHelper {
   hub: EventEmitter;
 
@@ -46,7 +117,7 @@ export class MethodDataHelper {
       let iface = this.#interfaceByNetworkClientId.get(networkClientId);
 
       if (!iface) {
-        iface = new Interface([]);
+        iface = new Interface(methodRegistryAbi);
         this.#interfaceByNetworkClientId.set(networkClientId, iface);
         log('Created interface', networkClientId);
       }
