934d4c6700cf35f9a3a36bbb41e5a1aa8e83140d
Salah-Eddine Saakoun <salah-eddine.saakoun@consensys.net>
2025-01-29 16:32:10 +0100

    refactor: move method registry ABI

--- packages/transaction-controller/src/helpers/MethodDataHelper.ts
+++ packages/transaction-controller/src/helpers/MethodDataHelper.ts
@@ -6,81 +6,11 @@ import { Mutex } from 'async-mutex';
 // eslint-disable-next-line import-x/no-nodejs-modules
 import EventEmitter from 'events';
 
+import { METHOD_REGISTRY_ABI } from '../constants';
 import { projectLogger } from '../logger';
 import type { MethodData } from '../TransactionController';
 
 const log = createModuleLogger(projectLogger, 'method-data');
-
-const methodRegistryAbi = [
-  {
-    constant: false,
-    inputs: [{ name: '_new', type: 'address' }],
-    name: 'setOwner',
-    outputs: [],
-    payable: false,
-    type: 'function',
-  },
-  {
-    constant: true,
-    inputs: [],
-    name: 'totalSignatures',
-    outputs: [{ name: '', type: 'uint256' }],
-    payable: false,
-    type: 'function',
-  },
-  {
-    constant: true,
-    inputs: [],
-    name: 'owner',
-    outputs: [{ name: '', type: 'address' }],
-    payable: false,
-    type: 'function',
-  },
-  {
-    constant: false,
-    inputs: [],
-    name: 'drain',
-    outputs: [],
-    payable: false,
-    type: 'function',
-  },
-  {
-    constant: true,
-    inputs: [{ name: '', type: 'bytes4' }],
-    name: 'entries',
-    outputs: [{ name: '', type: 'string' }],
-    payable: false,
-    type: 'function',
-  },
-  {
-    constant: false,
-    inputs: [{ name: '_method', type: 'string' }],
-    name: 'register',
-    outputs: [{ name: '', type: 'bool' }],
-    payable: false,
-    type: 'function',
-  },
-  { inputs: [], type: 'constructor' },
-  {
-    anonymous: false,
-    inputs: [
-      { indexed: true, name: 'creator', type: 'address' },
-      { indexed: true, name: 'signature', type: 'bytes4' },
-      { indexed: false, name: 'method', type: 'string' },
-    ],
-    name: 'Registered',
-    type: 'event',
-  },
-  {
-    anonymous: false,
-    inputs: [
-      { indexed: true, name: 'old', type: 'address' },
-      { indexed: true, name: 'current', type: 'address' },
-    ],
-    name: 'NewOwner',
-    type: 'event',
-  },
-];
 
 export class MethodDataHelper {
   hub: EventEmitter;
@@ -117,7 +47,7 @@ export class MethodDataHelper {
       let iface = this.#interfaceByNetworkClientId.get(networkClientId);
 
       if (!iface) {
-        iface = new Interface(methodRegistryAbi);
+        iface = new Interface(METHOD_REGISTRY_ABI);
         this.#interfaceByNetworkClientId.set(networkClientId, iface);
         log('Created interface', networkClientId);
       }
