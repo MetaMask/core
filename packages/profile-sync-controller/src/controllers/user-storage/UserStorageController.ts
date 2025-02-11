72e01c2f1f7f9baec9d3419353bd333ab7c1b78c
Mathieu Artu <mathieu.artu@consensys.net>
2025-02-11 20:01:28 +0100

    feat: expose all user storage methods through messenger actions

--- packages/profile-sync-controller/src/controllers/user-storage/UserStorageController.ts
+++ packages/profile-sync-controller/src/controllers/user-storage/UserStorageController.ts
@@ -192,6 +192,9 @@ type ActionsObj = CreateActionsObj<
   | 'performGetStorage'
   | 'performGetStorageAllFeatureEntries'
   | 'performSetStorage'
+  | 'performBatchSetStorage'
+  | 'performDeleteStorage'
+  | 'performBatchDeleteStorage'
   | 'getStorageKey'
   | 'enableProfileSyncing'
   | 'disableProfileSyncing'
@@ -211,6 +214,12 @@ export type UserStorageControllerPerformGetStorageAllFeatureEntries =
   ActionsObj['performGetStorageAllFeatureEntries'];
 export type UserStorageControllerPerformSetStorage =
   ActionsObj['performSetStorage'];
+export type UserStorageControllerPerformBatchSetStorage =
+  ActionsObj['performBatchSetStorage'];
+export type UserStorageControllerPerformDeleteStorage =
+  ActionsObj['performDeleteStorage'];
+export type UserStorageControllerPerformBatchDeleteStorage =
+  ActionsObj['performBatchDeleteStorage'];
 export type UserStorageControllerGetStorageKey = ActionsObj['getStorageKey'];
 export type UserStorageControllerEnableProfileSyncing =
   ActionsObj['enableProfileSyncing'];
@@ -422,6 +431,21 @@ export default class UserStorageController extends BaseController<
     this.messagingSystem.registerActionHandler(
       'UserStorageController:performSetStorage',
       this.performSetStorage.bind(this),
+    );
+
+    this.messagingSystem.registerActionHandler(
+      'UserStorageController:performBatchSetStorage',
+      this.performBatchSetStorage.bind(this),
+    );
+
+    this.messagingSystem.registerActionHandler(
+      'UserStorageController:performDeleteStorage',
+      this.performDeleteStorage.bind(this),
+    );
+
+    this.messagingSystem.registerActionHandler(
+      'UserStorageController:performBatchDeleteStorage',
+      this.performBatchDeleteStorage.bind(this),
     );
 
     this.messagingSystem.registerActionHandler(
