d4b71e048fac3181c98e073a28638c00e5fb756a
gantunesr <17601467+gantunesr@users.noreply.github.com>
2025-02-12 15:18:35 -0300

    chore: update exports

--- packages/multichain-network-controller/src/index.ts
+++ packages/multichain-network-controller/src/index.ts
@@ -3,6 +3,7 @@ export * from './constants';
 export * from './types';
 export {
   checkIfSupportedCaipChainId,
-  updateNetworkConfiguration,
-  updateNetworkConfigurations,
+  toMultichainNetworkConfiguration,
+  toMultichainNetworkConfigurationsByChainId,
+  toEvmCaipChainId,
 } from './utils';
