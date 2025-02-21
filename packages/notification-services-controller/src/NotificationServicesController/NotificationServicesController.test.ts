92924a7fb6343099fff1f0aab52dd3d6bd9c845e
Prithpal Sooriya <prithpal.sooriya@consensys.net>
2025-02-21 13:40:00 +0000

    feat: update notification services to support mobile. (#5120)
    ## Explanation
    
    This is a large PR that enables support for push notifications on
    mobile.
    
    **Makes Push Notification Controller Platform Agnostic**
    
    The `NotificationServicesPushController` was highly tied to web and was
    not compatible for react-native.
    We now have isolated the web logic in`web/push-utils.ts`, and allow
    platforms to overwrite and inject a push service into the controller.
    E.g. Mobile can inject push services using react-native modules.
    
    **Add support to toggle push notifications on/off in isolation**
    
    This allows us to decouple in-app notifications from push notifications.
    It is done by adding a `isPushEnabled` boolean inside the
    `NotificationServicesPushController`.
    
    We have also added public methods `enablePushNotifications` and
    `disablePushNotifications` inside the `NotificationServicesController`
    to allow us tie it to UI Actions.
    
    ## References
    
    ## Changelog
    
    ### `@metamask/notification-services-controller`
    
    - **ADDED**: `isPushEnabled` state to
    `NotificationServicesPushControllerState` to enable and disable this
    controller/
    - **ADDED**: `isUpdatingFCMToken` state to
    `NotificationServicesPushControllerState` to track when the controller
    is updating for firebase registration token.
    - **ADDED**: `PushService` interface and default web implementation for
    push notifciations
    - **CHANGED (BREAKING)**: `NotificationServicesPushController` config
    now allows injecting of a `PushService` interface during controller
    creation.
    - **ADDED**: `/push-services/web` subpath export to import web specific
    push services.
    - **ADDED**: `/shared` folder including some shared utils used in
    controllers for this package.
    - **ADDED**: public method `enablePushNotifications` and
    `disablePushNotifications` in `NotificationServicesController` to enable
    and disable push notifications in isolation.
    
    ## Checklist
    
    - [x] I've updated the test suite for new or updated code as appropriate
    - [x] I've updated documentation (JSDoc, Markdown, etc.) for new or
    updated code as appropriate
    - [x] I've highlighted breaking changes using the "BREAKING" category
    above as appropriate
    - [x] I've prepared draft pull requests for clients and consumer
    packages to resolve any breaking changes

--- packages/notification-services-controller/src/NotificationServicesController/NotificationServicesController.test.ts
+++ packages/notification-services-controller/src/NotificationServicesController/NotificationServicesController.test.ts
@@ -36,12 +36,8 @@ import NotificationServicesController, {
 import type {
   AllowedActions,
   AllowedEvents,
-  NotificationServicesPushControllerEnablePushNotifications,
-  NotificationServicesPushControllerDisablePushNotifications,
-  NotificationServicesPushControllerUpdateTriggerPushNotifications,
   NotificationServicesControllerMessenger,
   NotificationServicesControllerState,
-  NotificationServicesPushControllerSubscribeToNotifications,
 } from './NotificationServicesController';
 import { processFeatureAnnouncement } from './processors';
 import { processNotification } from './processors/process-notifications';
@@ -50,6 +46,12 @@ import * as OnChainNotifications from './services/onchain-notifications';
 import type { INotification } from './types';
 import type { UserStorage } from './types/user-storage/user-storage';
 import * as Utils from './utils/utils';
+import type {
+  NotificationServicesPushControllerDisablePushNotificationsAction,
+  NotificationServicesPushControllerEnablePushNotificationsAction,
+  NotificationServicesPushControllerSubscribeToNotificationsAction,
+  NotificationServicesPushControllerUpdateTriggerPushNotificationsAction,
+} from '../NotificationServicesPushController';
 
 // Mock type used for testing purposes
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -206,7 +208,7 @@ describe('metamask-notifications - constructor()', () => {
     return mocks;
   };
 
-  it('initializes push notifications', async () => {
+  it('initialises push notifications', async () => {
     const { mockEnablePushNotifications } =
       arrangeActInitialisePushNotifications();
 
@@ -471,7 +473,7 @@ describe('metamask-notifications - deleteOnChainTriggersByAccount', () => {
     const {
       messenger,
       nockMockDeleteTriggersAPI,
-      mockDisablePushNotifications,
+      mockUpdateTriggerPushNotifications,
     } = arrangeMocks();
     const controller = new NotificationServicesController({
       messenger,
@@ -482,7 +484,7 @@ describe('metamask-notifications - deleteOnChainTriggersByAccount', () => {
     ]);
     expect(Utils.traverseUserStorageTriggers(result)).toHaveLength(0);
     expect(nockMockDeleteTriggersAPI.isDone()).toBe(true);
-    expect(mockDisablePushNotifications).toHaveBeenCalled();
+    expect(mockUpdateTriggerPushNotifications).toHaveBeenCalled();
   });
 
   it('does nothing if account does not exist in storage', async () => {
@@ -1017,6 +1019,7 @@ describe('metamask-notifications - disableMetamaskNotifications()', () => {
     // Act - final state
     expect(controller.state.isUpdatingMetamaskNotifications).toBe(false);
     expect(controller.state.isNotificationServicesEnabled).toBe(false);
+    expect(controller.state.isFeatureAnnouncementsEnabled).toBe(false);
     expect(controller.state.metamaskNotificationsList).toStrictEqual([
       createMockSnapNotification(),
     ]);
@@ -1065,6 +1068,73 @@ describe('metamask-notifications - updateMetamaskNotificationsList', () => {
   });
 });
 
+describe('metamask-notifications - enablePushNotifications', () => {
+  const arrangeMocks = () => {
+    const messengerMocks = mockNotificationMessenger();
+    return messengerMocks;
+  };
+
+  it('calls push controller and enables notifications for accounts that have subscribed to notifications', async () => {
+    const { messenger, mockPerformGetStorage, mockEnablePushNotifications } =
+      arrangeMocks();
+    const controller = new NotificationServicesController({
+      messenger,
+      env: { featureAnnouncements: featureAnnouncementsEnv },
+      state: { isNotificationServicesEnabled: true },
+    });
+
+    // Act
+    await controller.enablePushNotifications();
+
+    // Assert
+    expect(mockPerformGetStorage).toHaveBeenCalled();
+    expect(mockEnablePushNotifications).toHaveBeenCalled();
+  });
+
+  it('throws error if fails to get notification triggers', async () => {
+    const { messenger, mockPerformGetStorage, mockEnablePushNotifications } =
+      arrangeMocks();
+
+    // Mock no storage
+    mockPerformGetStorage.mockResolvedValue(null);
+
+    const controller = new NotificationServicesController({
+      messenger,
+      env: { featureAnnouncements: featureAnnouncementsEnv },
+      state: { isNotificationServicesEnabled: true },
+    });
+
+    // Act
+    await expect(() => controller.enablePushNotifications()).rejects.toThrow(
+      expect.any(Error),
+    );
+
+    expect(mockEnablePushNotifications).not.toHaveBeenCalled();
+  });
+});
+
+describe('metamask-notifications - disablePushNotifications', () => {
+  const arrangeMocks = () => {
+    const messengerMocks = mockNotificationMessenger();
+    return messengerMocks;
+  };
+
+  it('calls push controller and enables notifications for accounts that have subscribed to notifications', async () => {
+    const { messenger, mockDisablePushNotifications } = arrangeMocks();
+    const controller = new NotificationServicesController({
+      messenger,
+      env: { featureAnnouncements: featureAnnouncementsEnv },
+      state: { isNotificationServicesEnabled: true },
+    });
+
+    // Act
+    await controller.disablePushNotifications();
+
+    // Assert
+    expect(mockDisablePushNotifications).toHaveBeenCalled();
+  });
+});
+
 // Type-Computation - we are extracting args and parameters from a generic type utility
 // Thus this `AnyFunc` can be used to help constrain the generic parameters correctly
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -1101,6 +1171,7 @@ function mockNotificationMessenger() {
       'KeyringController:lock',
       'KeyringController:unlock',
       'NotificationServicesPushController:onNewNotifications',
+      'NotificationServicesPushController:stateChange',
     ],
   });
 
@@ -1123,16 +1194,16 @@ function mockNotificationMessenger() {
     );
 
   const mockDisablePushNotifications =
-    typedMockAction<NotificationServicesPushControllerDisablePushNotifications>();
+    typedMockAction<NotificationServicesPushControllerDisablePushNotificationsAction>();
 
   const mockEnablePushNotifications =
-    typedMockAction<NotificationServicesPushControllerEnablePushNotifications>();
+    typedMockAction<NotificationServicesPushControllerEnablePushNotificationsAction>();
 
   const mockUpdateTriggerPushNotifications =
-    typedMockAction<NotificationServicesPushControllerUpdateTriggerPushNotifications>();
+    typedMockAction<NotificationServicesPushControllerUpdateTriggerPushNotificationsAction>();
 
   const mockSubscribeToPushNotifications =
-    typedMockAction<NotificationServicesPushControllerSubscribeToNotifications>();
+    typedMockAction<NotificationServicesPushControllerSubscribeToNotificationsAction>();
 
   const mockGetStorageKey =
     typedMockAction<UserStorageController.UserStorageControllerGetStorageKey>().mockResolvedValue(
@@ -1184,7 +1255,7 @@ function mockNotificationMessenger() {
       actionType ===
       'NotificationServicesPushController:disablePushNotifications'
     ) {
-      return mockDisablePushNotifications(params[0]);
+      return mockDisablePushNotifications();
     }
 
     if (
