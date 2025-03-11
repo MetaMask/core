07979192f25e137d4d30246582e1c2292948c17e
Derek Brans <dbrans@gmail.com>
2025-03-11 11:27:59 -0400

    replace export * with specific exports.

--- packages/sample-controllers/src/sample-gas-prices-service/index.ts
+++ packages/sample-controllers/src/sample-gas-prices-service/index.ts
@@ -1,2 +1,2 @@
-export type * from './sample-abstract-gas-prices-service';
-export * from './sample-gas-prices-service';
+export { type SampleAbstractGasPricesService } from './sample-abstract-gas-prices-service';
+export { SampleGasPricesService } from './sample-gas-prices-service';
