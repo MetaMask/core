a6e40d4afe6ce4562668b25a2b273b430aa6a270
Elliot Winkler <elliot.winkler@gmail.com>
2025-03-13 15:01:05 -0600

    Fix index test for chain-agnostic-permission

--- packages/chain-agnostic-permission/src/index.test.ts
+++ packages/chain-agnostic-permission/src/index.test.ts
@@ -4,12 +4,38 @@ describe('@metamask/chain-agnostic-permission', () => {
   it('has expected JavaScript exports', () => {
     expect(Object.keys(allExports)).toMatchInlineSnapshot(`
       Array [
+        "getEthAccounts",
+        "setEthAccounts",
+        "getPermittedEthChainIds",
+        "addPermittedEthChainId",
+        "setPermittedEthChainIds",
+        "getInternalScopesObject",
+        "getSessionScopes",
+        "validateAndNormalizeScopes",
+        "bucketScopes",
+        "assertIsInternalScopeString",
+        "KnownWalletRpcMethods",
+        "KnownRpcMethods",
+        "KnownWalletNamespaceRpcMethods",
+        "KnownNotifications",
+        "KnownWalletScopeString",
+        "getSupportedScopeObjects",
+        "parseScopeString",
+        "getUniqueArrayItems",
+        "normalizeScope",
+        "mergeScopeObject",
+        "mergeNormalizedScopes",
+        "mergeInternalScopes",
+        "normalizeAndMergeScopes",
         "caip25CaveatBuilder",
         "Caip25CaveatType",
         "createCaip25Caveat",
         "Caip25EndowmentPermissionName",
         "caip25EndowmentBuilder",
         "Caip25CaveatMutators",
+        "CaveatTypes",
+        "EndowmentTypes",
+        "RestrictedMethods",
       ]
     `);
   });
