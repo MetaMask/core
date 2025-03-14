c895fae2429159c9bd776b290137e335fec787ab
Elliot Winkler <elliot.winkler@gmail.com>
2025-03-14 07:55:42 -0600

    Rename multichainMethodCallValidator{,Middleware}

--- packages/multichain-api-middleware/src/index.ts
+++ packages/multichain-api-middleware/src/index.ts
@@ -2,6 +2,6 @@ export { walletGetSession } from './handlers/wallet-getSession';
 export { walletInvokeMethod } from './handlers/wallet-invokeMethod';
 export { walletRevokeSession } from './handlers/wallet-revokeSession';
 
-export { multichainMethodCallValidatorMiddleware } from './middlewares/multichainMethodCallValidator';
+export { multichainMethodCallValidatorMiddleware } from './middlewares/multichainMethodCallValidatorMiddleware';
 export { MultichainMiddlewareManager } from './middlewares/MultichainMiddlewareManager';
 export { MultichainSubscriptionManager } from './middlewares/MultichainSubscriptionManager';
