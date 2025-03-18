acca1ea89228efae7981499ed77e44334828fa17
Elliot Winkler <elliot.winkler@gmail.com>
2025-03-18 08:13:11 -0600

    Split up `multichain` package into 3 packages (#5476)
    ## Explanation
    
    <!--
    Thanks for your contribution! Take a moment to answer these questions so
    that reviewers have the information they need to properly understand
    your changes:
    
    * What is the current state of things and why does it need to change?
    * What is the solution your changes offer and how does it work?
    * Are there any changes whose purpose might not obvious to those
    unfamiliar with the domain?
    * If your primary goal was to update one package but you found you had
    to update another one along the way, why did you do so?
    * If you had to upgrade a dependency, why did you do so?
    -->
    
    Not everything in `@metamask/multichain` is strictly multichain-related.
    We could rename this package to `@metamask/caip25`, but not everything
    is CAIP-25-related, either.
    
    To better highlight the purpose of the different parts of
    `@metamask/multichain`, this commit splits it up into 3 packages:
    
    - `@metamask/chain-agnostic-permission`: Defines an endowment type
    permission designed to persist the account and chain components of a
    [CAIP-25](https://github.com/ChainAgnostic/CAIPs/blob/main/CAIPs/caip-25.md)
    request. This package also includes adapters and utility functions for
    interfacing with this permission.
    - `@metamask/eip1193-permission-middleware`: Implements the JSON-RPC
    methods for managing permissions as referenced in
    [EIP-2255](https://eips.ethereum.org/EIPS/eip-2255) and
    [MIP-2](https://github.com/MetaMask/metamask-improvement-proposals/blob/main/MIPs/mip-2.md),
    but adapted to support [chain-agnostic permission
    caveats](https://npmjs.com/package/@metamask/chain-agnostic-permission).
    - `@metamask/multichain-api-middleware`: JSON-RPC methods and middleware
    to support the the [MetaMask Multichain
    API](https://github.com/MetaMask/metamask-improvement-proposals/blob/main/MIPs/mip-5.md).
    
    Note that the existing code in `@metamask/multichain` has not been
    touched. We will deprecate and remove this package later.
    
    ## References
    
    <!--
    Are there any issues that this pull request is tied to?
    Are there other links that reviewers should consult to understand these
    changes better?
    Are there client or consumer pull requests to adopt any breaking
    changes?
    
    For example:
    
    * Fixes #12345
    * Related to #67890
    -->
    
    Closes https://github.com/MetaMask/MetaMask-planning/issues/4395.
    
    ## Changelog
    
    <!--
    If you're making any consumer-facing changes, list those changes here as
    if you were updating a changelog, using the template below as a guide.
    
    (CATEGORY is one of BREAKING, ADDED, CHANGED, DEPRECATED, REMOVED, or
    FIXED. For security-related issues, follow the Security Advisory
    process.)
    
    Please take care to name the exact pieces of the API you've added or
    changed (e.g. types, interfaces, functions, or methods).
    
    If there are any breaking changes, make sure to offer a solution for
    consumers to follow once they upgrade to the changes.
    
    Finally, if you're only making changes to development scripts or tests,
    you may replace the template below with "None".
    -->
    
    (No updates to changelogs needed)
    
    ## Checklist
    
    - [x] I've updated the test suite for new or updated code as appropriate
    - [x] I've updated documentation (JSDoc, Markdown, etc.) for new or
    updated code as appropriate
    - [x] I've highlighted breaking changes using the "BREAKING" category
    above as appropriate
    - [x] I've prepared draft pull requests for clients and consumer
    packages to resolve any breaking changes
    
    ---------
    
    Co-authored-by: Alex Donesky <adonesky@gmail.com>

---
+++ packages/chain-agnostic-permission/src/scope/supported.ts
@@ -0,0 +1,181 @@
+import { toHex, isEqualCaseInsensitive } from '@metamask/controller-utils';
+import type { CaipAccountId, CaipChainId, Hex } from '@metamask/utils';
+import {
+  isCaipChainId,
+  KnownCaipNamespace,
+  parseCaipAccountId,
+} from '@metamask/utils';
+
+import {
+  CaipReferenceRegexes,
+  KnownNotifications,
+  KnownRpcMethods,
+  KnownWalletNamespaceRpcMethods,
+  KnownWalletRpcMethods,
+} from './constants';
+import type { ExternalScopeString } from './types';
+import { parseScopeString } from './types';
+
+/**
+ * Determines if a scope string is supported.
+ *
+ * @param scopeString - The scope string to check.
+ * @param hooks - An object containing the following properties:
+ * @param hooks.isEvmChainIdSupported - A predicate that determines if an EVM chainID is supported.
+ * @param hooks.isNonEvmScopeSupported - A predicate that determines if an non EVM scopeString is supported.
+ * @returns A boolean indicating if the scope string is supported.
+ */
+export const isSupportedScopeString = (
+  scopeString: string,
+  {
+    isEvmChainIdSupported,
+    isNonEvmScopeSupported,
+  }: {
+    isEvmChainIdSupported: (chainId: Hex) => boolean;
+    isNonEvmScopeSupported: (scope: CaipChainId) => boolean;
+  },
+) => {
+  const { namespace, reference } = parseScopeString(scopeString);
+
+  switch (namespace) {
+    case KnownCaipNamespace.Wallet:
+      if (
+        isCaipChainId(scopeString) &&
+        reference !== KnownCaipNamespace.Eip155
+      ) {
+        return isNonEvmScopeSupported(scopeString);
+      }
+      return true;
+    case KnownCaipNamespace.Eip155:
+      return (
+        !reference ||
+        (CaipReferenceRegexes.eip155.test(reference) &&
+          isEvmChainIdSupported(toHex(reference)))
+      );
+    default:
+      return isCaipChainId(scopeString)
+        ? isNonEvmScopeSupported(scopeString)
+        : false;
+  }
+};
+
+/**
+ * Determines if an account is supported by the wallet (i.e. on a keyring known to the wallet).
+ *
+ * @param account - The CAIP account ID to check.
+ * @param hooks - An object containing the following properties:
+ * @param hooks.getEvmInternalAccounts - A function that returns the EVM internal accounts.
+ * @param hooks.getNonEvmAccountAddresses - A function that returns the supported CAIP-10 account addresses for a non EVM scope.
+ * @returns A boolean indicating if the account is supported by the wallet.
+ */
+export const isSupportedAccount = (
+  account: CaipAccountId,
+  {
+    getEvmInternalAccounts,
+    getNonEvmAccountAddresses,
+  }: {
+    getEvmInternalAccounts: () => { type: string; address: Hex }[];
+    getNonEvmAccountAddresses: (scope: CaipChainId) => string[];
+  },
+) => {
+  const {
+    address,
+    chainId,
+    chain: { namespace, reference },
+  } = parseCaipAccountId(account);
+
+  const isSupportedEip155Account = () =>
+    getEvmInternalAccounts().some(
+      (internalAccount) =>
+        ['eip155:eoa', 'eip155:erc4337'].includes(internalAccount.type) &&
+        isEqualCaseInsensitive(address, internalAccount.address),
+    );
+
+  const isSupportedNonEvmAccount = () =>
+    getNonEvmAccountAddresses(chainId).includes(account);
+
+  // We are trying to discern the type of `namespace`.
+  /* eslint-disable @typescript-eslint/no-unsafe-enum-comparison */
+  switch (namespace) {
+    case KnownCaipNamespace.Wallet:
+      if (reference === KnownCaipNamespace.Eip155) {
+        return isSupportedEip155Account();
+      }
+      return isSupportedNonEvmAccount();
+    case KnownCaipNamespace.Eip155:
+      return isSupportedEip155Account();
+    default:
+      return isSupportedNonEvmAccount();
+  }
+  /* eslint-enable @typescript-eslint/no-unsafe-enum-comparison */
+};
+
+/**
+ * Determines if a method is supported by the wallet.
+ *
+ * @param scopeString - The scope string to check.
+ * @param method - The method to check.
+ * @param hooks - An object containing the following properties:
+ * @param hooks.getNonEvmSupportedMethods - A function that returns the supported methods for a non EVM scope.
+ * @returns A boolean indicating if the method is supported by the wallet.
+ */
+export const isSupportedMethod = (
+  scopeString: ExternalScopeString,
+  method: string,
+  {
+    getNonEvmSupportedMethods,
+  }: {
+    getNonEvmSupportedMethods: (scope: CaipChainId) => string[];
+  },
+): boolean => {
+  const { namespace, reference } = parseScopeString(scopeString);
+
+  if (!namespace) {
+    return false;
+  }
+
+  const isSupportedNonEvmMethod = () =>
+    isCaipChainId(scopeString) &&
+    getNonEvmSupportedMethods(scopeString).includes(method);
+
+  // We are trying to discern the type of `namespace`.
+  /* eslint-disable @typescript-eslint/no-unsafe-enum-comparison */
+  if (namespace === KnownCaipNamespace.Wallet) {
+    if (!reference) {
+      return KnownWalletRpcMethods.includes(method);
+    }
+
+    if (reference === KnownCaipNamespace.Eip155) {
+      return KnownWalletNamespaceRpcMethods[reference].includes(method);
+    }
+
+    return isSupportedNonEvmMethod();
+  }
+
+  if (namespace === KnownCaipNamespace.Eip155) {
+    return KnownRpcMethods[namespace].includes(method);
+  }
+  /* eslint-enable @typescript-eslint/no-unsafe-enum-comparison */
+
+  return isSupportedNonEvmMethod();
+};
+
+/**
+ * Determines if a notification is supported by the wallet.
+ *
+ * @param scopeString - The scope string to check.
+ * @param notification - The notification to check.
+ * @returns A boolean indicating if the notification is supported by the wallet.
+ */
+export const isSupportedNotification = (
+  scopeString: ExternalScopeString,
+  notification: string,
+): boolean => {
+  const { namespace } = parseScopeString(scopeString);
+
+  if (namespace === KnownCaipNamespace.Eip155) {
+    return KnownNotifications[namespace].includes(notification);
+  }
+
+  return false;
+};
