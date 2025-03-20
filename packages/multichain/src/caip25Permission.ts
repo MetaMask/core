b770454766d9ea02b8a959d1bee5a9be03e7808b
Alex <adonesky@gmail.com>
2025-03-20 11:38:15 -0500

    revert @metamask/multichain package changes

--- packages/multichain/src/caip25Permission.ts
+++ packages/multichain/src/caip25Permission.ts
@@ -21,9 +21,9 @@ import {
 } from '@metamask/utils';
 import { cloneDeep, isEqual } from 'lodash';
 
-import { getEthAccounts } from './adapters/caip-permission-adapter-eth-accounts';
 import { assertIsInternalScopesObject } from './scope/assert';
-import { isSupportedScopeString } from './scope/supported';
+import { isSupportedAccount, isSupportedScopeString } from './scope/supported';
+import { mergeInternalScopes } from './scope/transform';
 import {
   parseScopeString,
   type ExternalScopeString,
@@ -68,10 +68,75 @@ export const createCaip25Caveat = (value: Caip25CaveatValue) => {
 
 type Caip25EndowmentCaveatSpecificationBuilderOptions = {
   findNetworkClientIdByChainId: (chainId: Hex) => NetworkClientId;
-  listAccounts: () => { address: Hex }[];
+  listAccounts: () => { type: string; address: Hex }[];
   isNonEvmScopeSupported: (scope: CaipChainId) => boolean;
   getNonEvmAccountAddresses: (scope: CaipChainId) => string[];
 };
+
+/**
+ * Calculates the difference between two provided CAIP-25 permission caveat values, but only considering a single scope property at a time.
+ *
+ * @param originalValue - The existing CAIP-25 permission caveat value.
+ * @param mergedValue - The result from merging existing and incoming CAIP-25 permission caveat values.
+ * @param scopeToDiff - The required or optional scopes from the [CAIP-25](https://github.com/ChainAgnostic/CAIPs/blob/main/CAIPs/caip-25.md) request.
+ * @returns The difference between original and merged CAIP-25 permission caveat values.
+ */
+export function diffScopesForCaip25CaveatValue(
+  originalValue: Caip25CaveatValue,
+  mergedValue: Caip25CaveatValue,
+  scopeToDiff: 'optionalScopes' | 'requiredScopes',
+): Caip25CaveatValue {
+  const diff = cloneDeep(originalValue);
+
+  const mergedScopeToDiff = mergedValue[scopeToDiff];
+  for (const [scopeString, mergedScopeObject] of Object.entries(
+    mergedScopeToDiff,
+  )) {
+    const internalScopeString = scopeString as keyof typeof mergedScopeToDiff;
+    const originalScopeObject = diff[scopeToDiff][internalScopeString];
+
+    if (originalScopeObject) {
+      const newAccounts = mergedScopeObject.accounts.filter(
+        (account) => !originalScopeObject?.accounts.includes(account),
+      );
+      if (newAccounts.length > 0) {
+        diff[scopeToDiff][internalScopeString] = {
+          accounts: newAccounts,
+        };
+        continue;
+      }
+      delete diff[scopeToDiff][internalScopeString];
+    } else {
+      diff[scopeToDiff][internalScopeString] = mergedScopeObject;
+    }
+  }
+
+  return diff;
+}
+
+/**
+ * Checks if every account in the given scopes object is supported.
+ *
+ * @param scopesObject - The scopes object to iterate over.
+ * @param listAccounts - The hook for getting internalAccount objects for all evm accounts.
+ * @param getNonEvmAccountAddresses - The hook that returns the supported CAIP-10 account addresses for a non EVM scope.
+ * addresses.
+ * @returns True if every account in the scopes object is supported, false otherwise.
+ */
+function isEveryAccountInScopesObjectSupported(
+  scopesObject: InternalScopesObject,
+  listAccounts: () => { type: string; address: Hex }[],
+  getNonEvmAccountAddresses: (scope: CaipChainId) => string[],
+) {
+  return Object.values(scopesObject).every((scopeObject) =>
+    scopeObject.accounts.every((account) =>
+      isSupportedAccount(account, {
+        getEvmInternalAccounts: listAccounts,
+        getNonEvmAccountAddresses,
+      }),
+    ),
+  );
+}
 
 /**
  * Helper that returns a `authorizedScopes` CAIP-25 caveat specification
@@ -80,13 +145,19 @@ type Caip25EndowmentCaveatSpecificationBuilderOptions = {
  * @param options - The specification builder options.
  * @param options.findNetworkClientIdByChainId - The hook for getting the networkClientId that serves a chainId.
  * @param options.listAccounts - The hook for getting internalAccount objects for all evm accounts.
+ * @param options.isNonEvmScopeSupported - The hook that determines if an non EVM scopeString is supported.
+ * @param options.getNonEvmAccountAddresses - The hook that returns the supported CAIP-10 account addresses for a non EVM scope.
  * @returns The specification for the `caip25` caveat.
  */
 export const caip25CaveatBuilder = ({
   findNetworkClientIdByChainId,
   listAccounts,
+  isNonEvmScopeSupported,
+  getNonEvmAccountAddresses,
 }: Caip25EndowmentCaveatSpecificationBuilderOptions): EndowmentCaveatSpecificationConstraint &
-  Required<Pick<EndowmentCaveatSpecificationConstraint, 'validator'>> => {
+  Required<
+    Pick<EndowmentCaveatSpecificationConstraint, 'validator' | 'merger'>
+  > => {
   return {
     type: Caip25CaveatType,
     validator: (
@@ -124,14 +195,14 @@ export const caip25CaveatBuilder = ({
         (scopeString) =>
           isSupportedScopeString(scopeString, {
             isEvmChainIdSupported,
-            isNonEvmScopeSupported: () => false,
+            isNonEvmScopeSupported,
           }),
       );
       const allOptionalScopesSupported = Object.keys(optionalScopes).every(
         (scopeString) =>
           isSupportedScopeString(scopeString, {
             isEvmChainIdSupported,
-            isNonEvmScopeSupported: () => false,
+            isNonEvmScopeSupported,
           }),
       );
       if (!allRequiredScopesSupported || !allOptionalScopesSupported) {
@@ -140,24 +211,56 @@ export const caip25CaveatBuilder = ({
         );
       }
 
-      // Fetch EVM accounts from native wallet keyring
-      // These addresses are lowercased already
-      const existingEvmAddresses = listAccounts().map(
-        (account) => account.address,
-      );
-      const ethAccounts = getEthAccounts({
-        requiredScopes,
-        optionalScopes,
-      }).map((address) => address.toLowerCase() as Hex);
-
-      const allEthAccountsSupported = ethAccounts.every((address) =>
-        existingEvmAddresses.includes(address),
-      );
-      if (!allEthAccountsSupported) {
+      const allRequiredAccountsSupported =
+        isEveryAccountInScopesObjectSupported(
+          requiredScopes,
+          listAccounts,
+          getNonEvmAccountAddresses,
+        );
+      const allOptionalAccountsSupported =
+        isEveryAccountInScopesObjectSupported(
+          optionalScopes,
+          listAccounts,
+          getNonEvmAccountAddresses,
+        );
+      if (!allRequiredAccountsSupported || !allOptionalAccountsSupported) {
         throw new Error(
-          `${Caip25EndowmentPermissionName} error: Received eip155 account value(s) for caveat of type "${Caip25CaveatType}" that were not found in the wallet keyring.`,
+          `${Caip25EndowmentPermissionName} error: Received account value(s) for caveat of type "${Caip25CaveatType}" that are not supported by the wallet.`,
         );
       }
+    },
+    merger: (
+      leftValue: Caip25CaveatValue,
+      rightValue: Caip25CaveatValue,
+    ): [Caip25CaveatValue, Caip25CaveatValue] => {
+      const mergedRequiredScopes = mergeInternalScopes(
+        leftValue.requiredScopes,
+        rightValue.requiredScopes,
+      );
+      const mergedOptionalScopes = mergeInternalScopes(
+        leftValue.optionalScopes,
+        rightValue.optionalScopes,
+      );
+
+      const mergedValue: Caip25CaveatValue = {
+        requiredScopes: mergedRequiredScopes,
+        optionalScopes: mergedOptionalScopes,
+        isMultichainOrigin: leftValue.isMultichainOrigin,
+      };
+
+      const partialDiff = diffScopesForCaip25CaveatValue(
+        leftValue,
+        mergedValue,
+        'requiredScopes',
+      );
+
+      const diff = diffScopesForCaip25CaveatValue(
+        partialDiff,
+        mergedValue,
+        'optionalScopes',
+      );
+
+      return [mergedValue, diff];
     },
   };
 };
