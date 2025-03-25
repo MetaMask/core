fd8330ba3f44da816627383f4263c344ed08bf61
Alex Donesky <adonesky@gmail.com>
2025-03-25 12:58:06 -0500

    chore: add more chain agnostic utility functions for interfacing w/ caip25 permission (#5536)
    ## Explanation
    Currently the utility/helper functions we expose to interface with and
    help construct a valid caip25 permission caveat are very eth/evm centric
    (i.e. `setPermittedAccounts`, `addPermittedEthChainId`,
    `getPermittedEthChainIds` etc)
    
    This PR adds some new helpers that are actually chain agnostic
    
    ## References
    
    see @david0xd 's PR
    [here](https://github.com/MetaMask/metamask-extension/pull/31203)
    
    and my extension PR ontop of it that uses these changes:
    https://github.com/MetaMask/metamask-extension/pull/31253
    
    ### `@metamask/chain-agnostic-permission`
    
    - **CHANGED**: Renamed `caip-permission-adapter-eth-accounts.ts` to
    `caip-permission-adapter-accounts.ts` to better reflect its more
    generalized functionality.
    - **ADDED**: New `setPermittedAccounts` function that allows setting
    accounts for any CAIP namespace, not just EVM scopes.
    - **ADDED**: New `addPermittedChainId` and `setPermittedChainIds`
    functions for managing permitted chains across any CAIP namespace.
    - **ADDED**: New `generateCaip25Caveat` function to simplify
    modification of CAIP-25 permissions after UI confirmation.
    - **ADDED**: New `isWalletScope` utility function to detect
    wallet-related scopes.
    
    ## Checklist
    
    - [ ] I've updated the test suite for new or updated code as appropriate
    - [ ] I've updated documentation (JSDoc, Markdown, etc.) for new or
    updated code as appropriate
    - [ ] I've highlighted breaking changes using the "BREAKING" category
    above as appropriate
    - [ ] I've prepared draft pull requests for clients and consumer
    packages to resolve any breaking changes

--- packages/chain-agnostic-permission/src/adapters/caip-permission-adapter-permittedChains.test.ts
+++ packages/chain-agnostic-permission/src/adapters/caip-permission-adapter-permittedChains.test.ts
@@ -2,6 +2,8 @@ import {
   addPermittedEthChainId,
   getPermittedEthChainIds,
   setPermittedEthChainIds,
+  addPermittedChainId,
+  setPermittedChainIds,
 } from './caip-permission-adapter-permittedChains';
 import type { Caip25CaveatValue } from '../caip25Permission';
 
@@ -260,6 +262,276 @@ describe('CAIP-25 permittedChains adapters', () => {
       };
 
       const result = setPermittedEthChainIds(input, ['0x1', '0x2', '0x3']);
+
+      expect(input).toStrictEqual({
+        requiredScopes: {
+          'eip155:1': {
+            accounts: ['eip155:1:0x1', 'eip155:1:0x2'],
+          },
+        },
+        optionalScopes: {},
+        sessionProperties: {},
+        isMultichainOrigin: false,
+      });
+      expect(input).not.toStrictEqual(result);
+    });
+  });
+
+  describe('addPermittedChainId', () => {
+    it('returns a version of the caveat value with a new optional scope for the passed chainId if it does not already exist in required or optional scopes', () => {
+      const result = addPermittedChainId(
+        {
+          requiredScopes: {
+            'eip155:1': {
+              accounts: ['eip155:1:0x1', 'eip155:1:0x2'],
+            },
+          },
+          optionalScopes: {
+            'eip155:100': {
+              accounts: ['eip155:100:0x100'],
+            },
+            'wallet:eip155': {
+              accounts: [],
+            },
+          },
+          sessionProperties: {},
+          isMultichainOrigin: false,
+        },
+        'bip122:000000000019d6689c085ae165831e93',
+      );
+
+      expect(result).toStrictEqual({
+        requiredScopes: {
+          'eip155:1': {
+            accounts: ['eip155:1:0x1', 'eip155:1:0x2'],
+          },
+        },
+        optionalScopes: {
+          'eip155:100': {
+            accounts: ['eip155:100:0x100'],
+          },
+          'wallet:eip155': {
+            accounts: [],
+          },
+          'bip122:000000000019d6689c085ae165831e93': {
+            accounts: [],
+          },
+        },
+        sessionProperties: {},
+        isMultichainOrigin: false,
+      });
+    });
+
+    it('does not modify the input CAIP-25 caveat value object', () => {
+      const input: Caip25CaveatValue = {
+        requiredScopes: {
+          'eip155:1': {
+            accounts: ['eip155:1:0x1', 'eip155:1:0x2'],
+          },
+        },
+        optionalScopes: {},
+        sessionProperties: {},
+        isMultichainOrigin: false,
+      };
+
+      const result = addPermittedChainId(
+        input,
+        'bip122:000000000019d6689c085ae165831e93',
+      );
+
+      expect(input).toStrictEqual({
+        requiredScopes: {
+          'eip155:1': {
+            accounts: ['eip155:1:0x1', 'eip155:1:0x2'],
+          },
+        },
+        optionalScopes: {},
+        sessionProperties: {},
+        isMultichainOrigin: false,
+      });
+      expect(input).not.toStrictEqual(result);
+    });
+
+    it('does not add an optional scope for the chainId if already exists in the required scopes', () => {
+      const existingScope = 'eip155:1';
+      const input: Caip25CaveatValue = {
+        requiredScopes: {
+          [existingScope]: {
+            accounts: ['eip155:1:0x1', 'eip155:1:0x2'],
+          },
+        },
+        optionalScopes: {},
+        sessionProperties: {},
+        isMultichainOrigin: false,
+      };
+
+      const result = addPermittedChainId(input, existingScope);
+
+      expect(result).toStrictEqual(input);
+    });
+
+    it('does not add an optional scope for the chainId if already exists in the optional scopes', () => {
+      const existingScope = 'eip155:1';
+      const input: Caip25CaveatValue = {
+        requiredScopes: {},
+        optionalScopes: {
+          [existingScope]: {
+            accounts: [],
+          },
+        },
+        sessionProperties: {},
+        isMultichainOrigin: false,
+      };
+
+      const result = addPermittedChainId(input, existingScope);
+
+      expect(result).toStrictEqual(input);
+    });
+  });
+
+  describe('setPermittedChainIds', () => {
+    it('returns a CAIP-25 caveat value with non-wallet scopes missing from the chainIds array removed', () => {
+      const result = setPermittedChainIds(
+        {
+          requiredScopes: {
+            'eip155:1': {
+              accounts: ['eip155:1:0x1', 'eip155:1:0x2'],
+            },
+            'bip122:000000000019d6689c085ae165831e93': {
+              accounts: [],
+            },
+            'eip155:100': {
+              accounts: ['eip155:100:0x100'],
+            },
+          },
+          optionalScopes: {
+            wallet: {
+              accounts: [],
+            },
+            'wallet:eip155': {
+              accounts: [],
+            },
+            'wallet:bip122': {
+              accounts: [],
+            },
+            'eip155:5': {
+              accounts: [],
+            },
+          },
+          sessionProperties: {},
+          isMultichainOrigin: false,
+        },
+        ['eip155:1', 'eip155:5'],
+      );
+
+      expect(result).toStrictEqual({
+        requiredScopes: {
+          'eip155:1': {
+            accounts: ['eip155:1:0x1', 'eip155:1:0x2'],
+          },
+        },
+        optionalScopes: {
+          wallet: {
+            accounts: [],
+          },
+          'eip155:5': {
+            accounts: [],
+          },
+          'wallet:bip122': {
+            accounts: [],
+          },
+          'wallet:eip155': {
+            accounts: [],
+          },
+        },
+        sessionProperties: {},
+        isMultichainOrigin: false,
+      });
+    });
+
+    it('returns a CAIP-25 caveat value with optional scopes added for missing chainIds', () => {
+      const result = setPermittedChainIds(
+        {
+          requiredScopes: {
+            'eip155:1': {
+              accounts: ['eip155:1:0x1', 'eip155:1:0x2'],
+            },
+          },
+          optionalScopes: {},
+          sessionProperties: {},
+          isMultichainOrigin: false,
+        },
+        ['eip155:1', 'bip122:000000000019d6689c085ae165831e93'],
+      );
+
+      expect(result).toStrictEqual({
+        requiredScopes: {
+          'eip155:1': {
+            accounts: ['eip155:1:0x1', 'eip155:1:0x2'],
+          },
+        },
+        optionalScopes: {
+          'bip122:000000000019d6689c085ae165831e93': {
+            accounts: [],
+          },
+        },
+        sessionProperties: {},
+        isMultichainOrigin: false,
+      });
+    });
+
+    it('preserves wallet namespace scopes when setting permitted chainIds', () => {
+      const result = setPermittedChainIds(
+        {
+          requiredScopes: {},
+          optionalScopes: {
+            wallet: {
+              accounts: [],
+            },
+            'wallet:eip155': {
+              accounts: ['wallet:eip155:0xabc'],
+            },
+          },
+          sessionProperties: {},
+          isMultichainOrigin: false,
+        },
+        ['eip155:1', 'eip155:5'],
+      );
+
+      expect(result).toStrictEqual({
+        requiredScopes: {},
+        optionalScopes: {
+          wallet: {
+            accounts: [],
+          },
+          'wallet:eip155': {
+            accounts: ['wallet:eip155:0xabc'],
+          },
+          'eip155:1': {
+            accounts: [],
+          },
+          'eip155:5': {
+            accounts: [],
+          },
+        },
+        sessionProperties: {},
+        isMultichainOrigin: false,
+      });
+    });
+
+    it('does not modify the input CAIP-25 caveat value object', () => {
+      const input: Caip25CaveatValue = {
+        requiredScopes: {
+          'eip155:1': {
+            accounts: ['eip155:1:0x1', 'eip155:1:0x2'],
+          },
+        },
+        optionalScopes: {},
+        sessionProperties: {},
+        isMultichainOrigin: false,
+      };
+
+      const result = setPermittedChainIds(input, ['eip155:1', 'eip155:2']);
 
       expect(input).toStrictEqual({
         requiredScopes: {
