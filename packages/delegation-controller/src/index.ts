f5d70b1602e033d0a65da15046a17b00e802ab88
Caainã Jeronimo <caainaje@gmail.com>
2025-04-01 18:39:17 -0300

    chore: add export types

--- packages/delegation-controller/src/index.ts
+++ packages/delegation-controller/src/index.ts
@@ -5,4 +5,18 @@ export type {
   Delegation,
   AllowedActions,
 } from './types';
+
+export type { MetaMaskSmartAccount } from '@metamask-private/delegator-core-viem';
+export {
+  toMetaMaskSmartAccount,
+  Implementation,
+  createCaveatBuilder,
+  createRootDelegation,
+  getDeleGatorEnvironment,
+  createDelegation,
+  getDelegationHashOffchain,
+  DelegationFramework,
+  SINGLE_DEFAULT_MODE,
+} from '@metamask-private/delegator-core-viem';
+
 export { DelegationController } from './delegation-controller';
