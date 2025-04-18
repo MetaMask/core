969053f7be6ae3a2c5a51e65730313573217c2d3
lwin <lwin@tor.us>
2025-04-18 14:59:35 +0800

    test: updated comment in jest.enviroment.js which defines 'webCrypto'

--- packages/seedless-onboarding-controller/jest.environment.js
+++ packages/seedless-onboarding-controller/jest.environment.js
@@ -1,7 +1,7 @@
 const NodeEnvironment = require('jest-environment-node');
 
 /**
- * KeyringController depends on @noble/hashes, which as of 1.3.2 relies on the
+ * SeedlessOnboardingController depends on @noble/hashes, which as of 1.7.1 relies on the
  * Web Crypto API in Node and browsers.
  */
 class CustomTestEnvironment extends NodeEnvironment {
