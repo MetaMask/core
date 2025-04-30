fd0c24f465ba2dac5900d93b2d66a3e869b0f87e
Shane T <shane.odlum@consensys.net>
2025-04-30 13:30:03 +0100

    chore: add metadata to account controller options for EVM HD accounts (#5618)
    ## Explanation
    
    In the multi-SRP context, we need to bubble up the Account metadata such
    that it is available in interactions with the AccountsController.
    This metadata will be used by the Backup & Sync feature to determine how
    to reconstruct the account when restoring from backup.
    
    ## References
    
    Fixes
    [Identity-90](https://consensyssoftware.atlassian.net/browse/IDENTITY-90)
    
    ---------
    
    Co-authored-by: Mircea Nistor <mirceanis@gmail.com>
    Co-authored-by: Michele Esposito <34438276+mikesposito@users.noreply.github.com>
    Co-authored-by: Charly Chevalier <charly.chevalier@consensys.net>

--- packages/accounts-controller/src/AccountsController.ts
+++ packages/accounts-controller/src/AccountsController.ts
@@ -38,7 +38,9 @@ import type { WritableDraft } from 'immer/dist/internal.js';
 
 import type { MultichainNetworkControllerNetworkDidChangeEvent } from './types';
 import {
+  getDerivationPathForIndex,
   getUUIDFromAddressOfNormalAccount,
+  isHdKeyringType,
   isNormalKeyringType,
   keyringTypeToName,
 } from './utils';
@@ -682,18 +684,31 @@ export class AccountsController extends BaseController<
    */
   async #listNormalAccounts(): Promise<InternalAccount[]> {
     const internalAccounts: InternalAccount[] = [];
-    const { keyrings } = await this.messagingSystem.call(
+    const { keyrings, keyringsMetadata } = this.messagingSystem.call(
       'KeyringController:getState',
     );
-    for (const keyring of keyrings) {
+
+    for (const [keyringIndex, keyring] of keyrings.entries()) {
       const keyringType = keyring.type;
       if (!isNormalKeyringType(keyringType as KeyringTypes)) {
         // We only consider "normal accounts" here, so keep looping
         continue;
       }
 
-      for (const address of keyring.accounts) {
+      for (const [accountIndex, address] of keyring.accounts.entries()) {
         const id = getUUIDFromAddressOfNormalAccount(address);
+
+        let options = {};
+
+        if (isHdKeyringType(keyring.type as KeyringTypes)) {
+          options = {
+            entropySource: keyringsMetadata[keyringIndex].id,
+            // NOTE: We are not using the `hdPath` from the associated keyring here and
+            // getting the keyring instance here feels a bit overkill.
+            // This will be naturally fixed once every keyring start using `KeyringAccount` and implement the keyring API.
+            derivationPath: getDerivationPathForIndex(accountIndex),
+          };
+        }
 
         const nameLastUpdatedAt = this.#populateExistingMetadata(
           id,
@@ -703,7 +718,7 @@ export class AccountsController extends BaseController<
         internalAccounts.push({
           id,
           address,
-          options: {},
+          options,
           methods: [
             EthMethod.PersonalSign,
             EthMethod.Sign,
