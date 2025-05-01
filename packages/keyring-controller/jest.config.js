f571d26c28b0e5b020c71c0f628732ad2b9300cf
Michele Esposito <michele@esposito.codes>
2025-05-01 10:44:39 +0200

    adjust coverage threshold

--- packages/keyring-controller/jest.config.js
+++ packages/keyring-controller/jest.config.js
@@ -17,7 +17,7 @@ module.exports = merge(baseConfig, {
   // An object that configures minimum threshold enforcement for coverage results
   coverageThreshold: {
     global: {
-      branches: 93.67,
+      branches: 93.64,
       functions: 100,
       lines: 98.92,
       statements: 98.93,
