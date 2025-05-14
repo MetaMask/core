9cdf8937cfa79b535142cf201cf9ed45b00176c0
Salah-Eddine Saakoun <salah-eddine.saakoun@consensys.net>
2025-05-15 01:17:15 +0200

    fix: network controller coverage

--- packages/network-controller/jest.config.js
+++ packages/network-controller/jest.config.js
@@ -17,7 +17,7 @@ module.exports = merge(baseConfig, {
   // An object that configures minimum threshold enforcement for coverage results
   coverageThreshold: {
     global: {
-      branches: 88.47,
+      branches: 88.02,
       functions: 97.43,
       lines: 94.47,
       statements: 94.29,
