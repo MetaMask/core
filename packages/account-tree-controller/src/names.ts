fc12e0ca64d2842b00ed4d14dfa8ed01b5788569
Charly Chevalier <charly.chevalier@consensys.net>
2025-06-05 13:57:36 +0200

    feat: add `AccountTreeController` (#5847)
    ## Explanation
    
    New controller to group accounts based on some pre-defined rules.
    
    ## References
    
    - https://github.com/MetaMask/decisions/pull/71
    
    ## Changelog
    
    N/A
    
    ## Checklist
    
    - [ ] I've updated the test suite for new or updated code as appropriate
    - [ ] I've updated documentation (JSDoc, Markdown, etc.) for new or
    updated code as appropriate
    - [ ] I've communicated my changes to consumers by [updating changelogs
    for packages I've
    changed](https://github.com/MetaMask/core/tree/main/docs/contributing.md#updating-changelogs),
    highlighting breaking changes as necessary
    - [ ] I've prepared draft pull requests for clients and consumer
    packages to resolve any breaking changes
    
    ---------
    
    Co-authored-by: Patryk Łucka <5708018+PatrykLucka@users.noreply.github.com>
    Co-authored-by: Elliot Winkler <elliot.winkler@gmail.com>

---
+++ packages/account-tree-controller/src/names.ts
@@ -0,0 +1,39 @@
+import { KeyringTypes } from '@metamask/keyring-controller';
+
+/**
+ * Get wallet name from a keyring type.
+ *
+ * @param type - Keyring's type.
+ * @returns Wallet name.
+ */
+export function getAccountWalletNameFromKeyringType(type: KeyringTypes) {
+  switch (type) {
+    case KeyringTypes.simple: {
+      return 'Private Keys';
+    }
+    case KeyringTypes.hd: {
+      return 'HD Wallet';
+    }
+    case KeyringTypes.trezor: {
+      return 'Trezor';
+    }
+    case KeyringTypes.oneKey: {
+      return 'OneKey';
+    }
+    case KeyringTypes.ledger: {
+      return 'Ledger';
+    }
+    case KeyringTypes.lattice: {
+      return 'Lattice';
+    }
+    case KeyringTypes.qr: {
+      return 'QR';
+    }
+    case KeyringTypes.snap: {
+      return 'Snap Wallet';
+    }
+    default: {
+      return 'Unknown';
+    }
+  }
+}
