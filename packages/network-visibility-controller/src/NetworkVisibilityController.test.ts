094453a3dd9073bd5564394815c93ad16871d8f4
Nicholas Gambino <nicholas.gambino@consensys.net>
2025-06-06 14:18:04 -0700

    chore: Init unit tests

--- packages/network-visibility-controller/src/NetworkVisibilityController.test.ts
+++ packages/network-visibility-controller/src/NetworkVisibilityController.test.ts
@@ -1,8 +1,21 @@
 import { Messenger } from '@metamask/base-controller';
+import { toEvmCaipChainId } from '@metamask/multichain-network-controller';
 import type { NetworkControllerStateChangeEvent } from '@metamask/network-controller';
+import type { CaipChainId } from '@metamask/utils';
 
-import { NetworkVisibilityController } from './NetworkVisibilityController';
-import type { NetworkVisibilityControllerMessengerActions } from './NetworkVisibilityController';
+import {
+  NetworkVisibilityController,
+  CHAIN_IDS,
+} from './NetworkVisibilityController';
+import type {
+  NetworkVisibilityControllerMessengerActions,
+  NetworkVisibilityControllerMessenger,
+} from './NetworkVisibilityController';
+
+const controllerName = 'NetworkVisibilityController';
+
+// Helper type to extract available actions from the messenger
+type AvailableAction = NetworkVisibilityControllerMessengerActions['type'];
 
 describe('NetworkVisibilityController', () => {
   let messenger: Messenger<
@@ -15,7 +28,7 @@ describe('NetworkVisibilityController', () => {
     messenger = new Messenger();
     controller = new NetworkVisibilityController({
       messenger: messenger.getRestricted({
-        name: 'NetworkVisibilityController',
+        name: controllerName,
         allowedActions: [],
         allowedEvents: ['NetworkController:stateChange'],
       }),
@@ -27,6 +40,38 @@ describe('NetworkVisibilityController', () => {
     expect(state).toStrictEqual({
       orderedNetworkList: [],
       enabledNetworkMap: {},
+    });
+  });
+
+  describe('updateNetworksList', () => {
+    it('should update the ordered network list', () => {
+      const chainIds: CaipChainId[] = [
+        toEvmCaipChainId(CHAIN_IDS.SEPOLIA),
+        toEvmCaipChainId(CHAIN_IDS.LINEA_SEPOLIA),
+      ];
+
+      controller.updateNetworksList(chainIds);
+
+      const { state } = controller;
+      expect(state.orderedNetworkList).toHaveLength(2);
+      chainIds.forEach((chainId, index) => {
+        expect(state.orderedNetworkList[index].networkId).toBe(chainId);
+      });
+    });
+
+    it('should handle empty network list', () => {
+      // First add some networks
+      const initialChainIds: CaipChainId[] = [
+        toEvmCaipChainId(CHAIN_IDS.SEPOLIA),
+        toEvmCaipChainId(CHAIN_IDS.LINEA_SEPOLIA),
+      ];
+      controller.updateNetworksList(initialChainIds);
+
+      // Then update with empty list
+      controller.updateNetworksList([]);
+
+      const { state } = controller;
+      expect(state.orderedNetworkList).toHaveLength(0);
     });
   });
 });
