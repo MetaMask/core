3cfba444bec8acc0eeeb0a9086d4547a60d8fea3
Salah-Eddine Saakoun <salah-eddine.saakoun@consensys.net>
2025-06-27 15:23:05 +0200

    refactor: extract actions to a separate file

--- packages/network-controller/src/NetworkController.ts
+++ packages/network-controller/src/NetworkController.ts
@@ -4,10 +4,7 @@ import type {
   MessengerMethodActions,
   RestrictedMessenger,
 } from '@metamask/base-controller';
-import {
-  BaseController,
-  registerMethodActionHandlers,
-} from '@metamask/base-controller';
+import { BaseController } from '@metamask/base-controller';
 import type { Partialize } from '@metamask/controller-utils';
 import {
   InfuraNetworkType,
@@ -517,72 +514,6 @@ export type NetworkControllerGetStateAction = ControllerGetStateAction<
 export type NetworkControllerGetEthQueryAction = {
   type: `NetworkController:getEthQuery`;
   handler: () => EthQuery | undefined;
-};
-
-export type NetworkControllerGetNetworkClientByIdAction = {
-  type: `NetworkController:getNetworkClientById`;
-  handler: NetworkController['getNetworkClientById'];
-};
-
-export type NetworkControllerGetSelectedNetworkClientAction = {
-  type: `NetworkController:getSelectedNetworkClient`;
-  handler: NetworkController['getSelectedNetworkClient'];
-};
-
-export type NetworkControllerGetSelectedChainIdAction = {
-  type: 'NetworkController:getSelectedChainId';
-  handler: NetworkController['getSelectedChainId'];
-};
-
-export type NetworkControllerGetEIP1559CompatibilityAction = {
-  type: `NetworkController:getEIP1559Compatibility`;
-  handler: NetworkController['getEIP1559Compatibility'];
-};
-
-export type NetworkControllerFindNetworkClientIdByChainIdAction = {
-  type: `NetworkController:findNetworkClientIdByChainId`;
-  handler: NetworkController['findNetworkClientIdByChainId'];
-};
-
-/**
- * Change the currently selected network to the given built-in network type.
- *
- * @deprecated This action has been replaced by `setActiveNetwork`, and will be
- * removed in a future release.
- */
-export type NetworkControllerSetProviderTypeAction = {
-  type: `NetworkController:setProviderType`;
-  handler: NetworkController['setProviderType'];
-};
-
-export type NetworkControllerSetActiveNetworkAction = {
-  type: `NetworkController:setActiveNetwork`;
-  handler: NetworkController['setActiveNetwork'];
-};
-
-export type NetworkControllerGetNetworkConfigurationByChainId = {
-  type: `NetworkController:getNetworkConfigurationByChainId`;
-  handler: NetworkController['getNetworkConfigurationByChainId'];
-};
-
-export type NetworkControllerGetNetworkConfigurationByNetworkClientId = {
-  type: `NetworkController:getNetworkConfigurationByNetworkClientId`;
-  handler: NetworkController['getNetworkConfigurationByNetworkClientId'];
-};
-
-export type NetworkControllerAddNetworkAction = {
-  type: 'NetworkController:addNetwork';
-  handler: NetworkController['addNetwork'];
-};
-
-export type NetworkControllerRemoveNetworkAction = {
-  type: 'NetworkController:removeNetwork';
-  handler: NetworkController['removeNetwork'];
-};
-
-export type NetworkControllerUpdateNetworkAction = {
-  type: 'NetworkController:updateNetwork';
-  handler: NetworkController['updateNetwork'];
 };
 
 // Define the methods we want to expose via the messenger
@@ -1242,11 +1173,7 @@ export class NetworkController extends BaseController<
         this.state.networkConfigurationsByChainId,
       );
 
-    registerMethodActionHandlers(
-      this,
-      this.messagingSystem,
-      MESSENGER_EXPOSED_METHODS,
-    );
+    this.registerActionHandlers(MESSENGER_EXPOSED_METHODS);
   }
 
   /**
