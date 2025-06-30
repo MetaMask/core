16b8c693a480d7d592a140bc3a527e28005b6fd2
soralit <soralitria@gmail.com>
2025-06-30 16:50:22 +0800

    feat: support keystone usb keyring

--- packages/accounts-controller/src/utils.ts
+++ packages/accounts-controller/src/utils.ts
@@ -30,6 +30,9 @@ export function keyringTypeToName(keyringType: string): string {
     case KeyringTypes.lattice: {
       return 'Lattice';
     }
+    case KeyringTypes.keystone: {
+      return 'Keystone';
+    }
     case KeyringTypes.qr: {
       return 'QR';
     }
