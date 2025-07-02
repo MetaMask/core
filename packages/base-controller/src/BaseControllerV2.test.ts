aab1f44b9f0b1b949e7942735574d9743df22810
Mark Stacey <markjstacey@gmail.com>
2025-07-02 14:32:47 -0230

    Add optional 'parent' Messenger constructor option to automatically delegate actions/events

--- packages/base-controller/src/BaseControllerV2.test.ts
+++ packages/base-controller/src/BaseControllerV2.test.ts
@@ -1097,11 +1097,7 @@ describe('getPersistentState', () => {
         VisitorControllerEvents
       >({
         namespace: visitorName,
-      });
-      visitorControllerMessenger.delegate({
-        actions: ['VisitorController:clear'],
-        events: ['VisitorController:stateChange'],
-        messenger: globalMessenger,
+        parent: globalMessenger,
       });
       const visitorController = new VisitorController(
         visitorControllerMessenger,
@@ -1114,11 +1110,7 @@ describe('getPersistentState', () => {
         | VisitorOverflowControllerDelegatedEvents
       >({
         namespace: visitorOverflowName,
-      });
-      visitorOverflowControllerMessenger.delegate({
-        actions: ['VisitorOverflowController:updateMax'],
-        events: ['VisitorOverflowController:stateChange'],
-        messenger: globalMessenger,
+        parent: globalMessenger,
       });
       globalMessenger.delegate({
         actions: ['VisitorController:clear'],
