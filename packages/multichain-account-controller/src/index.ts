0f72c5916ea01f1c84aaff53eb4de4884b8ce2a2
Charly Chevalier <charly.chevalier@consensys.net>
2025-07-03 22:57:38 +0200

    fix: fix exports

--- packages/multichain-account-controller/src/index.ts
+++ packages/multichain-account-controller/src/index.ts
@@ -1,9 +1,2 @@
-/**
- * Example function that returns a greeting for the given name.
- *
- * @param name - The name to greet.
- * @returns The greeting.
- */
-export default function greeter(name: string): string {
-  return `Hello, ${name}!`;
-}
+export type { MultichainAccountControllerMessenger } from './MultichainAccountController';
+export { MultichainAccountController } from './MultichainAccountController';
