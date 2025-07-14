59e49f017306732aaa7a1d15b176638c455f359f
Erik Marks <rekmarks@protonmail.com>
2025-07-14 10:46:22 -0700

    feat: MiddlewareEngine: types WIP

--- packages/json-rpc-engine/src/MiddlewareEngine.ts
+++ packages/json-rpc-engine/src/MiddlewareEngine.ts
@@ -2,44 +2,50 @@ import type {
   Json,
   JsonRpcRequest,
   JsonRpcNotification,
-  JsonRpcParams,
   NonEmptyArray,
 } from '@metamask/utils';
 import { freeze } from 'immer';
 import cloneDeep from 'lodash/clonedeep';
 
-import { stringify } from './utils';
+import { isRequest, stringify } from './utils';
 import type { JsonRpcCall } from './utils';
 
 export const EndNotification = Symbol.for('MiddlewareEngine:EndNotification');
 
 type Context = Record<string, unknown>;
 
-type ReturnHandler<
-  Result extends MiddlewareResultConstraint<JsonRpcCall<JsonRpcParams>>,
-> = (result: Readonly<Result>) => Result | Promise<Result>;
+type ReturnHandler<Result extends Json = Json> = (
+  result: Readonly<Result>,
+) => void | Result | Promise<void | Result>;
 
-type MiddlewareResultConstraint<Message extends JsonRpcCall<JsonRpcParams>> =
+type MiddlewareResultConstraint<Message extends JsonRpcCall> =
   Message extends JsonRpcNotification
-    ? void | typeof EndNotification
-    : void | Json | ReturnHandler<MiddlewareResultConstraint<Message>>;
+    ? Message extends JsonRpcRequest
+      ? void | Json | ReturnHandler
+      : void | typeof EndNotification
+    : void | Json | ReturnHandler;
+
+type HandledResult<Result extends MiddlewareResultConstraint<JsonRpcCall>> =
+  Exclude<Result, typeof EndNotification> | void;
 
 export type JsonRpcMiddleware<
-  Message extends JsonRpcCall<JsonRpcParams>,
+  Message extends JsonRpcCall,
   Result extends MiddlewareResultConstraint<Message>,
 > = (message: Readonly<Message>, context: Context) => Result | Promise<Result>;
 
 type Options<
-  Message extends JsonRpcCall<JsonRpcParams>,
+  Message extends JsonRpcCall,
   Result extends MiddlewareResultConstraint<Message>,
 > = {
   middleware: NonEmptyArray<JsonRpcMiddleware<Message, Result>>;
 };
 
 export class MiddlewareEngine<
-  Message extends JsonRpcCall<JsonRpcParams>,
+  Message extends JsonRpcCall,
   Result extends MiddlewareResultConstraint<Message>,
 > {
+  static readonly EndNotification = EndNotification;
+
   readonly #middleware: readonly JsonRpcMiddleware<Message, Result>[];
 
   constructor({ middleware }: Options<Message, Result>) {
@@ -52,57 +58,118 @@ export class MiddlewareEngine<
    * @param request - The JSON-RPC request to handle.
    * @returns The JSON-RPC response.
    */
-  async handle(request: JsonRpcRequest & Message): Promise<Result>;
+  async handle(
+    request: Message & JsonRpcRequest,
+  ): Promise<Exclude<HandledResult<Result>, void>>;
 
   /**
    * Handle a JSON-RPC notification. No response will be returned.
    *
    * @param message - The JSON-RPC notification to handle.
    */
-  async handle(message: JsonRpcNotification & Message): Promise<void>;
+  async handle(message: Message & JsonRpcNotification): Promise<void>;
 
-  async handle(req: Message): Promise<Result | void> {
+  async handle(req: Message): Promise<HandledResult<Result>> {
     const result = await this.#handle(req);
-    return result === EndNotification ? undefined : result;
+    return result === EndNotification
+      ? undefined
+      : (result as HandledResult<Result>);
+  }
+
+  // This exists because a JsonRpcCall overload of handle() cannot coexist with
+  // the other overloads due to type union / overload shenanigans.
+  /**
+   * Handle a JSON-RPC call. A response will be returned if the call is a request.
+   *
+   * @param message - The JSON-RPC call to handle.
+   * @returns The JSON-RPC response, if any.
+   */
+  async handleAny(message: Message): Promise<Extract<Result, Json> | void> {
+    return this.handle(message);
   }
 
   async #handle(message: Message, context: Context = {}): Promise<Result> {
     const immutableMessage = freeze(cloneDeep(message), true);
-    const returnHandlers: ReturnHandler<Result>[] = [];
 
-    let result: Result | undefined;
+    const { result, returnHandlers } = await this.#runMiddleware(
+      immutableMessage,
+      context,
+    );
+
+    return await this.#runReturnHandlers(message, result, returnHandlers);
+  }
+
+  async #runMiddleware(
+    message: Readonly<Message>,
+    context: Context,
+  ): Promise<{
+    result: Extract<Result, Json | typeof EndNotification>;
+    returnHandlers: ReturnHandler[];
+  }> {
+    const returnHandlers: ReturnHandler[] = [];
+
+    let result: Extract<Result, Json | typeof EndNotification> | undefined;
     for (const middleware of this.#middleware) {
-      const currentResult = await middleware(immutableMessage, context);
+      const currentResult = await middleware(message, context);
 
       if (typeof currentResult === 'function') {
         returnHandlers.push(currentResult);
       } else if (currentResult !== undefined) {
-        result = currentResult;
+        // Cast required due to incorrect type narrowing
+        result = currentResult as Extract<
+          Result,
+          Json | typeof EndNotification
+        >;
         break;
       }
     }
 
     if (result === undefined) {
-      throw new Error(`Nothing ended request:\n${stringify(message)}`);
-    }
-
-    if (returnHandlers.length > 0) {
+      throw new Error(`Nothing ended call:\n${stringify(message)}`);
+    } else if (isRequest(message)) {
       if (result === EndNotification) {
         throw new Error(
-          `Received return handlers for notification:\n${stringify(message)}`,
+          `Request handled as notification:\n${stringify(message)}`,
         );
       }
+    } else if (result !== EndNotification) {
+      throw new Error(
+        `Notification handled as request:\n${stringify(message)}`,
+      );
+    }
 
-      result = freeze(result, true);
-      for (const returnHandler of returnHandlers) {
-        const updatedResult = await returnHandler(result);
-        if (updatedResult !== undefined) {
-          result = freeze(updatedResult, true);
-        }
+    return {
+      result: result as Extract<Result, Json | typeof EndNotification>,
+      returnHandlers,
+    };
+  }
+
+  async #runReturnHandlers(
+    message: Readonly<Message>,
+    initialResult: Extract<Result, Json | typeof EndNotification>,
+    returnHandlers: ReturnHandler[],
+  ): Promise<Readonly<Result>> {
+    freeze(initialResult, true);
+
+    if (returnHandlers.length === 0) {
+      return initialResult;
+    }
+
+    if (initialResult === EndNotification) {
+      throw new Error(
+        `Received return handlers for notification:\n${stringify(message)}`,
+      );
+    }
+
+    let finalResult: Json = initialResult;
+    for (const returnHandler of returnHandlers) {
+      const updatedResult = await returnHandler(finalResult);
+      if (updatedResult !== undefined) {
+        finalResult = freeze(updatedResult, true);
       }
     }
 
-    return result;
+    return finalResult as Extract<Result, Json>;
   }
 
   /**
--- packages/json-rpc-engine/src/utils.ts
+++ packages/json-rpc-engine/src/utils.ts
@@ -1,17 +1,23 @@
 import type { Json } from '@metamask/utils';
 import {
   hasProperty,
-  type JsonRpcNotification,
+  type JsonRpcNotification as BaseJsonRpcNotification,
   type JsonRpcParams,
-  type JsonRpcRequest,
+  type JsonRpcRequest as BaseJsonRpcRequest,
 } from '@metamask/utils';
 
-export type JsonRpcCall<Params extends JsonRpcParams> =
+export type JsonRpcNotification<Params extends JsonRpcParams = JsonRpcParams> =
+  BaseJsonRpcNotification<Params>;
+
+export type JsonRpcRequest<Params extends JsonRpcParams = JsonRpcParams> =
+  BaseJsonRpcRequest<Params>;
+
+export type JsonRpcCall<Params extends JsonRpcParams = JsonRpcParams> =
   | JsonRpcNotification<Params>
   | JsonRpcRequest<Params>;
 
 export const isRequest = <Params extends JsonRpcParams>(
-  msg: JsonRpcCall<Params>,
+  msg: JsonRpcCall<Params> | Readonly<JsonRpcCall<Params>>,
 ): msg is JsonRpcRequest<Params> => hasProperty(msg, 'id');
 
 export const isNotification = <Params extends JsonRpcParams>(
