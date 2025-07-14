c0d0b566dc6bab833f83d0c9137c60c8cc2452f5
Erik Marks <rekmarks@protonmail.com>
2025-07-14 10:46:22 -0700

    feat: MiddlewareEngine: types WIP

---
+++ packages/json-rpc-engine/src/MiddlewareEngine.test.ts
@@ -0,0 +1,106 @@
+import { Json } from '@metamask/utils';
+
+import type { JsonRpcMiddleware } from './MiddlewareEngine';
+import { MiddlewareEngine, EndNotification } from './MiddlewareEngine';
+import type { JsonRpcCall, JsonRpcNotification, JsonRpcRequest } from './utils';
+
+describe('MiddlewareEngine', () => {
+  it('should handle a request', async () => {
+    const engine = new MiddlewareEngine({
+      middleware: [
+        (req: JsonRpcNotification, context): void => {},
+        (req: JsonRpcNotification, context): typeof EndNotification => {
+          return EndNotification;
+        },
+        (req: JsonRpcCall, context): void | typeof EndNotification => {
+          return EndNotification;
+        },
+        // (req: JsonRpcRequest, context): void | typeof EndNotification => {
+        //   return EndNotification;
+        // },
+      ],
+    });
+
+    const middleware: JsonRpcMiddleware<JsonRpcRequest, void> = (
+      req,
+      context,
+    ) => {};
+    const middleware2: JsonRpcMiddleware<
+      JsonRpcRequest,
+      // @ts-expect-error Should be illegal.
+      typeof EndNotification
+    > = (req, context) => {
+      return EndNotification;
+    };
+    type foo = ReturnType<typeof middleware2>;
+
+    const engine2 = new MiddlewareEngine({
+      middleware: [
+        // @ts-expect-error Should be illegal.
+        (req: JsonRpcRequest, context): void | typeof EndNotification => {
+          return EndNotification;
+        },
+      ],
+    });
+
+    const engine3 = new MiddlewareEngine({
+      middleware: [
+        ((req: JsonRpcRequest, context) => {
+          return null;
+        }) as JsonRpcMiddleware<JsonRpcRequest, null>,
+      ],
+    });
+
+    const engine4 = new MiddlewareEngine({
+      middleware: [
+        ((req: JsonRpcCall, context): null => {
+          return null;
+        }) as JsonRpcMiddleware<JsonRpcCall, null>,
+      ],
+    });
+
+    const reqRes = await engine4.handle({
+      id: '1',
+      method: 'foo',
+      jsonrpc: '2.0',
+      params: [],
+    });
+
+    const notifRes = await engine4.handle({
+      method: 'foo',
+      jsonrpc: '2.0',
+      params: [],
+    });
+
+    const callRes = await engine4.handleAny({
+      id: '1',
+      method: 'foo',
+      jsonrpc: '2.0',
+      params: [],
+    });
+
+    const a: JsonRpcRequest = {
+      id: '1',
+      method: 'foo',
+      jsonrpc: '2.0',
+      params: [],
+    };
+
+    const foo: JsonRpcCall = {
+      id: '1',
+      method: 'foo',
+      jsonrpc: '2.0',
+      params: [],
+    };
+
+    const bar: JsonRpcNotification = {
+      method: 'foo',
+      jsonrpc: '2.0',
+      params: [],
+    };
+
+    const fizz: JsonRpcNotification = foo;
+
+    const b: JsonRpcRequest = foo;
+  });
+});
