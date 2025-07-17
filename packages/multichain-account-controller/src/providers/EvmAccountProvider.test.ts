4588bad5e1f5d2f498f90a9bed3e8f90ef8d3e48
Charly Chevalier <charly.chevalier@consensys.net>
2025-07-17 18:51:50 +0200

    refactor: remove unused test code

--- packages/multichain-account-controller/src/providers/EvmAccountProvider.test.ts
+++ packages/multichain-account-controller/src/providers/EvmAccountProvider.test.ts
@@ -1,9 +1,5 @@
 import type { Messenger } from '@metamask/base-controller';
-import type { KeyringMetadata } from '@metamask/keyring-controller';
-import type {
-  EthKeyring,
-  InternalAccount,
-} from '@metamask/keyring-internal-api';
+import type { InternalAccount } from '@metamask/keyring-internal-api';
 
 import { EvmAccountProvider } from './EvmAccountProvider';
 import {
@@ -11,7 +7,6 @@ import {
   getRootMessenger,
   MOCK_HD_ACCOUNT_1,
   MOCK_HD_ACCOUNT_2,
-  MockAccountBuilder,
 } from '../tests';
 import type {
   AllowedActions,
@@ -19,51 +14,6 @@ import type {
   MultichainAccountControllerActions,
   MultichainAccountControllerEvents,
 } from '../types';
-
-class MockEthKeyring implements EthKeyring {
-  readonly type = 'MockEthKeyring';
-
-  readonly metadata: KeyringMetadata = {
-    id: 'mock-eth-keyring-id',
-    name: '',
-  };
-
-  readonly accounts: InternalAccount[];
-
-  constructor(accounts: InternalAccount[]) {
-    this.accounts = accounts;
-  }
-
-  async serialize() {
-    return 'serialized';
-  }
-
-  async deserialize(_: string) {
-    // Not required.
-  }
-
-  getAccounts = jest
-    .fn()
-    .mockImplementation(() => this.accounts.map((account) => account.address));
-
-  addAccounts = jest.fn().mockImplementation((numberOfAccounts: number) => {
-    const newAccountsIndex = this.accounts.length;
-
-    // Just generate a new address by appending the number of accounts owned by that fake keyring.
-    for (let i = 0; i < numberOfAccounts; i++) {
-      this.accounts.push(
-        MockAccountBuilder.from(MOCK_HD_ACCOUNT_1)
-          .withUuid()
-          .withAddressSuffix(`${this.accounts.length}`)
-          .get(),
-      );
-    }
-
-    return this.accounts
-      .slice(newAccountsIndex)
-      .map((account) => account.address);
-  });
-}
 
 /**
  * Sets up a EvmAccountProvider for testing.
@@ -88,31 +38,10 @@ function setup({
     MultichainAccountControllerActions | AllowedActions,
     MultichainAccountControllerEvents | AllowedEvents
   >;
-  keyring: MockEthKeyring;
-  mocks: {
-    getAccountsByAddress: jest.Mock;
-  };
 } {
-  const keyring = new MockEthKeyring(accounts);
-
   messenger.registerActionHandler(
     'AccountsController:listMultichainAccounts',
     () => accounts,
-  );
-
-  const mockGetAccountByAddress = jest
-    .fn()
-    .mockImplementation((address: string) =>
-      keyring.accounts.find((account) => account.address === address),
-    );
-  messenger.registerActionHandler(
-    'AccountsController:getAccountByAddress',
-    mockGetAccountByAddress,
-  );
-
-  messenger.registerActionHandler(
-    'KeyringController:withKeyring',
-    async (_, operation) => operation({ keyring, metadata: keyring.metadata }),
   );
 
   const provider = new EvmAccountProvider(
@@ -122,10 +51,6 @@ function setup({
   return {
     provider,
     messenger,
-    keyring,
-    mocks: {
-      getAccountsByAddress: mockGetAccountByAddress,
-    },
   };
 }
 
--- packages/multichain-account-controller/src/providers/SolAccountProvider.test.ts
+++ packages/multichain-account-controller/src/providers/SolAccountProvider.test.ts
@@ -1,10 +1,5 @@
 import type { Messenger } from '@metamask/base-controller';
-import type { SnapKeyring } from '@metamask/eth-snap-keyring';
-import type { KeyringMetadata } from '@metamask/keyring-controller';
-import type {
-  EthKeyring,
-  InternalAccount,
-} from '@metamask/keyring-internal-api';
+import type { InternalAccount } from '@metamask/keyring-internal-api';
 
 import { SolAccountProvider } from './SolAccountProvider';
 import {
@@ -12,7 +7,6 @@ import {
   getRootMessenger,
   MOCK_HD_ACCOUNT_1,
   MOCK_SNAP_ACCOUNT_1,
-  MockAccountBuilder,
 } from '../tests';
 import type {
   AllowedActions,
@@ -20,61 +14,6 @@ import type {
   MultichainAccountControllerActions,
   MultichainAccountControllerEvents,
 } from '../types';
-
-class MockSolanaKeyring {
-  readonly type = 'MockSolanaKeyring';
-
-  readonly metadata: KeyringMetadata = {
-    id: 'mock-solana-keyring-id',
-    name: '',
-  };
-
-  readonly accounts: InternalAccount[];
-
-  constructor(accounts: InternalAccount[]) {
-    this.accounts = accounts;
-  }
-
-  #getIndexFromDerivationPath(derivationPath: string): number {
-    // eslint-disable-next-line prefer-regex-literals
-    const derivationPathIndexRegex = new RegExp(
-      "m/44'/501'/(?<index>[0-9]+)'/0",
-      'u',
-    );
-
-    const matched = derivationPath.match(derivationPathIndexRegex);
-    if (matched?.groups?.index === undefined) {
-      throw new Error('Unable to extract index');
-    }
-
-    const { index } = matched.groups;
-    return Number(index);
-  }
-
-  createAccount: SnapKeyring['createAccount'] = jest
-    .fn()
-    .mockImplementation((_, options) => {
-      if (options.derivationPath !== undefined) {
-        const index = this.#getIndexFromDerivationPath(options.derivationPath);
-        const found = this.accounts.find(
-          (account) => account.options.index === index,
-        );
-
-        if (found) {
-          return found; // Idempotent.
-        }
-      }
-
-      const account = MockAccountBuilder.from(MOCK_SNAP_ACCOUNT_1)
-        .withUuid()
-        .withAddressSuffix(`${this.accounts.length}`)
-        .withGroupIndex(this.accounts.length)
-        .get();
-      this.accounts.push(account);
-
-      return account;
-    });
-}
 
 /**
  * Sets up a SolAccountProvider for testing.
@@ -99,40 +38,10 @@ function setup({
     MultichainAccountControllerActions | AllowedActions,
     MultichainAccountControllerEvents | AllowedEvents
   >;
-  keyring: MockSolanaKeyring;
-  mocks: {
-    handleRequest: jest.Mock;
-    keyring: {
-      createAccount: jest.Mock;
-    };
-  };
 } {
-  const keyring = new MockSolanaKeyring(accounts);
-
   messenger.registerActionHandler(
     'AccountsController:listMultichainAccounts',
     () => accounts,
-  );
-
-  const mockHandleRequest = jest
-    .fn()
-    .mockImplementation((address: string) =>
-      keyring.accounts.find((account) => account.address === address),
-    );
-  messenger.registerActionHandler(
-    'SnapController:handleRequest',
-    mockHandleRequest,
-  );
-
-  messenger.registerActionHandler(
-    'KeyringController:withKeyring',
-    async (_, operation) =>
-      operation({
-        // We type-cast here, since `withKeyring` defaults to `EthKeyring` and the
-        // Snap keyring does really implement this interface (this is expected).
-        keyring: keyring as unknown as EthKeyring,
-        metadata: keyring.metadata,
-      }),
   );
 
   const provider = new SolAccountProvider(
@@ -142,13 +51,6 @@ function setup({
   return {
     provider,
     messenger,
-    keyring,
-    mocks: {
-      handleRequest: mockHandleRequest,
-      keyring: {
-        createAccount: keyring.createAccount as jest.Mock,
-      },
-    },
   };
 }
 
