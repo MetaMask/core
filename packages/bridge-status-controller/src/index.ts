c711161fc7971e035882e4bdba8e5fc2738da5a7
IF <139582705+infiniteflower@users.noreply.github.com>
2025-07-21 16:15:03 -0400

    fix: exports

--- packages/bridge-status-controller/src/index.ts
+++ packages/bridge-status-controller/src/index.ts
@@ -21,7 +21,7 @@ export type {
   BridgeStatusControllerStartPollingForBridgeTxStatusAction,
   BridgeStatusControllerWipeBridgeStatusAction,
   BridgeStatusControllerResetStateAction,
-  BridgeStatusControllerResetAttemptsAction,
+  BridgeStatusControllerRestartPollingForFailedAttemptsAction,
   BridgeStatusControllerEvents,
   BridgeStatusControllerStateChangeEvent,
   StartPollingForBridgeTxStatusArgs,
