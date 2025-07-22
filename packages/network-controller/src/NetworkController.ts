b54c58c931c4c8fb5e108df5fea6977d0a2eb220
root <tengzuo.veritas@gmail.com>
2025-07-22 14:19:51 +0000

    chore: allow deletion of selected network

--- packages/network-controller/src/NetworkController.ts
+++ packages/network-controller/src/NetworkController.ts
@@ -2351,15 +2351,6 @@ export class NetworkController extends BaseController<
       );
     }
 
-    if (
-      existingNetworkConfiguration.rpcEndpoints.some(
-        (rpcEndpoint) =>
-          rpcEndpoint.networkClientId === this.state.selectedNetworkClientId,
-      )
-    ) {
-      throw new Error(`Cannot remove the currently selected network`);
-    }
-
     const autoManagedNetworkClientRegistry =
       this.#ensureAutoManagedNetworkClientRegistryPopulated();
 
