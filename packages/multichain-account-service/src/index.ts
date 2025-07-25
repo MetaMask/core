3eaf30a220af7fb689785ba5f2f9450c4d9b8b56
Charly Chevalier <charly.chevalier@consensys.net>
2025-07-25 14:19:41 +0200

    fix: add missing exports

--- packages/multichain-account-service/src/index.ts
+++ packages/multichain-account-service/src/index.ts
@@ -2,5 +2,9 @@ export type {
   MultichainAccountServiceActions,
   MultichainAccountServiceEvents,
   MultichainAccountServiceMessenger,
+  MultichainAccountServiceGetMultichainAccountAction,
+  MultichainAccountServiceGetMultichainAccountWalletAction,
+  MultichainAccountServiceGetMultichainAccountWalletsAction,
+  MultichainAccountServiceGetMultichainAccountsAction,
 } from './types';
 export { MultichainAccountService } from './MultichainAccountService';
