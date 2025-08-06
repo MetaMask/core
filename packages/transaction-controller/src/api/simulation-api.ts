c8fd414471f8550f0bde1c362d98c6c547587da4
Pedro Figueiredo <pedro.figueiredo@consensys.net>
2025-08-06 14:46:34 +0100

    Reorder properties

--- packages/transaction-controller/src/api/simulation-api.ts
+++ packages/transaction-controller/src/api/simulation-api.ts
@@ -175,6 +175,9 @@ export type SimulationResponseTokenFee = {
   /** Account address that token should be transferred to. */
   feeRecipient: Hex;
 
+  /** Whether the gas fee is sponsored meaning the transfer is not required. */
+  isSponsored?: boolean;
+
   /** Conversation rate of 1 token to native WEI. */
   rateWei: Hex;
 
@@ -183,9 +186,6 @@ export type SimulationResponseTokenFee = {
 
   /** Estimated gas limit required for fee transfer. */
   transferEstimate: Hex;
-
-  /** Whether the gas fee is sponsored by the network. */
-  isSponsored?: boolean;
 };
 
 /** Response from the simulation API for a single transaction. */
