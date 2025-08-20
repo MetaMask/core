8f86aad50ebc4cc928d0aff9f3f109ede16a893e
IF <139582705+infiniteflower@users.noreply.github.com>
2025-08-20 15:02:21 -0400

    feat: add AssetDetailTooltipClicked event

--- packages/bridge-controller/src/utils/metrics/types.ts
+++ packages/bridge-controller/src/utils/metrics/types.ts
@@ -189,6 +189,13 @@ export type RequiredEventContextFromClient = {
     price_impact: QuoteFetchData['price_impact'];
     can_submit: QuoteFetchData['can_submit'];
   };
+  [UnifiedSwapBridgeEventName.AssetDetailTooltipClicked]: {
+    token_name: string;
+    token_symbol: string;
+    token_contract: string;
+    chain_name: string;
+    chain_id: string;
+  };
 };
 
 /**
@@ -242,6 +249,7 @@ export type EventPropertiesFromControllerState = {
     RequestMetadata &
     QuoteFetchData &
     TradeData;
+  [UnifiedSwapBridgeEventName.AssetDetailTooltipClicked]: null;
 };
 
 /**
