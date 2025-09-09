048f5c5a906ca0a0e88d84fc3dc9ce7b805b7983
Tuna <tunguyenanh@tunatech.org>
2025-09-09 09:30:12 +0700

    fix: remove redundant test

--- packages/subscription-controller/src/constants.test.ts
+++ packages/subscription-controller/src/constants.test.ts
@@ -1,28 +1,8 @@
-import { Env, getEnvUrls, controllerName } from './constants';
+import type { Env } from './constants';
+import { getEnvUrls, controllerName } from './constants';
 
 describe('constants', () => {
   describe('getEnvUrls', () => {
-    it('should return correct URLs for dev environment', () => {
-      const result = getEnvUrls(Env.DEV);
-      expect(result).toStrictEqual({
-        subscriptionApiUrl: 'https://subscription.dev-api.cx.metamask.io',
-      });
-    });
-
-    it('should return correct URLs for uat environment', () => {
-      const result = getEnvUrls(Env.UAT);
-      expect(result).toStrictEqual({
-        subscriptionApiUrl: 'https://subscription.uat-api.cx.metamask.io',
-      });
-    });
-
-    it('should return correct URLs for prd environment', () => {
-      const result = getEnvUrls(Env.PRD);
-      expect(result).toStrictEqual({
-        subscriptionApiUrl: 'https://subscription.api.cx.metamask.io',
-      });
-    });
-
     it('should throw error for invalid environment', () => {
       // Type assertion to test invalid environment
       const invalidEnv = 'invalid' as Env;
