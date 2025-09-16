a718598210da1ebef03fdb5523d018d713e9246f
Julien Fontanel <julien.fontanel@consensys.net>
2025-09-16 15:05:10 +0200

    fix: unit test

--- packages/controller-utils/src/index.test.ts
+++ packages/controller-utils/src/index.test.ts
@@ -49,6 +49,7 @@ describe('@metamask/controller-utils', () => {
         "HOURS",
         "DAY",
         "DAYS",
+        "NETWORKS_BYPASSING_VALIDATION",
         "BNToHex",
         "convertHexToDecimal",
         "fetchWithErrorHandling",
@@ -90,7 +91,6 @@ describe('@metamask/controller-utils', () => {
         "parseDomainParts",
         "isValidSIWEOrigin",
         "detectSIWE",
-        "NETWORKS_BYPASSING_VALIDATION",
       ]
     `);
   });
