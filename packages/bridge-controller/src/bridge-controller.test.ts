cc1643d5b142f9bc8a7d6313ba42aa19475e04ce
Micaela Estabillo <micaela.estabillo@consensys.net>
2025-09-25 11:02:23 -0700

    test: bridge-controller unit tests

--- packages/bridge-controller/src/bridge-controller.test.ts
+++ packages/bridge-controller/src/bridge-controller.test.ts
@@ -219,20 +219,22 @@ describe('BridgeController', function () {
     } as never);
 
     await bridgeController.updateBridgeQuoteRequestParams(
-      { srcChainId: 1 },
+      { srcChainId: 1, walletAddress: '0x123' },
       metricsContext,
     );
     expect(bridgeController.state.quoteRequest).toStrictEqual({
+      walletAddress: '0x123',
       srcChainId: 1,
       srcTokenAddress: '0x0000000000000000000000000000000000000000',
     });
     expect(trackMetaMetricsFn).toHaveBeenCalledTimes(1);
 
     await bridgeController.updateBridgeQuoteRequestParams(
-      { destChainId: 10 },
+      { destChainId: 10, walletAddress: '0x123' },
       metricsContext,
     );
     expect(bridgeController.state.quoteRequest).toStrictEqual({
+      walletAddress: '0x123',
       destChainId: 10,
       srcTokenAddress: '0x0000000000000000000000000000000000000000',
     });
@@ -240,10 +242,12 @@ describe('BridgeController', function () {
     await bridgeController.updateBridgeQuoteRequestParams(
       {
         destChainId: undefined,
+        walletAddress: '0x123abc',
       },
       metricsContext,
     );
     expect(bridgeController.state.quoteRequest).toStrictEqual({
+      walletAddress: '0x123abc',
       destChainId: undefined,
       srcTokenAddress: '0x0000000000000000000000000000000000000000',
     });
@@ -251,10 +255,12 @@ describe('BridgeController', function () {
     await bridgeController.updateBridgeQuoteRequestParams(
       {
         srcTokenAddress: undefined,
+        walletAddress: '0x123',
       },
       metricsContext,
     );
     expect(bridgeController.state.quoteRequest).toStrictEqual({
+      walletAddress: '0x123',
       srcTokenAddress: undefined,
     });
 
@@ -264,10 +270,12 @@ describe('BridgeController', function () {
         destTokenAddress: '0x123',
         slippage: 0.5,
         srcTokenAddress: '0x0000000000000000000000000000000000000000',
+        walletAddress: '0x123',
       },
       metricsContext,
     );
     expect(bridgeController.state.quoteRequest).toStrictEqual({
+      walletAddress: '0x123',
       srcTokenAmount: '100000',
       destTokenAddress: '0x123',
       slippage: 0.5,
@@ -277,10 +285,12 @@ describe('BridgeController', function () {
     await bridgeController.updateBridgeQuoteRequestParams(
       {
         srcTokenAddress: '0x2ABC',
+        walletAddress: '0x123',
       },
       metricsContext,
     );
     expect(bridgeController.state.quoteRequest).toStrictEqual({
+      walletAddress: '0x123',
       srcTokenAddress: '0x2ABC',
     });
 
@@ -549,9 +559,7 @@ describe('BridgeController', function () {
             throw new Error('Currency rate error');
           }
 
-          if (
-            actionType === 'AccountsController:getSelectedMultichainAccount'
-          ) {
+          if (actionType === 'AccountsController:getAccountByAddress') {
             return {
               type: SolAccountType.DataAccount,
               id: 'account1',
@@ -995,7 +1003,7 @@ describe('BridgeController', function () {
       ): ReturnType<BridgeControllerMessenger['call']> => {
         const actionType = args[0];
 
-        if (actionType === 'AccountsController:getSelectedMultichainAccount') {
+        if (actionType === 'AccountsController:getAccountByAddress') {
           return {
             type: SolAccountType.DataAccount,
             id: 'account1',
@@ -1112,12 +1120,13 @@ describe('BridgeController', function () {
     const stopAllPollingSpy = jest.spyOn(bridgeController, 'stopAllPolling');
     const startPollingSpy = jest.spyOn(bridgeController, 'startPolling');
     messengerMock.call.mockReturnValue({
-      address: '0x123',
+      address: '0x123WalletAddress',
       provider: jest.fn(),
     } as never);
 
     await bridgeController.updateBridgeQuoteRequestParams(
       {
+        walletAddress: '0x123WalletAddress',
         srcChainId: 1,
         destChainId: 10,
         srcTokenAddress: '0x0000000000000000000000000000000000000000',
@@ -1136,7 +1145,7 @@ describe('BridgeController', function () {
           srcChainId: 1,
           slippage: 0.5,
           srcTokenAddress: '0x0000000000000000000000000000000000000000',
-          walletAddress: undefined,
+          walletAddress: '0x123WalletAddress',
           destChainId: 10,
           destTokenAddress: '0x123',
         },
@@ -1152,12 +1161,13 @@ describe('BridgeController', function () {
     const stopAllPollingSpy = jest.spyOn(bridgeController, 'stopAllPolling');
     const startPollingSpy = jest.spyOn(bridgeController, 'startPolling');
     messengerMock.call.mockReturnValue({
-      address: '0x123',
+      address: '0xabcWalletAddress',
       provider: jest.fn(),
     } as never);
 
     await bridgeController.updateBridgeQuoteRequestParams(
       {
+        walletAddress: '0xabcWalletAddress',
         srcChainId: 1,
         destChainId: ChainId.SOLANA,
         srcTokenAddress: '0x0000000000000000000000000000000000000000',
@@ -1176,7 +1186,7 @@ describe('BridgeController', function () {
           srcChainId: 1,
           slippage: 0.5,
           srcTokenAddress: '0x0000000000000000000000000000000000000000',
-          walletAddress: undefined,
+          walletAddress: '0xabcWalletAddress',
           destChainId: ChainId.SOLANA,
           destTokenAddress: '0x123',
         },
@@ -1663,9 +1673,7 @@ describe('BridgeController', function () {
         ): ReturnType<BridgeControllerMessenger['call']> => {
           const [actionType, params] = args;
 
-          if (
-            actionType === 'AccountsController:getSelectedMultichainAccount'
-          ) {
+          if (actionType === 'AccountsController:getAccountByAddress') {
             if (isEvmAccount) {
               return {
                 type: EthAccountType.Eoa,
@@ -1857,7 +1865,7 @@ describe('BridgeController', function () {
       ): ReturnType<BridgeControllerMessenger['call']> => {
         const [actionType, params] = args;
 
-        if (actionType === 'AccountsController:getSelectedMultichainAccount') {
+        if (actionType === 'AccountsController:getAccountByAddress') {
           return {
             type: 'btc:p2wpkh',
             id: 'btc-account-1',
@@ -1956,9 +1964,21 @@ describe('BridgeController', function () {
   });
 
   describe('trackUnifiedSwapBridgeEvent client-side calls', () => {
-    beforeEach(() => {
+    beforeEach(async () => {
       jest.clearAllMocks();
-      messengerMock.call.mockImplementation(
+      await bridgeController.updateBridgeQuoteRequestParams(
+        {
+          walletAddress: '0x123',
+        },
+        {
+          stx_enabled: false,
+          security_warnings: [],
+          token_symbol_source: 'ETH',
+          token_symbol_destination: 'USDC',
+        },
+      );
+      jest.clearAllMocks();
+      messengerMock.call.mockImplementationOnce(
         (): ReturnType<BridgeControllerMessenger['call']> => {
           return {
             provider: jest.fn() as never,
@@ -1966,6 +1986,39 @@ describe('BridgeController', function () {
             rpcUrl: 'https://mainnet.infura.io/v3/123',
             configuration: {
               chainId: 'eip155:1',
+            },
+          } as never;
+        },
+      );
+      messengerMock.call.mockImplementationOnce(
+        (): ReturnType<BridgeControllerMessenger['call']> => {
+          return {
+            provider: jest.fn() as never,
+            selectedNetworkClientId: 'selectedNetworkClientId',
+            rpcUrl: 'https://mainnet.infura.io/v3/123',
+            configuration: {
+              chainId: 'eip155:1',
+            },
+          } as never;
+        },
+      );
+      messengerMock.call.mockImplementationOnce(
+        (): ReturnType<BridgeControllerMessenger['call']> => {
+          return {
+            type: EthAccountType.Eoa,
+            id: 'account1',
+            scopes: [EthScope.Eoa],
+            methods: [],
+            address: '0x123',
+            metadata: {
+              name: 'Account 1',
+              importTime: 1717334400,
+              keyring: {
+                type: 'Keyring',
+              },
+            },
+            options: {
+              scope: 'mainnet',
             },
           } as never;
         },
@@ -2087,6 +2140,7 @@ describe('BridgeController', function () {
           can_submit: true,
         },
       );
+      expect(messengerMock.call.mock.calls).toMatchSnapshot();
       expect(trackMetaMetricsFn).toHaveBeenCalledTimes(1);
 
       expect(trackMetaMetricsFn.mock.calls).toMatchSnapshot();
@@ -2112,6 +2166,8 @@ describe('BridgeController', function () {
   describe('trackUnifiedSwapBridgeEvent bridge-status-controller calls', () => {
     beforeEach(() => {
       jest.clearAllMocks();
+
+      jest.restoreAllMocks();
       messengerMock.call.mockImplementation(() => {
         return {
           provider: jest.fn() as never,
@@ -2233,6 +2289,7 @@ describe('BridgeController', function () {
           security_warnings: [],
         },
       );
+      expect(messengerMock.call).toHaveBeenCalledTimes(2);
       expect(trackMetaMetricsFn).toHaveBeenCalledTimes(1);
 
       expect(trackMetaMetricsFn.mock.calls).toMatchSnapshot();
@@ -2262,6 +2319,7 @@ describe('BridgeController', function () {
         UnifiedSwapBridgeEventName.Failed,
         {
           error_message: 'Failed to submit tx',
+          is_hardware_wallet: false,
           usd_quoted_gas: 1,
           gas_included: false,
           gas_included_7702: false,
@@ -2320,9 +2378,7 @@ describe('BridgeController', function () {
           ...args: Parameters<BridgeControllerMessenger['call']>
         ): ReturnType<BridgeControllerMessenger['call']> => {
           const actionType = args[0];
-          if (
-            actionType === 'AccountsController:getSelectedMultichainAccount'
-          ) {
+          if (actionType === 'AccountsController:getAccountByAddress') {
             return {
               type: SolAccountType.DataAccount,
               id: 'account1',
@@ -2355,10 +2411,21 @@ describe('BridgeController', function () {
       );
     });
 
-    it('should not track the event if the account keyring type is not set', () => {
+    it('should not track the event if the account keyring type is not set', async () => {
       const errorSpy = jest
         .spyOn(console, 'error')
         .mockImplementation(jest.fn());
+      await bridgeController.updateBridgeQuoteRequestParams(
+        {
+          walletAddress: '0x123',
+        },
+        {
+          stx_enabled: false,
+          security_warnings: [],
+          token_symbol_source: 'ETH',
+          token_symbol_destination: 'USDC',
+        },
+      );
       bridgeController.trackUnifiedSwapBridgeEvent(
         UnifiedSwapBridgeEventName.QuotesReceived,
         {
@@ -2418,6 +2485,9 @@ describe('BridgeController', function () {
             },
           },
         });
+      (messengerMock.call as jest.Mock).mockReturnValueOnce(() => ({
+        address: '0x123',
+      }));
     });
 
     it('should override aggIds and noFee in perps request', async () => {
