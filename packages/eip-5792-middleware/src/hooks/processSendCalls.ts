7b145137ea21c5182c434f8f65e3b7fa62e97b20
Matthew Walsh <matthew.walsh@consensys.net>
2025-09-28 16:42:32 +0100

    Pass required assets from middleware

--- packages/eip-5792-middleware/src/hooks/processSendCalls.ts
+++ packages/eip-5792-middleware/src/hooks/processSendCalls.ts
@@ -208,11 +208,17 @@ async function processSingleTransaction({
 
   const batchId = generateBatchId();
 
+  const requiredAssets =
+    sendCalls.capabilities?.auxiliaryFunds?.requiredAssets?.filter(
+      (a) => a.standard === 'erc20',
+    );
+
   await addTransaction(txParams, {
+    batchId,
     networkClientId,
     origin,
+    requiredAssets,
     securityAlertResponse: { securityAlertId } as SecurityAlertResponse,
-    batchId,
   });
   return batchId;
 }
@@ -291,10 +297,16 @@ async function processMultipleTransaction({
 
   dedupeAuxiliaryFundsRequiredAssets(sendCalls);
 
+  const requiredAssets =
+    sendCalls.capabilities?.auxiliaryFunds?.requiredAssets?.filter(
+      (a) => a.standard === 'erc20',
+    );
+
   const result = await addTransactionBatch({
     from,
     networkClientId,
     origin,
+    requiredAssets,
     securityAlertId,
     transactions,
     validateSecurity,
