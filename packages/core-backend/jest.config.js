2b4d8fa2c6b4af6592080114845c9dd00c7c2b2c
Kriys94 <christian.trn@gmail.com>
2025-09-30 13:36:52 +0200

    feat(core-backend): clean tests

--- packages/core-backend/jest.config.js
+++ packages/core-backend/jest.config.js
@@ -14,6 +14,10 @@ module.exports = merge(baseConfig, {
   // The display name when running multiple projects
   displayName,
 
+  // Use jsdom for BackendWebSocketService tests
+  testEnvironment: 'jsdom',
+  testEnvironmentOptions: {},
+
   // An object that configures minimum threshold enforcement for coverage results
   coverageThreshold: {
     global: {
