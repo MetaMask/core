192dd4ab453a0e86f0314e2b6d1c634d3f114b5c
Matthias Geihs <matthias.geihs@consensys.net>
2025-10-08 12:28:17 +0200

    make fields optional

--- packages/shield-controller/src/backend.ts
+++ packages/shield-controller/src/backend.ts
@@ -42,8 +42,8 @@ export type GetCoverageResultRequest = {
 };
 
 export type GetCoverageResultResponse = {
-  message: string;
-  reasonCode: string;
+  message?: string;
+  reasonCode?: string;
   status: CoverageStatus;
 };
 
