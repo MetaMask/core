a92cc6f726b0598d7500103b9065cbd2ec1cc7f2
MoMannn <tadej.vengust@consensys.net>
2025-10-09 14:08:15 +0200

    fix: format index.ts exports

--- packages/eip-7702-internal-rpc-middleware/src/index.ts
+++ packages/eip-7702-internal-rpc-middleware/src/index.ts
@@ -3,10 +3,7 @@ export { walletUpgradeAccount } from './wallet_upgradeAccount';
 export { walletGetAccountUpgradeStatus } from './wallet_getAccountUpgradeStatus';
 
 // Utilities
-export {
-  validateParams,
-  validateAndNormalizeAddress,
-} from './utils';
+export { validateParams, validateAndNormalizeAddress } from './utils';
 
 // Constants
 export { METHOD_NAMES } from './constants';
