3287605d1c6db44b8fdeb2bfed5df20cc65b6472
MoMannn <tadej.vengust@consensys.net>
2025-10-09 11:45:05 +0200

    fix index exports

--- packages/eip-7702-internal-rpc-middleware/src/index.ts
+++ packages/eip-7702-internal-rpc-middleware/src/index.ts
@@ -1,10 +1,6 @@
-// Hook functions
-export { upgradeAccount } from './hooks/upgradeAccount';
-export { getAccountUpgradeStatus } from './hooks/getAccountUpgradeStatus';
-
-// Method wrappers
-export { walletUpgradeAccount } from './methods/wallet_upgradeAccount';
-export { walletGetAccountUpgradeStatus } from './methods/wallet_getAccountUpgradeStatus';
+// Method handlers
+export { walletUpgradeAccount } from './wallet_upgradeAccount';
+export { walletGetAccountUpgradeStatus } from './wallet_getAccountUpgradeStatus';
 
 // Utilities
 export {
@@ -22,6 +18,4 @@ export type {
   UpgradeAccountResult,
   GetAccountUpgradeStatusParams,
   GetAccountUpgradeStatusResult,
-  UpgradeAccountHooks,
-  GetAccountUpgradeStatusHooks,
 } from './types';
