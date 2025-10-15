1104e82a538069e145224a6a1ad09c9282c72d22
Michele Esposito <michele@esposito.codes>
2025-10-15 12:12:44 +0200

    Squash merge mikesposito/messenger/assets-controllers into mikesposito/messenger/preview

--- packages/approval-controller/src/ApprovalController.ts
+++ packages/approval-controller/src/ApprovalController.ts
@@ -1,9 +1,12 @@
-import type { ControllerGetStateAction } from '@metamask/base-controller';
+import type {
+  ControllerGetStateAction,
+  StateMetadata,
+} from '@metamask/base-controller/next';
 import {
   BaseController,
   type ControllerStateChangeEvent,
-  type RestrictedMessenger,
-} from '@metamask/base-controller';
+} from '@metamask/base-controller/next';
+import type { Messenger } from '@metamask/messenger';
 import type { JsonRpcError, DataWithOptionalCause } from '@metamask/rpc-errors';
 import { rpcErrors } from '@metamask/rpc-errors';
 import type { Json, OptionalField } from '@metamask/utils';
@@ -26,23 +29,23 @@ export const APPROVAL_TYPE_RESULT_SUCCESS = 'result_success';
 
 const controllerName = 'ApprovalController';
 
-const stateMetadata = {
+const stateMetadata: StateMetadata<ApprovalControllerState> = {
   pendingApprovals: {
     includeInStateLogs: true,
     persist: false,
-    anonymous: true,
+    includeInDebugSnapshot: true,
     usedInUi: true,
   },
   pendingApprovalCount: {
     includeInStateLogs: true,
     persist: false,
-    anonymous: false,
+    includeInDebugSnapshot: false,
     usedInUi: true,
   },
   approvalFlows: {
     includeInStateLogs: true,
     persist: false,
-    anonymous: false,
+    includeInDebugSnapshot: false,
     usedInUi: true,
   },
 };
@@ -134,12 +137,10 @@ export type ApprovalControllerState = {
   approvalFlows: ApprovalFlowState[];
 };
 
-export type ApprovalControllerMessenger = RestrictedMessenger<
+export type ApprovalControllerMessenger = Messenger<
   typeof controllerName,
   ApprovalControllerActions,
-  ApprovalControllerEvents,
-  never,
-  never
+  ApprovalControllerEvents
 >;
 
 // Option Types
@@ -413,12 +414,12 @@ export class ApprovalController extends BaseController<
    * actions.
    */
   private registerMessageHandlers(): void {
-    this.messagingSystem.registerActionHandler(
+    this.messenger.registerActionHandler(
       `${controllerName}:clearRequests` as const,
       this.clear.bind(this),
     );
 
-    this.messagingSystem.registerActionHandler(
+    this.messenger.registerActionHandler(
       `${controllerName}:addRequest` as const,
       (opts: AddApprovalOptions, shouldShowRequest: boolean) => {
         if (shouldShowRequest) {
@@ -428,47 +429,47 @@ export class ApprovalController extends BaseController<
       },
     );
 
-    this.messagingSystem.registerActionHandler(
+    this.messenger.registerActionHandler(
       `${controllerName}:hasRequest` as const,
       this.has.bind(this),
     );
 
-    this.messagingSystem.registerActionHandler(
+    this.messenger.registerActionHandler(
       `${controllerName}:acceptRequest` as const,
       this.accept.bind(this),
     );
 
-    this.messagingSystem.registerActionHandler(
+    this.messenger.registerActionHandler(
       `${controllerName}:rejectRequest` as const,
       this.reject.bind(this),
     );
 
-    this.messagingSystem.registerActionHandler(
+    this.messenger.registerActionHandler(
       `${controllerName}:updateRequestState` as const,
       this.updateRequestState.bind(this),
     );
 
-    this.messagingSystem.registerActionHandler(
+    this.messenger.registerActionHandler(
       `${controllerName}:startFlow` as const,
       this.startFlow.bind(this),
     );
 
-    this.messagingSystem.registerActionHandler(
+    this.messenger.registerActionHandler(
       `${controllerName}:endFlow` as const,
       this.endFlow.bind(this),
     );
 
-    this.messagingSystem.registerActionHandler(
+    this.messenger.registerActionHandler(
       `${controllerName}:setFlowLoadingText` as const,
       this.setFlowLoadingText.bind(this),
     );
 
-    this.messagingSystem.registerActionHandler(
+    this.messenger.registerActionHandler(
       `${controllerName}:showSuccess` as const,
       this.success.bind(this),
     );
 
-    this.messagingSystem.registerActionHandler(
+    this.messenger.registerActionHandler(
       `${controllerName}:showError` as const,
       this.error.bind(this),
     );
