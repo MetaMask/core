26bc40fcbcb4e86ddec91532dafdf2fda5695acd
Maarten Zuidhoorn <maarten@zuidhoorn.com>
2025-10-16 18:00:04 +0200

    Exclude `test-d.ts` from coverage collection

--- merged-packages/eth-json-rpc-middleware/jest.config.js
+++ merged-packages/eth-json-rpc-middleware/jest.config.js
@@ -14,6 +14,9 @@ module.exports = merge(baseConfig, {
   // The display name when running multiple projects
   displayName,
 
+  // An array of glob patterns indicating a set of files for which coverage information should be collected
+  collectCoverageFrom: ['!./src/**/*.test-d.ts'],
+
   // An object that configures minimum threshold enforcement for coverage results
   coverageThreshold: {
     global: {
