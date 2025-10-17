2e63d675ae039ff4fb0ccedf319b3447f127d2df
Erik Marks <25517051+rekmarks@users.noreply.github.com>
2025-10-17 15:19:19 -0700

    feat: Make MiddlewareContext type safe

--- packages/json-rpc-engine/src/v2/MiddlewareContext.ts
+++ packages/json-rpc-engine/src/v2/MiddlewareContext.ts
@@ -1,29 +1,54 @@
 /**
- * A append-only context object for middleware. Its interface is frozen.
+ * An context object for middleware that attempts to protect against accidental
+ * modifications. Its interface is frozen.
  *
- * Map keys may still be deleted. The append-only behavior is mostly intended
- * to prevent accidental naming collisions.
+ * Map keys may not be directly overridden with {@link set}. Instead, use
+ * {@link delete} to remove a key and then {@link set} to add a new value.
  *
- * The append-only behavior is overriden when using e.g. `Reflect.set`,
- * so don't do that.
+ * The override protections are circumvented when using e.g. `Reflect.set`, so
+ * don't do that.
  */
-export class MiddlewareContext extends Map<string | symbol, unknown> {
-  constructor(entries?: Iterable<readonly [string | symbol, unknown]>) {
+export class MiddlewareContext<
+  KeyValues extends Record<string | symbol, unknown>,
+> extends Map<keyof KeyValues, KeyValues[keyof KeyValues]> {
+  constructor(
+    entries?: Iterable<readonly [keyof KeyValues, KeyValues[keyof KeyValues]]>,
+  ) {
     super(entries);
     Object.freeze(this);
   }
 
-  assertGet<Value>(key: string | symbol): Value {
-    if (!this.has(key)) {
-      throw new Error(`Context key "${String(key)}" not found`);
-    }
-    return this.get(key) as Value;
+  get<K extends keyof KeyValues>(key: K): KeyValues[K] | undefined {
+    return super.get(key) as KeyValues[K] | undefined;
   }
 
-  set<Value>(key: string | symbol, value: Value): this {
-    if (this.has(key)) {
+  /**
+   * Get a value from the context. Throws if the key is not found.
+   *
+   * @param key - The key to get the value for.
+   * @returns The value.
+   */
+  assertGet<K extends keyof KeyValues>(key: K): KeyValues[K] {
+    if (!super.has(key)) {
+      throw new Error(`Context key "${String(key)}" not found`);
+    }
+    return super.get(key) as KeyValues[K];
+  }
+
+  /**
+   * Set a value in the context. Throws if the key already exists.
+   * {@link delete} an existing key before setting it to a new value.
+   *
+   * @throws If the key already exists.
+   * @param key - The key to set the value for.
+   * @param value - The value to set.
+   * @returns The context.
+   */
+  set<K extends keyof KeyValues>(key: K, value: KeyValues[K]): this {
+    if (super.has(key)) {
       throw new Error(`MiddlewareContext key "${String(key)}" already exists`);
     }
-    return super.set(key, value) as this;
+    super.set(key, value);
+    return this;
   }
 }
