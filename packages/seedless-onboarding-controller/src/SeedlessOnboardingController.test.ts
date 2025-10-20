03bacfbcacaa7902cab3e07d6453342f501da022
Michele Esposito <michele@esposito.codes>
2025-10-20 15:31:50 +0200

    remove unused imports

--- packages/seedless-onboarding-controller/src/SeedlessOnboardingController.test.ts
+++ packages/seedless-onboarding-controller/src/SeedlessOnboardingController.test.ts
@@ -9,7 +9,6 @@ import {
   decryptWithKey as decryptWithKeyBrowserPassworder,
   importKey as importKeyBrowserPassworder,
 } from '@metamask/browser-passworder';
-import type { Messenger } from '@metamask/messenger';
 import {
   TOPRFError,
   type FetchAuthPubKeyResult,
@@ -52,8 +51,6 @@ import type {
   VaultEncryptor,
 } from './types';
 import type {
-  AllSeedlessOnboardingControllerActions,
-  AllSeedlessOnboardingControllerEvents,
   MockKeyringControllerMessenger,
   RootMessenger,
 } from '../tests/__fixtures__/mockMessenger';
