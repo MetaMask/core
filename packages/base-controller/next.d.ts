d232e39de10520f70d71b16864dac1a8fe717640
Salah-Eddine Saakoun <salah-eddine.saakoun@consensys.net>
2025-10-22 13:43:00 +0200

    fix: disable import-x/extensions ESLint rule

--- packages/base-controller/next.d.ts
+++ packages/base-controller/next.d.ts
@@ -3,5 +3,5 @@
 // but legacy resolution needs explicit .d.ts files at the import path.
 
 // Re-export all types and values from the CommonJS build
-// eslint-disable-next-line import-x/no-useless-path-segments
+// eslint-disable-next-line import-x/no-useless-path-segments, import-x/extensions
 export * from './dist/next/index.cjs';
