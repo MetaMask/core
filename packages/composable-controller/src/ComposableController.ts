MetaMask/core

commit cfdd5e77b08462df6a6204b1ea801e820479c624

refactor: migrate `ComposableController` to `@metamask/messenger` (#6710)
## Explanation

<!--
Thanks for your contribution! Take a moment to answer these questions so
that reviewers have the information they need to properly understand
your changes:

* What is the current state of things and why does it need to change?
* What is the solution your changes offer and how does it work?
* Are there any changes whose purpose might not obvious to those
unfamiliar with the domain?
* If your primary goal was to update one package but you found you had
to update another one along the way, why did you do so?
* If you had to upgrade a dependency, why did you do so?
-->

## References

<!--
Are there any issues that this pull request is tied to?
Are there other links that reviewers should consult to understand these
changes better?
Are there client or consumer pull requests to adopt any breaking
changes?

For example:

* Fixes #12345
* Related to #67890
-->
* Related to #5626

## Checklist

- [ ] I've updated the test suite for new or updated code as appropriate
- [ ] I've updated documentation (JSDoc, Markdown, etc.) for new or
updated code as appropriate
- [ ] I've communicated my changes to consumers by [updating changelogs
for packages I've
changed](https://github.com/MetaMask/core/tree/main/docs/contributing.md#updating-changelogs),
highlighting breaking changes as necessary
- [ ] I've prepared draft pull requests for clients and consumer
packages to resolve any breaking changes

<!-- CURSOR_SUMMARY -->
---

> [!NOTE]
> Migrates `ComposableController` to `@metamask/messenger`, updates
types/metadata, rewrites tests, and updates dependencies and docs.
> 
> - **@metamask/composable-controller**:
> - Migrate from `RestrictedMessenger` to `Messenger`
(`@metamask/messenger`) and `@metamask/base-controller/next` APIs.
> - Update types: add `ComposableControllerActions/GetState`, refine
`ChildControllerStateChangeEvents`, and redefine

packages/composable-controller/src/ComposableController.ts
@@ -1,12 +1,13 @@
 import type {
-  RestrictedMessenger,
   StateConstraint,
   StateMetadata,
   StateMetadataConstraint,
   ControllerStateChangeEvent,
+  ControllerGetStateAction,
   BaseControllerInstance as ControllerInstance,
-} from '@metamask/base-controller';
-import { BaseController } from '@metamask/base-controller';
+} from '@metamask/base-controller/next';
+import { BaseController } from '@metamask/base-controller/next';
+import type { Messenger } from '@metamask/messenger';
 
 export const controllerName = 'ComposableController';
 
@@ -19,6 +20,15 @@ export const INVALID_CONTROLLER_ERROR =
 export type ComposableControllerStateConstraint = {
   [controllerName: string]: StateConstraint;
 };
+
+/**
+ * The `getState` action type for the {@link ComposableControllerMessenger}.
+ *
+ * @template ComposableControllerState - A type object that maps controller names to their state types.
+ */
+export type ComposableControllerGetStateAction<
+  ComposableControllerState extends ComposableControllerStateConstraint,
+> = ControllerGetStateAction<typeof controllerName, ComposableControllerState>;
 
 /**
  * The `stateChange` event type for the {@link ComposableControllerMessenger}.
@@ -42,6 +52,15 @@ export type ComposableControllerEvents<
 > = ComposableControllerStateChangeEvent<ComposableControllerState>;
 
 /**
+ * A union type of action types available to the {@link ComposableControllerMessenger}.
+ *
+ * @template ComposableControllerState - A type object that maps controller names to their state types.
+ */
+export type ComposableControllerActions<
+  ComposableControllerState extends ComposableControllerStateConstraint,
+> = ComposableControllerGetStateAction<ComposableControllerState>;
+
+/**
  * A utility type that extracts controllers from the {@link ComposableControllerState} type,
  * and derives a union type of all of their corresponding `stateChange` events.
  *
@@ -51,12 +70,10 @@ export type ChildControllerStateChangeEvents<
   ComposableControllerState extends ComposableControllerStateConstraint,
 > =
   ComposableControllerState extends Record<
-    infer ControllerName extends string,
-    infer ControllerState
+    infer ChildControllerName extends string,
+    infer ChildControllerState extends StateConstraint
   >
-    ? ControllerState extends StateConstraint
-      ? ControllerStateChangeEvent<ControllerName, ControllerState>
-      : never
+    ? ControllerStateChangeEvent<ChildControllerName, ChildControllerState>
     : never;
 
 /**
@@ -75,13 +92,11 @@ export type AllowedEvents<
  */
 export type ComposableControllerMessenger<
   ComposableControllerState extends ComposableControllerStateConstraint,
-> = RestrictedMessenger<
+> = Messenger<
   typeof controllerName,
-  never,
+  ComposableControllerActions<ComposableControllerState>,
   | ComposableControllerEvents<ComposableControllerState>
-  | AllowedEvents<ComposableControllerState>,
-  never,
-  AllowedEvents<ComposableControllerState>['type']
+  | AllowedEvents<ComposableControllerState>
 >;
 
 /**
@@ -106,7 +121,7 @@ export class ComposableController<
    *
    * @param options - Initial options used to configure this controller
    * @param options.controllers - An object that contains child controllers keyed by their names.
-   * @param options.messenger - A restricted messenger.
+   * @param options.messenger - A controller messenger.
    */
   constructor({
     controllers,
@@ -128,7 +143,7 @@ export class ComposableController<
         (metadata as StateMetadataConstraint)[name] = {
           includeInStateLogs: false,
           persist: true,
-          anonymous: true,
+          includeInDebugSnapshot: true,
           usedInUi: false,
         };
         return metadata;
@@ -163,24 +178,22 @@ export class ComposableController<
         delete this.metadata[name];
         delete this.state[name];
         // eslint-disable-next-line no-empty
-      } catch (_) {}
-      // False negative. `name` is a string type.
-      // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
+      } catch {}
       throw new Error(`${name} - ${INVALID_CONTROLLER_ERROR}`);
     }
     try {
-      this.messagingSystem.subscribe(
-        // False negative. `name` is a string type.
-        // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
-        `${name}:stateChange`,
-        (childState: StateConstraint) => {
-          this.update((state) => {
-            // Type assertion is necessary for property assignment to a generic type. This does not pollute or widen the type of the asserted variable.
-            // @ts-expect-error "Type instantiation is excessively deep"
-            (state as ComposableControllerStateConstraint)[name] = childState;
-          });
-        },
-      );
+      this.messenger.subscribe<
+        // The type intersection with "ComposableController:stateChange" is added by one of the `Messenger.subscribe` overloads, but that constraint is unnecessary here,
+        // since this method only subscribes the messenger to child controller `stateChange` events.
+        // @ts-expect-error "Type '`${string}:stateChange`' is not assignable to parameter of type '"ComposableController:stateChange" & ChildControllerStateChangeEvents<ComposableControllerState>["type"]'."
+        ChildControllerStateChangeEvents<ComposableControllerState>['type']
+      >(`${name}:stateChange`, (childState: StateConstraint) => {
+        this.update((state) => {
+          // Type assertion is necessary for property assignment to a generic type. This does not pollute or widen the type of the asserted variable.
+          // @ts-expect-error "Type instantiation is excessively deep"
+          (state as ComposableControllerStateConstraint)[name] = childState;
+        });
+      });
     } catch (error: unknown) {
       // False negative. `name` is a string type.
       // eslint-disable-next-line @typescript-eslint/restrict-template-expressions

commit ffafb7d80ae6dd90918468c2893e78d6e1083811

chore: Update obsolete property name in error message (#6955)
## Explanation

The `INVALID_CONTROLLER_ERROR` error message incorrectly referenced the
property name `messagingSystem`, which is no longer used in the next
version of the `BaseController` class (which the `ComposableController`
switched to in the PR #6710).

The error message was updated to no mention the messenger instance
variable at all, since the check that triggered the error doesn't look
for it anyway.

## References

Fixes minor error introduced by #6710

## Checklist

- [x] I've updated the test suite for new or updated code as appropriate
- [x] I've updated documentation (JSDoc, Markdown, etc.) for new or
updated code as appropriate
- [x] I've communicated my changes to consumers by [updating changelogs
for packages I've
changed](https://github.com/MetaMask/core/tree/main/docs/contributing.md#updating-changelogs),
highlighting breaking changes as necessary
- [x] I've prepared draft pull requests for clients and consumer
packages to resolve any breaking changes

<!-- CURSOR_SUMMARY -->
---

> [!NOTE]
> Update `INVALID_CONTROLLER_ERROR` to remove obsolete `messagingSystem`
reference, stating only that controllers must inherit from
`BaseController`.
> 
> <sup>Written by [Cursor
Bugbot](https://cursor.com/dashboard?tab=bugbot) for commit
05b6cb390808e22229be5c26a3605ba23a0a7def. This will update automatically
on new commits. Configure
[here](https://cursor.com/dashboard?tab=bugbot).</sup>
<!-- /CURSOR_SUMMARY -->

packages/composable-controller/src/ComposableController.ts
@@ -12,7 +12,7 @@ import type { Messenger } from '@metamask/messenger';
 export const controllerName = 'ComposableController';
 
 export const INVALID_CONTROLLER_ERROR =
-  'Invalid controller: controller must have a `messagingSystem` and inherit from `BaseController`.';
+  'Invalid controller: controller must inherit from `BaseController`.';
 
 /**
  * The narrowest supertype for the composable controller state object.