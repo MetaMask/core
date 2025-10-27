MetaMask/core

commit 4ce8eff8d6ebc34216c753e820d86f48bd8654f6

feat: Rename `messagingSystem` to `messenger` (#6337)
## Explanation

On the `next` version of the `BaseController`, the protected instance
variable `messagingSystem` has been renamed to `messenger`.

## References

Fixes #6336

## Checklist

- [x] I've updated the test suite for new or updated code as appropriate
- [x] I've updated documentation (JSDoc, Markdown, etc.) for new or
updated code as appropriate
- [x] I've communicated my changes to consumers by [updating changelogs
for packages I've
changed](https://github.com/MetaMask/core/tree/main/docs/contributing.md#updating-changelogs),
highlighting breaking changes as necessary
- [x] I've prepared draft pull requests for clients and consumer
packages to resolve any breaking changes

packages/base-controller/src/next/BaseController.ts
@@ -187,7 +187,7 @@ export class BaseController<
   /**
    * The controller messenger. This is used to interact with other parts of the application.
    */
-  protected messagingSystem: ControllerMessenger;
+  protected messenger: ControllerMessenger;
 
   /**
    * The controller messenger.
@@ -248,7 +248,7 @@ export class BaseController<
       ControllerActions<ControllerName, ControllerState>,
       ControllerEvents<ControllerName, ControllerState>
     >;
-    this.messagingSystem = messenger;
+    this.messenger = messenger;
     this.name = name;
     // Here we use `freeze` from Immer to enforce that the state is deeply
     // immutable. Note that this is a runtime check, not a compile-time check.
@@ -348,7 +348,7 @@ export class BaseController<
    * listeners from being garbage collected.
    */
   protected destroy() {
-    this.messagingSystem.clearEventSubscriptions(`${this.name}:stateChange`);
+    this.messenger.clearEventSubscriptions(`${this.name}:stateChange`);
   }
 }
 

packages/base-controller/src/next/BaseController.test.ts
@@ -1080,7 +1080,7 @@ describe('getPersistentState', () => {
 
       onVisit = ({ visitors }: VisitorControllerState) => {
         if (visitors.length > this.state.maxVisitors) {
-          this.messagingSystem.call('VisitorController:clear');
+          this.messenger.call('VisitorController:clear');
         }
       };
 

commit 59d4b7897524947e4541a66c1b05952729c6bc63

chore: Fix incorrect property name referenced in comment (#6956)
## Explanation

This comment has had this mistake since #6337

## References

Fixes mistake in comment introduced in #6337

## Checklist

- [x] I've updated the test suite for new or updated code as appropriate
- [x] I've updated documentation (JSDoc, Markdown, etc.) for new or
updated code as appropriate
- [x] I've communicated my changes to consumers by [updating changelogs
for packages I've
changed](https://github.com/MetaMask/core/tree/main/docs/contributing.md#updating-changelogs),
highlighting breaking changes as necessary
- [x] I've prepared draft pull requests for clients and consumer
packages to resolve any breaking changes

<!-- CURSOR_SUMMARY -->
---

> [!NOTE]
> Fixes a comment in `BaseController.ts` to reference `messenger`
instead of `messagingSystem`.
> 
> <sup>Written by [Cursor
Bugbot](https://cursor.com/dashboard?tab=bugbot) for commit
ca4c894f95339cc197dcba943df45b662f2139a0. This will update automatically
on new commits. Configure
[here](https://cursor.com/dashboard?tab=bugbot).</sup>
<!-- /CURSOR_SUMMARY -->

packages/base-controller/src/next/BaseController.ts
@@ -201,7 +201,7 @@ export class BaseController<
   /**
    * The controller messenger.
    *
-   * This is the same as the `messagingSystem` property, but has a type that only lets us use
+   * This is the same as the `messenger` property, but has a type that only lets us use
    * actions and events that are part of the `BaseController` class.
    */
   readonly #messenger: Messenger<