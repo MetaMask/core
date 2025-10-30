c6b509d6a36817659018fde12b29aa97cfb8f6a0
Remi ARQUEVAUX <remi.arquevaux@consensys.net>
2025-10-30 07:31:59 -0700

    feat(STX-271): add sentinel URL for Polygon (#546)

--- merged-packages/smart-transactions-controller/src/constants.ts
+++ merged-packages/smart-transactions-controller/src/constants.ts
@@ -8,6 +8,7 @@ type SentinelApiBaseUrlMap = {
 export const SENTINEL_API_BASE_URL_MAP: SentinelApiBaseUrlMap = {
   1: 'https://tx-sentinel-ethereum-mainnet.api.cx.metamask.io',
   56: 'https://tx-sentinel-bsc-mainnet.api.cx.metamask.io',
+  137: 'https://tx-sentinel-polygon-mainnet.api.cx.metamask.io',
   8453: 'https://tx-sentinel-base-mainnet.api.cx.metamask.io',
   42161: 'https://tx-sentinel-arbitrum-mainnet.api.cx.metamask.io',
   59144: 'https://tx-sentinel-linea-mainnet.api.cx.metamask.io',
