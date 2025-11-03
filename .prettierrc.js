9c4a1c32d1e12f45b26e3a9f7d91be231e91b4fa
Maarten Zuidhoorn <maarten@zuidhoorn.com>
2025-11-03 14:12:28 +0100

    Revert Prettier config back to CJS

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,14 +1,12 @@
 /**
  * @type {import('prettier').Options}
  */
-// All of these are defaults except singleQuote, but we specify them
-// for explicitness
-const config = {
+module.exports = {
+  // All of these are defaults except singleQuote, but we specify them
+  // for explicitness
   quoteProps: 'as-needed',
   singleQuote: true,
   tabWidth: 2,
   trailingComma: 'all',
   plugins: ['prettier-plugin-packagejson'],
 };
-
-export default config;
