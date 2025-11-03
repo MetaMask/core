1a9b161f1a44904a402c2977cee02e69b6a815e3
Maarten Zuidhoorn <maarten@zuidhoorn.com>
2025-11-03 13:58:15 +0100

    Add `build:all` script to build package with dependencies, and fix Prettier config

---
+++ .prettierrc.mjs
@@ -0,0 +1,14 @@
+/**
+ * @type {import('prettier').Options}
+ */
+// All of these are defaults except singleQuote, but we specify them
+// for explicitness
+const config = {
+  quoteProps: 'as-needed',
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'all',
+  plugins: ['prettier-plugin-packagejson'],
+};
+
+export default config;
