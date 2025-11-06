98c4fd8223f3f6169c01c3545027fb9a21b0441f
Patrick Kowalski <patrick.kowalski@consensys.net>
2025-11-06 07:49:31 -0600

    chore: clean up to patch build

--- packages/ramps-controller/src/index.ts
+++ packages/ramps-controller/src/index.ts
@@ -1,9 +1,10 @@
-/**
- * Example function that returns a greeting for the given name.
- *
- * @param name - The name to greet.
- * @returns The greeting.
- */
-export default function greeter(name: string): string {
-  return `Hello, ${name}!`;
-}
+export type {
+  RampsControllerState,
+  RampsControllerGetStateAction,
+  RampsControllerGetCountriesAction,
+  RampsControllerActions,
+  RampsControllerStateChangeEvent,
+  RampsControllerEvents,
+  RampsControllerMessenger,
+} from './RampsController';
+export { RampsController } from './RampsController';
