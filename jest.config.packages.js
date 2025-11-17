6c035ddf47f2310f5ef49b01fea171e97e9e00d3
Mark Stacey <markjstacey@gmail.com>
2025-11-17 14:43:27 -0330

    Add workaround for uuid incompatibility

--- jest.config.packages.js
+++ jest.config.packages.js
@@ -89,6 +89,7 @@ module.exports = {
       // so in that case use their published versions
       '<rootDir>/../../node_modules/@metamask/$1',
     ],
+    '^uuid$': require.resolve('uuid'),
   },
 
   // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
