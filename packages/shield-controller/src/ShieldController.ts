974556b89cc56783eab9df162eb6e126e985cb1e
lwin <lwin.kyaw@consensys.net>
2025-11-18 18:39:43 +0800

    fix: skipped coverage call for transaction submitted event

--- packages/shield-controller/src/ShieldController.ts
+++ packages/shield-controller/src/ShieldController.ts
@@ -289,15 +289,18 @@ export class ShieldController extends BaseController<
         // only check if the previous transaction has simulation data and if it has changed
         // this is to avoid checking coverage for the `TWICE` (once when it's added to the state and once when it's simulated for the first time).
         // we only need to update the coverage result when the simulation data has changed.
-        Boolean(previousTransaction?.simulationData) &&
+        previousTransaction?.simulationData &&
         !isEqual(
           transaction.simulationData,
-          previousTransaction?.simulationData,
+          previousTransaction.simulationData,
         );
 
       // Check coverage if the transaction is new or if the simulation data has
       // changed.
-      if (!previousTransaction || simulationDataChanged) {
+      if (
+        transaction.status !== TransactionStatus.submitted && // we don't need to check coverage for submitted transactions
+        (!previousTransaction || simulationDataChanged)
+      ) {
         this.checkCoverage(transaction).catch(
           // istanbul ignore next
           (error) => log('Error checking coverage:', error),
