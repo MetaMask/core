93df243ba6b33dae6fb9343e0c209ff68f05a4e8
lwin <lwin.kyaw@consensys.net>
2025-12-03 11:06:49 +0800

    feat: export 'SubscriptionControllerSubmitShieldSubscriptionCryptoApprovalAction'

--- packages/subscription-controller/src/index.ts
+++ packages/subscription-controller/src/index.ts
@@ -17,6 +17,7 @@ export type {
   SubscriptionControllerStateChangeEvent,
   SubscriptionControllerSubmitSponsorshipIntentsAction,
   SubscriptionControllerLinkRewardsAction,
+  SubscriptionControllerSubmitShieldSubscriptionCryptoApprovalAction,
   AllowedActions,
   AllowedEvents,
 } from './SubscriptionController';
