bd9d9a22d5d72f108d64ecb3e0bdba85f6b880e2
Maarten Zuidhoorn <maarten@zuidhoorn.com>
2025-12-09 15:41:02 +0100

    Fix suppressed ESLint errors in `sample-controllers` package

--- packages/sample-controllers/src/sample-gas-prices-controller.ts
+++ packages/sample-controllers/src/sample-gas-prices-controller.ts
@@ -4,6 +4,7 @@ import type {
   StateMetadata,
 } from '@metamask/base-controller';
 import { BaseController } from '@metamask/base-controller';
+import { ErrorReportingServiceCaptureExceptionAction } from '@metamask/error-reporting-service';
 import type { Messenger } from '@metamask/messenger';
 import type {
   NetworkClientId,
@@ -109,6 +110,7 @@ export type SampleGasPricesControllerActions =
  * Actions from other messengers that {@link SampleGasPricesMessenger} calls.
  */
 type AllowedActions =
+  | ErrorReportingServiceCaptureExceptionAction
   | NetworkControllerGetNetworkClientByIdAction
   | SampleGasPricesServiceFetchGasPricesAction;
 
@@ -273,7 +275,7 @@ export class SampleGasPricesController extends BaseController<
    * @param args - The arguments to the function.
    * @param args.chainId - The chain ID for which to fetch gas prices.
    */
-  async updateGasPrices({ chainId }: { chainId: Hex }) {
+  async updateGasPrices({ chainId }: { chainId: Hex }): Promise<void> {
     const gasPricesResponse = await this.messenger.call(
       'SampleGasPricesService:fetchGasPrices',
       chainId,
@@ -293,9 +295,9 @@ export class SampleGasPricesController extends BaseController<
    *
    * @param selectedNetworkClientId - The globally selected network client ID.
    */
-  async #onSelectedNetworkClientIdChange(
+  #onSelectedNetworkClientIdChange(
     selectedNetworkClientId: NetworkClientId,
-  ) {
+  ): void {
     const {
       configuration: { chainId },
     } = this.messenger.call(
@@ -305,7 +307,9 @@ export class SampleGasPricesController extends BaseController<
 
     if (chainId !== this.#selectedChainId) {
       this.#selectedChainId = chainId;
-      await this.updateGasPrices({ chainId });
+      this.updateGasPrices({ chainId }).catch((error) => {
+        this.messenger.call('ErrorReportingService:captureException', error);
+      });
     }
   }
 }
