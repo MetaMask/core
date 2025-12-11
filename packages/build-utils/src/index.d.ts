0accdc830d63110d00b10029335ff659f8cca036
Sébastien Van Eyck <sebastien.vaneyck@consensys.net>
2025-12-11 14:43:37 +0100

    feat: remove unused declaration file

--- packages/build-utils/src/index.d.ts
+++
@@ -1,3 +0,0 @@
-export type { FeatureLabels } from './transforms/remove-fenced-code';
-export { removeFencedCode } from './transforms/remove-fenced-code';
-export { lintTransformedFile } from './transforms/utils';
