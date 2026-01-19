9c6b7dc3256ca0f430b56d09f3f2917fadaee413
Arthur Breton <arthur.breton@consensys.net>
2026-01-19 08:17:13 +0800

    feat(perps): setup perps controller dir

---
+++ packages/perps-controller/src/index.ts
@@ -0,0 +1,9 @@
+/**
+ * @metamask/perps-controller
+ *
+ * Controller for perpetual trading functionality in MetaMask.
+ * This package provides platform-agnostic perps trading capabilities.
+ */
+
+// Placeholder export - will be replaced with actual exports in PR 2
+export const PERPS_CONTROLLER_VERSION = '0.0.0';
