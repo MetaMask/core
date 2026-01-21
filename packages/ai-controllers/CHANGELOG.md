0dd299de6af6409e711f5d3db2b90b171b1ee616
Antonio Regadas <antonio.regadas@consensys.net>
2026-01-21 16:20:01 +0000

    feat: lint fix

--- packages/ai-controllers/CHANGELOG.md
+++ packages/ai-controllers/CHANGELOG.md
@@ -7,4 +7,8 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+### Added
+
+- Initial release ([#7693](https://github.com/MetaMask/core/pull/7693))
+
 [Unreleased]: https://github.com/MetaMask/core/
--- packages/ai-controllers/src/index.ts
+++ packages/ai-controllers/src/index.ts
@@ -1,1 +1,9 @@
-// To add the first exports here
+/**
+ * Example function that returns a greeting for the given name.
+ *
+ * @param name - The name to greet.
+ * @returns The greeting.
+ */
+export default function greeter(name: string): string {
+  return `Hello, ${name}!`;
+}
