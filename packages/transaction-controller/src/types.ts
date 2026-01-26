fef72a96aa22599d58ddb9706faa1bdf2d401b14
Patryk Lucka <5708018+PatrykLucka@users.noreply.github.com>
2026-01-26 15:46:12 +0100

    feat: add merkl yield claim transaction type to TransactionType enum

--- packages/transaction-controller/src/types.ts
+++ packages/transaction-controller/src/types.ts
@@ -771,6 +771,11 @@ export enum TransactionType {
   lendingWithdraw = 'lendingWithdraw',
 
   /**
+   * A transaction that claims yield from a Merkl Yield contract.
+   */
+  merklYieldClaim = 'merklYieldClaim',
+
+  /**
    * A transaction that converts tokens to mUSD.
    */
   musdConversion = 'musdConversion',
