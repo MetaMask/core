079296783fe9839c13090b19b0a8ce85eb75505f
Maxime OUAIRY <maxime.ouairy-ext@consensys.net>
2026-01-28 15:19:03 +0100

    feat: bridge-controller add HyperEVM and MegaETH

--- packages/bridge-controller/src/types.ts
+++ packages/bridge-controller/src/types.ts
@@ -299,6 +299,8 @@ export enum ChainId {
   TRON = 728126428,
   SEI = 1329,
   MONAD = 143,
+  HYPEREVM = 999,
+  MEGAETH = 4326,
 }
 
 export type FeatureFlagsPlatformConfig = Infer<typeof PlatformConfigSchema>;
