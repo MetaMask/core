6e7ebdb3c71651169370ba514932887a13a2e210
Baptiste Marchand <75846779+baptiste-marchand@users.noreply.github.com>
2026-02-05 13:44:41 +0100

    refactor: remove /notification-services/ui

--- packages/notification-services-controller/src/NotificationServicesController/ui/constants.ts
+++
@@ -1,3 +0,0 @@
-export { NOTIFICATION_CHAINS_ID } from '../constants/notification-schema';
-export type { NOTIFICATION_CHAINS_IDS } from '../constants/notification-schema';
-export type { NetworkMetadata, BlockExplorer } from '../types/notification-api';
