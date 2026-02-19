e1b43f6b2561c9c348c66d0b66ff03ecfad7175c
Kriys94 <christian.trn@gmail.com>
2026-02-19 15:08:52 +0100

    remove ALLOWED_INCONSISTENT_DEPENDENCIES

--- yarn.config.cjs
+++ yarn.config.cjs
@@ -24,7 +24,6 @@ const { inspect } = require('util');
  * This should trend towards empty.
  */
 const ALLOWED_INCONSISTENT_DEPENDENCIES = {
-  '@metamask/core-backend': ['5.0.0'],
   // '@metamask/json-rpc-engine': ['^9.0.3'],
 };
 
