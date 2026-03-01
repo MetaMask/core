5ceb2a6f38d460378558ab448f91ad73ef825c6c
Goktug Poyraz <omergoktugpoyraz@gmail.com>
2026-03-01 09:22:10 +0100

    Update

--- packages/transaction-pay-controller/src/strategy/fiat/fiat-quotes.ts
+++ packages/transaction-pay-controller/src/strategy/fiat/fiat-quotes.ts
@@ -1,3 +1,4 @@
+import type { QuotesResponse, Quote } from '@metamask/ramps-controller';
 import { createModuleLogger } from '@metamask/utils';
 
 import { projectLogger } from '../../logger';
@@ -31,8 +32,9 @@ export async function getFiatQuotes(
   try {
     const quotes = await messenger.call('RampsController:getQuotes', {
       amount,
-      walletAddress,
+      fiat: 'usd',
       paymentMethods: [selectedPaymentMethodId],
+      walletAddress,
     });
 
     log('Fetched fiat quotes', {
@@ -42,9 +44,22 @@ export async function getFiatQuotes(
       transactionId,
       walletAddress,
     });
+
+    const quote = pickQuote(quotes as QuotesResponse);
+
+    if (quote) {
+      return [];
+    }
   } catch (error) {
     log('Failed to fetch fiat quotes', { error, transactionId });
   }
 
   return [];
+}
+
+function pickQuote(quotes: QuotesResponse): Quote | undefined {
+  return quotes.success?.find(
+    // Implement to find most reliable quote but return transak for now
+    (quote) => quote.provider === '/providers/transak-native-staging',
+  );
 }
