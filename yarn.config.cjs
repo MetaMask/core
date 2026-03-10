2c8324642292fdf92839ecb19ee2a9a88493aa24
Frederik Bolding <frederik.bolding@gmail.com>
2026-03-10 15:31:56 +0100

    Allow inconsistent tanstack dependency for now

--- yarn.config.cjs
+++ yarn.config.cjs
@@ -24,7 +24,7 @@ const { inspect } = require('util');
  * This should trend towards empty.
  */
 const ALLOWED_INCONSISTENT_DEPENDENCIES = {
-  // '@metamask/json-rpc-engine': ['^9.0.3'],
+  '@tanstack/query-core': ['^4.43.0'],
 };
 
 /**
