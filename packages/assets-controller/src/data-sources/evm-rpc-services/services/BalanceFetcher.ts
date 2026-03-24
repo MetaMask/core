ae422842e835427cff79b238d9cf7c18eb099682
Bernardo Garces Chapero <bernardo.chapero@consensys.net>
2026-03-24 17:17:25 +0000

    comment

--- packages/assets-controller/src/data-sources/evm-rpc-services/services/BalanceFetcher.ts
+++ packages/assets-controller/src/data-sources/evm-rpc-services/services/BalanceFetcher.ts
@@ -134,6 +134,8 @@ export class BalanceFetcher extends StaticIntervalPollingControllerOnly<BalanceP
       return [];
     }
 
+    // Convert hex chainId to decimal for CAIP-2 matching
+    // This is safe because we are filtring with an accountId that is for evm balances only
     const chainIdDecimal = parseInt(chainId, 16).toString();
 
     const assetsToFetch = new Map<string, AssetFetchEntry>();
