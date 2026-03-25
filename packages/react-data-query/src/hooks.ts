ebdf8718a96dde3eb62267e6d1b116ba2552094b
Frederik Bolding <frederik.bolding@gmail.com>
2026-03-25 13:28:39 +0100

    feat: Move data service specific query configuration to hooks

--- packages/react-data-query/src/hooks.ts
+++ packages/react-data-query/src/hooks.ts
@@ -13,6 +13,10 @@ import {
 
 // We provide re-exports of the underlying TanStack Query hooks with narrower types,
 // removing `staleTime` and `queryFn` which aren't useful when using data services.
+const DATA_SERVICE_QUERY_DEFAULTS = {
+  staleTime: 0,
+  retry: false,
+};
 
 /**
  * Consume a query from a data service.
@@ -37,7 +41,7 @@ export function useQuery<
       | NonUndefinedGuard<TQueryFnData>;
   },
 ): UseQueryResult<TData, TError> {
-  return useQueryTanStack(options);
+  return useQueryTanStack({ ...DATA_SERVICE_QUERY_DEFAULTS, ...options });
 }
 
 /**
@@ -64,5 +68,8 @@ export function useInfiniteQuery<
     'staleTime' | 'queryFn'
   >,
 ): UseInfiniteQueryResult<TData, TError> {
-  return useInfiniteQueryTanStack(options);
+  return useInfiniteQueryTanStack({
+    ...DATA_SERVICE_QUERY_DEFAULTS,
+    ...options,
+  });
 }
