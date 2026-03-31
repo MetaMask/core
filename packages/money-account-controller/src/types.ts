8306c976bca631504e0e344da1f0920e40e42bb1
Charly Chevalier <charly.chevalier@consensys.net>
2026-03-31 14:03:15 +0200

    refactor: narrow MoneyAccount type

--- packages/money-account-controller/src/types.ts
+++ packages/money-account-controller/src/types.ts
@@ -1,4 +1,14 @@
-import type { Bip44Account } from '@metamask/account-api';
-import type { KeyringAccount } from '@metamask/keyring-api';
+import type {
+  KeyringAccount,
+  KeyringAccountEntropyMnemonicOptions,
+} from '@metamask/keyring-api';
 
-export type MoneyAccount = Bip44Account<KeyringAccount>;
+/** A money account represents an account managed by the MoneyAccountController. */
+export type MoneyAccount = Omit<KeyringAccount, 'options'> & {
+  // We use stricter options for money accounts. They can be seen as BIP-44 accounts
+  // and we make them non-exportable too.
+  options: {
+    entropy: KeyringAccountEntropyMnemonicOptions;
+    exportable: false;
+  };
+};
