0ce3730a4db7b434f654b705e3b1f0cbacb9ef64
Frederik Bolding <frederik.bolding@gmail.com>
2026-04-08 12:34:24 +0200

    Add basic support KeyringController and AccountsController

---
+++ packages/wallet/src/utilities.ts
@@ -0,0 +1,29 @@
+// TODO: Determine if these should be available directly on Wallet.
+import { wordlist } from '@metamask/scure-bip39/dist/wordlists/english';
+
+import { Wallet } from './Wallet';
+
+/**
+ * Import a secret recovery phrase using the wallet object.
+ *
+ * @param wallet - The wallet object.
+ * @param password - The password to the MetaMask wallet (not the SRP).
+ * @param phrase - The SRP as a string.
+ */
+export async function importSecretRecoveryPhrase(
+  wallet: Wallet,
+  password: string,
+  phrase: string,
+) {
+  const { messenger } = wallet;
+
+  const indices = phrase.split(' ').map((word) => wordlist.indexOf(word));
+  const mnemonic = new Uint8Array(new Uint16Array(indices).buffer);
+
+  // TODO: This should use the new MultichainAccountService.
+  await messenger.call(
+    'KeyringController:createNewVaultAndRestore',
+    password,
+    mnemonic,
+  );
+}
