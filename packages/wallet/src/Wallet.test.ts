1b2f3b48ad706aa594a79be6c2c2b5249e429c05
Erik Marks <25517051+rekmarks@users.noreply.github.com>
2026-04-16 11:31:28 -0700

    test: Use local dev chain via anvil

--- packages/wallet/src/Wallet.test.ts
+++ packages/wallet/src/Wallet.test.ts
@@ -1,3 +1,4 @@
+import { RpcEndpointType } from '@metamask/network-controller';
 import {
   ClientConfigApiService,
   ClientType,
@@ -8,6 +9,8 @@ import { enableNetConnect } from 'nock';
 
 import { importSecretRecoveryPhrase, sendTransaction } from './utilities';
 import { Wallet } from './Wallet';
+import { startAnvil } from '../test/anvil';
+import type { AnvilInstance } from '../test/anvil';
 
 const TEST_PHRASE =
   'test test test test test test test test test test test ball';
@@ -66,37 +69,70 @@ describe('Wallet', () => {
     ).toStrictEqual(['0xc6d5a3c98ec9073b54fa0969957bd582e8d874bf']);
   });
 
-  it('signs transactions', async () => {
-    enableNetConnect();
+  describe('with local chain', () => {
+    let anvil: AnvilInstance;
 
-    wallet = await setupWallet();
+    beforeAll(async () => {
+      anvil = await startAnvil({ mnemonic: TEST_PHRASE });
+    });
 
-    const addresses = wallet.messenger
-      .call('AccountsController:listAccounts')
-      .map((account) => account.address);
+    afterAll(async () => {
+      await anvil?.stop();
+    });
 
-    const { result, transactionMeta } = await sendTransaction(
-      wallet,
-      { from: addresses[0], to: addresses[0], data: '0x00' },
-      { networkClientId: 'sepolia' },
-    );
+    it('signs transactions', async () => {
+      enableNetConnect();
 
-    // Advance timers by an arbitrary value to trigger downstream timer logic.
-    const hash = await jest.advanceTimersByTimeAsync(60_000).then(() => result);
+      wallet = await setupWallet();
 
-    expect(hash).toStrictEqual(expect.any(String));
-    expect(transactionMeta).toStrictEqual(
-      expect.objectContaining({
-        txParams: expect.objectContaining({
-          from: addresses[0],
-          to: addresses[0],
-          data: '0x00',
-          value: '0x0',
-          type: '0x2',
+      const networkConfig = wallet.messenger.call(
+        'NetworkController:addNetwork',
+        {
+          chainId: '0x7a69',
+          name: 'Anvil',
+          nativeCurrency: 'ETH',
+          blockExplorerUrls: [],
+          defaultRpcEndpointIndex: 0,
+          rpcEndpoints: [
+            {
+              type: RpcEndpointType.Custom,
+              url: anvil.rpcUrl,
+            },
+          ],
+        },
+      );
+
+      const { networkClientId } = networkConfig.rpcEndpoints[0];
+
+      const addresses = wallet.messenger
+        .call('AccountsController:listAccounts')
+        .map((account) => account.address);
+
+      const { result, transactionMeta } = await sendTransaction(
+        wallet,
+        { from: addresses[0], to: addresses[0], data: '0x00' },
+        { networkClientId },
+      );
+
+      // Advance timers by an arbitrary value to trigger downstream timer logic.
+      const hash = await jest
+        .advanceTimersByTimeAsync(60_000)
+        .then(() => result);
+
+      expect(hash).toStrictEqual(expect.any(String));
+      expect(transactionMeta).toStrictEqual(
+        expect.objectContaining({
+          txParams: expect.objectContaining({
+            from: addresses[0],
+            to: addresses[0],
+            data: '0x00',
+            value: '0x0',
+            type: '0x2',
+          }),
         }),
-      }),
-    );
-  }, 10_000);
+      );
+    }, 15_000);
+  });
 
   it('exposes state', async () => {
     wallet = await setupWallet();
