73db2ea4b5a962ac2ea85b66646f6352f24308e9
Bernardo Garces Chapero <bernardo.chapero@consensys.net>
2026-04-16 11:50:33 +0100

    better detection

--- packages/assets-controller/src/utils/isNativeAsset.ts
+++ packages/assets-controller/src/utils/isNativeAsset.ts
@@ -1,14 +1,30 @@
+import { SPOT_PRICES_SUPPORT_INFO } from '@metamask/assets-controllers';
 import { parseCaipAssetType } from '@metamask/utils';
 import type { Caip19AssetId } from '../types';
-import { SPOT_PRICES_SUPPORT_INFO } from '@metamask/assets-controllers';
+import { ZERO_ADDRESS } from './constants';
 
 export function isNativeAsset(assetId: Caip19AssetId): boolean {
-  const { assetNamespace } = parseCaipAssetType(assetId);
+  const { assetNamespace, assetReference } = parseCaipAssetType(assetId);
+
+  // Consider all SLIP44 assets
   if (assetNamespace === 'slip44') {
     return true;
   }
 
-  return Object.values(SPOT_PRICES_SUPPORT_INFO).some(
-    (nativeAssetId) => nativeAssetId.toLowerCase() === assetId.toLowerCase(),
-  );
+  // Consider assets in the list of native assets
+  if (
+    Object.values(SPOT_PRICES_SUPPORT_INFO).some(
+      (nativeAssetId) => nativeAssetId.toLowerCase() === assetId.toLowerCase(),
+    )
+  ) {
+    return true;
+  }
+
+  // Consider assets with a zero address
+  if (assetReference === ZERO_ADDRESS) {
+    return true;
+  }
+
+  // Otherwise, not a native asset
+  return false;
 }
