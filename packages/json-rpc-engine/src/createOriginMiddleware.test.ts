8642d5de7e54b104dab5c133e485fc5f5f36f085
Frederik Bolding <frederik.bolding@gmail.com>
2026-05-07 14:26:17 +0200

    feat: Add legacy createOriginMiddleware

---
+++ packages/json-rpc-engine/src/createOriginMiddleware.test.ts
@@ -0,0 +1,20 @@
+import { JsonRpcRequest } from "@metamask/utils";
+import { makeRequest } from "../tests/utils";
+import { JsonRpcEngine } from "./JsonRpcEngine"
+import { createOriginMiddleware } from "./createOriginMiddleware";
+
+describe('createOriginMiddleware', () => {
+    it('adds the origin property to the request', async () => {
+            const origin = 'https://metamask.io';
+        const engine = new JsonRpcEngine();
+
+        engine.push(createOriginMiddleware(origin));
+
+        engine.push((request, response, _next, end) => {
+            response.result = (request as unknown as JsonRpcRequest & { origin: string }).origin;
+            end();
+        })
+
+        expect(await engine.handle(makeRequest())).toStrictEqual({"id": "1", "jsonrpc": "2.0", result: origin});
+    })
+})
