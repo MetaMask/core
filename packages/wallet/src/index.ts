23ef6e2cd57f7998988a3ea38d99934bbc023f6b
Frederik Bolding <frederik.bolding@gmail.com>
2026-05-20 16:49:58 +0200

    Export DefaultInstances

--- packages/wallet/src/index.ts
+++ packages/wallet/src/index.ts
@@ -3,6 +3,7 @@ export type { WalletOptions } from './types';
 export type {
   DefaultActions,
   DefaultEvents,
+  DefaultInstances,
   RootMessenger,
   WalletDestroyedEvent,
 } from './initialization/defaults';
