ca1f7a1b0cb21095387eed11aa807cdfb64d1baf
Frederik Bolding <frederik.bolding@gmail.com>
2026-06-05 13:09:02 +0200

    Add delegation for state change

--- packages/network-controller/tests/helpers.ts
+++ packages/network-controller/tests/helpers.ts
@@ -149,6 +149,7 @@ export function buildNetworkControllerMessenger(
       'ConnectivityController:getState',
       'RemoteFeatureFlagController:getState',
     ],
+    events: ['RemoteFeatureFlagController:stateChange'],
   });
 
   return networkControllerMessenger;
