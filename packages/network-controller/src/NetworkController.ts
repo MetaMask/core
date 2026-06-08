e87bd1627219ef88668e29684c20747824ce7656
Frederik Bolding <frederik.bolding@gmail.com>
2026-06-08 13:01:49 +0200

    Fix lint + remove unused test

--- packages/network-controller/src/NetworkController.ts
+++ packages/network-controller/src/NetworkController.ts
@@ -67,7 +67,6 @@ import type {
   CustomNetworkClientConfiguration,
   InfuraNetworkClientConfiguration,
   NetworkClientConfiguration,
-  AdditionalDefaultNetwork,
 } from './types';
 
 const debugLog = createModuleLogger(projectLogger, 'NetworkController');
