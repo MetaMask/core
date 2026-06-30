a3c1595436367890e97f435c8e94efa6f6baf751
Nicholas Gambino <nicholas.gambino@consensys.net>
2026-06-30 09:14:04 -0700

    fix: oxfmt

--- packages/perps-controller/src/services/HyperLiquidClientService.ts
+++ packages/perps-controller/src/services/HyperLiquidClientService.ts
@@ -308,9 +308,9 @@ export class HyperLiquidClientService {
   public isInitialized(): boolean {
     return Boolean(
       this.#exchangeClient &&
-        this.#infoClient &&
-        this.#infoClientHttp &&
-        this.#subscriptionClient,
+      this.#infoClient &&
+      this.#infoClientHttp &&
+      this.#subscriptionClient,
     );
   }
 
