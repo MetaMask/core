684316f66e2c92dec0a35ca07b3899ffb212390b
Elliot Winkler <elliot.winkler@gmail.com>
2026-07-14 15:56:38 -0600

    Clarify purpose of type-test file, and fix tsconfig for it

--- packages/react-data-query/src/createUIQueryClient.type-test.ts
+++ packages/react-data-query/src/createUIQueryClient.type-test.ts
@@ -2,6 +2,10 @@
  * This file is intended to be run via `tsc` (using `tsconfig.type-tests.json`)
  * instead of Jest. If `tsc` runs successfully, the test file succeeds with no
  * output, otherwise it fails.
+ *
+ * This file exists because if a type error occurs in a test it is not caught by
+ * ESLint or Jest. In the future we may need to figure out how to run `tsc`
+ * across the monorepo; this is more of a stopgap solution.
  */
 
 import type {
