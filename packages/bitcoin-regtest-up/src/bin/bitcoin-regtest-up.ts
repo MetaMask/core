1227353c203fe67930d981a1dfbfca8e74e47a81
Maarten Zuidhoorn <maarten@zuidhoorn.com>
2026-07-16 20:53:19 +0200

    ESLint fix and `jest` global import for bitcoin-regtest-up

--- packages/bitcoin-regtest-up/src/bin/bitcoin-regtest-up.ts
+++ packages/bitcoin-regtest-up/src/bin/bitcoin-regtest-up.ts
@@ -5,7 +5,7 @@ import {
   installBitcoinRegtest,
   parseBitcoinRegtestInstallCliOptions,
   readBitcoinRegtestInstallOptionsFromPackageJson,
-} from '../install';
+} from '../install.js';
 
 async function main(): Promise<void> {
   const [command, ...args] = process.argv.slice(2);
