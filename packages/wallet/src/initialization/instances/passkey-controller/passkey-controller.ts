ded1aa14c6b643949678f79f5890f8433325380e
lwin <lwin.kyaw@consensys.net>
2026-07-17 11:29:25 +0800

    feat: updated passkey-controller-init

--- packages/wallet/src/initialization/instances/passkey-controller/passkey-controller.ts
+++ packages/wallet/src/initialization/instances/passkey-controller/passkey-controller.ts
@@ -6,10 +6,6 @@ import {
 
 import { InitializationConfiguration } from '../../types';
 
-const DEFAULT_PASSKEY_RP_NAME = 'MetaMask';
-const DEFAULT_PASSKEY_USER_NAME = 'MetaMask Wallet';
-const DEFAULT_PASSKEY_USER_DISPLAY_NAME = 'MetaMask Wallet';
-
 export const passkeyController: InitializationConfiguration<
   PasskeyController,
   PasskeyControllerMessenger
@@ -17,15 +13,9 @@ export const passkeyController: InitializationConfiguration<
   name: 'PasskeyController',
   init: ({ state, messenger, options }) =>
     new PasskeyController({
+      ...options,
       messenger,
       state,
-      rpId: options.rpId,
-      rpName: options.rpName ?? DEFAULT_PASSKEY_RP_NAME,
-      expectedRPID: options.expectedRPID,
-      expectedOrigin: options.expectedOrigin,
-      userName: options.userName ?? DEFAULT_PASSKEY_USER_NAME,
-      userDisplayName:
-        options.userDisplayName ?? DEFAULT_PASSKEY_USER_DISPLAY_NAME,
     }),
   getMessenger: (parent) =>
     new Messenger({
