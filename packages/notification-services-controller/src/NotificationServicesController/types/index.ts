88b94568e0988f8ef5adaf2447e5a14ff54417b8
Maarten Zuidhoorn <maarten@zuidhoorn.com>
2026-07-23 13:20:59 +0200

    Update packages/notification-services-controller/src/NotificationServicesController/types/index.ts
    Co-authored-by: zelkibuilds <94715286+zelkibuilds@users.noreply.github.com>

--- packages/notification-services-controller/src/NotificationServicesController/types/index.ts
+++ packages/notification-services-controller/src/NotificationServicesController/types/index.ts
@@ -1,5 +1,5 @@
 export type * from './feature-announcement/index.js';
 export type * from './notification-api/index.js';
 export type * from './notification/index.js';
-export type * from './snaps/snaps.js';
+export type * from './snaps/index.js';
 export type * from './perps/index.js';
