a4b2c7434eb3b659bdfd49e221efb50556845caa
Maarten Zuidhoorn <maarten@zuidhoorn.com>
2026-07-23 23:40:38 +0200

    Add .js import extensions to sentinel-api-service

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -443,6 +443,7 @@ const config = createConfig([
       'packages/remote-feature-flag-controller/**',
       'packages/sample-controllers/**',
       'packages/selected-network-controller/**',
+      'packages/sentinel-api-service/**',
       'packages/signature-controller/**',
       'packages/smart-transactions-controller/**',
       'packages/snap-account-service/**',
