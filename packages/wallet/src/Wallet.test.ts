bcc2c082f02161410eec65bfb10993d155a34afa
Tuna <tunguyenanh@tunatech.org>
2026-08-06 17:48:33 +0700

    fix: test

--- packages/wallet/src/Wallet.test.ts
+++ packages/wallet/src/Wallet.test.ts
@@ -187,7 +187,7 @@ describe('Wallet', () => {
 
     const results = await wallet.init();
 
-    expect(results).toHaveLength(4);
+    expect(results).toHaveLength(5);
   });
 
   it('disallows modifying the messenger', async () => {
