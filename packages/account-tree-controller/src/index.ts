bf35e90905b2760b6329434b0015ba914d6fe0fe
Charly Chevalier <charly.chevalier@consensys.net>
2026-08-11 18:12:15 +0200

    chore: revert exported types (will get exported in final PR)

--- packages/account-tree-controller/src/index.ts
+++ packages/account-tree-controller/src/index.ts
@@ -48,27 +48,3 @@ export {
   AccountTreeController,
   getDefaultAccountTreeControllerState,
 } from './AccountTreeController.js';
-
-export type {
-  AccountTreePayload,
-  AccountTreePayloadStructType,
-  AccountWalletMnemonicPayload,
-  AccountWalletPrivateKeyPayload,
-  AccountWalletMnemonicGroupEntry,
-  AccountWalletPrivateKeyGroupEntry,
-  AccountWalletPayloadId,
-  AccountGroupPayloadId,
-  AccountTreeSnapshotWallet,
-  AccountTreeSnapshotGroup,
-  ExportStateOptions,
-} from './state/payload.js';
-
-export {
-  AccountWalletPayloadType,
-  AccountWalletPrivateKeyEncoding,
-  AccountTreePayloadStruct,
-  assertAccountTreePayload,
-} from './state/payload.js';
-
-export { AccountTreeSnapshot } from './state/snapshot.js';
-export { IdMap } from './state/id-map.js';
