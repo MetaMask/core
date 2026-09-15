f276d99e5d4eea82471cc0ca50087607651bd230
Salah-Eddine Saakoun <salah-eddine.saakoun@consensys.net>
2026-09-15 15:42:56 +0200

    chore: correct stale exports comment

--- yarn.config.cjs
+++ yarn.config.cjs
@@ -168,8 +168,8 @@ module.exports = defineConfig({
           expectWorkspaceField(workspace, 'sideEffects', false);
         }
 
-        // All non-root packages must set up ESM- and CommonJS-compatible
-        // exports correctly (aside from tools).
+        // All non-root packages must set up ESM-only exports correctly (aside
+        // from tools).
         if (!TOOLS.includes(workspace.ident)) {
           expectCorrectWorkspaceExports(workspace);
         }
