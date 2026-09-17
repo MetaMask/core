54e42fee862770ffc638e938b393ce4734816efc
Charly Chevalier <charly.chevalier@consensys.net>
2026-09-17 13:03:33 +0200

    chore: lint

--- packages/multichain-account-service/src/MultichainAccountWallet.test.ts
+++ packages/multichain-account-service/src/MultichainAccountWallet.test.ts
@@ -17,10 +17,8 @@ import type { InternalAccount } from '@metamask/keyring-internal-api';
 import { createDeferredPromise } from '@metamask/utils';
 
 import type { WalletState } from './MultichainAccountWallet.js';
-import {
-  MultichainAccountWallet,
-  type RemoveMultichainAccountWalletFailureContext,
-} from './MultichainAccountWallet.js';
+import { MultichainAccountWallet } from './MultichainAccountWallet.js';
+import type { RemoveMultichainAccountWalletFailureContext } from './MultichainAccountWallet.js';
 import { TimeoutError } from './providers/index.js';
 import type { MockAccountProvider, RootMessenger } from './tests/index.js';
 import {
