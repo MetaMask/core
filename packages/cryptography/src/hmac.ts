32fb251d7b2f3eadb7a16e5e6b0292d2b94c6b0f
Frederik Bolding <frederik.bolding@gmail.com>
2026-09-24 15:15:03 +0200

    Fix lint

--- packages/cryptography/src/hmac.ts
+++ packages/cryptography/src/hmac.ts
@@ -40,12 +40,21 @@ export async function hmacSha512(
   return hmac(key, 'SHA-512', data);
 }
 
+
+/**
+ * Compute the HMAC-SHA of the given data using the given key.
+ *
+ * @param key - The key to use.
+ * @param hash - The hash to use.
+ * @param data - The data to hash.
+ * @returns The HMAC-SHA of the data.
+ */
 async function hmac(
   key: BufferSource,
   hash: 'SHA-256' | 'SHA-384' | 'SHA-512',
   data: BufferSource,
 ): Promise<Uint8Array> {
-  const subtleKey = await crypto.subtle.importKey(
+  const subtleKey = await globalThis.crypto.subtle.importKey(
     'raw',
     key,
     { name: 'HMAC', hash },
@@ -53,6 +62,6 @@ async function hmac(
     ['sign'],
   );
 
-  const result = await crypto.subtle.sign('HMAC', subtleKey, data);
+  const result = await globalThis.crypto.subtle.sign('HMAC', subtleKey, data);
   return new Uint8Array(result);
 }
